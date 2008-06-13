edd986dc816a1430af245b0ef417baee38ae9e9e
rli <rli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-06-13 09:19:40 +0000

    Feature 0004160 added: Set application title, change it by "Talend Open Profiler" for TOP product and by "Talend Data Quality" for TDQ product.
    git-svn-id: http://talendforge.org/svn/top/trunk@15040 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/service/IBrandingService.java
@@ -0,0 +1,26 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.service;
+
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * DOC rli class global comment. Detailled comment
+ */
+public interface IBrandingService extends IService {
+
+    public String getFullProductName();
+
+    public URL getLicenseFile() throws IOException;
+}
