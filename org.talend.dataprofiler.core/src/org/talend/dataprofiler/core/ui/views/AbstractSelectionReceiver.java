18b0f85d991f8dc76066b21beb6e4808fac5e0a1
qzhang <qzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-08-12 08:42:18 +0000

    fixed bug 4769: Add drag&drop support of analyses into editor.
    git-svn-id: http://talendforge.org/svn/top/trunk@16932 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/views/AbstractSelectionReceiver.java
@@ -0,0 +1,110 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.views;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.util.LocalSelectionTransfer;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.DropTargetEffect;
+import org.eclipse.swt.dnd.DropTargetEvent;
+import org.eclipse.swt.widgets.Control;
+
+/**
+ * DOC qzhang class global comment. Detailled comment <br/>
+ * 
+ * $Id: talend.epf 1 2006-09-29 17:06:40Z nrousseau $
+ * 
+ */
+public abstract class AbstractSelectionReceiver extends DropTargetEffect {
+
+    private String extension;
+
+    protected static int lastValidOperation;
+
+    /**
+     * DOC qzhang AbstractSelectionReceiver constructor comment.
+     */
+    public AbstractSelectionReceiver(Control control, String extension) {
+        super(control);
+        this.extension = extension;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.swt.dnd.DropTargetAdapter#dragEnter(org.eclipse.swt.dnd.DropTargetEvent)
+     */
+    public void dragEnter(DropTargetEvent event) {
+        super.dragEnter(event);
+        execValidation(event, doDropValidation(event, LocalSelectionTransfer.getTransfer()));
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.swt.dnd.DropTargetAdapter#dropAccept(org.eclipse.swt.dnd.DropTargetEvent)
+     */
+    @Override
+    public void dropAccept(DropTargetEvent event) {
+        super.dropAccept(event);
+        execValidation(event, doDropValidation(event, LocalSelectionTransfer.getTransfer()));
+    }
+
+    /**
+     * DOC qzhang Comment method "execValidation".
+     * 
+     * @param event
+     * @param valid
+     */
+    public void execValidation(DropTargetEvent event, boolean valid) {
+        if (event.detail != DND.DROP_NONE) {
+            lastValidOperation = event.detail;
+        }
+        if (valid) {
+            event.detail = lastValidOperation;
+        } else {
+            event.detail = DND.DROP_NONE;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.ui.views.ISelectionReceiver#doDropValidation(org.eclipse.swt.dnd.DropTargetEvent,
+     * org.eclipse.ui.navigator.CommonViewer)
+     */
+    public boolean doDropValidation(DropTargetEvent event, LocalSelectionTransfer transfer) {
+        boolean ret = false;
+        Object[] firstElements = ((StructuredSelection) transfer.getSelection()).toArray();
+        for (Object object : firstElements) {
+            if (object instanceof IFile) {
+                IFile fe = (IFile) object;
+                if (extension.equals(fe.getFileExtension())) {
+                    ret = true;
+                }
+            }
+            if (!ret) {
+                break;
+            }
+        }
+        return ret;
+    }
+
+    public void drop(DropTargetEvent event) {
+        super.drop(event);
+        drop(event, LocalSelectionTransfer.getTransfer());
+    }
+
+    public abstract void drop(DropTargetEvent event, LocalSelectionTransfer transfer);
+}
