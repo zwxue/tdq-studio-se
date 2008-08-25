c3fdea739137d711afab9deabfbc657034116aa9
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-08-25 09:14:33 +0000

    git-svn-id: http://talendforge.org/svn/top/trunk@17254 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.cwm.compare/java/src/org/talend/cwm/compare/DQStructureComparer.java
@@ -0,0 +1,89 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.cwm.compare;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.internal.resources.File;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.talend.dataprofiler.core.manager.DQStructureManager;
+
+/**
+ * DOC scorreia class global comment. Detailled comment
+ */
+public final class DQStructureComparer {
+
+    private static final Class<DQStructureComparer> THAT = DQStructureComparer.class;
+
+    protected static Logger log = Logger.getLogger(THAT);
+
+    private static DQStructureComparer comparer = new DQStructureComparer();
+
+    public static DQStructureComparer getInstance() {
+        return comparer;
+    }
+
+    private DQStructureComparer() {
+
+    }
+
+    @SuppressWarnings("restriction")
+    public static IFile copyCurrentResourceFile(File f) {
+        IFolder folder = ResourcesPlugin.getWorkspace().getRoot().getProject(DQStructureManager.METADATA).getFolder(
+                DQStructureManager.DB_CONNECTIONS);
+
+        String fileName = ".refresh.prv";
+        IFile file = folder.getFile(fileName);
+        try {
+            f.copy(file.getFullPath(), true, new NullProgressMonitor());
+        } catch (CoreException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    public static IFile createTempConnectionFile() {
+        IFolder folder = ResourcesPlugin.getWorkspace().getRoot().getProject(DQStructureManager.METADATA).getFolder(
+                DQStructureManager.DB_CONNECTIONS);
+
+        String fileName = ".refresh.prv";
+        IFile file = folder.getFile(fileName);
+        return file;
+    }
+
+    public static boolean deleteCopiedResourceFile() {
+
+        boolean retValue = false;
+        IFolder folder = ResourcesPlugin.getWorkspace().getRoot().getProject(DQStructureManager.METADATA).getFolder(
+                DQStructureManager.DB_CONNECTIONS);
+
+        String fileName = ".refresh.prv";
+        IFile file = folder.getFile(fileName);
+        if (file.exists()) {
+            try {
+                file.delete(true, new NullProgressMonitor());
+                retValue = true;
+            } catch (CoreException e) {
+                log.warn("Problem while trying to delete temp file" + fileName, e);
+                retValue = false;
+            }
+        } else {
+            retValue = true;
+        }
+        return retValue;
+    }
+}
