329d9971b2b2d355bddc7617d7059b2e624c4d2e
zqin <zqin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-08-27 04:50:02 +0000

    rebuild all of the wizard
    git-svn-id: http://talendforge.org/svn/top/trunk@17311 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/analysis/parameters/AnalysisParameter.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/analysis/parameters/AnalysisParameter.java
@@ -16,81 +16,80 @@ import java.util.HashMap;
 
 import org.talend.dataquality.analysis.AnalysisType;
 
-
 /**
- * DOC zqin  class global comment. Detailled comment
- * <br/>
- *
+ * DOC zqin class global comment. Detailled comment <br/>
+ * 
  * $Id: talend.epf 1 2006-09-29 17:06:40Z zqin $
- *
+ * 
  */
 public class AnalysisParameter extends ConnectionParameter {
 
-    private static String analysisTypeName;
-    
-    
+    private String analysisTypeName;
+
     /**
      * Sets the analysisMetadate.
+     * 
      * @param analysisMetadate the analysisMetadate to set
      */
     public void setAnalysisMetadate(HashMap<String, String> analysisMetadate) {
         super.setMetadate(analysisMetadate);
     }
-    
+
     /**
      * Getter for analysisTypeName.
+     * 
      * @return the analysisTypeName
      */
     public String getAnalysisTypeName() {
         return analysisTypeName;
     }
 
-    
     /**
      * Sets the analysisTypeName.
+     * 
      * @param analysisTypeName the analysisTypeName to set
      */
     public void setAnalysisTypeName(String typeName) {
         analysisTypeName = typeName;
     }
-    
+
     public AnalysisType getAnalysisType() {
         if (getAnalysisTypeName() != null) {
-            
+
             return AnalysisType.get(getAnalysisTypeName());
         } else {
-            
+
             return null;
         }
     }
-    
+
     public String getAnalysisName() {
-        
+
         return super.getName();
     }
-    
+
     public String getAnalysisPurpose() {
-        
+
         return super.getPurpose();
     }
-    
+
     public String getAnalysisDescription() {
-        
+
         return super.getDescription();
     }
 
     public String getAnalysisAuthor() {
-        
+
         return super.getAuthor();
     }
-    
+
     public String getAnalysisStatus() {
-        
+
         return super.getStatus();
     }
 
     public String getAnalysisVersion() {
-        
+
         return super.getVersion();
     }
 }
