14cc93ddc10b62ddeeb4a15de29b01fdef82a9ec
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-09-19 16:13:36 +0000

    null pointer exception fixed
    git-svn-id: http://talendforge.org/svn/top/trunk@18070 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- net.sourceforge.sqlexplorer/src/net/sourceforge/sqlexplorer/sqleditor/actions/SQLEditorSessionSwitcher.java
+++ net.sourceforge.sqlexplorer/src/net/sourceforge/sqlexplorer/sqleditor/actions/SQLEditorSessionSwitcher.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2007 SQL Explorer Development Team
  * http://sourceforge.net/projects/eclipsesql
- *
+ * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- *
+ * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -41,10 +41,12 @@ import org.eclipse.swt.widgets.Control;
 
 public class SQLEditorSessionSwitcher extends ControlContribution implements ConnectionListener, SessionEstablishedListener {
 
-	private SwitchableSessionEditor _editor;
+    private SwitchableSessionEditor _editor;
+
     private Combo _sessionCombo;
+
     private HashMap<Integer, User> sessionIndexes = new HashMap<Integer, User>();
-    
+
     /**
      * @param editor SQLEditor to which this session switcher belongs
      */
@@ -52,13 +54,13 @@ public class SQLEditorSessionSwitcher extends ControlContribution implements Con
         super("net.sourceforge.sqlexplorer.sessionswitcher");
         _editor = editor;
     }
-    
+
     protected Control createControl(Composite parent) {
-    	SQLExplorerPlugin.getDefault().getAliasManager().addListener(this);
-    	
+        SQLExplorerPlugin.getDefault().getAliasManager().addListener(this);
+
         _sessionCombo = new Combo(parent, SWT.READ_ONLY);
         _sessionCombo.setToolTipText(Messages.getString("SQLEditor.Actions.ChooseSession.ToolTip"));
-        
+
         _sessionCombo.addSelectionListener(new SelectionAdapter() {
 
             public void widgetSelected(SelectionEvent event) {
@@ -67,86 +69,91 @@ public class SQLEditorSessionSwitcher extends ControlContribution implements Con
                 User user = null;
                 int selIndex = _sessionCombo.getSelectionIndex();
                 if (selIndex != 0)
-                	user = sessionIndexes.get(selIndex - 1);
-                
+                    user = sessionIndexes.get(selIndex - 1);
+
                 // Nothing to do?
                 if (user != null && _editor.getSession() != null && _editor.getSession().getUser() == user)
-                	return;
-                
-                // Disconnect from the current session while we try to connect
-            	_editor.setSession(null);
-    			_sessionCombo.deselectAll();
-                _editor.refreshToolbars();
-    			
-    			if (user == null)
-    				return;
+                    return;
 
-    			// Start the connection job
-    			ConnectionJob.createSession(user.getAlias(), user, SQLEditorSessionSwitcher.this, false);
+                // Disconnect from the current session while we try to connect
+                _editor.setSession(null);
+                _sessionCombo.deselectAll();
+                _editor.refreshToolbars();
+
+                if (user == null)
+                    return;
+
+                // Start the connection job
+                ConnectionJob.createSession(user.getAlias(), user, SQLEditorSessionSwitcher.this, false);
             }
         });
         setSessionOptions();
         if (_sessionCombo.getItemCount() == 1)
-        	MessageDialog.openInformation(parent.getShell(), Messages.getString("SQLEditor.Actions.ChooseSession.NoConnections.Title"), Messages.getString("SQLEditor.Actions.ChooseSession.NoConnections.Message"));
+            MessageDialog.openInformation(parent.getShell(), Messages
+                    .getString("SQLEditor.Actions.ChooseSession.NoConnections.Title"), Messages
+                    .getString("SQLEditor.Actions.ChooseSession.NoConnections.Message"));
 
         return _sessionCombo;
     }
-    
-    @Override
-	public void dispose() {
-    	SQLExplorerPlugin.getDefault().getAliasManager().removeListener(this);
-		super.dispose();
-	}
 
-	private void setSessionOptions() {
-    	if (_sessionCombo.isDisposed())
-    		return;
+    @Override
+    public void dispose() {
+        SQLExplorerPlugin.getDefault().getAliasManager().removeListener(this);
+        super.dispose();
+    }
+
+    private void setSessionOptions() {
+        if (_sessionCombo.isDisposed())
+            return;
         _sessionCombo.removeAll();
         _sessionCombo.add("");
-        
+
         int index = 0;
         User currentUser = null;
         if (_editor.getSession() != null)
-        	currentUser = _editor.getSession().getUser();
-    	for (Alias alias : SQLExplorerPlugin.getDefault().getAliasManager().getAliases())
-    		for (User user : alias.getUsers()) {
-    			_sessionCombo.add(user.getDescription());
-    			sessionIndexes.put(new Integer(index++), user);
-    			if (currentUser == user)
-    				_sessionCombo.select(_sessionCombo.getItemCount() - 1);
-        }
+            currentUser = _editor.getSession().getUser();
+        for (Alias alias : SQLExplorerPlugin.getDefault().getAliasManager().getAliases())
+            for (User user : alias.getUsers()) {
+                _sessionCombo.add(user.getDescription());
+                sessionIndexes.put(new Integer(index++), user);
+                if (currentUser == user)
+                    _sessionCombo.select(_sessionCombo.getItemCount() - 1);
+            }
     }
-    
-	public void modelChanged() {
-		if (!_sessionCombo.isDisposed())
-	        _editor.getSite().getShell().getDisplay().asyncExec(new Runnable() {
-	            public void run() {
-	            	setSessionOptions();
-	            }
-	        });
-	}
 
-	public void cannotEstablishSession(User user) {
-    	if (_sessionCombo.isDisposed())
-    		return;
+    public void modelChanged() {
+        if (_sessionCombo != null && !_sessionCombo.isDisposed())
+            _editor.getSite().getShell().getDisplay().asyncExec(new Runnable() {
+
+                public void run() {
+                    setSessionOptions();
+                }
+            });
+    }
+
+    public void cannotEstablishSession(User user) {
+        if (_sessionCombo.isDisposed())
+            return;
         _editor.getSite().getShell().getDisplay().asyncExec(new Runnable() {
+
             public void run() {
-				_sessionCombo.deselectAll();
-		        _editor.refreshToolbars();
+                _sessionCombo.deselectAll();
+                _editor.refreshToolbars();
             }
         });
-	}
+    }
 
-	public void sessionEstablished(final Session session) {
-    	if (_sessionCombo.isDisposed())
-    		return;
+    public void sessionEstablished(final Session session) {
+        if (_sessionCombo.isDisposed())
+            return;
         _editor.getSite().getShell().getDisplay().asyncExec(new Runnable() {
+
             public void run() {
-				_editor.setSession(session);
-				setSessionOptions();
-				SQLExplorerPlugin.getDefault().getConnectionsView().setDefaultUser(session.getUser());
-		        _editor.refreshToolbars();
+                _editor.setSession(session);
+                setSessionOptions();
+                SQLExplorerPlugin.getDefault().getConnectionsView().setDefaultUser(session.getUser());
+                _editor.refreshToolbars();
             }
         });
-	}
+    }
 }
