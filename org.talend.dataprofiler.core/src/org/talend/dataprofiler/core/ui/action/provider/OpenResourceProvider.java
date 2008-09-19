08bd0ceda0b8e7583e7db4b20589dd0bcba475f0
rli <rli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-09-19 07:45:26 +0000

    Bug 5145 fixed: Remove the "Open with" menu from the DQ repository view.
    git-svn-id: http://talendforge.org/svn/top/trunk@18023 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/provider/OpenResourceProvider.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/provider/OpenResourceProvider.java
@@ -13,34 +13,15 @@
 package org.talend.dataprofiler.core.ui.action.provider;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.mapping.ResourceMapping;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.jface.action.GroupMarker;
 import org.eclipse.jface.action.IMenuManager;
-import org.eclipse.jface.action.MenuManager;
-import org.eclipse.jface.util.OpenStrategy;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IActionBars;
-import org.eclipse.ui.IEditorDescriptor;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.OpenFileAction;
-import org.eclipse.ui.actions.OpenSystemEditorAction;
-import org.eclipse.ui.actions.OpenWithMenu;
-import org.eclipse.ui.ide.IDE;
-import org.eclipse.ui.internal.ide.DialogUtil;
-import org.eclipse.ui.internal.ide.IDEWorkbenchMessages;
-import org.eclipse.ui.internal.ide.IIDEHelpContextIds;
-import org.eclipse.ui.internal.navigator.AdaptabilityUtility;
 import org.eclipse.ui.navigator.CommonActionProvider;
 import org.eclipse.ui.navigator.ICommonActionConstants;
 import org.eclipse.ui.navigator.ICommonActionExtensionSite;
 import org.eclipse.ui.navigator.ICommonMenuConstants;
 import org.eclipse.ui.navigator.ICommonViewerWorkbenchSite;
-import org.eclipse.ui.part.FileEditorInput;
-import org.talend.dataprofiler.core.CorePlugin;
 
 /**
  * DOC rli class global comment. Detailled comment
@@ -72,7 +53,6 @@ public class OpenResourceProvider extends CommonActionProvider {
         if (openFileAction.isEnabled()) {
             aMenu.insertAfter(ICommonMenuConstants.GROUP_OPEN, openFileAction);
         }
-        addOpenWithMenu(aMenu);
     }
 
     public void fillActionBars(IActionBars theActionBars) {
@@ -83,114 +63,6 @@ public class OpenResourceProvider extends CommonActionProvider {
         if (selection.size() == 1 && selection.getFirstElement() instanceof IFile) {
             openFileAction.selectionChanged(selection);
             theActionBars.setGlobalActionHandler(ICommonActionConstants.OPEN, openFileAction);
-        }
-
-    }
-
-    private void addOpenWithMenu(IMenuManager aMenu) {
-        IStructuredSelection ss = (IStructuredSelection) getContext().getSelection();
-
-        if (ss == null || ss.size() != 1) {
-            return;
-        }
-
-        Object o = ss.getFirstElement();
-
-        // first try IResource
-        IAdaptable openable = (IAdaptable) AdaptabilityUtility.getAdapter(o, IResource.class);
-        // otherwise try ResourceMapping
-        if (openable == null) {
-            openable = (IAdaptable) AdaptabilityUtility.getAdapter(o, ResourceMapping.class);
-        } else if (((IResource) openable).getType() != IResource.FILE) {
-            openable = null;
-        }
-
-        if (openable != null) {
-            // Create a menu flyout.
-            IMenuManager submenu = new MenuManager("Open With", ICommonMenuConstants.GROUP_OPEN_WITH);
-            submenu.add(new GroupMarker(ICommonMenuConstants.GROUP_TOP));
-            submenu.add(new OpenWithMenu(viewSite.getPage(), openable));
-            submenu.add(new GroupMarker(ICommonMenuConstants.GROUP_ADDITIONS));
-
-            // Add the submenu.
-            if (submenu.getItems().length > 2 && submenu.isEnabled()) {
-                aMenu.appendToGroup(ICommonMenuConstants.GROUP_OPEN_WITH, submenu);
-            }
-        }
-    }
-
-    /**
-     * DOC rli OpenResourceProvider class global comment. Detailled comment
-     */
-    class OpenResourceAction extends OpenSystemEditorAction {
-
-        /**
-         * The id of this action.
-         */
-        public static final String ID = PlatformUI.PLUGIN_ID + ".OpenResourceAction";//$NON-NLS-1$
-
-        /**
-         * The editor to open.
-         */
-        private IEditorDescriptor editorDescriptor;
-
-        /**
-         * Creates a new action that will open editors on the then-selected file resources. Equivalent to
-         * <code>OpenFileAction(page,null)</code>.
-         * 
-         * @param page the workbench page in which to open the editor
-         */
-        public OpenResourceAction(IWorkbenchPage page) {
-            this(page, null);
-        }
-
-        /**
-         * Creates a new action that will open instances of the specified editor on the then-selected file resources.
-         * 
-         * @param page the workbench page in which to open the editor
-         * @param descriptor the editor descriptor, or <code>null</code> if unspecified
-         */
-        public OpenResourceAction(IWorkbenchPage page, IEditorDescriptor descriptor) {
-            super(page);
-            setText(descriptor == null ? "Op&en" : descriptor.getLabel());
-            setToolTipText("Edit File");
-            setId(ID);
-            this.editorDescriptor = descriptor;
-        }
-
-        /**
-         * Ensures that the contents of the given file resource are local.
-         * 
-         * @param file the file resource
-         * @return <code>true</code> if the file is local, and <code>false</code> if it could not be made local for
-         * some reason
-         */
-        boolean ensureFileLocal(final IFile file) {
-            // Currently fails due to Core PR. Don't do it for now
-            // 1G5I6PV: ITPCORE:WINNT - IResource.setLocal() attempts to modify immutable tree
-            // file.setLocal(true, IResource.DEPTH_ZERO);
-            return true;
-        }
-
-        /**
-         * Opens an editor on the given file resource.
-         * 
-         * @param file the file resource
-         */
-        void openFile(IFile file) {
-//            try {
-//                boolean activate = OpenStrategy.activateOnOpen();
-//                if (editorDescriptor == null) {
-//                    CorePlugin.getDefault().openEditor(file.);
-//                } else {
-//                    if (ensureFileLocal(file)) {
-//                        getWorkbenchPage().openEditor(new FileEditorInput(file), editorDescriptor.getId(), activate);
-//                    }
-//                }
-//            } catch (PartInitException e) {
-//                DialogUtil.openError(getWorkbenchPage().getWorkbenchWindow().getShell(),
-//                        IDEWorkbenchMessages.OpenFileAction_openFileShellTitle, e.getMessage(), e);
-//            }
         }
 
     }
