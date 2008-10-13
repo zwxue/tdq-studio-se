3e2eab4ac45ffff9be50d7576732876ae6921431
rli <rli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-10-13 09:19:28 +0000

    Feature 0004872 added: Move the Non-UI code from org.talend.dataprofiler.coreto plugin org.talend.cwm.management.
    git-svn-id: http://talendforge.org/svn/top/trunk@18919 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.cwm.management/src/main/java/org/talend/dq/helper/EObjectHelper.java
@@ -0,0 +1,179 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dq.helper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.talend.commons.emf.EMFUtil;
+import org.talend.commons.emf.FactoriesUtil;
+import org.talend.cwm.dependencies.DependenciesHandler;
+import org.talend.cwm.helper.ColumnHelper;
+import org.talend.cwm.helper.ColumnSetHelper;
+import org.talend.cwm.helper.DataProviderHelper;
+import org.talend.cwm.helper.SwitchHelpers;
+import org.talend.cwm.relational.TdCatalog;
+import org.talend.cwm.relational.TdColumn;
+import org.talend.cwm.relational.TdSchema;
+import org.talend.cwm.softwaredeployment.TdDataProvider;
+import org.talend.dataquality.reports.TdReport;
+import org.talend.dq.helper.resourcehelper.AnaResourceFileHelper;
+import org.talend.dq.helper.resourcehelper.PatternResourceFileHelper;
+import org.talend.dq.helper.resourcehelper.PrvResourceFileHelper;
+import org.talend.dq.helper.resourcehelper.RepResourceFileHelper;
+import org.talend.utils.sugars.TypedReturnCode;
+import orgomg.cwm.analysis.informationvisualization.RenderedObject;
+import orgomg.cwm.objectmodel.core.Dependency;
+import orgomg.cwm.objectmodel.core.ModelElement;
+import orgomg.cwm.objectmodel.core.Package;
+import orgomg.cwm.resource.relational.ColumnSet;
+
+/**
+ * @author rli
+ * 
+ */
+public final class EObjectHelper {
+
+    private EObjectHelper() {
+
+    }
+
+    public static TdColumn[] getColumns(ColumnSet columnSet) {
+        List<TdColumn> columns = ColumnSetHelper.getColumns(columnSet);
+        return columns.toArray(new TdColumn[columns.size()]);
+    }
+
+    public static Package getParent(ColumnSet columnSet) {
+        TdCatalog catalog = SwitchHelpers.CATALOG_SWITCH.doSwitch(columnSet.eContainer());
+        if (catalog != null) {
+            return catalog;
+        } else {
+            TdSchema schema = SwitchHelpers.SCHEMA_SWITCH.doSwitch(columnSet.eContainer());
+            return schema;
+        }
+    }
+
+    public static TdDataProvider getTdDataProvider(TdColumn column) {
+        ColumnSet columnSetOwner = ColumnHelper.getColumnSetOwner(column);
+        Package parentCatalogOrSchema = ColumnSetHelper.getParentCatalogOrSchema(columnSetOwner);
+        return DataProviderHelper.getTdDataProvider(parentCatalogOrSchema);
+
+    }
+
+    public static void removeDependencys(IResource[] resources) {
+        for (IResource selectedObj : resources) {
+            IFile file = ((IFile) selectedObj);
+            // String fileName = file.getName();
+            if (file.getFileExtension() == null) {
+                continue;
+            }
+            ModelElement elementToDelete = getModelElement(file);
+            if (elementToDelete != null) {
+                List<Resource> modifiedResources = DependenciesHandler.getInstance().clearDependencies(elementToDelete);
+
+                // save now modified resources (that contain the Dependency objects)
+                for (Resource resource : modifiedResources) {
+                    EMFUtil.saveSingleResource(resource);
+                }
+            }
+        }
+    }
+
+    private static ModelElement getModelElement(IFile file) {
+        ModelElement findModelElement = null;
+        if (file.getFileExtension().equalsIgnoreCase(FactoriesUtil.PROV)) {
+            TypedReturnCode<TdDataProvider> returnValue = PrvResourceFileHelper.getInstance().findProvider(file);
+            findModelElement = returnValue.getObject();
+        } else if (file.getFileExtension().equalsIgnoreCase(FactoriesUtil.ANA)) {
+            findModelElement = AnaResourceFileHelper.getInstance().findAnalysis(file);
+        } else if (file.getFileExtension().equalsIgnoreCase(FactoriesUtil.REP)) {
+            findModelElement = RepResourceFileHelper.getInstance().findReport(file);
+            if (findModelElement == null) {
+                return findModelElement;
+            }
+            List<Resource> modifiedResources = new ArrayList<Resource>();
+            for (RenderedObject renderedObj : ((TdReport) findModelElement).getComponent()) {
+                EList<Dependency> supplierDependencies = renderedObj.getSupplierDependency();
+                for (Dependency dependency : supplierDependencies) {
+                    EList<ModelElement> client = dependency.getClient();
+                    // get the resource of each client
+                    for (ModelElement modelElement : client) {
+                        Resource clientResource = modelElement.eResource();
+                        URI resURI = clientResource.getURI();
+                        if (resURI.toString().equals(findModelElement.eResource().getURI().toString())) {
+                            modifiedResources.add(renderedObj.eResource());
+                            AnaResourceFileHelper.getInstance().clear();
+                        }
+                    }
+                    // clear the dependencies of all clients
+                    client.clear();
+                }
+            }
+            // save now modified resources (that contain the Dependency objects)
+            for (Resource resource : modifiedResources) {
+                EMFUtil.saveSingleResource(resource);
+            }
+        } else if (file.getFileExtension().equalsIgnoreCase(FactoriesUtil.PATTERN)) {
+            findModelElement = PatternResourceFileHelper.getInstance().findPattern(file);
+        }
+        return findModelElement;
+    }
+
+    public static List<ModelElement> getDependencySuppliers(IFile file) {
+        ModelElement findElement = getModelElement(file);
+        EList<Dependency> clientDependencys = findElement.getClientDependency();
+        // locate resource of each Dependency object
+        List<ModelElement> supplierList = new ArrayList<ModelElement>();
+        for (Dependency dependency : clientDependencys) {
+            EList<ModelElement> supplier = dependency.getSupplier();
+            if (supplier != null) {
+                supplierList.addAll(supplier);
+            }
+        }
+        return supplierList;
+    }
+
+    public static List<ModelElement> getDependencyClients(IFile file) {
+        ModelElement findElement = getModelElement(file);
+        EList<Dependency> clientDependencys = findElement.getSupplierDependency();
+        // locate resource of each Dependency object
+        List<ModelElement> supplierList = new ArrayList<ModelElement>();
+        for (Dependency dependency : clientDependencys) {
+            EList<ModelElement> client = dependency.getClient();
+            if (client != null) {
+                supplierList.addAll(client);
+            }
+        }
+        return supplierList;
+    }
+
+    public static void addDependenciesForFile(IFile file, List<ModelElement> modelElements) {
+        ModelElement findElement = getModelElement(file);
+        for (int i = 0; i < modelElements.size(); i++) {
+            DependenciesHandler.getInstance().setUsageDependencyOn(findElement, modelElements.get(i));
+        }
+    }
+
+    public static void addDependenciesForModelElement(IFile file, List<ModelElement> modelElements) {
+        ModelElement findElement = getModelElement(file);
+        for (int i = 0; i < modelElements.size(); i++) {
+            DependenciesHandler.getInstance().setUsageDependencyOn(modelElements.get(i), findElement);
+        }
+    }
+
+}
---
+++ org.talend.cwm.management/src/main/java/org/talend/dq/helper/resourcehelper/AnaResourceFileHelper.java
@@ -0,0 +1,180 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dq.helper.resourcehelper;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.talend.dataquality.analysis.Analysis;
+import org.talend.dataquality.analysis.util.AnalysisSwitch;
+import org.talend.dq.analysis.AnalysisWriter;
+import org.talend.utils.sugars.ReturnCode;
+import orgomg.cwm.analysis.informationvisualization.RenderedObject;
+
+/**
+ * DOC rli class global comment. Detailled comment
+ */
+public final class AnaResourceFileHelper extends ResourceFileMap {
+
+    private static Logger log = Logger.getLogger(AnaResourceFileHelper.class);
+
+    private static AnaResourceFileHelper instance;
+
+    private Map<IFile, Analysis> allAnalysisMap = new HashMap<IFile, Analysis>();
+
+    private AnaResourceFileHelper() {
+        super();
+    }
+
+    public static AnaResourceFileHelper getInstance() {
+        if (instance == null) {
+            instance = new AnaResourceFileHelper();
+        }
+        return instance;
+    }
+
+    public Collection<Analysis> getAllAnalysis(IFolder analysesFolder) {
+        if (resourcesNumberChanged) {
+            try {
+                searchAllAnalysis(analysesFolder);
+            } catch (CoreException e) {
+                e.printStackTrace();
+            }
+            resourcesNumberChanged = false;
+        }
+        return allAnalysisMap.values();
+    }
+
+    private void searchAllAnalysis(IFolder folder) throws CoreException {
+        for (IResource resource : folder.members()) {
+            if (resource.getType() == IResource.FOLDER) {
+                searchAllAnalysis(folder.getFolder(resource.getName()));
+                continue;
+            }
+            IFile file = (IFile) resource;
+            findAnalysis(file);
+
+        }
+    }
+
+    /**
+     * DOC xy Comment method "findPathAnalysis".
+     * 
+     * @param file
+     * @return
+     */
+    public Analysis findAnalysis(IFile file) {
+        Analysis analysisEntity = allAnalysisMap.get(file);
+        if (analysisEntity != null) {
+            return analysisEntity;
+        }
+        return readFromFile(file);
+    }
+
+    private Analysis readFromFile(IFile file) {
+        this.remove(file);
+        Resource fileResource = getFileResource(file);
+        Iterator<IFile> fileIterator = allAnalysisMap.keySet().iterator();
+        while (fileIterator.hasNext()) {
+            IFile key = fileIterator.next();
+            Analysis entity = allAnalysisMap.get(key);
+            Resource resourceObj = entity.eResource();
+            if (resourceObj == fileResource) {
+                registedResourceMap.remove(key);
+                allAnalysisMap.remove(key);
+                break;
+            }
+        }
+        Analysis analysis = retireAnalysis(fileResource);
+        if (analysis != null) {
+            allAnalysisMap.put(file, analysis);
+        }
+        return analysis;
+    }
+
+    public List<IFile> findCorrespondingFile(List<RenderedObject> renderObjs, IFolder analysesFolder) {
+        this.getAllAnalysis(analysesFolder);
+        List<IFile> fileList = new ArrayList<IFile>();
+        for (int i = 0; i < renderObjs.size(); i++) {
+            // if (this.registedResourceMap.containsValue(renderObjs.get(i).eResource())) {
+            Iterator<IFile> iterator = this.registedResourceMap.keySet().iterator();
+            Resource renderObjResource = renderObjs.get(i).eResource();
+            while (iterator.hasNext()) {
+                IFile next = iterator.next();
+                if (registedResourceMap.get(next).getURI().toString().equals(renderObjResource.getURI().toString())) {
+                    // this.register(next, renderObjResource);
+                    fileList.add(next);
+                    break;
+                }
+            }
+            // }
+        }
+        return fileList;
+    }
+
+    /**
+     * DOC rli Comment method "retireAnalysis".
+     * 
+     * @param fileResource
+     * @return
+     */
+    private Analysis retireAnalysis(Resource fileResource) {
+        EList<EObject> contents = fileResource.getContents();
+        if (contents.isEmpty()) {
+            log.error("No content in " + fileResource);
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("Nb elements in contents " + contents.size());
+        }
+        AnalysisSwitch<Analysis> mySwitch = new AnalysisSwitch<Analysis>() {
+
+            public Analysis caseAnalysis(Analysis object) {
+                return object;
+            }
+        };
+        Analysis analysis = null;
+        if (contents != null && contents.size() != 0) {
+            analysis = mySwitch.doSwitch(contents.get(0));
+        }
+        return analysis;
+    }
+
+    public void remove(IFile file) {
+        super.remove(file);
+        this.allAnalysisMap.remove(file);
+    }
+
+    public void clear() {
+        super.clear();
+        this.allAnalysisMap.clear();
+    }
+
+    public ReturnCode save(Analysis analysis) {
+        AnalysisWriter writer = new AnalysisWriter();
+        // MODSCA 20080425 do not overwrite existing file.
+        ReturnCode saved = writer.save(analysis);
+        return saved;
+    }
+}
