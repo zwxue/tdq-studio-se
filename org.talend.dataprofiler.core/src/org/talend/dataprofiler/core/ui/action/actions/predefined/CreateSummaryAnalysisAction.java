fec56d72acd50eed752304ec6d9e438cf6db5a7b
zqin <zqin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-10-31 08:34:23 +0000

    feature 5508  finished : Add a menu "Summary statistics analysis" on numeric columns
    git-svn-id: http://talendforge.org/svn/top/trunk@19671 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/predefined/CreateSummaryAnalysisAction.java
@@ -0,0 +1,88 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.action.actions.predefined;
+
+import org.eclipse.jface.wizard.WizardDialog;
+import org.talend.cwm.relational.TdColumn;
+import org.talend.dataprofiler.core.model.ColumnIndicator;
+import org.talend.dataprofiler.core.ui.action.AbstractPredefinedAnalysisAction;
+import org.talend.dq.nodes.indicator.type.IndicatorEnum;
+import org.talend.utils.sql.Java2SqlType;
+
+/**
+ * DOC Zqin class global comment. Detailled comment
+ */
+public class CreateSummaryAnalysisAction extends AbstractPredefinedAnalysisAction {
+
+    /**
+     * DOC Zqin CreateSummaryAnalysisAction constructor comment.
+     * 
+     */
+    public CreateSummaryAnalysisAction() {
+        super("Summary statistics analysis", null);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.ui.action.AbstractPredefinedAnalysisAction#getPredefinedColumnIndicator()
+     */
+    @Override
+    protected ColumnIndicator[] getPredefinedColumnIndicator() {
+        IndicatorEnum[] allowedEnumes = new IndicatorEnum[3];
+        allowedEnumes[0] = IndicatorEnum.BoxIIndicatorEnum;
+        allowedEnumes[1] = IndicatorEnum.RowCountIndicatorEnum;
+        allowedEnumes[2] = IndicatorEnum.NullCountIndicatorEnum;
+
+        return composePredefinedColumnIndicator(allowedEnumes);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.ui.action.AbstractPredefinedAnalysisAction#getPredefinedDialog()
+     */
+    @Override
+    protected WizardDialog getPredefinedDialog() {
+        return getStandardAnalysisWizardDialog();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.ui.action.AbstractPredefinedAnalysisAction#isAllowed()
+     */
+    @Override
+    protected boolean isAllowed() {
+
+        for (TdColumn column : getColumns()) {
+            if (!Java2SqlType.isNumbericInSQL(column.getJavaType())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.ui.action.AbstractPredefinedAnalysisAction#preDo()
+     */
+    @Override
+    protected boolean preDo() {
+
+        return true;
+    }
+
+}
