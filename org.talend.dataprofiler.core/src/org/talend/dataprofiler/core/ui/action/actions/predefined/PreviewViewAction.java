c824d4903adc9f27c830cbb8ebf40fe3b56b4ccc
zqin <zqin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-10-31 07:29:25 +0000

    feature 5511 finished : Add a menu "Preview view" on views
    git-svn-id: http://talendforge.org/svn/top/trunk@19668 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/predefined/PreviewViewAction.java
@@ -0,0 +1,123 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.action.actions.predefined;
+
+import java.util.Collection;
+
+import net.sourceforge.sqlexplorer.dbproduct.Alias;
+import net.sourceforge.sqlexplorer.plugin.SQLExplorerPlugin;
+import net.sourceforge.sqlexplorer.plugin.editors.SQLEditor;
+import net.sourceforge.sqlexplorer.plugin.editors.SQLEditorInput;
+import net.sourceforge.sqlexplorer.sqleditor.actions.ExecSQLAction;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jface.action.Action;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.talend.cwm.helper.CatalogHelper;
+import org.talend.cwm.helper.ColumnSetHelper;
+import org.talend.cwm.helper.DataProviderHelper;
+import org.talend.cwm.relational.RelationalPackage;
+import org.talend.cwm.relational.TdCatalog;
+import org.talend.cwm.relational.TdView;
+import org.talend.cwm.softwaredeployment.TdDataProvider;
+import org.talend.cwm.softwaredeployment.TdProviderConnection;
+import org.talend.dataprofiler.core.PluginConstant;
+import org.talend.dataprofiler.core.ui.perspective.ChangePerspectiveAction;
+import org.talend.dq.dbms.DbmsLanguage;
+import org.talend.dq.dbms.DbmsLanguageFactory;
+import org.talend.utils.sugars.TypedReturnCode;
+import orgomg.cwm.objectmodel.core.Package;
+
+/**
+ * DOC Zqin class global comment. Detailled comment
+ */
+public class PreviewViewAction extends Action {
+
+    private static Logger log = Logger.getLogger(PreviewViewAction.class);
+
+    private TdView view;
+
+    /**
+     * DOC Zqin PreviewViewAction constructor comment.
+     */
+    public PreviewViewAction(TdView view) {
+        super("Preview View");
+        this.view = view;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.action.Action#run()
+     */
+    @Override
+    public void run() {
+        ChangePerspectiveAction perspectiveAction = new ChangePerspectiveAction(PluginConstant.SE_ID);
+        perspectiveAction.run();
+        SQLExplorerPlugin default1 = SQLExplorerPlugin.getDefault();
+        Collection<Alias> aliases = default1.getAliasManager().getAliases();
+        TdDataProvider tdDataProvider = DataProviderHelper.getDataProvider(view);
+        TypedReturnCode<TdProviderConnection> tdPc = DataProviderHelper.getTdProviderConnection(tdDataProvider);
+        TdProviderConnection providerConnection = tdPc.getObject();
+        String url = providerConnection.getConnectionString();
+        for (Alias alias : aliases) {
+            if (alias.getUrl().equals(url)) {
+                String qualifiedName = getTableQualifiedName(tdDataProvider);
+                String query = "select * from " + qualifiedName;
+                SQLEditorInput input = new SQLEditorInput("SQL Editor (" + SQLExplorerPlugin.getDefault().getEditorSerialNo()
+                        + ").sql");
+                input.setUser(alias.getDefaultUser());
+                try {
+                    IWorkbenchPage page = SQLExplorerPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow()
+                            .getActivePage();
+                    SQLEditor editorPart = (SQLEditor) page.openEditor((IEditorInput) input, SQLEditor.class.getName());
+                    editorPart.setText(query);
+                    ExecSQLAction execSQLAction = new ExecSQLAction(editorPart);
+                    execSQLAction.run();
+                } catch (PartInitException e) {
+                    log.error(e, e);
+                }
+            }
+        }
+    }
+
+    /**
+     * DOC scorreia Comment method "getQualifiedName".
+     * 
+     * @param tdDataProvider
+     * @return
+     */
+    private String getTableQualifiedName(TdDataProvider tdDataProvider) {
+        DbmsLanguage dbmsLanguage = DbmsLanguageFactory.createDbmsLanguage(tdDataProvider);
+        Package catalogOrSchema = ColumnSetHelper.getParentCatalogOrSchema(view);
+        if (catalogOrSchema == null) {
+            return view.getName();
+        }
+        // else
+        String catalogName = null;
+        String schemaName = null;
+        if (catalogOrSchema != null && RelationalPackage.eINSTANCE.getTdSchema().equals(catalogOrSchema.eClass())) {
+            schemaName = catalogOrSchema.getName();
+            TdCatalog parentCatalog = CatalogHelper.getParentCatalog(catalogOrSchema);
+            if (parentCatalog != null) {
+                catalogName = parentCatalog.getName();
+            }
+        } else {
+            catalogName = catalogOrSchema.getName();
+        }
+
+        return dbmsLanguage.toQualifiedName(catalogName, schemaName, view.getName());
+    }
+}
---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/provider/predefined/PreviewViewProvider.java
@@ -0,0 +1,50 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.action.provider.predefined;
+
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.ui.navigator.CommonActionProvider;
+import org.talend.cwm.relational.TdView;
+import org.talend.dataprofiler.core.ui.action.actions.predefined.PreviewViewAction;
+
+/**
+ * DOC Zqin class global comment. Detailled comment
+ */
+public class PreviewViewProvider extends CommonActionProvider {
+
+    /**
+     * DOC Zqin PreviewViewProvider constructor comment.
+     */
+    public PreviewViewProvider() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.actions.ActionGroup#fillContextMenu(org.eclipse.jface.action.IMenuManager)
+     */
+    @Override
+    public void fillContextMenu(IMenuManager menu) {
+        TreeSelection treeSelection = ((TreeSelection) this.getContext().getSelection());
+        if (treeSelection.size() == 1) {
+            Object obj = treeSelection.getFirstElement();
+            if (obj instanceof TdView) {
+                TdView view = (TdView) obj;
+                PreviewViewAction action = new PreviewViewAction(view);
+                menu.add(action);
+            }
+        }
+    }
+}
