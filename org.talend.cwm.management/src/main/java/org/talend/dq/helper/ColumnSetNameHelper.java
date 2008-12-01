0abca49b911af06fa462bba159a187601369162c
zqin <zqin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-12-01 10:21:16 +0000

    feature 5664  finished : Locate columns in DQ repository view
    git-svn-id: http://talendforge.org/svn/top/trunk@20494 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.cwm.management/src/main/java/org/talend/dq/helper/ColumnSetNameHelper.java
@@ -0,0 +1,61 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dq.helper;
+
+import org.talend.cwm.helper.CatalogHelper;
+import org.talend.cwm.helper.ColumnSetHelper;
+import org.talend.cwm.relational.RelationalPackage;
+import org.talend.cwm.relational.TdCatalog;
+import org.talend.cwm.softwaredeployment.TdDataProvider;
+import org.talend.dq.dbms.DbmsLanguage;
+import org.talend.dq.dbms.DbmsLanguageFactory;
+import orgomg.cwm.objectmodel.core.Package;
+import orgomg.cwm.resource.relational.ColumnSet;
+
+/**
+ * DOC Zqin class global comment. Detailled comment
+ */
+public final class ColumnSetNameHelper {
+
+    private ColumnSetNameHelper() {
+
+    }
+
+    /**
+     * DOC zqin Comment method "getQualifiedName".
+     * 
+     * @param tdDataProvider
+     * @return
+     */
+    public static String getColumnSetQualifiedName(TdDataProvider tdDataProvider, ColumnSet columnset) {
+        DbmsLanguage dbmsLanguage = DbmsLanguageFactory.createDbmsLanguage(tdDataProvider);
+        Package catalogOrSchema = ColumnSetHelper.getParentCatalogOrSchema(columnset);
+        if (catalogOrSchema == null) {
+            return columnset.getName();
+        }
+        // else
+        String catalogName = null;
+        String schemaName = null;
+        if (catalogOrSchema != null && RelationalPackage.eINSTANCE.getTdSchema().equals(catalogOrSchema.eClass())) {
+            schemaName = catalogOrSchema.getName();
+            TdCatalog parentCatalog = CatalogHelper.getParentCatalog(catalogOrSchema);
+            if (parentCatalog != null) {
+                catalogName = parentCatalog.getName();
+            }
+        } else {
+            catalogName = catalogOrSchema.getName();
+        }
+
+        return dbmsLanguage.toQualifiedName(catalogName, schemaName, columnset.getName());
+    }
+}
