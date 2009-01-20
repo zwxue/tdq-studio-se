b5054a85ccb4654b5bdec1084aa624960716301e
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-01-20 16:39:27 +0000

    bug 6132: added function to extract pattern
    git-svn-id: http://talendforge.org/svn/top/trunk@21284 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/dbms/SQLiteDbmsLanguage.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/dbms/SQLiteDbmsLanguage.java
@@ -12,6 +12,8 @@
 // ============================================================================
 package org.talend.dq.dbms;
 
+import org.apache.commons.lang.StringUtils;
+
 
 /**
  * DOC scorreia  class global comment. Detailled comment
@@ -35,4 +37,23 @@ public class SQLiteDbmsLanguage extends DbmsLanguage {
         super(dbmsType, majorVersion, minorVersion);
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.cwm.management.api.DbmsLanguage#getPatternFinderDefaultFunction(java.lang.String)
+     */
+    @Override
+    public String getPatternFinderDefaultFunction(String expression) {
+        // stack overflow issue in SQLite with this implementation: see
+        // http://www.mail-archive.com/sqlite-users@sqlite.org/msg22286.html for a possible workaround
+        return StringUtils.repeat("REPLACE(", 59) + expression
+                + ",'B','A'),'C','A'),'D','A'),'E','A'),'F','A'),'G','A'),'H','A')"
+                + ",'I','A'),'J','A'),'K','A'),'L','A'),'M','A'),'N','A'),'O','A')"
+                + ",'P','A'),'Q','A'),'R','A'),'S','A'),'T','A'),'U','A'),'V','A')"
+                + ",'W','A'),'X','A'),'Y','A'),'Z','A'),'b','a'),'c','a'),'d','a')"
+                + ",'e','a'),'f','a'),'g','a'),'h','a'),'i','a'),'j','a'),'k','a')"
+                + ",'l','a'),'m','a'),'n','a'),'o','a'),'p','a'),'q','a'),'r','a')"
+                + ",'s','a'),'t','a'),'u','a'),'v','a'),'w','a'),'x','a'),'y','a')"
+                + ",'z','a'),'1','9'),'2','9'),'3','9'),'4','9'),'5','9'),'6','9')" + ",'7','9'),'8','9'),'0','9')";
+    }
 }
