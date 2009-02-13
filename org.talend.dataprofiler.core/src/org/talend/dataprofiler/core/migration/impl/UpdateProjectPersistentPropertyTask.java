f924da4c1a08d365690e667189cbf6d8865788c8
xqliu <xqliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-02-13 07:36:48 +0000

    feature 5597 finished : start TOP with a workspace from TOP 1.1.3, the DQ repository is empty . add a migration task to resolve it.
    git-svn-id: http://talendforge.org/svn/top/trunk@21888 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/UpdateProjectPersistentPropertyTask.java
@@ -0,0 +1,73 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2009 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.migration.impl;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.talend.dataprofiler.core.exception.ExceptionHandler;
+import org.talend.dataprofiler.core.manager.DQStructureManager;
+import org.talend.dataprofiler.core.migration.AbstractMigrationTask;
+
+/**
+ * DOC xqliu class global comment. Detailled comment
+ */
+public class UpdateProjectPersistentPropertyTask extends AbstractMigrationTask {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.IWorkspaceMigrationTask#execute()
+     */
+    public boolean execute() {
+        String[] projects = { DQStructureManager.DATA_PROFILING, DQStructureManager.LIBRARIES, DQStructureManager.METADATA };
+        try {
+            checkProjectPersistentProperty(projects);
+        } catch (CoreException e) {
+            ExceptionHandler.process(e);
+        }
+        return false;
+    }
+
+    /**
+     * 
+     * DOC xqliu Comment method "checkProjectPersistentProperty".
+     * 
+     * @param projects
+     * @throws CoreException
+     */
+    private void checkProjectPersistentProperty(String[] projects) throws CoreException {
+        IProject prjct;
+        for (String project : projects) {
+            prjct = ResourcesPlugin.getWorkspace().getRoot().getProject(project);
+            if (prjct != null && prjct.getPersistentProperty(DQStructureManager.PROJECT_TDQ_KEY) == null) {
+                prjct.setPersistentProperty(DQStructureManager.PROJECT_TDQ_KEY, DQStructureManager.PROJECT_TDQ_PROPERTY);
+            }
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.IWorkspaceMigrationTask#getOrder()
+     */
+    public Date getOrder() {
+        Calendar calender = Calendar.getInstance();
+        calender.set(2009, 2, 13);
+        return calender.getTime();
+    }
+
+}
