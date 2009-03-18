92f39307b7464bc597fcf48d6c9c8b1316eab7a7
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-03-18 03:10:03 +0000

    Feature 6066 added: move tdq folders into tos platform.
    git-svn-id: http://talendforge.org/svn/top/trunk@22793 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataquality/src/org/talend/dataquality/PluginConstant.java
@@ -0,0 +1,25 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2009 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+
+package org.talend.dataquality;
+
+/**
+ * 
+ * DOC mzhao Plugin constants. This class is originally developed for handle
+ * TDCP launch issues. As root project name must defined at "low level plugin".
+ */
+public class PluginConstant {
+	// MOD mzhao 2009-03-13 TDQ/TOP root project
+	public static String ROOTPROJECTNAME = null;
+
+}
