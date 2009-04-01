10cb6608738b71d66f480f36407a17762f96d108
bzhou <bzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-04-01 09:16:46 +0000

    Bug 6824 fixed: "reports" should not be visible in TOP
    git-svn-id: http://talendforge.org/svn/top/trunk@23250 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/views/filters/ReportingFilter.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/views/filters/ReportingFilter.java
@@ -13,33 +13,42 @@
 package org.talend.dataprofiler.core.ui.views.filters;
 
 import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.Viewer;
-import org.talend.dataprofiler.core.PluginConstant;
-
+import org.talend.dataprofiler.core.manager.DQStructureManager;
 
 /**
- * DOC rli  class global comment. Detailled comment
+ * DOC rli class global comment. Detailled comment
  */
 public class ReportingFilter extends AbstractViewerFilter {
-    
+
     public static final int FILTER_ID = 2;
 
-    /* (non-Javadoc)
-     * @see org.eclipse.jface.viewers.ViewerFilter#select(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.viewers.ViewerFilter#select(org.eclipse.jface.viewers.Viewer, java.lang.Object,
+     * java.lang.Object)
      */
     @Override
     public boolean select(Viewer viewer, Object parentElement, Object element) {
         if (element instanceof IFolder) {
-            IPath projectRelativePath = ((IFolder) element).getFullPath();
-            if (projectRelativePath.toString().equals(PluginConstant.REPORT_FOLDER_PATH)) {
-                return false;
+            IFolder folder = (IFolder) element;
+            try {
+                if (DQStructureManager.REPORT_FOLDER_PROPERTY.equals(folder
+                        .getPersistentProperty(DQStructureManager.FOLDER_CLASSIFY_KEY))) {
+                    return false;
+                }
+            } catch (CoreException e) {
+                e.printStackTrace();
             }
         }
         return true;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.talend.dataprofiler.core.ui.views.filters.IFilter#getId()
      */
     public int getId() {
