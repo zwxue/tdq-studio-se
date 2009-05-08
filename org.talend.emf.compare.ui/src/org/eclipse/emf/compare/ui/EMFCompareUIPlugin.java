fc9fcb07bf20dd1c89e09dd8395e5cb204c7f163
bzhou <bzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-05-08 03:12:55 +0000

    bug fixed: correct the plugin id for emf compare ui.
    git-svn-id: http://talendforge.org/svn/top/trunk@24499 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.emf.compare.ui/src/org/eclipse/emf/compare/ui/EMFCompareUIPlugin.java
+++ org.talend.emf.compare.ui/src/org/eclipse/emf/compare/ui/EMFCompareUIPlugin.java
@@ -1,12 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2007, 2008 Obeo.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
+ * Copyright (c) 2006, 2007, 2008 Obeo. All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
- *     Obeo - initial API and implementation
+ * Contributors: Obeo - initial API and implementation
  *******************************************************************************/
 package org.eclipse.emf.compare.ui;
 
@@ -21,54 +18,52 @@ import org.osgi.framework.BundleContext;
  * @author <a href="mailto:laurent.goubet@obeo.fr">Laurent Goubet</a>
  */
 public class EMFCompareUIPlugin extends AbstractUIPlugin {
-	/** This plugin's ID. */
-	public static final String PLUGIN_ID = "org.eclipse.emf.compare.ui"; //$NON-NLS-1$
 
-	/** This plugin's shared instance. */
-	private static EMFCompareUIPlugin plugin;
+    /** This plugin's ID. */
+    public static final String PLUGIN_ID = "org.talend.emf.compare.ui"; //$NON-NLS-1$
 
-	/** Default Constructor. */
-	public EMFCompareUIPlugin() {
-		super();
-		plugin = this;
-	}
+    /** This plugin's shared instance. */
+    private static EMFCompareUIPlugin plugin;
 
-	/**
-	 * Returns the plugin's shared instance.
-	 * 
-	 * @return The plugin's shared instance.
-	 */
-	public static EMFCompareUIPlugin getDefault() {
-		return plugin;
-	}
+    /** Default Constructor. */
+    public EMFCompareUIPlugin() {
+        super();
+        plugin = this;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		EMFComparePlugin.getDefault().getPluginPreferences().setValue(
-				EMFComparePreferenceKeys.PREFERENCES_KEY_SEARCH_WINDOW,
-				getPreferenceStore().getInt(EMFComparePreferenceKeys.PREFERENCES_KEY_SEARCH_WINDOW));
-		EMFComparePlugin.getDefault().getPluginPreferences().setValue(
-				EMFComparePreferenceKeys.PREFERENCES_KEY_IGNORE_ID,
-				getPreferenceStore().getBoolean(EMFComparePreferenceKeys.PREFERENCES_KEY_IGNORE_ID));
-		EMFComparePlugin.getDefault().getPluginPreferences().setValue(
-				EMFComparePreferenceKeys.PREFERENCES_KEY_IGNORE_XMIID,
-				getPreferenceStore().getBoolean(EMFComparePreferenceKeys.PREFERENCES_KEY_IGNORE_XMIID));
-	}
+    /**
+     * Returns the plugin's shared instance.
+     * 
+     * @return The plugin's shared instance.
+     */
+    public static EMFCompareUIPlugin getDefault() {
+        return plugin;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see AbstractUIPlugin#stop(BundleContext)
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        EMFComparePlugin.getDefault().getPluginPreferences().setValue(EMFComparePreferenceKeys.PREFERENCES_KEY_SEARCH_WINDOW,
+                getPreferenceStore().getInt(EMFComparePreferenceKeys.PREFERENCES_KEY_SEARCH_WINDOW));
+        EMFComparePlugin.getDefault().getPluginPreferences().setValue(EMFComparePreferenceKeys.PREFERENCES_KEY_IGNORE_ID,
+                getPreferenceStore().getBoolean(EMFComparePreferenceKeys.PREFERENCES_KEY_IGNORE_ID));
+        EMFComparePlugin.getDefault().getPluginPreferences().setValue(EMFComparePreferenceKeys.PREFERENCES_KEY_IGNORE_XMIID,
+                getPreferenceStore().getBoolean(EMFComparePreferenceKeys.PREFERENCES_KEY_IGNORE_XMIID));
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see AbstractUIPlugin#stop(BundleContext)
+     */
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
 }
