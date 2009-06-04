df2e6cc115d047e52308c7f4486d92a2d32603c8
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-06-04 17:20:12 +0000

    bug 7571 fixed: Driver instance should be loaded once.
    git-svn-id: http://talendforge.org/svn/top/trunk@25366 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/cwm/db/connection/ConnectionUtils.java
+++ org.talend.cwm.management/src/main/java/org/talend/cwm/db/connection/ConnectionUtils.java
@@ -23,8 +23,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import net.sourceforge.sqlexplorer.dbproduct.ManagedDriver;
@@ -42,208 +44,241 @@ import org.talend.utils.sugars.ReturnCode;
  */
 public final class ConnectionUtils {
 
-    private static Logger log = Logger.getLogger(ConnectionUtils.class);
+	private static Logger log = Logger.getLogger(ConnectionUtils.class);
 
-    // MOD xqliu 2009-02-02 bug 5261
-    public static final int LOGIN_TEMEOUT_MILLISECOND = 20000;
+	// MOD xqliu 2009-02-02 bug 5261
+	public static final int LOGIN_TEMEOUT_MILLISECOND = 20000;
 
-    public static final int LOGIN_TIMEOUT_SECOND = 20;
+	public static final int LOGIN_TIMEOUT_SECOND = 20;
 
-    private static boolean timeout = CWMPlugin.getDefault().getPluginPreferences().getBoolean(PluginConstant.CONNECTION_TIMEOUT);
+	private static boolean timeout = CWMPlugin.getDefault()
+			.getPluginPreferences().getBoolean(
+					PluginConstant.CONNECTION_TIMEOUT);
 
-    public static boolean isTimeout() {
-        return timeout;
-    }
+	// MOD mzhao 2009-06-05 Bug 7571
+	private static final Map<String, Driver> DRIVER_CACHE = new HashMap<String, Driver>();
 
-    public static void setTimeout(boolean timeout) {
-        ConnectionUtils.timeout = timeout;
-    }
+	public static boolean isTimeout() {
+		return timeout;
+	}
 
-    /**
-     * The query to execute in order to verify the connection.
-     */
-    // private static final String PING_SELECT = "SELECT 1";
-    /**
-     * private constructor.
-     */
-    private ConnectionUtils() {
-    }
+	public static void setTimeout(boolean timeout) {
+		ConnectionUtils.timeout = timeout;
+	}
 
-    /**
-     * Method "createConnection".
-     * 
-     * @param url the database url
-     * @param driverClassName the Driver classname
-     * @param props properties passed to the driver manager for getting the connection (normally at least a "user" and
-     * "password" property should be included)
-     * @return the connection
-     * @throws SQLException
-     * @throws InstantiationException
-     * @throws IllegalAccessException
-     * @throws ClassNotFoundException
-     * @throws IOException
-     */
-    public static Connection createConnection(String url, String driverClassName, Properties props) throws SQLException,
-            InstantiationException, IllegalAccessException, ClassNotFoundException {
-        Driver driver = getClassDriver(driverClassName);
-        if (driver != null) {
-            DriverManager.registerDriver(driver);
-            if (log.isDebugEnabled()) {
-                log.debug("SQL driver found and registered: " + driverClassName);
-                log.debug("Enumerating all drivers:");
-                Enumeration<Driver> drivers = DriverManager.getDrivers();
-                while (drivers.hasMoreElements()) {
-                    log.debug(drivers.nextElement());
-                }
-            }
-            Connection connection = null;
-            if (driverClassName.equals("org.hsqldb.jdbcDriver")) { //$NON-NLS-1$getClassDriver 
-                // MOD mzhao 2009-04-13, Try to load driver first as there will cause exception: No suitable driver
-                // found... if not load.
-                try {
-                    Class.forName("org.hsqldb.jdbcDriver");
-                } catch (ClassNotFoundException e) {
-                    log.error(e, e);
-                }
-                // MOD xqliu 2009-02-02 bug 5261
-                if (isTimeout()) {
-                    DriverManager.setLoginTimeout(LOGIN_TIMEOUT_SECOND);
-                }
-                connection = DriverManager.getConnection(url, props);
-            } else {
-                // MOD xqliu 2009-02-02 bug 5261
-                connection = createConnectionWithTimeout(driver, url, props);
-            }
+	/**
+	 * The query to execute in order to verify the connection.
+	 */
+	// private static final String PING_SELECT = "SELECT 1";
+	/**
+	 * private constructor.
+	 */
+	private ConnectionUtils() {
+	}
 
-            return connection;
-        }
-        return null;
+	/**
+	 * Method "createConnection".
+	 * 
+	 * @param url
+	 *            the database url
+	 * @param driverClassName
+	 *            the Driver classname
+	 * @param props
+	 *            properties passed to the driver manager for getting the
+	 *            connection (normally at least a "user" and "password" property
+	 *            should be included)
+	 * @return the connection
+	 * @throws SQLException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	public static Connection createConnection(String url,
+			String driverClassName, Properties props) throws SQLException,
+			InstantiationException, IllegalAccessException,
+			ClassNotFoundException {
+		Driver driver = getClassDriver(driverClassName);
+		if (driver != null) {
+			DriverManager.registerDriver(driver);
+			if (log.isDebugEnabled()) {
+				log
+						.debug("SQL driver found and registered: "
+								+ driverClassName);
+				log.debug("Enumerating all drivers:");
+				Enumeration<Driver> drivers = DriverManager.getDrivers();
+				while (drivers.hasMoreElements()) {
+					log.debug(drivers.nextElement());
+				}
+			}
+			Connection connection = null;
+			if (driverClassName.equals("org.hsqldb.jdbcDriver")) { //$NON-NLS-1$getClassDriver 
+				// MOD mzhao 2009-04-13, Try to load driver first as there will
+				// cause exception: No suitable driver
+				// found... if not load.
+				try {
+					Class.forName("org.hsqldb.jdbcDriver");
+				} catch (ClassNotFoundException e) {
+					log.error(e, e);
+				}
+				// MOD xqliu 2009-02-02 bug 5261
+				if (isTimeout()) {
+					DriverManager.setLoginTimeout(LOGIN_TIMEOUT_SECOND);
+				}
+				connection = DriverManager.getConnection(url, props);
+			} else {
+				// MOD xqliu 2009-02-02 bug 5261
+				connection = createConnectionWithTimeout(driver, url, props);
+			}
 
-    }
+			return connection;
+		}
+		return null;
 
-    /**
-     * 
-     * DOC xqliu Comment method "createConnectionWithTimeout".
-     * 
-     * @param driver
-     * @param url
-     * @param props
-     * @return
-     * @throws SQLException
-     */
-    public synchronized static Connection createConnectionWithTimeout(Driver driver, String url, Properties props)
-            throws SQLException {
-        Connection ret = null;
-        if (isTimeout()) {
-            ConnectionCreator cc = new ConnectionCreator(driver, url, props);
-            new Thread(cc).start();
-            long begin = System.currentTimeMillis();
-            boolean isTimeout = false;
-            boolean isOK = false;
-            boolean isException = false;
-            while (true) {
-                if (cc.getConnection() != null) {
-                    isOK = true;
-                    ret = cc.getConnection();
-                    break;
-                }
-                if (cc.getExecption() != null) {
-                    isException = true;
-                    break;
-                }
-                if (System.currentTimeMillis() - begin > LOGIN_TEMEOUT_MILLISECOND) {
-                    isTimeout = true;
-                    break;
-                }
-            }
-            if (isTimeout) {
-                cc = null;
-                throw new SQLException(Messages.getString("ConnectionUtils.ConnectionTimeout")); //$NON-NLS-1$
-            }
-            if (isException) {
-                SQLException e = cc.getExecption();
-                cc = null;
-                throw e;
-            }
-            if (isOK) {
-                ret = cc.getConnection();
-                cc = null;
-            }
-        } else {
-            ret = driver.connect(url, props);
-        }
-        return ret;
-    }
+	}
 
-    /**
-     * DOC qzhang Comment method "getClassDriver".
-     * 
-     * @param driverClassName
-     * @return
-     * @throws IllegalAccessException
-     * @throws InstantiationException
-     * @throws ClassNotFoundException
-     */
-    public static Driver getClassDriver(String driverClassName) throws InstantiationException, IllegalAccessException,
-            ClassNotFoundException {
-        SQLExplorerPlugin sqlExplorerPlugin = SQLExplorerPlugin.getDefault();
-        Driver driver = null;
-        if (sqlExplorerPlugin != null) {
-            net.sourceforge.sqlexplorer.dbproduct.DriverManager driverModel = sqlExplorerPlugin.getDriverModel();
-            try {
-                Collection<ManagedDriver> drivers = driverModel.getDrivers();
-                for (ManagedDriver managedDriver : drivers) {
-                    LinkedList<String> jars = managedDriver.getJars();
-                    List<URL> urls = new ArrayList<URL>();
-                    for (int i = 0; i < jars.size(); i++) {
-                        File file = new File(jars.get(i));
-                        if (file.exists()) {
-                            urls.add(file.toURL());
-                        }
-                    }
-                    if (!urls.isEmpty()) {
-                        try {
-                            MyURLClassLoader cl;
-                            cl = new MyURLClassLoader(urls.toArray(new URL[0]));
-                            Class clazz = cl.findClass(driverClassName);
-                            if (clazz != null) {
-                                driver = (Driver) clazz.newInstance();
-                                return driver; // driver is found
-                            }
-                        } catch (ClassNotFoundException e) {
-                            // do nothings
-                        }
-                    }
+	/**
+	 * 
+	 * DOC xqliu Comment method "createConnectionWithTimeout".
+	 * 
+	 * @param driver
+	 * @param url
+	 * @param props
+	 * @return
+	 * @throws SQLException
+	 */
+	public synchronized static Connection createConnectionWithTimeout(
+			Driver driver, String url, Properties props) throws SQLException {
+		Connection ret = null;
+		if (isTimeout()) {
+			ConnectionCreator cc = new ConnectionCreator(driver, url, props);
+			new Thread(cc).start();
+			long begin = System.currentTimeMillis();
+			boolean isTimeout = false;
+			boolean isOK = false;
+			boolean isException = false;
+			while (true) {
+				if (cc.getConnection() != null) {
+					isOK = true;
+					ret = cc.getConnection();
+					break;
+				}
+				if (cc.getExecption() != null) {
+					isException = true;
+					break;
+				}
+				if (System.currentTimeMillis() - begin > LOGIN_TEMEOUT_MILLISECOND) {
+					isTimeout = true;
+					break;
+				}
+			}
+			if (isTimeout) {
+				cc = null;
+				throw new SQLException(Messages
+						.getString("ConnectionUtils.ConnectionTimeout")); //$NON-NLS-1$
+			}
+			if (isException) {
+				SQLException e = cc.getExecption();
+				cc = null;
+				throw e;
+			}
+			if (isOK) {
+				ret = cc.getConnection();
+				cc = null;
+			}
+		} else {
+			ret = driver.connect(url, props);
+		}
+		return ret;
+	}
 
-                }
-            } catch (MalformedURLException e) {
-                // do nothings
-            }
-        }
-        if (driver == null) {
-            driver = (Driver) Class.forName(driverClassName).newInstance();
-        }
-        return driver;
-    }
+	/**
+	 * DOC qzhang Comment method "getClassDriver".
+	 * 
+	 * @param driverClassName
+	 * @return
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 * @throws ClassNotFoundException
+	 */
+	public static Driver getClassDriver(String driverClassName)
+			throws InstantiationException, IllegalAccessException,
+			ClassNotFoundException {
+		// MOD mzhao 2009-06-05,Bug 7571 Get driver from catch first, if not
+		// exist then get a new instance.
+		Driver driver = DRIVER_CACHE.get(driverClassName);
+		if (driver != null) {
+			return driver;
+		}
 
-    /**
-     * Method "isValid".
-     * 
-     * @param connection the connection to test
-     * @return a return code with the appropriate message (never null)
-     */
-    public static ReturnCode isValid(final Connection connection) {
-        return org.talend.utils.sql.ConnectionUtils.isValid(connection);
-    }
+		SQLExplorerPlugin sqlExplorerPlugin = SQLExplorerPlugin.getDefault();
+		if (sqlExplorerPlugin != null) {
+			net.sourceforge.sqlexplorer.dbproduct.DriverManager driverModel = sqlExplorerPlugin
+					.getDriverModel();
+			try {
+				Collection<ManagedDriver> drivers = driverModel.getDrivers();
+				for (ManagedDriver managedDriver : drivers) {
+					LinkedList<String> jars = managedDriver.getJars();
+					List<URL> urls = new ArrayList<URL>();
+					for (int i = 0; i < jars.size(); i++) {
+						File file = new File(jars.get(i));
+						if (file.exists()) {
+							urls.add(file.toURL());
+						}
+					}
+					if (!urls.isEmpty()) {
+						try {
+							MyURLClassLoader cl;
+							cl = new MyURLClassLoader(urls.toArray(new URL[0]));
+							Class clazz = cl.findClass(driverClassName);
+							if (clazz != null) {
+								driver = (Driver) clazz.newInstance();
+								// MOD mzhao 2009-06-05,Bug 7571 Get driver from
+								// catch first, if not
+								// exist then get a new instance.
+								DRIVER_CACHE.put(driverClassName, driver);
+								return driver; // driver is found
+							}
+						} catch (ClassNotFoundException e) {
+							// do nothings
+						}
+					}
 
-    /**
-     * Method "closeConnection".
-     * 
-     * @param connection the connection to close.
-     * @return a ReturnCode with true if ok, false if problem. {@link ReturnCode#getMessage()} gives the error message
-     * when there is a problem.
-     */
-    public static ReturnCode closeConnection(final Connection connection) {
-        return org.talend.utils.sql.ConnectionUtils.closeConnection(connection);
-    }
+				}
+			} catch (MalformedURLException e) {
+				// do nothings
+			}
+		}
+		if (driver == null) {
+			driver = (Driver) Class.forName(driverClassName).newInstance();
+		}
+		// MOD mzhao 2009-06-05,Bug 7571 Get driver from catch first, if not
+		// exist then get a new instance.
+		DRIVER_CACHE.put(driverClassName, driver);
+		return driver;
+	}
+
+	/**
+	 * Method "isValid".
+	 * 
+	 * @param connection
+	 *            the connection to test
+	 * @return a return code with the appropriate message (never null)
+	 */
+	public static ReturnCode isValid(final Connection connection) {
+		return org.talend.utils.sql.ConnectionUtils.isValid(connection);
+	}
+
+	/**
+	 * Method "closeConnection".
+	 * 
+	 * @param connection
+	 *            the connection to close.
+	 * @return a ReturnCode with true if ok, false if problem.
+	 *         {@link ReturnCode#getMessage()} gives the error message when
+	 *         there is a problem.
+	 */
+	public static ReturnCode closeConnection(final Connection connection) {
+		return org.talend.utils.sql.ConnectionUtils.closeConnection(connection);
+	}
 }
