ac5619c51b912d1214a33d711c2d52228215d03c
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-06-18 05:51:22 +0000

    feature 5887 added Allow the user to change the connection of an analysis
    git-svn-id: http://talendforge.org/svn/top/trunk@25764 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/analysis/AnalysisWriter.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/analysis/AnalysisWriter.java
@@ -35,121 +35,153 @@ import org.talend.utils.sugars.TypedReturnCode;
 /**
  * @author scorreia
  * 
- * This class saves the analysis.
+ *         This class saves the analysis.
  */
 public class AnalysisWriter {
 
-    private static Logger log = Logger.getLogger(AnalysisWriter.class);
+	private static Logger log = Logger.getLogger(AnalysisWriter.class);
 
-    public static final String VALID_EXTENSION = FactoriesUtil.ANA;
+	public static final String VALID_EXTENSION = FactoriesUtil.ANA;
 
-    /**
-     * Method "save".
-     * 
-     * @param analysis the analysis to save
-     * @return true if no problem, false with a message if a problem occured.
-     */
-    public ReturnCode save(Analysis analysis) {
-        assert analysis != null : "No analysis to save (null)";
-        ReturnCode rc = new ReturnCode();
-        Resource resource = analysis.eResource();
-        if (resource == null) {
-            rc.setReturnCode(Messages.getString("AnalysisWriter.Error",//$NON-NLS-1$
-                    analysis.getName()), false);
-            return rc;
-        }
-        // --- store descriptions (description and purpose) in the same resource
-        EList<EObject> resourceContents = resource.getContents();
-        // resourceContents.addAll(analysis.getDescription());
-        // --- store the data filter in the same resource
-        EList<Domain> dataFilter = AnalysisHelper.getDataFilter(analysis);
+	/**
+	 * Method "save".
+	 * 
+	 * @param analysis
+	 *            the analysis to save
+	 * @return true if no problem, false with a message if a problem occured.
+	 */
+	public ReturnCode save(Analysis analysis) {
+		assert analysis != null : "No analysis to save (null)";
+		ReturnCode rc = new ReturnCode();
+		Resource resource = analysis.eResource();
+		if (resource == null) {
+			rc.setReturnCode(Messages.getString("AnalysisWriter.Error",//$NON-NLS-1$
+					analysis.getName()), false);
+			return rc;
+		}
+		// --- store descriptions (description and purpose) in the same resource
+		EList<EObject> resourceContents = resource.getContents();
+		// resourceContents.addAll(analysis.getDescription());
+		// --- store the data filter in the same resource
+		EList<Domain> dataFilter = AnalysisHelper.getDataFilter(analysis);
 
-        // remove any existing domain (and replace by the new domain)
-        List<Domain> domains = DomainHelper.getDomains(resourceContents);
-        resourceContents.removeAll(domains);
+		// remove any existing domain (and replace by the new domain)
+		List<Domain> domains = DomainHelper.getDomains(resourceContents);
+		resourceContents.removeAll(domains);
 
-        if (dataFilter != null) {
-            // scorreia save them in their own file? -> no, it's ok to save them in the analysis file.
-            for (Domain domain : dataFilter) {
-                if (!resourceContents.contains(domain)) {
-                    resourceContents.add(domain);
-                }
-            }
-        }
+		if (dataFilter != null) {
+			// scorreia save them in their own file? -> no, it's ok to save them
+			// in the analysis file.
+			for (Domain domain : dataFilter) {
+				if (!resourceContents.contains(domain)) {
+					resourceContents.add(domain);
+				}
+			}
+		}
 
-        // save the resource and related resources (when needed, for example when we change the data mining type of a
-        // column)
-        boolean saved = EMFUtil.saveResource(resource);
+		// save the resource and related resources (when needed, for example
+		// when we change the data mining type of a
+		// column)
+		boolean saved = EMFUtil.saveResource(resource);
 
-        if (!saved) {
-            rc.setReturnCode(Messages.getString("AnalysisWriter.ProblemSavingAnalysis", analysis.getName()), saved); //$NON-NLS-1$
-        }
-        return rc;
+		if (!saved) {
+			rc
+					.setReturnCode(
+							Messages
+									.getString(
+											"AnalysisWriter.ProblemSavingAnalysis", analysis.getName()), saved); //$NON-NLS-1$
+		}
+		return rc;
 
-    }
+	}
 
-    /**
-     * Method "save" writes the analysis in the given file. If the file already exists, it is overridden. This method
-     * should not be for saving an analysis already saved. In this case, use save(Analysis).
-     * 
-     * @param analysis the analysis to save
-     * @param file the file in which the analysis will be save
-     * @return whether everything is ok
-     */
-    protected ReturnCode save(Analysis analysis, IFile file) {
-        assert file != null : "Cannot save analysis: No file name given.";
-        assert analysis != null : "No analysis to save (null)";
+	/**
+	 * Method "save" writes the analysis in the given file. If the file already
+	 * exists, it is overridden. This method should not be for saving an
+	 * analysis already saved. In this case, use save(Analysis).
+	 * 
+	 * @param analysis
+	 *            the analysis to save
+	 * @param file
+	 *            the file in which the analysis will be save
+	 * @return whether everything is ok
+	 */
+	protected ReturnCode save(Analysis analysis, IFile file) {
+		assert file != null : "Cannot save analysis: No file name given.";
+		assert analysis != null : "No analysis to save (null)";
 
-        ReturnCode rc = new ReturnCode();
-        if (!checkFileExtension(file)) {
-            rc.setReturnCode(Messages.getString("AnalysisWriter.BadFileExtension", file.getFullPath(), VALID_EXTENSION), false); //$NON-NLS-1$
-            return rc;
-        }
-        EMFSharedResources util = EMFSharedResources.getInstance();
+		ReturnCode rc = new ReturnCode();
+		if (!checkFileExtension(file)) {
+			rc
+					.setReturnCode(
+							Messages
+									.getString(
+											"AnalysisWriter.BadFileExtension", file.getFullPath(), VALID_EXTENSION), false); //$NON-NLS-1$
+			return rc;
+		}
+		EMFSharedResources util = EMFSharedResources.getInstance();
 
-        boolean added = util.addEObjectToResourceSet(file.getFullPath().toString(), analysis);
+		boolean added = util.addEObjectToResourceSet(file.getFullPath()
+				.toString(), analysis);
 
-        if (!added) {
-            rc.setReturnCode(Messages.getString("AnalysisWriter.AnalysisNotSave", util.getLastErrorMessage()), added); //$NON-NLS-1$
-            return rc;
-        }
+		if (!added) {
+			rc
+					.setReturnCode(
+							Messages
+									.getString(
+											"AnalysisWriter.AnalysisNotSave", util.getLastErrorMessage()), added); //$NON-NLS-1$
+			return rc;
+		}
 
-        return save(analysis);
-    }
+		return save(analysis);
+	}
 
-    /**
-     * Method "createAnalysisFile" creates a new file (or overwrite the existing file) with the analysis data. This
-     * method must not be used to update an existing analysis. The file is created by
-     * {@link DqRepositoryViewService#createFilename(String, String, String)}.
-     * 
-     * @param analysis the analysis to save
-     * @param folder the folder where to save the analysis
-     * @return the return code and the created file.
-     */
-    public TypedReturnCode<IFile> createAnalysisFile(Analysis analysis, IFolder folder) {
-        assert analysis != null;
-        TypedReturnCode<IFile> rc = new TypedReturnCode<IFile>();
-        String filename = DqRepositoryViewService.createFilename(analysis.getName(), FactoriesUtil.ANA);
-        IFile file = folder.getFile(filename);
-        // File file = new File(filename);
-        if (file.exists()) {
-            rc.setReturnCode(Messages.getString("AnalysisWriter.CannotSaveAnalysis", analysis.getName(), filename), false); //$NON-NLS-1$
-            return rc;
-        }
-        ReturnCode saved = save(analysis, file);
-        if (saved.isOk()) {
-            if (log.isDebugEnabled()) {
-                log.debug("Saved in  " + file.getFullPath().toString());
-            }
-            rc.setObject(file);
-            analysis.setFileName(file.getFullPath().toString());
-        } else {
-            rc.setReturnCode(Messages.getString("AnalysisWriter.FailedToSaveAnalysis", analysis.getName(), filename), false); //$NON-NLS-1$
-        }
-        return rc;
-    }
+	/**
+	 * Method "createAnalysisFile" creates a new file (or overwrite the existing
+	 * file) with the analysis data. This method must not be used to update an
+	 * existing analysis. The file is created by
+	 * {@link DqRepositoryViewService#createFilename(String, String, String)}.
+	 * 
+	 * @param analysis
+	 *            the analysis to save
+	 * @param folder
+	 *            the folder where to save the analysis
+	 * @return the return code and the created file.
+	 */
+	public TypedReturnCode<IFile> createAnalysisFile(Analysis analysis,
+			IFolder folder) {
+		assert analysis != null;
+		TypedReturnCode<IFile> rc = new TypedReturnCode<IFile>();
+		String filename = DqRepositoryViewService.createFilename(analysis
+				.getName(), FactoriesUtil.ANA);
+		IFile file = folder.getFile(filename);
+		// File file = new File(filename);
+		if (file.exists()) {
+			rc
+					.setReturnCode(
+							Messages
+									.getString(
+											"AnalysisWriter.CannotSaveAnalysis", analysis.getName(), filename), false); //$NON-NLS-1$
+			return rc;
+		}
+		ReturnCode saved = save(analysis, file);
+		if (saved.isOk()) {
+			if (log.isDebugEnabled()) {
+				log.debug("Saved in  " + file.getFullPath().toString());
+			}
+			rc.setObject(file);
+			analysis.setFileName(file.getFullPath().toString());
+		} else {
+			rc
+					.setReturnCode(
+							Messages
+									.getString(
+											"AnalysisWriter.FailedToSaveAnalysis", analysis.getName(), filename), false); //$NON-NLS-1$
+		}
+		return rc;
+	}
 
-    private boolean checkFileExtension(IFile file) {
-        return file.getFileExtension().equalsIgnoreCase(VALID_EXTENSION);
-    }
+	private boolean checkFileExtension(IFile file) {
+		return file.getFileExtension().equalsIgnoreCase(VALID_EXTENSION);
+	}
 }
