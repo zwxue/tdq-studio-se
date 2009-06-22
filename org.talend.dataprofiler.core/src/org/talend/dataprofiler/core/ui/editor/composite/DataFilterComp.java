26b93720f7d93f90117cabfc8d2013da41818ce6
hcheng <hcheng@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-06-22 03:42:17 +0000

    feature 0007803 added: indicate that the user don't have to write "where" in data filter
    git-svn-id: http://talendforge.org/svn/top/trunk@25829 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/composite/DataFilterComp.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/composite/DataFilterComp.java
@@ -16,9 +16,12 @@ import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.talend.dataprofiler.core.PluginConstant;
 import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
@@ -30,50 +33,52 @@ import org.talend.dataprofiler.core.ui.views.DataFilterDND;
  */
 public class DataFilterComp extends AbstractPagePart {
 
-	private final String stringDataFilter;
+    private final String stringDataFilter;
 
-	private Text dataFilterText;
+    private Text dataFilterText;
 
-	public DataFilterComp(Composite parent, String stringDataFilter) {
-		this.stringDataFilter = stringDataFilter;
-		this.createContent(parent);
-	}
+    public DataFilterComp(Composite parent, String stringDataFilter) {
+        this.stringDataFilter = stringDataFilter;
+        this.createContent(parent);
+    }
 
-	public void createContent(Composite parent) {
-		parent.setLayout(new GridLayout());
+    public void createContent(Composite parent) {
+        parent.setLayout(new GridLayout(2, false));
+        // MOD hcheng,2009-06-22,for 7803
+        Label label = new Label(parent, SWT.NULL);
+        label.setText("Where");
+        Font boldFont = new Font(Display.getDefault(), "Arial", 10, SWT.BOLD);
+        label.setFont(boldFont);
+        GridData gd = new GridData();
+        gd.verticalAlignment = GridData.BEGINNING;
+        label.setLayoutData(gd);
+        dataFilterText = new Text(parent, SWT.BORDER | SWT.MULTI);
+        DataFilterDND.installDND(dataFilterText);
+        dataFilterText.setToolTipText(DefaultMessagesImpl.getString("DataFilterComp.here")); //$NON-NLS-1$
+        dataFilterText.setText(stringDataFilter == null ? PluginConstant.EMPTY_STRING : stringDataFilter);
+        GridDataFactory.fillDefaults().grab(true, true).applyTo(dataFilterText);
+        ((GridData) dataFilterText.getLayoutData()).heightHint = 150;
+        dataFilterText.addModifyListener(new ModifyListener() {
 
-		dataFilterText = new Text(parent, SWT.BORDER | SWT.MULTI);
-		DataFilterDND.installDND(dataFilterText);
-		dataFilterText.setToolTipText(DefaultMessagesImpl
-				.getString("DataFilterComp.here")); //$NON-NLS-1$
-		dataFilterText
-				.setText(stringDataFilter == null ? PluginConstant.EMPTY_STRING
-						: stringDataFilter);
-		GridDataFactory.fillDefaults().grab(true, true).applyTo(dataFilterText);
-		((GridData) dataFilterText.getLayoutData()).heightHint = 150;
-		dataFilterText.addModifyListener(new ModifyListener() {
+            public void modifyText(ModifyEvent e) {
+                setDirty(true);
+                propertyChangeSupport.firePropertyChange(PluginConstant.DATAFILTER_PROPERTY, null, dataFilterText.getText());
+            }
 
-			public void modifyText(ModifyEvent e) {
-				setDirty(true);
-				propertyChangeSupport.firePropertyChange(
-						PluginConstant.DATAFILTER_PROPERTY, null,
-						dataFilterText.getText());
-			}
+        });
+    }
 
-		});
-	}
+    public String getDataFilterString() {
+        return dataFilterText.getText();
+    }
 
-	public String getDataFilterString() {
-		return dataFilterText.getText();
-	}
+    public void addModifyListener(ModifyListener listener) {
+        dataFilterText.addModifyListener(listener);
+    }
 
-	public void addModifyListener(ModifyListener listener) {
-		dataFilterText.addModifyListener(listener);
-	}
+    @Override
+    public void updateModelViewer() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void updateModelViewer() {
-		// TODO Auto-generated method stub
-
-	}
+    }
 }
