55de6077f88b1a1152ce4da1dd9f1cd192cf2d00
bzhou <bzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-06-25 12:34:14 +0000

    bug 5669 fixed: fixed a bug to preview data in sqlite
    git-svn-id: http://talendforge.org/svn/top/trunk@25956 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- net.sourceforge.sqlexplorer/src/net/sourceforge/sqlexplorer/EDriverName.java
+++ net.sourceforge.sqlexplorer/src/net/sourceforge/sqlexplorer/EDriverName.java
@@ -29,166 +29,181 @@ import org.osgi.framework.Constants;
  * 
  */
 public enum EDriverName {
-	ODBCDEFAULTURL("Generic ODBC", //$NON-NLS-1$
-			"sun.jdbc.odbc.JdbcOdbcDriver", //$NON-NLS-1$
-			"-1"), //$NON-NLS-1$ 
-	MYSQLDEFAULTURL("Mysql", //$NON-NLS-1$
-			"org.gjt.mm.mysql.Driver", //$NON-NLS-1$
-			"-6", "lib/mysql-connector-java-5.1.3-bin.jar"), HSQLDEFAULTURL(
-			"HSql", //$NON-NLS-1$
-			"org.hsqldb.jdbcDriver", //$NON-NLS-1$
-			"-18", "lib/hsqldb.jar"), ORACLEDEFAULTURL("Oracle", //$NON-NLS-1$
-			// MOD scorreia 2008-08-22: oracle.jdbc.driver package is not
-			// supported anymore: replaced
-			"oracle.jdbc.OracleDriver", //$NON-NLS-1$
-			"-4", "lib/ojdbc14.jar"), MSSQLDEFAULTURL("MSSQL", //$NON-NLS-1$
-			"net.sourceforge.jtds.jdbc.Driver", //$NON-NLS-1$
-			"-11", "lib/jtds-1.2.jar"), //$NON-NLS-1$
-	DB2DEFAULTURL(
-			"DB2", //$NON-NLS-1$
-			"com.ibm.db2.jcc.DB2Driver", //$NON-NLS-1$
-			"-24", "lib/db2jcc_license_cu.jar", "lib/db2jcc_license_cisuz.jar",
-			"lib/db2jcc.jar"), POSTGRESQLEFAULTURL("PostgreSQL", //$NON-NLS-1$
-			"org.postgresql.Driver", //$NON-NLS-1$
-			"-7", "lib/postgresql-8.1-405.jdbc3.jar"), INTERBASEDEFAULTURL(
-			"Interbase", //$NON-NLS-1$
-			"interbase.interclient.Driver", //$NON-NLS-1$
-			"-3", "lib/interclient.jar"), SYBASEDEFAULTURL("Sybase", //$NON-NLS-1$
-			"com.sybase.jdbc3.jdbc.SybDriver", //$NON-NLS-1$
-			"-9", "lib/jconn3.jar"), INFORMIXDEFAULTURL("Informix", //$NON-NLS-1$
-			"com.informix.jdbc.IfxDriver", //$NON-NLS-1$
-			null, "lib/ifxjdbc.jar"), //$NON-NLS-1$ 
-	FIREBIRDDEFAULTURL("FireBird", //$NON-NLS-1$
-			"org.firebirdsql.jdbc.FBDriver", //$NON-NLS-1$
-			"-25", "lib/jaybird-2.1.1.jar"), TERADATADEFAULTURL("Teradata",
-			"com.ncr.teradata.TeraDriver", "-50", "lib/terajdbc4.jar",
-			"lib/tdgssconfig.jar", "lib/tdgssjava.jar"), SQLITE3DEFAULTURL(
-			"SQLite3", "org.sqlite.JDBC", "-30",
-			"lib/sqlitejdbc_v037_nested.jar"),
+    ODBCDEFAULTURL("Generic ODBC", //$NON-NLS-1$
+                   "sun.jdbc.odbc.JdbcOdbcDriver", //$NON-NLS-1$
+                   "-1"), //$NON-NLS-1$ 
+    MYSQLDEFAULTURL("Mysql", //$NON-NLS-1$
+                    "org.gjt.mm.mysql.Driver", //$NON-NLS-1$
+                    "-6",
+                    "lib/mysql-connector-java-5.1.3-bin.jar"),
+    HSQLDEFAULTURL("HSql", //$NON-NLS-1$
+                   "org.hsqldb.jdbcDriver", //$NON-NLS-1$
+                   "-18",
+                   "lib/hsqldb.jar"),
+    ORACLEDEFAULTURL("Oracle", //$NON-NLS-1$
+                     // MOD scorreia 2008-08-22: oracle.jdbc.driver package is not
+                     // supported anymore: replaced
+                     "oracle.jdbc.OracleDriver", //$NON-NLS-1$
+                     "-4",
+                     "lib/ojdbc14.jar"),
+    MSSQLDEFAULTURL("MSSQL", //$NON-NLS-1$
+                    "net.sourceforge.jtds.jdbc.Driver", //$NON-NLS-1$
+                    "-11", "lib/jtds-1.2.jar"), //$NON-NLS-1$
+    DB2DEFAULTURL("DB2", //$NON-NLS-1$
+                  "com.ibm.db2.jcc.DB2Driver", //$NON-NLS-1$
+                  "-24",
+                  "lib/db2jcc_license_cu.jar",
+                  "lib/db2jcc_license_cisuz.jar",
+                  "lib/db2jcc.jar"),
+    POSTGRESQLEFAULTURL("PostgreSQL", //$NON-NLS-1$
+                        "org.postgresql.Driver", //$NON-NLS-1$
+                        "-7",
+                        "lib/postgresql-8.1-405.jdbc3.jar"),
+    INTERBASEDEFAULTURL("Interbase", //$NON-NLS-1$
+                        "interbase.interclient.Driver", //$NON-NLS-1$
+                        "-3",
+                        "lib/interclient.jar"),
+    SYBASEDEFAULTURL("Sybase", //$NON-NLS-1$
+                     "com.sybase.jdbc3.jdbc.SybDriver", //$NON-NLS-1$
+                     "-9",
+                     "lib/jconn3.jar"),
+    INFORMIXDEFAULTURL("Informix", //$NON-NLS-1$
+                       "com.informix.jdbc.IfxDriver", //$NON-NLS-1$
+                       null,
+                       "lib/ifxjdbc.jar"), //$NON-NLS-1$ 
+    FIREBIRDDEFAULTURL("FireBird", //$NON-NLS-1$
+                       "org.firebirdsql.jdbc.FBDriver", //$NON-NLS-1$
+                       "-25",
+                       "lib/jaybird-2.1.1.jar"),
+    TERADATADEFAULTURL(
+                       "Teradata",
+                       "com.ncr.teradata.TeraDriver",
+                       "-50",
+                       "lib/terajdbc4.jar",
+                       "lib/tdgssconfig.jar",
+                       "lib/tdgssjava.jar"),
+    SQLITE3DEFAULTURL("SQLite3", "org.sqlite.JDBC", "-30", "lib/sqlitejdbc_v037_nested.jar"),
     AS400DEFAULTURL("AS400", "com.ibm.as400.access.AS400JDBCDriver", "-51", "lib/jt400_V5R3.jar");
 
-	private final String dbKey;
+    private final String dbKey;
 
-	private final String dbDriver;
+    private final String dbDriver;
 
-	private String sqlEid;
+    private String sqlEid;
 
-	private String[] jars;
+    private String[] jars;
 
-	EDriverName(String dbKey, String dbDriver, String sqlEid, String... jars) {
-		this.dbKey = dbKey;
-		this.dbDriver = dbDriver;
-		this.sqlEid = sqlEid;
-		this.jars = jars;
-	}
+    EDriverName(String dbKey, String dbDriver, String sqlEid, String... jars) {
+        this.dbKey = dbKey;
+        this.dbDriver = dbDriver;
+        this.sqlEid = sqlEid;
+        this.jars = jars;
+    }
 
-	public String getDBKey() {
-		return dbKey;
-	}
+    public String getDBKey() {
+        return dbKey;
+    }
 
-	/**
-	 * Getter for dbDriver.
-	 * 
-	 * @return the dbDriver
-	 */
-	public String getDbDriver() {
-		return dbDriver;
-	}
+    /**
+     * Getter for dbDriver.
+     * 
+     * @return the dbDriver
+     */
+    public String getDbDriver() {
+        return dbDriver;
+    }
 
-	/**
-	 * Getter for sqlEid.
-	 * 
-	 * @return the sqlEid
-	 */
-	public String getSqlEid() {
-		return this.sqlEid;
-	}
+    /**
+     * Getter for sqlEid.
+     * 
+     * @return the sqlEid
+     */
+    public String getSqlEid() {
+        return this.sqlEid;
+    }
 
-	/**
-	 * DOC qzhang Comment method "getJars".
-	 * 
-	 * @return
-	 */
-	public LinkedList<String> getJars() {
-		LinkedList<String> linkedList = new LinkedList<String>();
-		String plugins = "org.talend.libraries";
-		switch (this) {
-		case DB2DEFAULTURL:
-			plugins = "org.talend.libraries.jdbc.db2";
-			break;
-		case MSSQLDEFAULTURL:
-			plugins = "org.talend.libraries.jdbc.jtds";
-			break;
-		case MYSQLDEFAULTURL:
-			plugins = "org.talend.libraries.jdbc.mysql";
-			break;
-		case ORACLEDEFAULTURL:
-			plugins = "org.talend.libraries.jdbc.oracle";
-			break;
-		case POSTGRESQLEFAULTURL:
-			plugins = "org.talend.libraries.jdbc.postgresql";
-			break;
-		case SYBASEDEFAULTURL:
-			plugins = "org.talend.libraries.jdbc.sybase";
-			break;
-		case HSQLDEFAULTURL:
-			plugins = "org.talend.libraries.jdbc.hsql";
-			break;
-		case TERADATADEFAULTURL:
-			plugins = "org.talend.libraries.jdbc.teradata";
-			break;
-		case INFORMIXDEFAULTURL:
-			plugins = "org.talend.libraries.jdbc.informix";
-			break;
-		case SQLITE3DEFAULTURL:
-			plugins = "org.talend.libraries.jdbc.sqlite3";
-			break;
-		case AS400DEFAULTURL:
+    /**
+     * DOC qzhang Comment method "getJars".
+     * 
+     * @return
+     */
+    public LinkedList<String> getJars() {
+        LinkedList<String> linkedList = new LinkedList<String>();
+        String plugins = "org.talend.libraries";
+        switch (this) {
+        case DB2DEFAULTURL:
+            plugins = "org.talend.libraries.jdbc.db2";
+            break;
+        case MSSQLDEFAULTURL:
+            plugins = "org.talend.libraries.jdbc.jtds";
+            break;
+        case MYSQLDEFAULTURL:
+            plugins = "org.talend.libraries.jdbc.mysql";
+            break;
+        case ORACLEDEFAULTURL:
+            plugins = "org.talend.libraries.jdbc.oracle";
+            break;
+        case POSTGRESQLEFAULTURL:
+            plugins = "org.talend.libraries.jdbc.postgresql";
+            break;
+        case SYBASEDEFAULTURL:
+            plugins = "org.talend.libraries.jdbc.sybase";
+            break;
+        case HSQLDEFAULTURL:
+            plugins = "org.talend.libraries.jdbc.hsql";
+            break;
+        case TERADATADEFAULTURL:
+            plugins = "org.talend.libraries.jdbc.teradata";
+            break;
+        case INFORMIXDEFAULTURL:
+            plugins = "org.talend.libraries.jdbc.informix";
+            break;
+        case SQLITE3DEFAULTURL:
+            plugins = "org.talend.libraries.jdbc.sqlite3";
+            break;
+        case AS400DEFAULTURL:
             plugins = "org.talend.libraries.jdbc.as400";
             break;
-		default:
-			return linkedList;
-		}
-		Bundle bundle = Platform.getBundle(plugins);
-		if (bundle != null) {
-			try {
-				String requires = (String) bundle.getHeaders().get(
-						Constants.BUNDLE_CLASSPATH);
-				ManifestElement[] elements = ManifestElement.parseHeader(
-						Constants.BUNDLE_CLASSPATH, requires);
-				URL hsqldbJar = null;
-				if (jars != null) {
-					for (String jar : jars) {
-						String value = elements[0].getValue();
-						for (ManifestElement element : elements) {
-							if (jar.equals(element.getValue())) {
-								value = element.getValue();
-								hsqldbJar = FileLocator.toFileURL(bundle
-										.getEntry(value));
-								linkedList.add(hsqldbJar.getPath());
-							}
-						}
-					}
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return linkedList;
-	}
+        default:
+            return linkedList;
+        }
+        Bundle bundle = Platform.getBundle(plugins);
+        if (bundle != null) {
+            try {
+                String requires = (String) bundle.getHeaders().get(Constants.BUNDLE_CLASSPATH);
+                ManifestElement[] elements = ManifestElement.parseHeader(Constants.BUNDLE_CLASSPATH, requires);
+                URL hsqldbJar = null;
+                if (jars != null) {
+                    for (String jar : jars) {
+                        String value = elements[0].getValue();
+                        for (ManifestElement element : elements) {
+                            if (jar.equals(element.getValue())) {
+                                value = element.getValue();
+                                hsqldbJar = FileLocator.toFileURL(bundle.getEntry(value));
+                                linkedList.add(hsqldbJar.getPath());
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return linkedList;
+    }
 
-	/**
-	 * DOC qzhang Comment method "getId".
-	 * 
-	 * @param string
-	 */
-	public static String getId(String driver) {
-		String id = ODBCDEFAULTURL.sqlEid;
-		for (EDriverName driverName : values()) {
-			if (driverName.dbDriver.equalsIgnoreCase(driver)) {
-				return driverName.sqlEid;
-			}
-		}
-		return id;
-	}
+    /**
+     * DOC qzhang Comment method "getId".
+     * 
+     * @param string
+     */
+    public static String getId(String driver) {
+        String id = ODBCDEFAULTURL.sqlEid;
+        for (EDriverName driverName : values()) {
+            if (driverName.dbDriver.equalsIgnoreCase(driver)) {
+                return driverName.sqlEid;
+            }
+        }
+        return id;
+    }
 }
--- net.sourceforge.sqlexplorer/src/net/sourceforge/sqlexplorer/dbproduct/ExecutionResultImpl.java
+++ net.sourceforge.sqlexplorer/src/net/sourceforge/sqlexplorer/dbproduct/ExecutionResultImpl.java
@@ -3,8 +3,8 @@
  */
 package net.sourceforge.sqlexplorer.dbproduct;
 
-import java.sql.ResultSet;
 import java.sql.CallableStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -18,165 +18,178 @@ import net.sourceforge.sqlexplorer.dbproduct.DatabaseProduct.ExecutionResults;
 import net.sourceforge.sqlexplorer.parsers.NamedParameter;
 
 public final class ExecutionResultImpl implements ExecutionResults {
-	
-	// Current state - IE, which set of results we're currently looking for
-	private enum State {
-		PRIMARY_RESULTS,		// We're providing the main results, from Statement.getResults()
-		SECONDARY_RESULTS,		// We're providing resultsets from Statement.getMoreResults()
-		PARAMETER_RESULTS,		// We're returning resultsets from output parameters
-		OUTPUT_PARAMETERS,		// We're returning a fake result set listing output parameters
-		CLOSED					// All done
-	}
-	
-	/*
-	 * Temporary class used by nextDataSet() to collate parameters
-	 */
-	private class ParamValues {
-		private NamedParameter param;
-		private ArrayList<Integer> columnIndexes = new ArrayList<Integer>();
-		
-		public ParamValues(NamedParameter param, int columnIndex) {
-			super();
-			this.param = param;
-			add(columnIndex);
-		}
-		public void add(int columnIndex) {
-			this.columnIndexes.add(new Integer(columnIndex));
-		}
-	}
-	
-	private State state = State.PRIMARY_RESULTS;
-	private AbstractDatabaseProduct product;
-	private Statement stmt;
-	private LinkedList<NamedParameter> parameters;
-	private int maxRows;
-	private int paramColumnIndex;
-	private Iterator<NamedParameter> paramIter;
-	private int updateCount;
-	private ResultSet currentResultSet;
 
-	public ExecutionResultImpl(AbstractDatabaseProduct product, Statement stmt, boolean hasResults, LinkedList<NamedParameter> parameters, int maxRows) throws SQLException {
-		super();
-		this.product = product;
-		this.stmt = stmt;
-		this.parameters = parameters;
-		this.maxRows = maxRows;
-		
-		if (!hasResults)
-			state = State.SECONDARY_RESULTS;
-	}
+    // Current state - IE, which set of results we're currently looking for
+    private enum State {
+        PRIMARY_RESULTS, // We're providing the main results, from Statement.getResults()
+        SECONDARY_RESULTS, // We're providing resultsets from Statement.getMoreResults()
+        PARAMETER_RESULTS, // We're returning resultsets from output parameters
+        OUTPUT_PARAMETERS, // We're returning a fake result set listing output parameters
+        CLOSED
+        // All done
+    }
 
-	public DataSet nextDataSet() throws SQLException {
-		// Close the current one
-		if (currentResultSet != null) {
-			currentResultSet.close();
-			currentResultSet = null;
-		}
+    /*
+     * Temporary class used by nextDataSet() to collate parameters
+     */
+    private class ParamValues {
 
-		// Anything more to do?
-		if (state == State.CLOSED)
-			return null;
-		
-		// Get the first set
-		if (state == State.PRIMARY_RESULTS) {
-			currentResultSet = stmt.getResultSet();
-			state = State.SECONDARY_RESULTS;
-			if (currentResultSet != null)
-				return new DataSet(currentResultSet, null, maxRows);
-		}
-		
-		// While we have more secondary results (i.e. those that come directly from Statement but after the first getResults())
-		while (state == State.SECONDARY_RESULTS) {
-			if (stmt.getMoreResults())
-				currentResultSet = stmt.getResultSet();
-			else {
-				int updateCount = stmt.getUpdateCount();
-				if (updateCount != -1)
-					this.updateCount += updateCount;
-				else
-					state = State.PARAMETER_RESULTS;
-			}
-		}
-		
-		// Got one? Then exit
-		if (currentResultSet != null) {
-			this.updateCount += stmt.getUpdateCount();
-			return new DataSet(currentResultSet, null, maxRows);
-		}
-		
-		// Look for output parameters which return resultsets
-		if (state == State.PARAMETER_RESULTS && parameters != null) {
-			CallableStatement stmt = (CallableStatement)this.stmt;
-			if (paramIter == null) {
-				paramIter = parameters.iterator();
-				paramColumnIndex = 1;
-			}
-			while (paramIter.hasNext()) {
-				NamedParameter param = paramIter.next();
-				if (param.getDataType() == NamedParameter.DataType.CURSOR)
-					currentResultSet = product.getResultSet(stmt, param, paramColumnIndex);
-				paramColumnIndex++;
-				if (currentResultSet != null)
-					return new DataSet(Messages.getString("DataSet.Cursor") + ' ' + param.getName(), currentResultSet, null, maxRows);
-			}
-		}
+        private NamedParameter param;
 
-		// Generate a dataset for output parameters
-		state = State.CLOSED;
-		if (parameters == null)
-			return null;
-		if (!(stmt instanceof CallableStatement))
-			return null;
-		CallableStatement stmt = (CallableStatement)this.stmt;
-		TreeMap<NamedParameter, ParamValues> params = new TreeMap<NamedParameter, ParamValues>();
-		int columnIndex = 1;
-		int numValues = 0;
-		for (NamedParameter param : parameters) {
-			if (param.getDataType() != NamedParameter.DataType.CURSOR && param.isOutput()) {
-				ParamValues pv = params.get(param);
-				if (pv == null)
-					params.put(param, new ParamValues(param, columnIndex));
-				else
-					pv.add(columnIndex);
-				numValues++;
-			}
-			columnIndex++;
-		}
-		if (numValues == 0)
-			return null;
-		Comparable[][] rows = new Comparable[numValues][2];
-		columnIndex = 1;
-		int rowIndex = 0;
-		for (ParamValues pv : params.values()) {
-			int valueIndex = 1;
-			for (Integer index : pv.columnIndexes) {
-				Comparable[] row = rows[rowIndex++];
-				row[0] = pv.param.getName();
-				if (pv.columnIndexes.size() > 1)
-					row[0] = (pv.param.getName() + '[' + valueIndex + ']');
-				else
-					row[0] = pv.param.getName();
-				row[1] = stmt.getString(index);
-				valueIndex++;
-			}
-		}
-		return new DataSet(Messages.getString("DataSet.Parameters"), new String[] { 
-				Messages.getString("SQLExecution.ParameterName"),
-				Messages.getString("SQLExecution.ParameterValue")
-			}, rows);
-	}
-	
-	public void close() throws SQLException {
-		try {
-			stmt.close();
-		} catch(SQLException e) {
-			// Nothing
-		}
-		if (currentResultSet != null)
-			currentResultSet.close();
-	}
+        private ArrayList<Integer> columnIndexes = new ArrayList<Integer>();
 
-	public int getUpdateCount() throws SQLException {
-		return updateCount;
-	}
+        public ParamValues(NamedParameter param, int columnIndex) {
+            super();
+            this.param = param;
+            add(columnIndex);
+        }
+
+        public void add(int columnIndex) {
+            this.columnIndexes.add(new Integer(columnIndex));
+        }
+    }
+
+    private State state = State.PRIMARY_RESULTS;
+
+    private AbstractDatabaseProduct product;
+
+    private Statement stmt;
+
+    private LinkedList<NamedParameter> parameters;
+
+    private int maxRows;
+
+    private int paramColumnIndex;
+
+    private Iterator<NamedParameter> paramIter;
+
+    private int updateCount;
+
+    private ResultSet currentResultSet;
+
+    public ExecutionResultImpl(AbstractDatabaseProduct product, Statement stmt, boolean hasResults,
+            LinkedList<NamedParameter> parameters, int maxRows) throws SQLException {
+        super();
+        this.product = product;
+        this.stmt = stmt;
+        this.parameters = parameters;
+        this.maxRows = maxRows;
+
+        if (!hasResults)
+            state = State.SECONDARY_RESULTS;
+    }
+
+    public DataSet nextDataSet() throws SQLException {
+        // Close the current one
+        if (currentResultSet != null) {
+            currentResultSet.close();
+            currentResultSet = null;
+        }
+
+        // Anything more to do?
+        if (state == State.CLOSED)
+            return null;
+
+        // Get the first set
+        if (state == State.PRIMARY_RESULTS) {
+            currentResultSet = stmt.getResultSet();
+            state = State.SECONDARY_RESULTS;
+            if (currentResultSet != null)
+                return new DataSet(currentResultSet, null, maxRows);
+        }
+
+        // While we have more secondary results (i.e. those that come directly from Statement but after the first
+        // getResults())
+        while (state == State.SECONDARY_RESULTS) {
+            if (stmt.getMoreResults())
+                currentResultSet = stmt.getResultSet();
+            else {
+                int updateCount = stmt.getUpdateCount();
+                if (updateCount != -1 && updateCount != 0)
+                    this.updateCount += updateCount;
+                else
+                    state = State.PARAMETER_RESULTS;
+            }
+        }
+
+        // Got one? Then exit
+        if (currentResultSet != null) {
+            this.updateCount += stmt.getUpdateCount();
+            return new DataSet(currentResultSet, null, maxRows);
+        }
+
+        // Look for output parameters which return resultsets
+        if (state == State.PARAMETER_RESULTS && parameters != null) {
+            CallableStatement stmt = (CallableStatement) this.stmt;
+            if (paramIter == null) {
+                paramIter = parameters.iterator();
+                paramColumnIndex = 1;
+            }
+            while (paramIter.hasNext()) {
+                NamedParameter param = paramIter.next();
+                if (param.getDataType() == NamedParameter.DataType.CURSOR)
+                    currentResultSet = product.getResultSet(stmt, param, paramColumnIndex);
+                paramColumnIndex++;
+                if (currentResultSet != null)
+                    return new DataSet(Messages.getString("DataSet.Cursor") + ' ' + param.getName(), currentResultSet, null,
+                            maxRows);
+            }
+        }
+
+        // Generate a dataset for output parameters
+        state = State.CLOSED;
+        if (parameters == null)
+            return null;
+        if (!(stmt instanceof CallableStatement))
+            return null;
+        CallableStatement stmt = (CallableStatement) this.stmt;
+        TreeMap<NamedParameter, ParamValues> params = new TreeMap<NamedParameter, ParamValues>();
+        int columnIndex = 1;
+        int numValues = 0;
+        for (NamedParameter param : parameters) {
+            if (param.getDataType() != NamedParameter.DataType.CURSOR && param.isOutput()) {
+                ParamValues pv = params.get(param);
+                if (pv == null)
+                    params.put(param, new ParamValues(param, columnIndex));
+                else
+                    pv.add(columnIndex);
+                numValues++;
+            }
+            columnIndex++;
+        }
+        if (numValues == 0)
+            return null;
+        Comparable[][] rows = new Comparable[numValues][2];
+        columnIndex = 1;
+        int rowIndex = 0;
+        for (ParamValues pv : params.values()) {
+            int valueIndex = 1;
+            for (Integer index : pv.columnIndexes) {
+                Comparable[] row = rows[rowIndex++];
+                row[0] = pv.param.getName();
+                if (pv.columnIndexes.size() > 1)
+                    row[0] = (pv.param.getName() + '[' + valueIndex + ']');
+                else
+                    row[0] = pv.param.getName();
+                row[1] = stmt.getString(index);
+                valueIndex++;
+            }
+        }
+        return new DataSet(Messages.getString("DataSet.Parameters"), new String[] {
+                Messages.getString("SQLExecution.ParameterName"), Messages.getString("SQLExecution.ParameterValue") }, rows);
+    }
+
+    public void close() throws SQLException {
+        try {
+            stmt.close();
+        } catch (SQLException e) {
+            // Nothing
+        }
+        if (currentResultSet != null)
+            currentResultSet.close();
+    }
+
+    public int getUpdateCount() throws SQLException {
+        return updateCount;
+    }
 }
