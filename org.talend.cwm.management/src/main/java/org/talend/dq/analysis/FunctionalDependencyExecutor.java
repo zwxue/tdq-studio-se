2244e4a4407a7cd123a45657229d09971afeefd2
xqliu <xqliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-07-23 06:35:46 +0000

    feature  8134 fixed:add executor
    git-svn-id: http://talendforge.org/svn/top/trunk@26786 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/analysis/FunctionalDependencyExecutor.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/analysis/FunctionalDependencyExecutor.java
@@ -14,7 +14,6 @@ package org.talend.dq.analysis;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -23,45 +22,35 @@ import org.talend.cwm.db.connection.ConnectionUtils;
 import org.talend.cwm.exception.AnalysisExecutionException;
 import org.talend.cwm.helper.ColumnHelper;
 import org.talend.cwm.helper.ColumnSetHelper;
-import org.talend.cwm.helper.SwitchHelpers;
-import org.talend.cwm.management.i18n.Messages;
-import org.talend.cwm.relational.TdColumn;
 import org.talend.dataquality.analysis.Analysis;
 import org.talend.dataquality.indicators.Indicator;
-import org.talend.dataquality.indicators.IndicatorsPackage;
 import org.talend.dataquality.indicators.columnset.ColumnDependencyIndicator;
 import org.talend.dataquality.indicators.columnset.ColumnsetPackage;
-import org.talend.dataquality.indicators.columnset.RowMatchingIndicator;
 import org.talend.dataquality.indicators.definition.IndicatorDefinition;
 import org.talend.dq.dbms.GenericSQLHandler;
-import org.talend.dq.indicators.IndicatorEvaluator;
-import org.talend.utils.sugars.ReturnCode;
 import org.talend.utils.sugars.TypedReturnCode;
-
 import orgomg.cwm.objectmodel.core.CoreFactory;
 import orgomg.cwm.objectmodel.core.Expression;
 import orgomg.cwm.objectmodel.core.Package;
 import orgomg.cwm.resource.relational.Column;
 import orgomg.cwm.resource.relational.ColumnSet;
 
-
 /**
- * DOC jet  class global comment. Detailled comment
+ * DOC jet class global comment. Detailled comment
  */
 public class FunctionalDependencyExecutor extends ColumnAnalysisSqlExecutor {
 
     private static Logger log = Logger.getLogger(FunctionalDependencyExecutor.class);
-    
+
     private String catalogOrSchema = null;
-    
+
     @Override
     protected boolean check(Analysis analysis) {
-        
+
         return true;
-    
+
     }
 
-  
     protected boolean runAnalysis(Analysis analysis, String sqlStatement) {
         boolean ok = true;
         TypedReturnCode<Connection> trc = this.getConnection(analysis);
@@ -79,7 +68,7 @@ public class FunctionalDependencyExecutor extends ColumnAnalysisSqlExecutor {
                 if (deactivatedIndicators.contains(indicator)) {
                     continue; // do not evaluate this indicator
                 }
-               
+
                 Expression query = dbms().getInstantiatedExpression(indicator);
 
                 if (query == null || !executeQuery(indicator, connection, query)) {
@@ -104,60 +93,59 @@ public class FunctionalDependencyExecutor extends ColumnAnalysisSqlExecutor {
         }
         return ok;
     }
-   
+
     private boolean executeQuery(Indicator indicator, Connection connection, Expression query) throws AnalysisExecutionException {
         try {
-           
+
             List<Object[]> myResultSet = executeQuery(catalogOrSchema, connection, query.getBody());
-           if(indicator instanceof ColumnDependencyIndicator){
-               ((ColumnDependencyIndicator)indicator).setACount(getNB(myResultSet));
-               ((ColumnDependencyIndicator)indicator).setDistinctACount(getNBDistinct(myResultSet));
-               
-           }
-           //computer result
-        
-        }catch (SQLException e) {
+            if (indicator instanceof ColumnDependencyIndicator) {
+                ((ColumnDependencyIndicator) indicator).setACount(getNB(myResultSet));
+                ((ColumnDependencyIndicator) indicator).setDistinctACount(getNBDistinct(myResultSet));
+
+            }
+            // computer result
+
+        } catch (SQLException e) {
             log.error(e, e);
             return false;
         }
-        
+
         return true;
     }
-    
-    
+
     /**
      * DOC jet Comment method "getNBDistinct".
+     * 
      * @param myResultSet
      * @return
      */
     private Long getNBDistinct(List<Object[]> myResultSet) {
-        
-        if(myResultSet.size() == 1 && myResultSet.get(0).length == 2){
+
+        if (myResultSet.size() == 1 && myResultSet.get(0).length == 2) {
             return (Long) myResultSet.get(0)[1];
         }
-        
+
         return null;
     }
 
-
     /**
      * DOC jet Comment method "getNB".
+     * 
      * @param myResultSet
      * @return
      */
     private Long getNB(List<Object[]> myResultSet) {
-        if(myResultSet.size() == 1 && myResultSet.get(0).length == 2){
+        if (myResultSet.size() == 1 && myResultSet.get(0).length == 2) {
             return (Long) myResultSet.get(0)[0];
         }
-        
+
         return null;
     }
-
 
     @Override
     protected String createSqlStatement(Analysis analysis) {
         this.cachedAnalysis = analysis;
-        
+
         EList<Indicator> indicators = analysis.getResults().getIndicators();
         for (Indicator indicator : indicators) {
             instantiateQuery(indicator);
@@ -167,25 +155,19 @@ public class FunctionalDependencyExecutor extends ColumnAnalysisSqlExecutor {
         return ""; //$NON-NLS-1$
     }
 
-
-
-
-
     private boolean instantiateQuery(Indicator indicator) {
         // (but is not need, hence we keep it commented)
-        
+
         if (ColumnsetPackage.eINSTANCE.getColumnDependencyIndicator().equals(indicator.eClass())) {
             ColumnDependencyIndicator rowMatchingIndicator = (ColumnDependencyIndicator) indicator;
             Column columnA = rowMatchingIndicator.getColumnA();
             Column columnB = rowMatchingIndicator.getColumnB();
-            
-           
 
             IndicatorDefinition indicatorDefinition = indicator.getIndicatorDefinition();
             Expression sqlGenericExpression = dbms().getSqlExpression(indicatorDefinition);
 
             boolean useNulls = false; // TODO scorreia create an indicator for each option
-            
+
             Expression instantiatedSqlExpression = createInstantiatedSqlExpression(sqlGenericExpression, columnA, columnB);
             indicator.setInstantiatedExpression(instantiatedSqlExpression);
             return true;
@@ -193,12 +175,9 @@ public class FunctionalDependencyExecutor extends ColumnAnalysisSqlExecutor {
         return traceError("Unhandled given indicator: " + indicator.getName());
     }
 
-
-
-
-
     /**
      * DOC jet Comment method "createInstantiatedSqlExpression".
+     * 
      * @param sqlGenericExpression
      * @param columnA
      * @param columnB
@@ -208,34 +187,31 @@ public class FunctionalDependencyExecutor extends ColumnAnalysisSqlExecutor {
     private Expression createInstantiatedSqlExpression(Expression sqlGenericExpression, Column columnA, Column columnB) {
         assert columnA != null;
         assert columnB != null;
-        
+
         String genericSQL = sqlGenericExpression.getBody();
         GenericSQLHandler sqlHandler = new GenericSQLHandler(genericSQL);
-        sqlHandler.replaceColumnA(columnA.getName()).replaceColumnB(columnB.getName()).replaceTable(getTableNameFromColumn(columnA));
+        sqlHandler.replaceColumnA(columnA.getName()).replaceColumnB(columnB.getName()).replaceTable(
+                getTableNameFromColumn(columnA));
         Expression instantiatedExpression = CoreFactory.eINSTANCE.createExpression();
         instantiatedExpression.setLanguage(sqlGenericExpression.getLanguage());
         String instantiatedSQL = sqlHandler.getSqlString();
         instantiatedExpression.setBody(instantiatedSQL);
         return instantiatedExpression;
     }
-    
-    private String getTableNameFromColumn(Column column){
-        
-        
-        
+
+    private String getTableNameFromColumn(Column column) {
+
         ColumnSet columnSetOwner = ColumnHelper.getColumnSetOwner(column);
         if (columnSetOwner == null) {
             log.error("ColumnSet Owner of column " + column.getName() + " is null");
         } else {
-            //this is so bad code。
+            // this is so bad code
             Package schema = ColumnSetHelper.getParentCatalogOrSchema(columnSetOwner);
             catalogOrSchema = schema.getName();
             return columnSetOwner.getName();
-            
+
         }
         return null;
     }
-    
-    
-    
+
 }
