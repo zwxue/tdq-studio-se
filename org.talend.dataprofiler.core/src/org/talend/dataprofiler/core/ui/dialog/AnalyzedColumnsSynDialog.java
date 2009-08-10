694b892da4f8aa599a223d418123ea083e332db8
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-08-10 10:34:49 +0000

    bug 8567: Add functionality of changing connection for MS SQL Server.
    git-svn-id: http://talendforge.org/svn/top/trunk@27293 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/dialog/AnalyzedColumnsSynDialog.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/dialog/AnalyzedColumnsSynDialog.java
@@ -17,15 +17,19 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.swt.widgets.Shell;
+import org.talend.cwm.helper.CatalogHelper;
 import org.talend.cwm.helper.ColumnHelper;
 import org.talend.cwm.helper.ColumnSetHelper;
 import org.talend.cwm.helper.PackageHelper;
+import org.talend.cwm.relational.TdCatalog;
 import org.talend.cwm.relational.TdColumn;
+import org.talend.cwm.relational.TdSchema;
 import org.talend.cwm.relational.TdTable;
 import org.talend.cwm.softwaredeployment.TdDataProvider;
 import org.talend.dataquality.analysis.Analysis;
 import orgomg.cwm.objectmodel.core.ModelElement;
 import orgomg.cwm.objectmodel.core.Package;
+import orgomg.cwm.resource.relational.Catalog;
 import orgomg.cwm.resource.relational.ColumnSet;
 
 /**
@@ -34,104 +38,124 @@ import orgomg.cwm.resource.relational.ColumnSet;
  */
 public class AnalyzedColumnsSynDialog extends AnalyzedElementSynDialog {
 
-	private static Logger log = Logger
-			.getLogger(AnalyzedColumnsSynDialog.class);
-	private EList<ModelElement> analyzedElements;
+    private static Logger log = Logger.getLogger(AnalyzedColumnsSynDialog.class);
 
-	public AnalyzedColumnsSynDialog(Shell parent, Analysis analysis,
-			TdDataProvider dataprovider, EList<ModelElement> analyzedElements) {
-		super(parent, analysis, dataprovider);
-		this.analyzedElements = analyzedElements;
-	}
+    private EList<ModelElement> analyzedElements;
 
-	@Override
-	public void reloadInputModel() {
-		TdColumn column = null;
-		modelInput.clear();
-		synedEleMap.clear();
-		for (ModelElement element : analyzedElements) {
-			try {
+    public AnalyzedColumnsSynDialog(Shell parent, Analysis analysis, TdDataProvider dataprovider,
+            EList<ModelElement> analyzedElements) {
+        super(parent, analysis, dataprovider);
+        this.analyzedElements = analyzedElements;
+    }
 
-				column = (TdColumn) element;
-				synedEleMap.put(column, null);
+    @Override
+    public void reloadInputModel() {
+        TdColumn column = null;
+        modelInput.clear();
+        synedEleMap.clear();
+        for (ModelElement element : analyzedElements) {
+            try {
 
-				ColumnSet anaColumnSet = ColumnHelper.getColumnSetOwner(column);
-				Package anaPackage = ColumnSetHelper.getParentCatalogOrSchema(anaColumnSet);
+                column = (TdColumn) element;
+                synedEleMap.put(column, null);
+
+                ColumnSet anaColumnSet = ColumnHelper.getColumnSetOwner(column);
+                Package anaPackage = ColumnSetHelper.getParentCatalogOrSchema(anaColumnSet);
                 if (anaPackage == null) {
                     return;
                 }
-				Package connPackage = null;
+                Package connPackage = null;
 
-				for (Package pk : newDataProvider.getDataPackage()) {
-					//
-					if (pk.getName().equalsIgnoreCase(anaPackage.getName())) {
-						connPackage = pk;
-						break;
-					}
-				}
-				if (connPackage == null) {
-					SynTreeModel synTreeModel = new SynTreeModel(column);
-					synTreeModel.setOldDataProvElement(anaPackage);
-					// synTreeModel.setNewDataProvElement(connPackage);
-					modelInput.add(synTreeModel);
-					continue;
-				}
+                for (Package pk : newDataProvider.getDataPackage()) {
+                    // MOD mzhao bug 8567. 2009-08-10 (Case of MS SQL Server)
+                    if (pk instanceof TdCatalog && anaPackage instanceof TdSchema) {
+                        Catalog catl = CatalogHelper.getParentCatalog(anaPackage);
+                        if (pk.getName().equalsIgnoreCase(catl.getName())) {
+                            connPackage = pk;
+                            break;
+                        }
+                    } else if (pk.getName().equalsIgnoreCase(anaPackage.getName())) {
+                        connPackage = pk;
+                        break;
+                    }
+                }
+                if (connPackage == null) {
+                    SynTreeModel synTreeModel = new SynTreeModel(column);
+                    synTreeModel.setOldDataProvElement(anaPackage);
+                    // synTreeModel.setNewDataProvElement(connPackage);
+                    modelInput.add(synTreeModel);
+                    continue;
+                }
 
-				List connColumnSetList = null;
-				if (anaColumnSet instanceof TdTable) {
-					connColumnSetList = PackageHelper.getTables(connPackage);
-				} else {
-					connColumnSetList = PackageHelper.getViews(connPackage);
-				}
-				ColumnSet connColumnSet = null;
-				for (Object colSet : connColumnSetList) {
-					if (((ColumnSet) colSet).getName().equalsIgnoreCase(
-							anaColumnSet.getName())) {
-						connColumnSet = (ColumnSet) colSet;
-						break;
-					}
-				}
+                List<?> connColumnSetList = null;
+                // MOD mzhao bug 8567. 2009-08-10 (Case of MS SQL Server)
+                if (connPackage instanceof TdCatalog && anaPackage instanceof TdSchema) {
+                    for (TdSchema sche : CatalogHelper.getSchemas((TdCatalog) connPackage)) {
+                        if (sche.getName().equalsIgnoreCase(anaPackage.getName())) {
+                            if (anaColumnSet instanceof TdTable) {
+                                connColumnSetList = PackageHelper.getTables(sche);
+                            } else {
+                                connColumnSetList = PackageHelper.getViews(sche);
+                            }
+                            break;
+                        }
 
-				if (connColumnSet == null) {
-					SynTreeModel synTreeModel = new SynTreeModel(column);
-					synTreeModel.setOldDataProvElement(anaColumnSet);
-					synTreeModel.setNewDataProvElement(connPackage);
-					modelInput.add(synTreeModel);
-					continue;
-				}
+                    }
 
-				TdColumn connColumn = null;
-				for (TdColumn loopColumn : ColumnSetHelper
-						.getColumns(connColumnSet)) {
-					if (loopColumn.getName().equalsIgnoreCase(column.getName())) {
-						connColumn = loopColumn;
-						break;
-					}
-				}
-				if (connColumn == null) {
-					SynTreeModel synTreeModel = new SynTreeModel(column);
-					synTreeModel.setOldDataProvElement(column);
-					synTreeModel.setNewDataProvElement(connColumnSet);
-					modelInput.add(synTreeModel);
-					continue;
-				}
-				synedEleMap.put(column, connColumn);
-				if (!connColumn.getSqlDataType().getName().equals(
-						column.getSqlDataType().getName())) {
-					SynTreeModel synTreeModel = new SynTreeModel(column);
-					synTreeModel.setOldDataProvElement(column.getSqlDataType());
-					// synTreeModel.setNewDataProvElement(connColumn
-					// .getSqlDataType());
-					modelInput.add(synTreeModel);
-					continue;
-				}
+                } else {
+                    if (anaColumnSet instanceof TdTable) {
+                        connColumnSetList = PackageHelper.getTables(connPackage);
+                    } else {
+                        connColumnSetList = PackageHelper.getViews(connPackage);
+                    }
+                }
 
-			} catch (Exception e) {
-				log.error(e, e);
-				e.printStackTrace();
-			}
-		}
+                ColumnSet connColumnSet = null;
+                for (Object colSet : connColumnSetList) {
 
-	}
+                    if (((ColumnSet) colSet).getName().equalsIgnoreCase(anaColumnSet.getName())) {
+                        connColumnSet = (ColumnSet) colSet;
+                        break;
+                    }
+                }
 
+                if (connColumnSet == null) {
+                    SynTreeModel synTreeModel = new SynTreeModel(column);
+                    synTreeModel.setOldDataProvElement(anaColumnSet);
+                    synTreeModel.setNewDataProvElement(connPackage);
+                    modelInput.add(synTreeModel);
+                    continue;
+                }
+
+                TdColumn connColumn = null;
+                for (TdColumn loopColumn : ColumnSetHelper.getColumns(connColumnSet)) {
+                    if (loopColumn.getName().equalsIgnoreCase(column.getName())) {
+                        connColumn = loopColumn;
+                        break;
+                    }
+                }
+                if (connColumn == null) {
+                    SynTreeModel synTreeModel = new SynTreeModel(column);
+                    synTreeModel.setOldDataProvElement(column);
+                    synTreeModel.setNewDataProvElement(connColumnSet);
+                    modelInput.add(synTreeModel);
+                    continue;
+                }
+                synedEleMap.put(column, connColumn);
+                if (!connColumn.getSqlDataType().getName().equals(column.getSqlDataType().getName())) {
+                    SynTreeModel synTreeModel = new SynTreeModel(column);
+                    synTreeModel.setOldDataProvElement(column.getSqlDataType());
+                    // synTreeModel.setNewDataProvElement(connColumn
+                    // .getSqlDataType());
+                    modelInput.add(synTreeModel);
+                    continue;
+                }
+
+            } catch (Exception e) {
+                log.error(e, e);
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
