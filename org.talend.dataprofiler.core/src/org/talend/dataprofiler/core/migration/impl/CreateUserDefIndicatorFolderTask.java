14ac73dadf7a8cde8561c6a1b0e9e6c1cc42a1ce
xqliu <xqliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-08-14 11:18:12 +0000

    bug 7810 fixed: Add the possibility for the user to create his own indicators. add a migration task
    git-svn-id: http://talendforge.org/svn/top/trunk@27440 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/CreateUserDefIndicatorFolderTask.java
@@ -0,0 +1,61 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2009 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.migration.impl;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IFolder;
+import org.talend.dataprofiler.core.manager.DQStructureManager;
+import org.talend.dataprofiler.core.migration.AbstractMigrationTask;
+import org.talend.resource.ResourceManager;
+
+/**
+ * DOC xqliu class global comment. Detailled comment
+ */
+public class CreateUserDefIndicatorFolderTask extends AbstractMigrationTask {
+
+    protected static Logger log = Logger.getLogger(CreateUserDefIndicatorFolderTask.class);
+
+    public boolean execute() {
+        try {
+            DQStructureManager manager = DQStructureManager.getInstance();
+            
+            // creator Indicators
+            IFolder createNewFoler = manager.createNewFoler(ResourceManager.getLibrariesFolder(), DQStructureManager.INDICATORS);
+            createNewFoler.setPersistentProperty(DQStructureManager.FOLDER_CLASSIFY_KEY,
+                    DQStructureManager.INDICATORS_FOLDER_PROPERTY);
+            createNewFoler.setPersistentProperty(DQStructureManager.NO_SUBFOLDER_KEY, DQStructureManager.NO_SUBFOLDER_PROPERTY);
+            // create User Defined Indicators
+            createNewFoler = manager.createNewFoler(createNewFoler, DQStructureManager.USER_DEFINED_INDICATORS);
+            createNewFoler.setPersistentProperty(DQStructureManager.FOLDER_CLASSIFY_KEY, DQStructureManager.UDI_FOLDER_PROPERTY);
+
+        } catch (Exception e) {
+            log.error(e, e);
+            return false;
+        }
+        return true;
+    }
+
+    public MigrationTaskType getMigrationTaskType() {
+        return MigrationTaskType.STUCTRUE;
+    }
+
+    public Date getOrder() {
+        Calendar calender = Calendar.getInstance();
+        calender.set(2009, 8, 13);
+        return calender.getTime();
+    }
+
+}
