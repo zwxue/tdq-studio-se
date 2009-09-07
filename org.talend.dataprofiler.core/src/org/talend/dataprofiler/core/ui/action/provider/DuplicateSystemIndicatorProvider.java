0e3ad0896947a3e7719b09b3cf50e586c42ec93e
yyi <yyi@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-09-07 09:36:10 +0000

    feature 8882 fixed: Add "Duplicate" menu on the system indicators.
    git-svn-id: http://talendforge.org/svn/top/trunk@29378 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/provider/DuplicateSystemIndicatorProvider.java
@@ -0,0 +1,148 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2009 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.action.provider;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.navigator.CommonActionProvider;
+import org.eclipse.ui.part.ISetSelectionTarget;
+import org.talend.dataprofiler.core.ImageLib;
+import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
+import org.talend.dataprofiler.core.manager.DQStructureManager;
+import org.talend.dataquality.indicators.definition.IndicatorDefinition;
+import org.talend.dq.writer.EMFSharedResources;
+import org.talend.resource.ResourceManager;
+import orgomg.cwm.objectmodel.core.ModelElement;
+
+/**
+ * DOC yyi 2009-09-07, Feature:8882.
+ */
+public class DuplicateSystemIndicatorProvider extends CommonActionProvider {
+
+    @Override
+    public void fillContextMenu(IMenuManager menu) {
+        TreeSelection selection = (TreeSelection) this.getContext().getSelection();
+        if (!selection.isEmpty()) {
+            Object[] objs = selection.toArray();
+            IndicatorDefinition[] definitions = new IndicatorDefinition[objs.length];
+            for (int i = 0; i < objs.length; i++) {
+                IndicatorDefinition definition = (IndicatorDefinition) objs[i];
+                definitions[i] = definition;
+            }
+
+            DuplicateSystemIndicationAction duplicate = new DuplicateSystemIndicationAction(definitions);
+            menu.add(duplicate);
+        }
+    }
+
+    /**
+     * DOC yyi 2009-09-07, Feature:8882.
+     */
+    private class DuplicateSystemIndicationAction extends Action {
+
+        private IndicatorDefinition[] definitions;
+
+        public DuplicateSystemIndicationAction(IndicatorDefinition[] definitions) {
+            super(DefaultMessagesImpl.getString("DuplicateCWMResourceAction.Duplicate")); //$NON-NLS-1$
+            setImageDescriptor(ImageLib.getImageDescriptor(ImageLib.EDIT_COPY));
+            this.definitions = definitions;
+        }
+
+        @Override
+        public void run() {
+            if (definitions != null && definitions.length > 0) {
+                for (IndicatorDefinition definition : definitions) {
+
+                    ModelElement oldObject = (ModelElement) EMFSharedResources.getInstance().copyEObject(definition);
+                    if (oldObject != null) {
+                        ModelElement newObject = (ModelElement) EMFSharedResources.getInstance().copyEObject(oldObject);
+
+                        IFile newFile = getNewFile(definition);
+                        newObject.setName("copy of " + newObject.getName()); //$NON-NLS-1$
+
+                        EMFSharedResources.getInstance().addEObjectToResourceSet(newFile.getFullPath().toString(), newObject);
+                        EMFSharedResources.getInstance().saveLastResource();
+
+                        selectAndReveal(newFile);
+                    }
+                }
+            }
+        } // end of run
+
+        /**
+         * DOC yyi Comment method "getNewFile".
+         * 
+         * @param file
+         * @return
+         */
+        private IFile getNewFile(IndicatorDefinition definition) {
+            IFile newFile = null;
+            int idx = 1;
+            while (true) {
+                final String newFilename = "copy" + idx + definition.getName() + ".definition"; //$NON-NLS-1$
+                newFile = getFolder(DQStructureManager.INDICATORS).getFolder(DQStructureManager.USER_DEFINED_INDICATORS).getFile(
+                        newFilename);
+                if (!newFile.exists()) {
+                    break;
+                }
+                idx++;
+            }
+            return newFile;
+        } // end of getNewFile
+
+        /**
+         * DOC yyi Comment method "getFolder".
+         * 
+         * @param dest
+         * @return
+         */
+        private IFolder getFolder(String dest) {
+            return ResourceManager.getLibrariesFolder().getFolder(dest);
+        }
+
+        /**
+         * DOC yyi 2009-09-07, Feature:8882.
+         * 
+         * Selects and reveals the newly added resource in all parts of the active workbench window's active page.
+         * 
+         * @param resource
+         */
+        private void selectAndReveal(IResource resource) {
+            IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+            IWorkbenchPage page = workbenchWindow.getActivePage();
+            final IWorkbenchPart activePart = page.getActivePart();
+            if (activePart instanceof ISetSelectionTarget) {
+                final ISelection selection = new StructuredSelection(resource);
+                Display.getDefault().asyncExec(new Runnable() {
+
+                    public void run() {
+                        ((ISetSelectionTarget) activePart).selectReveal(selection);
+                    }
+                });
+            }
+        }
+
+    } // end of DuplicateSystemIndicationAction
+
+}
