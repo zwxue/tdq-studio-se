15af69d1c3820e51665118eddc19afcf20a69f2b
bzhou <bzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-09-08 08:07:05 +0000

    Feature 8988 finished : Add data filter in functional dependency analysis.
    git-svn-id: http://talendforge.org/svn/top/trunk@29419 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/analysis/ColumnDependencyMasterDetailsPage.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/analysis/ColumnDependencyMasterDetailsPage.java
@@ -33,7 +33,6 @@ import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
 import org.eclipse.ui.forms.widgets.Section;
 import org.talend.cwm.helper.ColumnHelper;
-import org.talend.cwm.helper.ColumnSetHelper;
 import org.talend.cwm.helper.DataProviderHelper;
 import org.talend.cwm.relational.TdColumn;
 import org.talend.cwm.softwaredeployment.TdDataProvider;
@@ -41,144 +40,190 @@ import org.talend.dataprofiler.core.PluginConstant;
 import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
 import org.talend.dataprofiler.core.ui.editor.composite.AnalysisColumnCompareTreeViewer;
 import org.talend.dataprofiler.core.ui.editor.composite.DataFilterComp;
-import org.talend.dataquality.analysis.Analysis;
 import org.talend.dataquality.exception.DataprofilerCoreException;
 import org.talend.dataquality.helpers.AnalysisHelper;
-import org.talend.dataquality.helpers.IndicatorHelper;
 import org.talend.dataquality.indicators.Indicator;
 import org.talend.dataquality.indicators.columnset.ColumnDependencyIndicator;
 import org.talend.dataquality.indicators.columnset.ColumnsetFactory;
 import org.talend.dataquality.indicators.columnset.ColumnsetPackage;
-import org.talend.dataquality.indicators.columnset.RowMatchingIndicator;
 import org.talend.dq.analysis.AnalysisBuilder;
 import org.talend.dq.analysis.AnalysisHandler;
 import org.talend.dq.analysis.ColumnDependencyAnalysisHandler;
 import org.talend.dq.helper.resourcehelper.AnaResourceFileHelper;
 import org.talend.dq.indicators.definitions.DefinitionHandler;
-import org.talend.utils.sql.Java2SqlType;
 import org.talend.utils.sugars.ReturnCode;
-
 import orgomg.cwm.objectmodel.core.ModelElement;
 import orgomg.cwm.resource.relational.Column;
 import orgomg.cwm.resource.relational.ColumnSet;
 
 /**
- * This is a Master Detail Page for <a href="http://www.talendforge.org/bugs/view.php?id=8134">Functional Dependency Analysis</a>.
+ * This is a Master Detail Page for <a href="http://www.talendforge.org/bugs/view.php?id=8134">Functional Dependency
+ * Analysis</a>.
+ * 
  * @author jet
- *
+ * 
  */
-public class ColumnDependencyMasterDetailsPage extends
-    AbstractAnalysisMetadataPage implements PropertyChangeListener{
+public class ColumnDependencyMasterDetailsPage extends AbstractAnalysisMetadataPage implements PropertyChangeListener {
 
     private static Logger log = Logger.getLogger(ColumnDependencyMasterDetailsPage.class);
-    
+
     private ScrolledForm form;
-    
+
     private Section columnsComparisonSection = null;
-    
+
     List<Column> columnListA = null;
+
     List<Column> columnListB = null;
-    
+
+    DataFilterComp dataFilterCompA;
+
+    DataFilterComp dataFilterCompB;
+
+    private Section dataFilterSection = null;
+
+    private String stringDataFilterA;
+
+    private String stringDataFilterB;
+
     private AnalysisColumnCompareTreeViewer anaColumnCompareViewer;
-    
-	@Override
+
+    @Override
     protected void createFormContent(IManagedForm managedForm) {
-	    super.createFormContent(managedForm);
+        super.createFormContent(managedForm);
         form = managedForm.getForm();
-        form
-                .setText(DefaultMessagesImpl
-                        .getString("ColumnDependencyMasterDetailsPage.ColumnDependencyAnalysis")); //$NON-NLS-1$
-        
-        this.metadataSection
-                .setText(DefaultMessagesImpl
-                        .getString("ColumnDependencyMasterDetailsPage.analysisMeta")); //$NON-NLS-1$
-        
-        this.metadataSection
-                .setDescription(DefaultMessagesImpl
-                        .getString("ColumnsComparisonMasterDetailsPage.setAnalysisProperties")); //$NON-NLS-1$
-        
-        
-        anaColumnCompareViewer = new AnalysisColumnCompareTreeViewer((AbstractAnalysisMetadataPage)this,  topComp, getColumnLeftSet(),getColumnRightSet(),DefaultMessagesImpl
-                .getString("FunctionalDependencyMasterDetailsPage.Title"),DefaultMessagesImpl
-                .getString("FunctionalDependencyMasterDetailsPage.Description"),false);
-        
-        
+        form.setText(DefaultMessagesImpl.getString("ColumnDependencyMasterDetailsPage.ColumnDependencyAnalysis")); //$NON-NLS-1$
+
+        this.metadataSection.setText(DefaultMessagesImpl.getString("ColumnDependencyMasterDetailsPage.analysisMeta")); //$NON-NLS-1$
+
+        this.metadataSection.setDescription(DefaultMessagesImpl
+                .getString("ColumnsComparisonMasterDetailsPage.setAnalysisProperties")); //$NON-NLS-1$
+
+        anaColumnCompareViewer = new AnalysisColumnCompareTreeViewer((AbstractAnalysisMetadataPage) this, topComp,
+                getColumnLeftSet(), getColumnRightSet(), DefaultMessagesImpl
+                        .getString("FunctionalDependencyMasterDetailsPage.Title"), DefaultMessagesImpl
+                        .getString("FunctionalDependencyMasterDetailsPage.Description"), false);
+
         anaColumnCompareViewer.addPropertyChangeListener(this);
-        
+
+        createDataFilterSection(form, topComp);
+
         columnListA = anaColumnCompareViewer.getColumnListA();
         columnListB = anaColumnCompareViewer.getColumnListB();
-        
-        columnsComparisonSection = anaColumnCompareViewer
-                .getColumnsComparisonSection();
+
+        columnsComparisonSection = anaColumnCompareViewer.getColumnsComparisonSection();
         anaColumnCompareViewer.addPropertyChangeListener(this);
-        
-        currentEditor
-        .registerSections(new Section[] { columnsComparisonSection });
+
+        currentEditor.registerSections(new Section[] { columnsComparisonSection, dataFilterSection });
     }
-	
-	public ScrolledForm getScrolledForm() {
+
+    void createDataFilterSection(final ScrolledForm form, Composite anasisDataComp) {
+        dataFilterSection = createSection(
+                form,
+                anasisDataComp,
+                DefaultMessagesImpl.getString("ColumnsComparisonMasterDetailsPage.dataFilter"), false, DefaultMessagesImpl.getString("ColumnsComparisonMasterDetailsPage.editDataFilter")); //$NON-NLS-1$ //$NON-NLS-2$
+        Composite sectionClient = toolkit.createComposite(dataFilterSection);
+        sectionClient.setLayoutData(new GridData(GridData.FILL_BOTH));
+        sectionClient.setLayout(new GridLayout());
+
+        SashForm sashForm = new SashForm(sectionClient, SWT.NULL);
+        sashForm.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+        Composite leftComp = toolkit.createComposite(sashForm);
+        leftComp.setLayoutData(new GridData(GridData.FILL_BOTH));
+        leftComp.setLayout(new GridLayout());
+        dataFilterCompA = new DataFilterComp(leftComp, stringDataFilterA);
+        dataFilterCompA.addPropertyChangeListener(this);
+        dataFilterCompA.addModifyListener(new ModifyListener() {
+
+            public void modifyText(ModifyEvent e) {
+                AnalysisHelper.setStringDataFilter(analysis, dataFilterCompA.getDataFilterString(), 0);
+            }
+        });
+
+        Composite rightComp = toolkit.createComposite(sashForm);
+        rightComp.setLayoutData(new GridData(GridData.FILL_BOTH));
+        rightComp.setLayout(new GridLayout());
+        dataFilterCompB = new DataFilterComp(rightComp, stringDataFilterB);
+        dataFilterCompB.addPropertyChangeListener(this);
+        dataFilterCompB.addModifyListener(new ModifyListener() {
+
+            public void modifyText(ModifyEvent e) {
+                AnalysisHelper.setStringDataFilter(analysis, dataFilterCompB.getDataFilterString(), 1);
+            }
+        });
+
+        dataFilterSection.setClient(sectionClient);
+    }
+
+    public ScrolledForm getScrolledForm() {
         return form;
     }
 
     /**
-	 * @param editor
-	 * @param id
-	 * @param title
-	 */
-	public ColumnDependencyMasterDetailsPage(FormEditor editor, String id,
-			String title) {
-		super(editor, id, title);
-	}
+     * @param editor
+     * @param id
+     * @param title
+     */
+    public ColumnDependencyMasterDetailsPage(FormEditor editor, String id, String title) {
+        super(editor, id, title);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.talend.dataprofiler.core.ui.editor.analysis.AbstractAnalysisMetadataPage#canRun()
-	 */
-	@Override
-	protected ReturnCode canRun() {
-	    
-	    return new ReturnCode(columnListA.size() > 0);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.ui.editor.analysis.AbstractAnalysisMetadataPage#canRun()
+     */
+    @Override
+    protected ReturnCode canRun() {
 
-	/* (non-Javadoc)
-	 * @see org.talend.dataprofiler.core.ui.editor.analysis.AbstractAnalysisMetadataPage#canSave()
-	 */
-	@Override
-	protected ReturnCode canSave() {
-	    //do some validator.
-	    return validator(columnListA,columnListB);
-	}
+        return new ReturnCode(columnListA.size() > 0);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.talend.dataprofiler.core.ui.editor.analysis.AbstractAnalysisMetadataPage#refresh()
-	 */
-	@Override
-	public void refresh() {
-	   
-	    
-	}
-	
-	public AnalysisHandler getAnalysisHandler() {
-	    ColumnDependencyAnalysisHandler analysisHandler = new ColumnDependencyAnalysisHandler();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.ui.editor.analysis.AbstractAnalysisMetadataPage#canSave()
+     */
+    @Override
+    protected ReturnCode canSave() {
+        // do some validator.
+        return validator(columnListA, columnListB);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.ui.editor.analysis.AbstractAnalysisMetadataPage#refresh()
+     */
+    @Override
+    public void refresh() {
+
+    }
+
+    public AnalysisHandler getAnalysisHandler() {
+        ColumnDependencyAnalysisHandler analysisHandler = new ColumnDependencyAnalysisHandler();
         analysisHandler.setAnalysis(this.analysis);
         return analysisHandler;
     }
 
-	/* (non-Javadoc)
-	 * @see org.talend.dataprofiler.core.ui.editor.analysis.AbstractAnalysisMetadataPage#saveAnalysis()
-	 */
-	@Override
-	protected void saveAnalysis() throws DataprofilerCoreException {
-	    getAnalysisHandler().clearAnalysis();
-	    
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.ui.editor.analysis.AbstractAnalysisMetadataPage#saveAnalysis()
+     */
+    @Override
+    protected void saveAnalysis() throws DataprofilerCoreException {
+        getAnalysisHandler().clearAnalysis();
+
         List<ModelElement> analysedElements = new ArrayList<ModelElement>();
         List<Column> columnListA = anaColumnCompareViewer.getColumnListA();
         List<Column> columnListB = anaColumnCompareViewer.getColumnListB();
-        
+
         AnalysisBuilder anaBuilder = new AnalysisBuilder();
         anaBuilder.setAnalysis(this.analysis);
-        
+
         for (int i = 0; i < columnListA.size(); i++) {
-            if(columnListB.size() > i){
+            if (columnListB.size() > i) {
                 ColumnDependencyIndicator indicator = ColumnsetFactory.eINSTANCE.createColumnDependencyIndicator();
                 indicator.setColumnA(columnListA.get(i));
                 indicator.setColumnB(columnListB.get(i));
@@ -187,75 +232,65 @@ public class ColumnDependencyMasterDetailsPage extends
                 anaBuilder.addElementToAnalyze(columnListA.get(i), indicator);
             }
         }
-        
-       
+
         if (columnListA.size() > 0) {
-            TdDataProvider tdDataProvider = DataProviderHelper
-                    .getTdDataProvider(columnListA.get(0));
+            TdDataProvider tdDataProvider = DataProviderHelper.getTdDataProvider(columnListA.get(0));
             analysis.getContext().setConnection(tdDataProvider);
         } else {
             analysis.getContext().setConnection(null);
             analysis.getClientDependency().clear();
         }
-       
-      
-        
-        
+
         ReturnCode save = AnaResourceFileHelper.getInstance().save(analysis);
         if (save.isOk()) {
-            log
-                    .info("Success to save connection analysis:" + analysis.getFileName()); //$NON-NLS-1$
+            log.info("Success to save connection analysis:" + analysis.getFileName()); //$NON-NLS-1$
         }
 
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
-	 */
-	public void propertyChange(PropertyChangeEvent evt) {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
+     */
+    public void propertyChange(PropertyChangeEvent evt) {
 
-	    if (PluginConstant.ISDIRTY_PROPERTY.equals(evt.getPropertyName())) {
+        if (PluginConstant.ISDIRTY_PROPERTY.equals(evt.getPropertyName())) {
             currentEditor.firePropertyChange(IEditorPart.PROP_DIRTY);
             currentEditor.setRefreshResultPage(true);
-        } else if (PluginConstant.DATAFILTER_PROPERTY.equals(evt
-                .getPropertyName())) {
+        } else if (PluginConstant.DATAFILTER_PROPERTY.equals(evt.getPropertyName())) {
             this.setDirty(true);
         }
-	}
-	
-	private List<Column> getColumnLeftSet(){
-	    return getColumnSet(ColumnsetPackage.Literals.COLUMN_DEPENDENCY_INDICATOR__COLUMN_A);
-	}
-	
-	private List<Column> getColumnRightSet(){
-	    return getColumnSet(ColumnsetPackage.Literals.COLUMN_DEPENDENCY_INDICATOR__COLUMN_B);
-	}
-	
-	private List<Column> getColumnSet(EReference reference){
-	    List<Column> columns = new ArrayList<Column>();
+    }
+
+    private List<Column> getColumnLeftSet() {
+        return getColumnSet(ColumnsetPackage.Literals.COLUMN_DEPENDENCY_INDICATOR__COLUMN_A);
+    }
+
+    private List<Column> getColumnRightSet() {
+        return getColumnSet(ColumnsetPackage.Literals.COLUMN_DEPENDENCY_INDICATOR__COLUMN_B);
+    }
+
+    private List<Column> getColumnSet(EReference reference) {
+        List<Column> columns = new ArrayList<Column>();
         EList<Indicator> indicators = analysis.getResults().getIndicators();
         for (Indicator indicator : indicators) {
             columns.add((Column) indicator.eGet(reference));
         }
         return columns;
-	}
-	
-	private ReturnCode validator(List<Column> columnASet , List<Column> columnBSet){
-	    
-	    if(columnASet.size() == 0 || columnBSet.size() == 0){
-	        return new  ReturnCode(
-                    DefaultMessagesImpl
-                    .getString("ColumnDependencyMasterDetailsPage.columnsBlankMessag"), false);
-	    }
-	    if(columnASet.size() != columnBSet.size()){
-	        //two set must have equal number.
-	        return new ReturnCode(
-                    DefaultMessagesImpl
-                    .getString("ColumnsComparisonMasterDetailsPage.columnsSameMessage"), false);
-	    }
-	    
-	    
-	    for (int i = 0; i < columnASet.size(); i++) {
+    }
+
+    private ReturnCode validator(List<Column> columnASet, List<Column> columnBSet) {
+
+        if (columnASet.size() == 0 || columnBSet.size() == 0) {
+            return new ReturnCode(DefaultMessagesImpl.getString("ColumnDependencyMasterDetailsPage.columnsBlankMessag"), false);
+        }
+        if (columnASet.size() != columnBSet.size()) {
+            // two set must have equal number.
+            return new ReturnCode(DefaultMessagesImpl.getString("ColumnsComparisonMasterDetailsPage.columnsSameMessage"), false);
+        }
+
+        for (int i = 0; i < columnASet.size(); i++) {
             Column columnA = columnASet.get(i);
             Column columnB = columnBSet.get(i);
 
@@ -264,19 +299,15 @@ public class ColumnDependencyMasterDetailsPage extends
 
             int typeA = ((TdColumn) columnA).getJavaType();
             int typeB = ((TdColumn) columnB).getJavaType();
-            
 
             if (ownerA != ownerB) {
-                //must come from one table
+                // must come from one table
                 return new ReturnCode(
-                        DefaultMessagesImpl
-                                .getString("ColumnsComparisonMasterDetailsPage.schemaSameMessage"), false); //$NON-NLS-1$
+                        DefaultMessagesImpl.getString("ColumnsComparisonMasterDetailsPage.schemaSameMessage"), false); //$NON-NLS-1$
             }
         }
 
-	    return new ReturnCode(true);
-	}
-	
-	
+        return new ReturnCode(true);
+    }
 
 }
