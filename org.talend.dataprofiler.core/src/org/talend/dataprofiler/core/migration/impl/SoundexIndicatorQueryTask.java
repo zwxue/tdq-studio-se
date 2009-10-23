19626c20d61c9805fec64ed5003a06ea247bf23b
bzhou <bzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-10-23 09:09:46 +0000

    bug 9732 fixed:Soundex indicator query template is not correct
    git-svn-id: http://talendforge.org/svn/top/trunk@31585 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/SoundexIndicatorQueryTask.java
@@ -0,0 +1,95 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2009 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.migration.impl;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.talend.commons.utils.StringUtils;
+import org.talend.dataprofiler.core.ResourceManager;
+import org.talend.dataprofiler.core.migration.AbstractMigrationTask;
+import org.talend.dq.indicators.definitions.DefinitionHandler;
+
+/**
+ * DOC bZhou class global comment. Detailled comment
+ */
+public class SoundexIndicatorQueryTask extends AbstractMigrationTask {
+
+    private static Logger log = Logger.getLogger(SoundexIndicatorQueryTask.class);
+
+    private final String oldSoundexQuery = "ORDER BY d,c DESC";
+
+    private final String newSoundexQuery = "ORDER BY d DESC,c DESC";
+
+    private final String talendDefinitionFileName = ".Talend.definition";
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.IWorkspaceMigrationTask#execute()
+     */
+    public boolean execute() {
+        IFile definitionFile = ResourceManager.getLibrariesFolder().getFile(talendDefinitionFileName);
+        if (definitionFile.exists()) {
+            try {
+                definitionFile.delete(true, null);
+            } catch (CoreException e) {
+                log.error(e.getMessage(), e);
+                return false;
+            }
+        }
+
+        DefinitionHandler.getInstance();
+
+        if (definitionFile != null) {
+            File file = new File(definitionFile.getLocationURI());
+            try {
+                String content = FileUtils.readFileToString(file);
+                content = StringUtils.replace(content, oldSoundexQuery, newSoundexQuery);
+                FileUtils.writeStringToFile(file, content);
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.IWorkspaceMigrationTask#getMigrationTaskType()
+     */
+    public MigrationTaskType getMigrationTaskType() {
+        return MigrationTaskType.FILE;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.IWorkspaceMigrationTask#getOrder()
+     */
+    public Date getOrder() {
+        Calendar calender = Calendar.getInstance();
+        calender.set(2009, 10, 23);
+        return calender.getTime();
+    }
+
+}
