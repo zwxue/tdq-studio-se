fb6dbdf637e9a6987a9457158a9626e69989e425
bzhou <bzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-11-04 05:19:03 +0000

    bug 9743 fixed: switch perspective when create job in TDQ.
    git-svn-id: http://talendforge.org/svn/top/trunk@32126 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/utils/WorkbenchUtils.java
@@ -0,0 +1,44 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2009 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.utils;
+
+import org.apache.log4j.Logger;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
+
+/**
+ * DOC bZhou class global comment. Detailled comment
+ */
+public final class WorkbenchUtils {
+
+    private static Logger log = Logger.getLogger(WorkbenchUtils.class);
+
+    private WorkbenchUtils() {
+
+    }
+
+    /**
+     * DOC bZhou Comment method "changePerspective".
+     * 
+     * @param perspectiveID
+     */
+    public static void changePerspective(String perspectiveID) {
+        IWorkbenchWindow activeWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        try {
+            PlatformUI.getWorkbench().showPerspective(perspectiveID, activeWindow);
+        } catch (WorkbenchException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+}
