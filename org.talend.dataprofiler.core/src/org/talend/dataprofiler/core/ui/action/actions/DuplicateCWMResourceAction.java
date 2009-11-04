04242a9a15040866c593b9b1f8f179c15ce76104
bzhou <bzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-11-04 06:20:52 +0000

    bug 9776 fixed: Duplicate an analysis does not change the author
    git-svn-id: http://talendforge.org/svn/top/trunk@32135 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/DuplicateCWMResourceAction.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/DuplicateCWMResourceAction.java
@@ -14,22 +14,19 @@ package org.talend.dataprofiler.core.ui.action.actions;
 
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.jface.action.Action;
-import org.talend.commons.emf.FactoriesUtil;
+import org.talend.commons.bridge.ReponsitoryContextBridge;
 import org.talend.cwm.dependencies.DependenciesHandler;
 import org.talend.dataprofiler.core.ImageLib;
 import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
 import org.talend.dataquality.analysis.Analysis;
+import org.talend.dataquality.helpers.MetadataHelper;
 import org.talend.dataquality.helpers.ReportHelper;
 import org.talend.dataquality.reports.TdReport;
-import org.talend.dq.helper.resourcehelper.AnaResourceFileHelper;
-import org.talend.dq.helper.resourcehelper.DQRuleResourceFileHelper;
-import org.talend.dq.helper.resourcehelper.PatternResourceFileHelper;
-import org.talend.dq.helper.resourcehelper.RepResourceFileHelper;
-import org.talend.dq.helper.resourcehelper.UDIResourceFileHelper;
-import org.talend.dq.writer.AElementPersistance;
+import org.talend.dq.factory.ModelElementFileFactory;
 import org.talend.dq.writer.EMFSharedResources;
 import org.talend.dq.writer.impl.ElementWriterFactory;
 import orgomg.cwm.objectmodel.core.ModelElement;
@@ -52,29 +49,45 @@ public class DuplicateCWMResourceAction extends Action {
         if (files != null && files.length > 0) {
             for (IFile file : files) {
 
-                ModelElement oldObject = getOldEObject(file);
+                ModelElement oldObject = ModelElementFileFactory.getModelElement(file);
 
                 if (oldObject != null) {
                     ModelElement newObject = (ModelElement) EMFSharedResources.getInstance().copyEObject(oldObject);
 
+                    newObject = update(oldObject, newObject);
+
                     IFile newFile = getNewFile(file);
-                    newObject.setName("copy of " + newObject.getName()); //$NON-NLS-1$
-                    // MOD 2009-01-06 mzhao copy analysis reference.
-                    if (oldObject instanceof TdReport) {
-                        List<Analysis> anaLs = ReportHelper.getAnalyses((TdReport) oldObject);
-                        for (Analysis analysis : anaLs) {
-                            DependenciesHandler.getInstance().setDependencyOn((TdReport) newObject, analysis);
-                            ((TdReport) newObject).addAnalysis(analysis);
-                        }
-                    }
-
-                    AElementPersistance elementPersistance = ElementWriterFactory.getInstance()
-                            .create(newFile.getFileExtension());
-
-                    elementPersistance.save(newObject, newFile);
+                    ElementWriterFactory.getInstance().create(newFile.getFileExtension()).save(newObject, newFile);
                 }
             }
         }
+    }
+
+    /**
+     * DOC bZhou Comment method "update".
+     * 
+     * @param oldObject
+     * @param newObject
+     * @return
+     */
+    private ModelElement update(ModelElement oldObject, ModelElement newObject) {
+        newObject.setName("copy of " + newObject.getName()); //$NON-NLS-1$
+
+        String author = ReponsitoryContextBridge.getAuthor();
+        if (!StringUtils.isEmpty(author)) {
+            MetadataHelper.setAuthor(newObject, author);
+        }
+
+        // MOD 2009-01-06 mzhao copy analysis reference.
+        if (newObject instanceof TdReport) {
+            List<Analysis> anaLs = ReportHelper.getAnalyses((TdReport) oldObject);
+            for (Analysis analysis : anaLs) {
+                DependenciesHandler.getInstance().setDependencyOn((TdReport) newObject, analysis);
+                ((TdReport) newObject).addAnalysis(analysis);
+            }
+        }
+
+        return newObject;
     }
 
     /**
@@ -95,31 +108,5 @@ public class DuplicateCWMResourceAction extends Action {
             idx++;
         }
         return newFile;
-    }
-
-    private ModelElement getOldEObject(IFile file) {
-        ModelElement object = null;
-        if (FactoriesUtil.ANA.equals(file.getFileExtension())) {
-            object = AnaResourceFileHelper.getInstance().findAnalysis(file);
-        }
-
-        if (FactoriesUtil.PATTERN.equals(file.getFileExtension())) {
-            object = PatternResourceFileHelper.getInstance().findPattern(file);
-        }
-        // MOD 2009-01-03 mzhao Support duplicate report files.
-        if (FactoriesUtil.REP.equals(file.getFileExtension())) {
-            object = RepResourceFileHelper.getInstance().findReport(file);
-        }
-        // MOD 2009-04-28 mzhao Support duplicate dq rules files.
-        if (FactoriesUtil.DQRULE.equals(file.getFileExtension())) {
-            object = DQRuleResourceFileHelper.getInstance().findWhereRule(file);
-        }
-
-        // MOD 2009-09-02 yyi 8881: Add "Duplicate" menu on the user defined indicators
-        if (FactoriesUtil.UDI.equals(file.getFileExtension())) {
-            object = UDIResourceFileHelper.getInstance().findUDI(file);
-        }
-
-        return object;
     }
 }
