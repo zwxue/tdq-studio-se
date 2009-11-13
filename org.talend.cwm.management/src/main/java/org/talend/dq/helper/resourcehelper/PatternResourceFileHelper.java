92ac18ba0af04b5605f2fe3b31d3483d7bd639a9
bzhou <bzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-11-13 03:11:31 +0000

    bug 9787 fixed: NPE in some ETL components
    git-svn-id: http://talendforge.org/svn/top/trunk@32602 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/helper/resourcehelper/PatternResourceFileHelper.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/helper/resourcehelper/PatternResourceFileHelper.java
@@ -118,7 +118,7 @@ public final class PatternResourceFileHelper extends ResourceFileMap {
                 }
 
                 IFile file = (IFile) resource;
-                if (file.getFileExtension().equals(FactoriesUtil.PATTERN)) {
+                if (FactoriesUtil.isPatternFile(file)) {
                     Resource fileResource = getFileResource(file);
                     Pattern pattern = retirePattern(fileResource);
                     if (pattern != null) {
