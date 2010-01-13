37ba20aa3538e6d93a6c7e8803e130b0b6f53315
bzhou <bzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-01-13 10:29:35 +0000

    bug 10238 fixed: Add migration to mdm support.
    git-svn-id: http://talendforge.org/svn/top/trunk@35576 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/UpdateDefintionFileTask.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/UpdateDefintionFileTask.java
@@ -14,8 +14,10 @@ package org.talend.dataprofiler.core.migration.impl;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.talend.dataprofiler.core.migration.AProjectTask;
 import org.talend.dq.indicators.definitions.DefinitionHandler;
+import org.talend.resource.ResourceManager;
 
 /**
  * DOC bZhou class global comment. Detailled comment
@@ -30,10 +32,15 @@ public class UpdateDefintionFileTask extends AProjectTask {
      * @see org.talend.dataprofiler.core.migration.IMigrationTask#execute()
      */
     public boolean execute() {
+        // TODO compare new file and old file to extract the udis and then add them into new file.
+
         IFile talendDefinitionFile = DefinitionHandler.getTalendDefinitionFile();
         if (talendDefinitionFile.exists()) {
             try {
-                // talendDefinitionFile.delete(true, null);
+                talendDefinitionFile.delete(true, null);
+
+                IFolder librariesFolder = ResourceManager.getLibrariesFolder();
+                DefinitionHandler.getInstance().copyDefinitionsIntoFolder(librariesFolder);
             } catch (Exception e) {
                 log.error(e, e);
                 return false;
