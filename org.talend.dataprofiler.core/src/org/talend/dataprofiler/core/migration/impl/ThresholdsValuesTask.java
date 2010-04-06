cea06ed39933ff9e437415aff51625ee219daf3b
bzhou <bzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-04-06 09:52:44 +0000

    bug 12315 fixed: add a migration task to change the thresholds values.
    git-svn-id: http://talendforge.org/svn/top/trunk@39867 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/ThresholdsValuesTask.java
@@ -0,0 +1,95 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.migration.impl;
+
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.EList;
+import org.talend.dataprofiler.core.migration.AWorkspaceTask;
+import org.talend.dataquality.analysis.Analysis;
+import org.talend.dataquality.helpers.IndicatorHelper;
+import org.talend.dataquality.indicators.Indicator;
+import org.talend.dq.helper.resourcehelper.AnaResourceFileHelper;
+import org.talend.dq.writer.impl.ElementWriterFactory;
+
+/**
+ * DOC bZhou class global comment. Detailled comment
+ */
+public class ThresholdsValuesTask extends AWorkspaceTask {
+
+    private static Logger log = Logger.getLogger(ThresholdsValuesTask.class);
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.IWorkspaceMigrationTask#getMigrationTaskType()
+     */
+    public MigrationTaskType getMigrationTaskType() {
+        return MigrationTaskType.FILE;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.IMigrationTask#execute()
+     */
+    public boolean execute() {
+        try {
+            Collection<Analysis> allAnalysis = AnaResourceFileHelper.getInstance().getAllAnalysis();
+            for (Analysis analysis : allAnalysis) {
+                EList<Indicator> indicators = analysis.getResults().getIndicators();
+                for (Indicator indicator : indicators) {
+                    String[] pThresholds = IndicatorHelper.getIndicatorThresholdInPercent(indicator);
+                    if (pThresholds != null) {
+                        String sOldMin = pThresholds[0];
+                        String sOldMax = pThresholds[1];
+
+                        String sNewMin = null, sNewMax = null;
+                        if (StringUtils.isNotBlank(sOldMin)) {
+                            sNewMin = String.valueOf(Double.valueOf(sOldMin) / 100);
+                        }
+
+                        if (StringUtils.isNotBlank(sOldMax)) {
+                            sNewMax = String.valueOf(Double.valueOf(sOldMax) / 100);
+                        }
+
+                        IndicatorHelper.setIndicatorThresholdInPercent(indicator.getParameters(), sNewMin, sNewMax);
+                    }
+                }
+
+                ElementWriterFactory.getInstance().createAnalysisWrite().save(analysis);
+            }
+
+            return true;
+        } catch (Exception e) {
+            log.error(e);
+            return false;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.IMigrationTask#getOrder()
+     */
+    public Date getOrder() {
+        Calendar calender = Calendar.getInstance();
+        calender.set(2010, 4, 6);
+        return calender.getTime();
+    }
+
+}
