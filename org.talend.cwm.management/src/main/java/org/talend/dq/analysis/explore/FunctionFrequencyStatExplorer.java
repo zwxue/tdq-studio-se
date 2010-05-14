5b77cad80a05981bd35bfda1cd96ce25b5af7650
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-05-14 10:31:14 +0000

    bug 12675 : modify dill down.
    git-svn-id: http://talendforge.org/svn/top/trunk@42286 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/analysis/explore/FunctionFrequencyStatExplorer.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/analysis/explore/FunctionFrequencyStatExplorer.java
@@ -15,9 +15,14 @@ package org.talend.dq.analysis.explore;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.eclipse.emf.common.util.EList;
 import org.talend.cwm.relational.TdColumn;
+import org.talend.dataquality.indicators.IndicatorsPackage;
+import org.talend.dataquality.indicators.definition.CharactersMapping;
+import org.talend.dataquality.indicators.definition.IndicatorDefinition;
 import org.talend.dq.dbms.DB2DbmsLanguage;
 import org.talend.dq.dbms.DbmsLanguageFactory;
+import org.talend.dq.indicators.definitions.DefinitionHandler;
 import org.talend.utils.sql.Java2SqlType;
 import orgomg.cwm.objectmodel.core.Expression;
 
@@ -53,11 +58,15 @@ public class FunctionFrequencyStatExplorer extends FrequencyStatisticsExplorer {
 
     protected String getInstantiatedClause() {
         // get function which convert data into a pattern
+        String colName = columnName;
         String function = null;
         TdColumn column = (TdColumn) indicator.getAnalyzedElement();
         int javaType = column.getJavaType();
         if (!Java2SqlType.isNumbericInSQL(javaType)) {
             function = getFunction();
+        } else {
+            colName = columnNameConvert();
+
         }
         // MOD mzhao bug 9681 2009-11-09
 
@@ -68,7 +77,7 @@ public class FunctionFrequencyStatExplorer extends FrequencyStatisticsExplorer {
             value = "'" + entity.getKey() + "'";
         }
 
-        String clause = entity.isLabelNull() ? columnName + dbmsLanguage.isNull() : ((function == null ? columnName : function)
+        String clause = entity.isLabelNull() ? columnName + dbmsLanguage.isNull() : ((function == null ? colName : function)
                 + dbmsLanguage.equal() + value); //$NON-NLS-1$ //$NON-NLS-2$
         return clause;
     }
@@ -97,5 +106,40 @@ public class FunctionFrequencyStatExplorer extends FrequencyStatisticsExplorer {
         // MOD MOD mzhao 2010-04-12 bug 11554
         String group = matcher.group(1);
         return group;
+    }
+
+    private String addFunctionTypeConvert(String colName) {
+        if (colName == null) {
+            return colName;
+        }
+        colName = " CAST(" + colName + " AS CHAR(20)) ";
+        return colName;
+    }
+
+    private String columnNameConvert() {
+        String colName = columnName;
+        TdColumn column = (TdColumn) indicator.getAnalyzedElement();
+        IndicatorDefinition indicatorDefinition;
+        String label = indicator.getIndicatorDefinition().getLabel();
+        if (label == null || "".equals(label)) { //$NON-NLS-1$
+            indicatorDefinition = indicator.getIndicatorDefinition();
+        } else {
+            indicatorDefinition = DefinitionHandler.getInstance().getIndicatorDefinition(label);
+        }
+
+        if (indicator.eClass().equals(IndicatorsPackage.eINSTANCE.getPatternFreqIndicator())
+                || indicator.eClass().equals(IndicatorsPackage.eINSTANCE.getPatternLowFreqIndicator())) {
+            // MOD zshen for bug 12675 2010-05-12
+            if (Java2SqlType.isNumbericInSQL(column.getJavaType())) {
+                colName = addFunctionTypeConvert(colName);
+            }
+            // ~12675
+            final EList<CharactersMapping> charactersMapping = indicatorDefinition.getCharactersMapping();
+            colName = dbmsLanguage.getPatternFinderFunction(colName, charactersMapping);
+            if (columnName == null) { // no replacement found, try the default one
+                colName = dbmsLanguage.getPatternFinderDefaultFunction(colName);
+            }
+        }
+        return colName;
     }
 }
