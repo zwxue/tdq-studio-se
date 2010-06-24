85a3d65e5568fb025129ed04d9c3d2e5fd428bab
qiongli <qiongli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-06-24 08:23:04 +0000

    bug 13654:run as java engine ,soundex low indicator work well as sql engine
    git-svn-id: http://talendforge.org/svn/top/trunk@44470 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataquality/src/org/talend/dataquality/indicators/impl/SoundexLowFreqIndicatorImpl.java
+++ org.talend.dataquality/src/org/talend/dataquality/indicators/impl/SoundexLowFreqIndicatorImpl.java
@@ -5,9 +5,14 @@
  */
 package org.talend.dataquality.indicators.impl;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EClass;
+import org.talend.dataquality.PluginConstant;
 import org.talend.dataquality.indicators.IndicatorsPackage;
 import org.talend.dataquality.indicators.SoundexLowFreqIndicator;
+import org.talend.utils.collections.MapValueSorter;
 
 /**
  * <!-- begin-user-doc --> An implementation of the model object '<em><b>Soundex Low Freq Indicator</b></em>'. <!--
@@ -41,6 +46,23 @@ public class SoundexLowFreqIndicatorImpl extends SoundexFreqIndicatorImpl implem
     @Override
     public boolean handle(Object data) {
         return super.handle(data);
+    }
+    @Override
+    /*
+     * MOD qiongli bug 13654,the low soudex should order by ValueToDistinct asc
+     */
+    public boolean finalizeComputation() {
+        final int topN = (parameters != null) ? parameters.getTopN() : PluginConstant.DEFAULT_TOP_N;
+        MapValueSorter mvs=new MapValueSorter();
+        List<Object> ls=mvs.sortMap(this.getValueToDistinctFreq(), true);
+        List<Object> mostFrequent = getOrderElements(ls, topN,true);
+        HashMap<Object, Long> map = new HashMap<Object, Long>();
+        for (Object object : mostFrequent) {
+            map.put(object, valueToFreq.get(object));
+        }
+        this.setValueToFreq(map);
+        // this.distinctComputed = true;
+        return true;
     }
 
 } // SoundexLowFreqIndicatorImpl
