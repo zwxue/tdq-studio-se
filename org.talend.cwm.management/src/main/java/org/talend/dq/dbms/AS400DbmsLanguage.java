e5bd3ec85578877c3fdebedba73e6f3b812a263d
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-08-03 03:07:03 +0000

    bug 14464: Use TRANSLATE instead of REPLACE.
    git-svn-id: http://talendforge.org/svn/top/trunk@46216 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/dbms/AS400DbmsLanguage.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/dbms/AS400DbmsLanguage.java
@@ -12,7 +12,6 @@
 // ============================================================================
 package org.talend.dq.dbms;
 
-import org.apache.commons.lang.StringUtils;
 import org.talend.dataquality.indicators.DateGrain;
 import org.talend.utils.ProductVersion;
 
@@ -30,22 +29,6 @@ public class AS400DbmsLanguage extends DbmsLanguage {
         super(dbmsType, dbVersion);
     }
 
-    // /*
-    // * (non-Javadoc)
-    // *
-    // * @see org.talend.cwm.management.api.DbmsLanguage#getPatternFinderDefaultFunction(java.lang.String)
-    // */
-    // @Override
-    // public String getPatternFinderDefaultFunction(String expression) {
-    //        return "REPLACE(CHAR(" + expression + ") ,VARCHAR(REPEAT('9',10) || REPEAT('A',25)||REPEAT('a',25)), " //$NON-NLS-1$ //$NON-NLS-2$
-    //                + " '1234567890BCDEFGHIJKLMNOPQRSTUVWXYZbcdefghijklmnopqrstuvwxyz')"; // cannot put accents //$NON-NLS-1$
-    // }
-    //
-    // @Override
-    // protected String getPatternFinderFunction(String expression, String charsToReplace, String replacementChars) {
-    //        return "REPLACE(CHAR(" + expression + ") ,VARCHAR('" + replacementChars + "'),'" //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-    //                + charsToReplace + "')"; //$NON-NLS-1$
-    // }
     /*
      * (non-Javadoc)
      * 
@@ -53,27 +36,16 @@ public class AS400DbmsLanguage extends DbmsLanguage {
      */
     @Override
     public String getPatternFinderDefaultFunction(String expression) {
-        return StringUtils.repeat("REPLACE(", 59) + expression //$NON-NLS-1$
-                + ",'B','A'),'C','A'),'D','A'),'E','A'),'F','A'),'G','A'),'H','A')" //$NON-NLS-1$
-                + ",'I','A'),'J','A'),'K','A'),'L','A'),'M','A'),'N','A'),'O','A')" //$NON-NLS-1$
-                + ",'P','A'),'Q','A'),'R','A'),'S','A'),'T','A'),'U','A'),'V','A')" //$NON-NLS-1$
-                + ",'W','A'),'X','A'),'Y','A'),'Z','A'),'b','a'),'c','a'),'d','a')" //$NON-NLS-1$
-                + ",'e','a'),'f','a'),'g','a'),'h','a'),'i','a'),'j','a'),'k','a')" //$NON-NLS-1$
-                + ",'l','a'),'m','a'),'n','a'),'o','a'),'p','a'),'q','a'),'r','a')" //$NON-NLS-1$
-                + ",'s','a'),'t','a'),'u','a'),'v','a'),'w','a'),'x','a'),'y','a')" //$NON-NLS-1$
-                + ",'z','a'),'1','9'),'2','9'),'3','9'),'4','9'),'5','9'),'6','9')" + ",'7','9'),'8','9'),'0','9')"; //$NON-NLS-1$ //$NON-NLS-2$
+        return "TRANSLATE(CHAR(" + expression + ") ,VARCHAR(REPEAT('9',10) || REPEAT('A',25)||REPEAT('a',25)), " //$NON-NLS-1$ //$NON-NLS-2$
+                + " '1234567890BCDEFGHIJKLMNOPQRSTUVWXYZbcdefghijklmnopqrstuvwxyz')"; // cannot put accents //$NON-NLS-1$
     }
 
     @Override
     protected String getPatternFinderFunction(String expression, String charsToReplace, String replacementChars) {
-        assert charsToReplace != null && replacementChars != null && charsToReplace.length() == replacementChars.length();
-        for (int i = 0; i < charsToReplace.length(); i++) {
-            final char charToReplace = charsToReplace.charAt(i);
-            final char replacement = replacementChars.charAt(i);
-            expression = replaceOneChar(expression, charToReplace, replacement);
-        }
-        return expression;
+        return "TRANSLATE(CHAR(" + expression + ") ,'" + replacementChars + "','" //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                + charsToReplace + "')"; //$NON-NLS-1$
     }
+
     /*
      * (non-Javadoc)
      * 
