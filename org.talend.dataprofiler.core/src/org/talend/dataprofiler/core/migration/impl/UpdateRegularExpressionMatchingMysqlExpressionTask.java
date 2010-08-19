324e9926cc97fb9d913f35596dd8496bb2cf2c4e
xqliu <xqliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-08-19 07:26:39 +0000

    bug 14540: [Uppercased Single Word] Pattern matching bad string on mysql
    git-svn-id: http://talendforge.org/svn/top/trunk@46997 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/UpdateRegularExpressionMatchingMysqlExpressionTask.java
@@ -0,0 +1,83 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.migration.impl;
+
+import java.util.Date;
+
+import org.eclipse.emf.common.util.EList;
+import org.talend.cwm.dburl.SupportDBUrlType;
+import org.talend.dataprofiler.core.migration.AWorkspaceTask;
+import org.talend.dataquality.expressions.TdExpression;
+import org.talend.dataquality.helpers.BooleanExpressionHelper;
+import org.talend.dataquality.indicators.definition.IndicatorDefinition;
+import org.talend.dq.indicators.definitions.DefinitionHandler;
+
+
+/**
+ * DOC xqliu  class global comment. Detailled comment
+ */
+public class UpdateRegularExpressionMatchingMysqlExpressionTask extends AWorkspaceTask {
+
+    private static final String REGULAR_EXPRESSION_MATCHING_LABEL = "Regular Expression Matching";
+
+    private static final String MYSQL_EXPRESSION = "SELECT COUNT(CASE WHEN <%=__COLUMN_NAMES__%> REGEXP BINARY <%=__PATTERN_EXPR__%> THEN 1 END), COUNT(*) FROM <%=__TABLE_NAME__%> <%=__WHERE_CLAUSE__%>";
+
+    /* (non-Javadoc)
+     * @see org.talend.dataprofiler.core.migration.IWorkspaceMigrationTask#getMigrationTaskType()
+     */
+    public MigrationTaskType getMigrationTaskType() {
+        return MigrationTaskType.FILE;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.IMigrationTask#getOrder()
+     */
+    public Date getOrder() {
+        return createDate(2010, 8, 18);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.AMigrationTask#doExecute()
+     */
+    @Override
+    protected boolean doExecute() throws Exception {
+        IndicatorDefinition indicatorDefinition = DefinitionHandler.getInstance().getIndicatorDefinition(
+                REGULAR_EXPRESSION_MATCHING_LABEL);
+        if (indicatorDefinition != null) {
+            EList<TdExpression> sqlGenericExpression = indicatorDefinition.getSqlGenericExpression();
+            if (sqlGenericExpression != null && sqlGenericExpression.size() > 0) {
+                boolean notFoundMysqlExpression = true;
+                for (TdExpression expression : sqlGenericExpression) {
+                    if (expression != null && SupportDBUrlType.MYSQLDEFAULTURL.getLanguage().equals(expression.getLanguage())) {
+                        notFoundMysqlExpression = false;
+                        expression.setBody(MYSQL_EXPRESSION);
+                        break;
+                    }
+                }
+                if (notFoundMysqlExpression) {
+                    TdExpression newExpression = BooleanExpressionHelper.createTdExpression(SupportDBUrlType.MYSQLDEFAULTURL
+                            .getLanguage(), MYSQL_EXPRESSION);
+                    sqlGenericExpression.add(newExpression);
+                } else {
+                    // mysql expression have exist, so needn't to create a new mysql expression
+                }
+            }
+        }
+        return true;
+    }
+
+}
