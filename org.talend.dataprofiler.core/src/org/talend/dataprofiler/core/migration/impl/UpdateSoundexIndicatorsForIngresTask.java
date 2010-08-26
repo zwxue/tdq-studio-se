8f177db391d1d463e8cef049d4e656f747d9ce06
qiongli <qiongli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-08-26 05:27:36 +0000

    bug 14579:make soundex on Ingres db can run
    git-svn-id: http://talendforge.org/svn/top/trunk@47311 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/UpdateSoundexIndicatorsForIngresTask.java
@@ -0,0 +1,74 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.migration.impl;
+
+import java.util.Date;
+
+import org.talend.cwm.dburl.SupportDBUrlType;
+import org.talend.dataprofiler.core.migration.AWorkspaceTask;
+import org.talend.dataprofiler.core.migration.helper.IndicatorDefinitionFileHelper;
+import org.talend.dataquality.indicators.definition.IndicatorDefinition;
+
+/**
+ * DOC qiongli class global comment. Detailled comment
+ */
+public class UpdateSoundexIndicatorsForIngresTask extends AWorkspaceTask {
+
+    private static final String SOUNDEX_FREQUENCY = "Soundex Frequency Table"; //$NON-NLS-1$
+
+    private static final String SOUNDEX_LOW_FREQUENCY = "Soundex Low Frequency Table"; //$NON-NLS-1$
+
+    private static final String INGRES = SupportDBUrlType.INGRESDEFAULTURL.getLanguage();
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.AMigrationTask#doExecute()
+     */
+    @Override
+    protected boolean doExecute() throws Exception {
+        IndicatorDefinition definitionHigh = IndicatorDefinitionFileHelper.getSystemIndicatorByName(SOUNDEX_FREQUENCY);
+        IndicatorDefinitionFileHelper
+                .addSqlExpression(
+                        definitionHigh,
+                        INGRES,
+                        "SELECT MAX(<%=__COLUMN_NAMES__%>), SOUNDEX(<%=__COLUMN_NAMES__%>) , COUNT(*) c, COUNT(DISTINCT <%=__COLUMN_NAMES__%>) d FROM <%=__TABLE_NAME__%> t <%=__WHERE_CLAUSE__%> GROUP BY 2 ORDER BY d DESC,c DESC");
+        IndicatorDefinition definitionLow = IndicatorDefinitionFileHelper.getSystemIndicatorByName(SOUNDEX_LOW_FREQUENCY);
+        IndicatorDefinitionFileHelper
+                .addSqlExpression(
+                        definitionLow,
+                        INGRES,
+                        "SELECT MAX(<%=__COLUMN_NAMES__%>), SOUNDEX(<%=__COLUMN_NAMES__%>) , COUNT(*) c, COUNT(DISTINCT <%=__COLUMN_NAMES__%>) d FROM <%=__TABLE_NAME__%> t <%=__WHERE_CLAUSE__%> GROUP BY 2 ORDER BY d,c ASC");
+
+        return IndicatorDefinitionFileHelper.save(definitionHigh) & IndicatorDefinitionFileHelper.save(definitionLow);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.IMigrationTask#getMigrationTaskType()
+     */
+    public MigrationTaskType getMigrationTaskType() {
+        return MigrationTaskType.FILE;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.migration.IMigrationTask#getOrder()
+     */
+    public Date getOrder() {
+        return createDate(2010, 8, 25);
+    }
+
+}
