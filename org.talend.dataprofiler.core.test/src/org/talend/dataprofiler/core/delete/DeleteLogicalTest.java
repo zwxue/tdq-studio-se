2f3d88e713d43b86d9e5161fc596ae4021fa8f8a
qiongli <qiongli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-08-27 10:28:15 +0000

    bug 14525:JUnit tests for feature 9486: Add a trash to resume delete operations on dq elements
    git-svn-id: http://talendforge.org/svn/top/trunk@47416 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/delete/DeleteLogicalTest.java
@@ -0,0 +1,103 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.delete;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.junit.Test;
+import org.talend.commons.emf.FactoriesUtil;
+import org.talend.dataprofiler.core.PluginConstant;
+import org.talend.dataprofiler.core.recycle.LogicalDeleteFileHandle;
+import org.talend.dataprofiler.core.ui.action.actions.handle.ActionHandleFactory;
+import org.talend.dataprofiler.core.ui.action.actions.handle.IDeletionHandle;
+import org.talend.resource.ResourceManager;
+import orgomg.cwm.objectmodel.core.ModelElement;
+
+/**
+ * DOC qiongli class global comment. Detailled comment
+ */
+public class DeleteLogicalTest {
+
+    @Test
+    public void testLogilcalDelElements() throws Exception {
+        // find the delete elements parent(folder).I only delete elements under Ananlysis folder now.
+        IFolder folder = ResourceManager.getAnalysisFolder();
+        IResource res[] = folder.members();
+        IDeletionHandle handle = null;
+        for (IResource re : res) {
+            if (re instanceof IFile) {
+                IFile file = (IFile) re;
+                if (file.exists() && FactoriesUtil.isEmfFile(file.getFileExtension())) {
+                    handle = ActionHandleFactory.createDeletionHandle(file);
+                    boolean runStatus = handle.delete(false);
+                    if (runStatus) {
+                        System.out.println("Logical delete " + file.getName() + " successful!");
+                    }
+                }
+            } else if (re instanceof IFolder) {
+                IFolder subfolder = (IFolder) re;
+                List<IFile> filsLs = new ArrayList<IFile>();
+                getAllSubFiles(subfolder, filsLs);
+                boolean hasDelFailing = false;
+                for (IFile file : filsLs) {
+                    if (file.exists() && FactoriesUtil.isEmfFile(file.getFileExtension())) {
+                        handle = ActionHandleFactory.createDeletionHandle(file);
+                        List<ModelElement> dependencies = handle.getDependencies();
+                        if (dependencies != null && !dependencies.isEmpty()) {
+                            hasDelFailing = true;
+                            break;
+                        }
+                        if (handle.delete(false)) {
+                            System.out.println("Logical delete " + file.getName() + " successful!");
+                        } else {
+                            hasDelFailing = true;
+                        }
+                    }
+                }
+                if (!hasDelFailing) {
+                    LogicalDeleteFileHandle.replaceInFile(LogicalDeleteFileHandle.folderType
+                            + subfolder.getFullPath().toOSString(), PluginConstant.EMPTY_STRING);
+                    LogicalDeleteFileHandle.saveElement(LogicalDeleteFileHandle.folderType, subfolder.getFullPath().toOSString());
+                    System.out.println("Logical delete " + subfolder.getName() + " successful!");
+                }
+
+            }
+        }
+    }
+
+    private void getAllSubFiles(IFolder folder, List<IFile> fileList) {
+        IResource[] members = null;
+        try {
+            members = folder.members();
+        } catch (CoreException e) {
+            e.printStackTrace();
+        }
+        for (IResource res : members) {
+            if (res.getType() == IResource.FILE) {
+                IFile file = (IFile) res;
+                if (!StringUtils.equals(file.getFileExtension(), FactoriesUtil.PROPERTIES_EXTENSION)) {
+                    fileList.add(file);
+                }
+            } else if (res.getType() == IResource.FOLDER) {
+                getAllSubFiles((IFolder) res, fileList);
+            }
+        }
+
+    }
+}
