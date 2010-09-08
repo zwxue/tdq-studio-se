362cf3b981c8a8fc9baf7a8b3262e12c2936b152
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-09-08 10:10:41 +0000

    bug 15083: Not use common branding service regristration for RC1.
    git-svn-id: http://talendforge.org/svn/top/trunk@47973 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/license/LicenseWizard.java
@@ -0,0 +1,58 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.license;
+
+import org.eclipse.jface.wizard.Wizard;
+import org.talend.dataprofiler.core.ImageLib;
+import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
+
+/**
+ * Wizard for the creation of a new project. <br/>
+ * 
+ */
+public class LicenseWizard extends Wizard {
+
+    /** Main page. */
+    private LicenseWizardPage mainPage;
+
+    /**
+     * Constructs a new LicenseWizard.
+     * 
+     * @param author Project author.
+     * @param server
+     * @param password
+     * @param port2
+     */
+    public LicenseWizard() {
+        super();
+    }
+
+    /**
+     * @see org.eclipse.jface.wizard.Wizard#addPages()
+     */
+    @Override
+    public void addPages() {
+        mainPage = new LicenseWizardPage();
+        addPage(mainPage);
+        setWindowTitle(DefaultMessagesImpl.getString("LicenseWizard.license")); //$NON-NLS-1$
+        setDefaultPageImageDescriptor(ImageLib.getImageDescriptor(ImageLib.LICENSE_WIZ));
+    }
+
+    /**
+     * @see org.eclipse.jface.wizard.Wizard#performFinish()
+     */
+    @Override
+    public boolean performFinish() {
+        return true;
+    }
+}
