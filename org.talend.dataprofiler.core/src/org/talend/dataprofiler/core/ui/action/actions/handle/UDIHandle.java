c5656a45a8887dd070a69fe040502155dd87ec82
klliu <klliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-09-08 08:37:20 +0000

    bug 13645: the sql template is not correct about the duplicate system indicator
    git-svn-id: http://talendforge.org/svn/top/trunk@47959 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/handle/UDIHandle.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/handle/UDIHandle.java
@@ -12,8 +12,13 @@
 // ============================================================================
 package org.talend.dataprofiler.core.ui.action.actions.handle;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.talend.core.model.properties.Property;
+import org.talend.dataprofiler.core.PluginConstant;
+import org.talend.dataquality.indicators.definition.IndicatorDefinition;
+import org.talend.dq.factory.ModelElementFileFactory;
+import org.talend.dq.helper.resourcehelper.IndicatorResourceFileHelper;
 import org.talend.resource.ResourceManager;
 import org.talend.utils.sugars.ReturnCode;
 import orgomg.cwm.objectmodel.core.ModelElement;
@@ -42,8 +47,35 @@ public class UDIHandle extends EMFResourceHandle {
         return ResourceManager.getUDIFolder();
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataprofiler.core.ui.action.actions.handle.EMFResourceHandle#duplicate()
+     */
+    @Override
+    public IFile duplicate() {
+        IFile duplicatedFile = super.duplicate();
+        IndicatorDefinition definition = (IndicatorDefinition) ModelElementFileFactory.getModelElement(duplicatedFile);
+
+        definition.getCategories().get(0).setLabel("User Defined Count");
+        IndicatorResourceFileHelper.getInstance().save(definition);
+        return duplicatedFile;
+    }
+
     @Override
     public ReturnCode validDuplicated() {
-        return super.validDuplicated();
+        ReturnCode returnCode = new ReturnCode(true);
+        String label = getProperty().getLabel();
+        if (label.equals(PluginConstant.UNIQUE_COUNT) || label.equals(PluginConstant.BLANK_COUNT)
+                || label.equals(PluginConstant.DEFAULT_VALUE_COUNT) || label.equals(PluginConstant.ROW_COUNT)
+                || label.equals(PluginConstant.DUPLICATE_COUNT) || label.equals(PluginConstant.NULL_COUNT)
+                || label.equals("Pattern Frequency Table") || label.equals("Date Pattern Frequency Table")
+                || label.equals("Pattern Low Frequency Table")) {
+            return returnCode;
+        }
+        returnCode.setMessage("This '" + label + "' of Indicator's type could not be duplicated!");//$NON-NLS-1$
+
+        returnCode.setOk(false);
+        return returnCode;
     }
 }
