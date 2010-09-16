9c8b78af84bb0635d03b67778f4ca3e76628c379
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-09-16 10:32:58 +0000

    feature 14891: implement the resourceChange method and make connection can be modify.
    git-svn-id: http://talendforge.org/svn/top/trunk@48410 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/service/TDQResourceChangeHandler.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/service/TDQResourceChangeHandler.java
@@ -12,13 +12,22 @@
 // ============================================================================
 package org.talend.dataprofiler.core.service;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.talend.commons.exception.PersistenceException;
 import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
 import org.talend.core.model.metadata.builder.connection.MDMConnection;
+import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
+import org.talend.dataprofiler.core.ui.dialog.message.DeleteModelElementConfirmDialog;
+import org.talend.dq.helper.EObjectHelper;
+import org.talend.dq.helper.PropertyHelper;
 import org.talend.dq.helper.ProxyRepositoryViewObject;
+import org.talend.repository.model.ProxyRepositoryFactory;
 import org.talend.repository.utils.AbstractResourceChangesService;
+import org.talend.repository.utils.XmiResourceManager;
 import orgomg.cwm.objectmodel.core.ModelElement;
 
 /**
@@ -29,7 +38,9 @@ public class TDQResourceChangeHandler extends AbstractResourceChangesService {
 
     private static Logger log = Logger.getLogger(TDQResourceChangeHandler.class);
 
-    // private XmiResourceManager xmiResourceManager = new XmiResourceManager();
+    private XmiResourceManager xmiResourceManager = ProxyRepositoryFactory.getInstance().getRepositoryFactoryFromProvider()
+            .getResourceManager();
+
     public TDQResourceChangeHandler() {
     }
 
@@ -37,15 +48,29 @@ public class TDQResourceChangeHandler extends AbstractResourceChangesService {
     public void handleUnload(Resource toBeUnloadedResource) {
         for (EObject eObject : toBeUnloadedResource.getContents()) {
             // try {
-                if (eObject instanceof DatabaseConnection) {
-                    ProxyRepositoryViewObject.registerURI((DatabaseConnection) eObject,
-                            toBeUnloadedResource.getURI());
-                    // XmiResourceManager.getInstance().saveResource(toBeUnloadedResource);
-                } else if (eObject instanceof MDMConnection) {
-                    ProxyRepositoryViewObject.registerURI((MDMConnection) eObject,
-                            toBeUnloadedResource.getURI());
-                    // xmiResourceManager .saveResource(toBeUnloadedResource);
+            if (eObject instanceof DatabaseConnection) {
+                ProxyRepositoryViewObject.registerURI((DatabaseConnection) eObject, toBeUnloadedResource.getURI());
+                if (xmiResourceManager != null) {
+                    try {
+                        xmiResourceManager.saveResource(toBeUnloadedResource);
+                    } catch (PersistenceException e) {
+                        log.error(e, e);
+                    }
+
                 }
+            } else if (eObject instanceof MDMConnection) {
+
+                ProxyRepositoryViewObject.registerURI((MDMConnection) eObject, toBeUnloadedResource.getURI());
+                if (xmiResourceManager != null) {
+                    try {
+                        xmiResourceManager.saveResource(toBeUnloadedResource);
+                    } catch (PersistenceException e) {
+                        log.error(e, e);
+                    }
+
+                }
+            }
+
             // } catch (PersistenceException e) {
             // log.error(e, e);
             // }
@@ -56,7 +81,15 @@ public class TDQResourceChangeHandler extends AbstractResourceChangesService {
 
     @Override
     public boolean handleResourceChange(ModelElement modelElement) {
-        // EList<Dependency> clientDependencys = modelElement.getSupplierDependency();
+        List<ModelElement> clientDependencys = EObjectHelper.getDependencyClients(modelElement);
+        if (clientDependencys.size() > 0) {
+            ModelElement[] dependencyElements = clientDependencys.toArray(new ModelElement[clientDependencys.size()]);
+
+            DeleteModelElementConfirmDialog.showDialog(null,
+                    PropertyHelper.getItemFile(PropertyHelper.getProperty(modelElement)), dependencyElements, DefaultMessagesImpl
+                            .getString("TDQResourceChangeHandler.ConnectionNotBeSave"));
+            return false;
+        }
         // List<ModelElement> supplierList = new ArrayList<ModelElement>();
         // for (Dependency dependency : clientDependencys) {
         // EList<ModelElement> client = dependency.getClient();
