098bcd90833ef62b472cfd07f97f67f271d6d9b7
klliu <klliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-09-19 11:18:03 +0000

    bug 15520: Login dialog update
    git-svn-id: http://talendforge.org/svn/top/trunk@48565 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.rcp/src/org/talend/dataprofiler/rcp/intro/ApplicationWorkbenchAdvisor.java
+++ org.talend.dataprofiler.rcp/src/org/talend/dataprofiler/rcp/intro/ApplicationWorkbenchAdvisor.java
@@ -12,22 +12,15 @@
 // ============================================================================
 package org.talend.dataprofiler.rcp.intro;
 
-import java.lang.management.ManagementFactory;
-
 import org.apache.log4j.Logger;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPreferenceConstants;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.eclipse.ui.internal.ide.application.IDEWorkbenchAdvisor;
-import org.talend.dataprofiler.core.CorePlugin;
 import org.talend.repository.registeruser.RegisterManagement;
-import org.talend.repository.ui.wizards.register.RegisterWizard;
-import org.talend.repository.ui.wizards.register.RegisterWizardDialog;
 
 /**
  * DOC rli class global comment. Detailled comment <br/>
@@ -66,39 +59,7 @@ public class ApplicationWorkbenchAdvisor extends IDEWorkbenchAdvisor {
         super.postStartup();
         // Start Web Service Registration
         try {
-            if (!RegisterManagement.getInstance().isProductRegistered()) {
-                RegisterWizard registerWizard = new RegisterWizard();
-                Shell shell = getWorkbenchConfigurer().getWorkbench().getActiveWorkbenchWindow().getShell();
-                WizardDialog dialog = new RegisterWizardDialog(shell, registerWizard);
-                dialog.setTitle(""); //$NON-NLS-1$
-                if (dialog.open() == WizardDialog.OK) {
-                    String projectLanguage = "java"; //$NON-NLS-1$
-                    // OS
-                    String osName = System.getProperty("os.name"); //$NON-NLS-1$
-                    String osVersion = System.getProperty("os.version"); //$NON-NLS-1$
-
-                    // Java version
-                    String javaVersion = System.getProperty("java.version"); //$NON-NLS-1$
-
-                    // Java Memory
-                    long totalMemory = Runtime.getRuntime().totalMemory();
-
-                    // RAM
-                    com.sun.management.OperatingSystemMXBean composantSystem = (com.sun.management.OperatingSystemMXBean) ManagementFactory
-                            .getOperatingSystemMXBean();
-                    Long memRAM = new Long(composantSystem.getTotalPhysicalMemorySize() / 1024);
-
-                    // CPU
-                    int nbProc = Runtime.getRuntime().availableProcessors();
-
-                    RegisterManagement.getInstance().register(registerWizard.getEmail(), registerWizard.getCountry(), registerWizard
-                            .isProxyEnabled(), registerWizard.getProxyHost(), registerWizard.getProxyPort(), CorePlugin
-                            .getDefault().getBundle().getHeaders().get(org.osgi.framework.Constants.BUNDLE_VERSION).toString(),
-                            projectLanguage, osName, osVersion, javaVersion, totalMemory, memRAM, nbProc);
-                } else {
-                    RegisterManagement.decrementTry();
-                }
-            }
+            RegisterManagement.decrementTry();
         } catch (Exception e) {
             log.error("Problem during the registration wizard:" + e.getLocalizedMessage(), e); //$NON-NLS-1$
         }
