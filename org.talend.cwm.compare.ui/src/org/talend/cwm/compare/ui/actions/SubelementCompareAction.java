0544fcd0bad2b596ad2afbf44d318345be169570
klliu <klliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-09-30 09:06:40 +0000

    bug 15822: The bug is caused by eproxy,so need to resolve object
    git-svn-id: http://talendforge.org/svn/top/trunk@49307 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.compare.ui/src/org/talend/cwm/compare/ui/actions/SubelementCompareAction.java
+++ org.talend.cwm.compare.ui/src/org/talend/cwm/compare/ui/actions/SubelementCompareAction.java
@@ -27,6 +27,7 @@ import org.talend.cwm.helper.CatalogHelper;
 import org.talend.cwm.helper.ConnectionHelper;
 import org.talend.cwm.helper.PackageHelper;
 import org.talend.dataprofiler.core.helper.FolderNodeHelper;
+import org.talend.dq.helper.EObjectHelper;
 import org.talend.dq.nodes.foldernode.IFolderNode;
 import org.talend.dq.writer.EMFSharedResources;
 import orgomg.cwm.objectmodel.core.ModelElement;
@@ -41,147 +42,134 @@ import orgomg.cwm.resource.relational.Schema;
  */
 public class SubelementCompareAction extends Action {
 
-	public static final int COLUMN_COMPARE = 0;
+    public static final int COLUMN_COMPARE = 0;
 
-	public static final int TABLE_COMPARE = 1;
+    public static final int TABLE_COMPARE = 1;
 
-	public static final int VIEW_COMPARE = 2;
+    public static final int VIEW_COMPARE = 2;
 
-	private String text = null;
+    private String text = null;
 
-	private ModelContentMergeDiffTab diffTabLeft = null;
+    private ModelContentMergeDiffTab diffTabLeft = null;
 
-	private int actionType;
+    private int actionType;
 
-	private Object selectedOjbect = null;
+    private Object selectedOjbect = null;
 
-	private PopComparisonUIAction popCompUIAction = null;
+    private PopComparisonUIAction popCompUIAction = null;
 
-	public SubelementCompareAction(String showText,
-			ModelContentMergeDiffTab diffTabLeft, Object selectedOjbect,
-			int actionType) {
-		text = showText;
-		this.diffTabLeft = diffTabLeft;
-		this.selectedOjbect = selectedOjbect;
-		this.actionType = actionType;
-		popCompUIAction = new PopComparisonUIAction(text);
-	}
+    public SubelementCompareAction(String showText, ModelContentMergeDiffTab diffTabLeft, Object selectedOjbect, int actionType) {
+        text = showText;
+        this.diffTabLeft = diffTabLeft;
+        this.selectedOjbect = selectedOjbect;
+        this.actionType = actionType;
+        popCompUIAction = new PopComparisonUIAction(text);
+    }
 
-	@Override
-	public String getText() {
-		return text;
-	}
+    @Override
+    public String getText() {
+        return text;
+    }
 
-	@Override
-	public void run() {
-		IStructuredSelection selection = (IStructuredSelection) diffTabLeft
-				.getSelection();
-		EObject selectedElement = null;
-		if (selection.toList().size() == 1) {
-			selectedElement = (EObject) selection.getFirstElement();
-			IFolderNode folderNode = null;
-			if (selectedElement instanceof ColumnSet) {
-				Package parentPackage = (Package) ((IFolderNode) selectedOjbect)
-						.getParent();
-				// MOD mzhao 2009-08-05 Bug 8581
-				// PackageHelper.addColumnSet((ColumnSet) selectedElement,
-				// parentPackage);
-				// ((ColumnSet) selectedElement).setNamespace(parentPackage);
-				ModelElement matchedElement = findMatchedModelElement(
-						parentPackage, selectedElement);
-				folderNode = FolderNodeHelper.getFolderNodes(matchedElement)[0];
-				openComparisonEditor(folderNode);
+    @Override
+    public void run() {
+        IStructuredSelection selection = (IStructuredSelection) diffTabLeft.getSelection();
+        EObject selectedElement = null;
+        if (selection.toList().size() == 1) {
+            selectedElement = (EObject) selection.getFirstElement();
+            IFolderNode folderNode = null;
+            if (selectedElement instanceof ColumnSet) {
+                Package parentPackage = (Package) ((IFolderNode) selectedOjbect).getParent();
+                // MOD mzhao 2009-08-05 Bug 8581
+                // PackageHelper.addColumnSet((ColumnSet) selectedElement,
+                // parentPackage);
+                // ((ColumnSet) selectedElement).setNamespace(parentPackage);
+                ModelElement matchedElement = findMatchedModelElement(parentPackage, selectedElement);
+                folderNode = FolderNodeHelper.getFolderNodes(matchedElement)[0];
+                openComparisonEditor(folderNode);
             } else if (selectedElement instanceof Catalog) {
-				// Judge and see if there are schemas under the catalog.(Case of
-				// SQL Servers)
+                // Judge and see if there are schemas under the catalog.(Case of
+                // SQL Servers)
                 List<Schema> schemas = CatalogHelper.getSchemas((Catalog) selectedElement);
-				if (schemas != null && schemas.size() > 0) {
+                if (schemas != null && schemas.size() > 0) {
                     IRepositoryViewObject reposViewObj = (IRepositoryViewObject) selectedOjbect;
                     ConnectionItem item = (ConnectionItem) reposViewObj.getProperty().getItem();
                     Connection conn = item.getConnection();
-					popCompUIAction.setSelectedObject(findMatchedModelElement(
-							conn, selectedElement));
-					popCompUIAction.run();
-				} else {
-					folderNode = getTableOrViewFolder(selectedElement);
-					openComparisonEditor(folderNode);
-				}
+                    popCompUIAction.setSelectedObject(findMatchedModelElement(conn, selectedElement));
+                    popCompUIAction.run();
+                } else {
+                    folderNode = getTableOrViewFolder(selectedElement);
+                    openComparisonEditor(folderNode);
+                }
             } else if (selectedElement instanceof Schema) {
-				folderNode = getTableOrViewFolder(selectedElement);
-				openComparisonEditor(folderNode);
-			} else {
-				return;
-			}
+                folderNode = getTableOrViewFolder(selectedElement);
+                openComparisonEditor(folderNode);
+            } else {
+                return;
+            }
 
-			// Remove namespace link
-			if (selectedElement instanceof ColumnSet) {
-				Package parentPackage = (Package) ((IFolderNode) selectedOjbect)
-						.getParent();
-				Package originPackage = parentPackage;
-				PackageHelper.removeColumnSet((ColumnSet) selectedElement,
-						originPackage);
-				EMFSharedResources.getInstance().saveResource(
-						originPackage.eResource());
-			}
-		}
-	}
+            // Remove namespace link
+            if (selectedElement instanceof ColumnSet) {
+                Package parentPackage = (Package) ((IFolderNode) selectedOjbect).getParent();
+                Package originPackage = parentPackage;
+                PackageHelper.removeColumnSet((ColumnSet) selectedElement, originPackage);
+                EMFSharedResources.getInstance().saveResource(originPackage.eResource());
+            }
+        }
+    }
 
-	public void openComparisonEditor(IFolderNode folderNode) {
-		folderNode.loadChildren();
-		// closeCurrentEditor();
-		popCompUIAction.setSelectedObject(folderNode);
-		popCompUIAction.run();
-	}
+    public void openComparisonEditor(IFolderNode folderNode) {
+        folderNode.loadChildren();
+        // closeCurrentEditor();
+        popCompUIAction.setSelectedObject(folderNode);
+        popCompUIAction.run();
+    }
 
-	private IFolderNode getTableOrViewFolder(EObject selectedElement) {
-		IFolderNode folderNode = null;
+    private IFolderNode getTableOrViewFolder(EObject selectedElement) {
+        IFolderNode folderNode = null;
         Connection conn = null;
         if (selectedOjbect instanceof Catalog) {
             conn = ConnectionHelper.getTdDataProvider(((Catalog) selectedOjbect));
-		} else {
+        } else {
             IRepositoryViewObject reposViewObj = (IRepositoryViewObject) selectedOjbect;
             ConnectionItem item = (ConnectionItem) reposViewObj.getProperty().getItem();
             conn = item.getConnection();
-		}
-		// ((Package) selectedElement).getDataManager().add(dataProvider);
-		ModelElement matchedElement = findMatchedModelElement(conn,
-				selectedElement);
-		if (actionType == TABLE_COMPARE) {
-			folderNode = FolderNodeHelper.getFolderNodes(matchedElement)[0];
-		} else {
-			folderNode = FolderNodeHelper.getFolderNodes(matchedElement)[1];
-		}
+        }
+        // ((Package) selectedElement).getDataManager().add(dataProvider);
+        ModelElement matchedElement = findMatchedModelElement(conn, selectedElement);
+        if (actionType == TABLE_COMPARE) {
+            folderNode = FolderNodeHelper.getFolderNodes(matchedElement)[0];
+        } else {
+            folderNode = FolderNodeHelper.getFolderNodes(matchedElement)[1];
+        }
 
-		return folderNode;
-	}
+        return folderNode;
+    }
 
-	private ModelElement findMatchedModelElement(EObject parent,
-			EObject similarElement) {
+    private ModelElement findMatchedModelElement(EObject parent, EObject similarElement) {
         if (parent instanceof Connection) {
             if (similarElement instanceof Schema) {
-				// Case of MS SQL Server.
+                // Case of MS SQL Server.
                 if (selectedOjbect instanceof Catalog) {
                     List<Schema> schemas = CatalogHelper.getSchemas((Catalog) selectedOjbect);
-					if (schemas != null && schemas.size() > 0) {
+                    if (schemas != null && schemas.size() > 0) {
                         for (Schema tdSchema : schemas) {
-							if (tdSchema.getName().equalsIgnoreCase(
-((Schema) similarElement).getName())) {
-								return tdSchema;
-							}
-						}
-					}
-				}
-				// Case of Oracle
+                            if (tdSchema.getName().equalsIgnoreCase(((Schema) similarElement).getName())) {
+                                return tdSchema;
+                            }
+                        }
+                    }
+                }
+                // Case of Oracle
                 List<Schema> schames = ConnectionHelper.getSchema((Connection) parent);
                 for (Schema schame : schames) {
-					if (schame.getName().equalsIgnoreCase(
-((Schema) similarElement).getName())) {
-						return schame;
-					}
-				}
-			}
+                    if (schame.getName().equalsIgnoreCase(((Schema) similarElement).getName())) {
+                        return schame;
+                    }
+                }
+            }
 
-			// Case of Mysql
+            // Case of Mysql
             if (similarElement instanceof Catalog) {
                 List<Catalog> catalogs = ConnectionHelper.getCatalogs((Connection) parent);
                 for (Catalog catalog : catalogs) {
@@ -191,25 +179,28 @@ public class SubelementCompareAction extends Action {
                 }
             }
 
-		} else if (parent instanceof Package) {
-			// MOD mzhao bug 8581 2009-08-05
-			List<ColumnSet> columnSets = new ArrayList<ColumnSet>();
-			columnSets.addAll(PackageHelper.getTables((Package) parent));
-			columnSets.addAll(PackageHelper.getViews((Package) parent));
-			for (ModelElement columnSet : columnSets) {
-				if (((ColumnSet) columnSet).getName().equalsIgnoreCase(
-						((ColumnSet) similarElement).getName())) {
-					return columnSet;
-				}
-			}
-		}
-		return null;
-	}
+        } else if (parent instanceof Package) {
+            // MOD klliu bug 15822 resolve proxy caused two Instance,so we must use one Instance
+            if (parent != null && parent.eIsProxy()) {
+                parent = EObjectHelper.resolveObject(parent);
+            }
+            // MOD mzhao bug 8581 2009-08-05
+            List<ColumnSet> columnSets = new ArrayList<ColumnSet>();
+            columnSets.addAll(PackageHelper.getTables((Package) parent));
+            columnSets.addAll(PackageHelper.getViews((Package) parent));
+            for (ModelElement columnSet : columnSets) {
+                if (((ColumnSet) columnSet).getName().equalsIgnoreCase(((ColumnSet) similarElement).getName())) {
+                    return columnSet;
+                }
+            }
+        }
+        return null;
+    }
 
-	// private void closeCurrentEditor() {
-	// IWorkbenchPage activePage =
-	// PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-	// activePage.closeEditor(activePage.getActiveEditor(), false);
-	// }
+    // private void closeCurrentEditor() {
+    // IWorkbenchPage activePage =
+    // PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+    // activePage.closeEditor(activePage.getActiveEditor(), false);
+    // }
 
 }
