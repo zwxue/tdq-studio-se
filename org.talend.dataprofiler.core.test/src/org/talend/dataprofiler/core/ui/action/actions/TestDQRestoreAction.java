52d165e311115b61eca998a04b8f0b91e0b537c3
qiongli <qiongli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-10-29 09:19:17 +0000

    bug 12747:Refactor Unit Test for automatic testing .
    git-svn-id: http://talendforge.org/svn/top/trunk@50726 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/ui/action/actions/TestDQRestoreAction.java
@@ -0,0 +1,48 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.action.actions;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swtbot.swt.finder.matchers.WidgetOfType;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+import org.junit.Test;
+import org.talend.dataquality.test.TDQSWTBotTest;
+
+/**
+ * DOC qiongli class global comment. Detailled comment
+ */
+public class TestDQRestoreAction extends TDQSWTBotTest {
+
+    /**
+     * Test method for {@link org.talend.dataprofiler.core.ui.action.actions.DQRestoreAction#run()}.
+     */
+    @Test
+    public void testRun() {
+        view = bot.viewByTitle("DQ Repository");
+        view.setFocus();
+        SWTBotTree tree = new SWTBotTree(bot.widget(WidgetOfType.widgetOfType(Tree.class), view.getWidget()));
+        tree.setFocus();
+        SWTBotTreeItem node = tree.expandNode("Recycle Bin");
+        tree.select(node.getItems());
+        DQRestoreAction dqRes = new DQRestoreAction();
+        dqRes.run();
+        assertEquals(node.getItems().length, 0);
+
+    }
+
+}
+
