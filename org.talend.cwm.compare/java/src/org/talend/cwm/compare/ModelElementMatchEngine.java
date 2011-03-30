39b86a9652823cf7907a894fe4c21d7a6c59f4fb
yyi <yyi@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-30 09:59:34 +0000

    bug 19137 fixed:reload database list on editing connection url
    git-svn-id: http://talendforge.org/svn/top/trunk@57645 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.cwm.compare/java/src/org/talend/cwm/compare/ModelElementMatchEngine.java
@@ -0,0 +1,56 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2010 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.cwm.compare;
+
+import org.eclipse.emf.compare.FactoryException;
+import org.eclipse.emf.compare.match.engine.GenericMatchEngine;
+import org.eclipse.emf.ecore.EObject;
+import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
+import org.talend.cwm.helper.SwitchHelpers;
+
+/**
+ * yyi 2011-03-30 19137:reload database list on editing connection url
+ */
+public class ModelElementMatchEngine extends GenericMatchEngine {
+
+    private static boolean isChangedconnectionUrl = false;
+
+    /*
+     * ADD yyi 2011-03-30 19137:reload database list on editing connection url
+     * 
+     * @see org.eclipse.emf.compare.match.engine.GenericMatchEngine#isSimilar(org.eclipse.emf.ecore.EObject,
+     * org.eclipse.emf.ecore.EObject)
+     */
+    @Override
+    public boolean isSimilar(EObject obj1, EObject obj2) throws FactoryException {
+
+        DatabaseConnection connection1 = SwitchHelpers.DATABASECONNECTION_SWITCH.doSwitch(obj1);
+        DatabaseConnection connection2 = SwitchHelpers.DATABASECONNECTION_SWITCH.doSwitch(obj2);
+
+        if (connection1 != null && connection2 != null) {
+            isChangedconnectionUrl = connection1.getURL().equals(connection2.getURL());
+        }
+        return isChangedconnectionUrl ? false : super.isSimilar(obj1, obj2);
+    }
+
+    /*
+     * ADD yyi 2011-03-30 19137:reload database list on editing connection url
+     * 
+     * @see org.eclipse.emf.compare.match.engine.GenericMatchEngine#reset()
+     */
+    @Override
+    public void reset() {
+        isChangedconnectionUrl = false;
+        super.reset();
+    }
+}
