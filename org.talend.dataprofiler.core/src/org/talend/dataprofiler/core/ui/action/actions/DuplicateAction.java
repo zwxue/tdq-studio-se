bd41cea9eb7e6077d97a066c7faabccb5e8f29da
klliu <klliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-04-25 07:48:31 +0000

    bug 20349:Duplicating item of system indicators or others, then the new node will be selected whether the parent node is initlized.
    git-svn-id: http://talendforge.org/svn/top/trunk@59120 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/DuplicateAction.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/DuplicateAction.java
@@ -13,6 +13,8 @@
 package org.talend.dataprofiler.core.ui.action.actions;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
@@ -24,16 +26,27 @@ import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.navigator.CommonViewer;
 import org.eclipse.ui.part.ISetSelectionTarget;
+import org.talend.commons.emf.FactoriesUtil;
+import org.talend.commons.utils.VersionUtils;
+import org.talend.core.model.properties.Item;
+import org.talend.core.model.properties.Property;
+import org.talend.cwm.helper.ResourceHelper;
 import org.talend.dataprofiler.core.CorePlugin;
 import org.talend.dataprofiler.core.ImageLib;
 import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
 import org.talend.dataprofiler.core.ui.action.actions.handle.ActionHandleFactory;
 import org.talend.dataprofiler.core.ui.action.actions.handle.IDuplicateHandle;
+import org.talend.dataquality.properties.TDQJrxmlItem;
+import org.talend.dataquality.properties.TDQSourceFileItem;
 import org.talend.dq.factory.ModelElementFileFactory;
+import org.talend.dq.helper.PropertyHelper;
 import org.talend.dq.helper.RepositoryNodeHelper;
 import org.talend.repository.model.IRepositoryNode;
 import org.talend.repository.model.RepositoryNode;
+import org.talend.resource.EResourceConstant;
+import org.talend.resource.ResourceManager;
 import org.talend.utils.sugars.ReturnCode;
 import orgomg.cwm.objectmodel.core.ModelElement;
 
@@ -145,10 +158,46 @@ public class DuplicateAction extends Action {
         IWorkbenchPage page = workbenchWindow.getActivePage();
         IWorkbenchPart activePart = page.getActivePart();
         ModelElement modelElement = ModelElementFileFactory.getModelElement((IFile) duplicateObject);
-        RepositoryNode recursiveFind = RepositoryNodeHelper.recursiveFind(modelElement);
+        RepositoryNode recursiveFind = null;
+        recursiveFind = getSelctionNode(duplicateObject, modelElement, recursiveFind);
         if (activePart instanceof ISetSelectionTarget) {
             ISelection selection = new StructuredSelection(recursiveFind);
             ((ISetSelectionTarget) activePart).selectReveal(selection);
         }
+
+    }
+
+    private RepositoryNode getSelctionNode(Object duplicateObject, ModelElement modelElement, RepositoryNode recursiveFind) {
+        if (modelElement != null) {
+            recursiveFind = RepositoryNodeHelper.recursiveFind(modelElement);
+            RepositoryNode parent = recursiveFind.getParent();
+            CommonViewer dqCommonViewer = RepositoryNodeHelper.getDQCommonViewer();
+            if (dqCommonViewer != null && !dqCommonViewer.getExpandedState(parent)) {
+                dqCommonViewer.setExpandedState(parent, true);
+            }
+
+        } else {
+            IPath filePath = new Path(((IFile) duplicateObject).getFullPath().removeFileExtension().toString()
+                    .concat("_" + VersionUtils.DEFAULT_VERSION)) //$NON-NLS-1$
+                    .addFileExtension(FactoriesUtil.PROPERTIES_EXTENSION);
+            Property itemProperty = PropertyHelper.getProperty(ResourceManager.getRootProject().getFile(filePath));
+            Item item = itemProperty.getItem();
+            String uuid = ResourceHelper.getUUID(item);
+            IRepositoryNode librariesFolderNode = null;
+            if (item instanceof TDQSourceFileItem) {
+                librariesFolderNode = RepositoryNodeHelper.getLibrariesFolderNode(EResourceConstant.SOURCE_FILES);
+
+            } else if (item instanceof TDQJrxmlItem) {
+                librariesFolderNode = RepositoryNodeHelper.getLibrariesFolderNode(EResourceConstant.JRXML_TEMPLATE);
+            }
+            for (IRepositoryNode node : librariesFolderNode.getChildren()) {
+                Item sourceIitem = node.getObject().getProperty().getItem();
+                if (ResourceHelper.getUUID(sourceIitem).equals(uuid)) {
+                    recursiveFind = (RepositoryNode) node;
+                    break;
+                }
+            }
+        }
+        return recursiveFind;
     }
 }
