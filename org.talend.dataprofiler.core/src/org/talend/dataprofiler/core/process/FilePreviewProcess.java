934c7d224d5ea24c96d03811a5471722f4707fe6
qiongli <qiongli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-15 08:06:45 +0000

    bug 21850:backport on trunk.
    git-svn-id: http://talendforge.org/svn/top/trunk@64418 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/process/FilePreviewProcess.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/process/FilePreviewProcess.java
@@ -12,13 +12,22 @@
 // ============================================================================
 package org.talend.dataprofiler.core.process;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.talend.core.utils.CsvArray;
 import org.talend.core.utils.TalendQuoteUtils;
+import org.talend.cwm.management.i18n.Messages;
+import org.talend.dataprofiler.core.ui.utils.MessageUI;
+import org.talend.dq.helper.ParameterUtil;
 import org.talend.repository.preview.IPreview;
 import org.talend.repository.preview.IProcessDescription;
+
+import com.csvreader.CsvReader;
 
 /**
  * DOC bZhou class global comment. Detailled comment <br/>
@@ -46,36 +55,53 @@ public class FilePreviewProcess implements IPreview {
      */
     public CsvArray preview(IProcessDescription description, String type, boolean errorOutputAsException) throws CoreException {
         CsvArray csvArray = new CsvArray();
-
+        CsvReader csvReader = null;
         try {
 
             if (description.getLoopLimit() == null) {
                 description.setLoopLimit(-1);
             }
 
-            final String filePath = TalendQuoteUtils.removeQuotes(description.getFilepath());
-            final String fileEncode = TalendQuoteUtils.removeQuotes(description.getEncoding());
+            // MOD qiongli 2011-6-20 bug 21850,use CsvReader to parser
+            final String pathStr = TalendQuoteUtils.removeQuotes(description.getFilepath());
             String fileSeparator = TalendQuoteUtils.removeQuotes(description.getFieldSeparator());
-            String rowSeparator = TalendQuoteUtils.removeQuotes(description.getRowSeparator());
+            String encoding = TalendQuoteUtils.removeQuotes(description.getEncoding());
+            int headValue = description.getHeaderRow();
 
             if (fileSeparator.contains("t")) { //$NON-NLS-1$
                 fileSeparator = String.valueOf('\t');
             }
 
-            FileInputDelimited inputDelimited = new FileInputDelimited(filePath, fileEncode, fileSeparator, rowSeparator,
-                    description.getRemoveEmptyRowsToSkip(), description.getHeaderRow(), description.getFooterRow(),
-                    description.getLimitRows(), description.getLoopLimit(), description.isSplitRecord());
+            IPath filePath = new Path(pathStr);
+            File file = filePath.toFile();
 
-            while (inputDelimited.nextRecord()) {
-                String[] csvStrArray = new String[inputDelimited.getColumnsCountOfCurrentRow()];
-                for (int i = 0; i < inputDelimited.getColumnsCountOfCurrentRow(); i++) {
-                    csvStrArray[i] = inputDelimited.get(i);
-                }
-                csvArray.add(csvStrArray);
+            if (!file.exists()) {
+                MessageUI.openWarning(Messages.getString("System can not find the file specified"));
+                return csvArray;
+            }
+            csvReader = new CsvReader(new BufferedReader(new InputStreamReader(new java.io.FileInputStream(file),
+                    encoding == null ? "UTF-8" : encoding)), ParameterUtil //$NON-NLS-1$
+                    .trimParameter(fileSeparator).charAt(0));
+
+            initCsvReader(csvReader, description);
+            for (int i = 0; i < headValue && csvReader.readRecord(); i++) {
+                // do nothing, just ignore the header part
+            }
+            String[] rowValues = null;
+            long currentRecord = csvReader.getCurrentRecord();
+            // just preview the top of 50
+            while (csvReader.readRecord() && currentRecord < 50) {
+                currentRecord = csvReader.getCurrentRecord();
+                rowValues = csvReader.getValues();
+                csvArray.add(rowValues);
             }
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (csvReader != null) {
+                csvReader.close();
+            }
         }
 
         return csvArray;
@@ -93,6 +119,34 @@ public class FilePreviewProcess implements IPreview {
 
     public boolean isTopPreview() {
         return true;
+    }
+
+    /**
+     * 
+     * DOC qiongli Comment method "initCsvReader".
+     * 
+     * @param csvReader
+     * @param description
+     */
+    private void initCsvReader(CsvReader csvReader, IProcessDescription description) {
+        String rowSep = description.getRowSeparator();
+        if (!rowSep.equals("\"\\n\"") && !rowSep.equals("\"\\r\"")) { //$NON-NLS-1$ //$NON-NLS-2$
+            csvReader.setRecordDelimiter(ParameterUtil.trimParameter(rowSep).charAt(0));
+        }
+
+        csvReader.setSkipEmptyRecords(true);
+        String textEnclosure = description.getTextEnclosure();
+        if (!textEnclosure.equals("\"\"") && textEnclosure.length() > 0) {
+            csvReader.setTextQualifier(ParameterUtil.trimParameter(textEnclosure).charAt(0));
+        } else {
+            csvReader.setUseTextQualifier(false);
+        }
+        String escapeChar = description.getEscapeCharacter();
+        if (escapeChar == null || escapeChar.equals("\"\\\\\"") || escapeChar.equals("\"\"")) { //$NON-NLS-1$ //$NON-NLS-2$
+            csvReader.setEscapeMode(CsvReader.ESCAPE_MODE_BACKSLASH);
+        } else {
+            csvReader.setEscapeMode(CsvReader.ESCAPE_MODE_DOUBLED);
+        }
     }
 
 }
