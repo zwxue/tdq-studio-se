96bb17594932a0ca4a9b294fd9c9287b37b472f2
msjian <msjian@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-20 10:15:39 +0000

    TDQ-3495 fixed: modified the reload the database method
    git-svn-id: http://talendforge.org/svn/top/trunk@68338 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/SwitchContextAction.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/SwitchContextAction.java
@@ -12,32 +12,18 @@
 // ============================================================================
 package org.talend.dataprofiler.core.ui.action.actions;
 
-import java.lang.reflect.InvocationTargetException;
-
 import org.apache.log4j.Logger;
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
-import org.talend.commons.utils.platform.PluginChecker;
+import org.talend.core.GlobalServiceRegister;
+import org.talend.core.ITDQRepositoryService;
+import org.talend.core.model.context.ContextUtils;
 import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
 import org.talend.core.model.properties.ConnectionItem;
 import org.talend.core.model.properties.Item;
-import org.talend.cwm.compare.exception.ReloadCompareException;
-import org.talend.cwm.compare.factory.ComparisonLevelFactory;
-import org.talend.cwm.compare.factory.IComparisonLevel;
-import org.talend.dataprofiler.core.CorePlugin;
 import org.talend.dataprofiler.core.ImageLib;
 import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
-import org.talend.dataprofiler.core.ui.progress.ProgressUI;
-import org.talend.dataprofiler.core.ui.utils.MessageUI;
-import org.talend.dataprofiler.core.ui.utils.WorkbenchUtils;
 import org.talend.dq.nodes.DBConnectionRepNode;
 import org.talend.repository.ui.utils.SwitchContextGroupNameImpl;
-import orgomg.cwm.foundation.softwaredeployment.DataProvider;
 /**
  * DOC msjian class global comment. Detailled comment
  */
@@ -73,56 +59,20 @@ public class SwitchContextAction extends Action {
                     if (log.isDebugEnabled()) {
                         log.debug(DefaultMessagesImpl.getString("SwitchContextAction.saveMessage", fileStr, "successful"));//$NON-NLS-1$ //$NON-NLS-2$
                     }
-                    reloadDatabase();
+
+                    ITDQRepositoryService tdqRepService = null;
+                    if (GlobalServiceRegister.getDefault().isServiceRegistered(ITDQRepositoryService.class)) {
+                        tdqRepService = (ITDQRepositoryService) GlobalServiceRegister.getDefault().getService(
+                                ITDQRepositoryService.class);
+                    }
+                    if (tdqRepService != null) {
+                        String contextId = dbConnectionRepNode.getDatabaseConnection().getContextId();
+                        tdqRepService.reloadDatabase(ContextUtils.getContextItemById2(contextId));
+                    }
                 } else {
                     log.error(DefaultMessagesImpl.getString("SwitchContextAction.saveMessage", fileStr, "failed"));//$NON-NLS-1$ //$NON-NLS-2$
                 }
             }
         }
-    }
-
-    /**
-     * DOC msjian Comment method "reloadDatabase".
-     */
-    public void reloadDatabase() {
-        String confirmMessage = PluginChecker.isOnlyTopLoaded() ? DefaultMessagesImpl
-                .getString("ReloadDatabaseAction.ConfirmMessage1") : DefaultMessagesImpl
-                .getString("ReloadDatabaseAction.ConfirmMessage0"); //$NON-NLS-1$
-        boolean openConfirm = MessageDialog.openConfirm(PlatformUI.getWorkbench().getDisplay().getActiveShell(),
-                DefaultMessagesImpl.getString("ReloadDatabaseAction.ConfirmMessageTitle"), //$NON-NLS-1$
-                confirmMessage);
-        if (!openConfirm) {
-            return;
-        }
-
-        IRunnableWithProgress op = new IRunnableWithProgress() {
-            public void run(IProgressMonitor monitor) throws InvocationTargetException {
-                final IComparisonLevel creatComparisonLevel = ComparisonLevelFactory.creatComparisonLevel(selectedObject);
-                Display.getDefault().asyncExec(new Runnable() {
-
-                    public void run() {
-                        try {
-                            DataProvider oldDataProvider = creatComparisonLevel.reloadCurrentLevelElement();
-                            WorkbenchUtils.impactExistingAnalyses(oldDataProvider);
-                        } catch (ReloadCompareException e) {
-                            log.error(e, e);
-                        } catch (PartInitException e) {
-                            log.error(e, e);
-                        }
-                    }
-                });
-            }
-        };
-        try {
-            ProgressUI.popProgressDialog(op);
-            CorePlugin.getDefault().refreshDQView();
-        } catch (InvocationTargetException e) {
-            MessageUI.openError(DefaultMessagesImpl.getString(
-                    "ReloadDatabaseAction.checkConnectionFailured", e.getCause().getMessage())); //$NON-NLS-1$
-            log.error(e, e);
-        } catch (InterruptedException e) {
-            log.error(e, e);
-        }
-
     }
 }
