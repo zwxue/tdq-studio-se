480a8df0aa7ddb4cc20f667959d4bab509149993
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-11-14 11:19:13 +0000

    TDQ-3939     TDQ-3940 change execute status and add some message.
    git-svn-id: http://talendforge.org/svn/top/trunk@72230 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/UpdateJUDITask.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/UpdateJUDITask.java
@@ -31,6 +31,7 @@ import org.talend.core.model.metadata.builder.database.PluginConstant;
 import org.talend.core.model.properties.PropertiesPackage;
 import org.talend.core.model.properties.Property;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
 import org.talend.dataprofiler.core.migration.AbstractWorksapceUpdateTask;
 import org.talend.dataquality.indicators.definition.IndicatorDefinition;
 import org.talend.dataquality.properties.impl.TDQIndicatorDefinitionItemImpl;
@@ -76,7 +77,7 @@ public class UpdateJUDITask extends AbstractWorksapceUpdateTask {
             // find all udi and check jarfile
             File parentFile = udiJarfile.getParentFile();
             if (parentFile.isDirectory()) {
-                handleUDIFile(parentFile.listFiles());
+                return handleUDIFile(parentFile.listFiles());
             }
         }
 
@@ -88,15 +89,19 @@ public class UpdateJUDITask extends AbstractWorksapceUpdateTask {
      * 
      * @param listFiles
      */
-    private void handleUDIFile(File[] listFiles) {
-        if (listFiles == null)
-            return;
+    private boolean handleUDIFile(File[] listFiles) {
+        boolean returnCode = true;
+        File jarFile = null;
+        if (listFiles == null) {
+            returnCode |= false;
+            return returnCode;
+        }
         for (File udiFile : listFiles) {
             if (udiFile == null) {
                 continue;
             }
             if (udiFile.isDirectory()) {
-                handleUDIFile(udiFile.listFiles());
+                returnCode |= handleUDIFile(udiFile.listFiles());
             } else {
                 // there only use old path to find jar. After that we can try to open a dialog to let user provider it.
                 if (!udiFile.getName().endsWith(FactoriesUtil.PROPERTIES_EXTENSION)) {
@@ -110,18 +115,16 @@ public class UpdateJUDITask extends AbstractWorksapceUpdateTask {
                     // ProxyRepositoryFactory.getInstance().getAll(ERepositoryObjectType.TDQ_USERDEFINE_INDICATORS);
                     newProperty = ProxyRepositoryFactory.getInstance().getLastVersion(newProperty.getId()).getProperty();
                 } catch (PersistenceException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
+                    log.error(e, e);
                 }
                 IndicatorDefinition uDIindicatorDefinition = ((TDQIndicatorDefinitionItemImpl) newProperty.getItem())
                         .getIndicatorDefinition();
 
                 EList<TaggedValue> taggedValues = uDIindicatorDefinition.getTaggedValue();
-                String userJavaClassName = null;
                 String jarPath = null;
                 for (TaggedValue tv : taggedValues) {
                     if (tv.getTag().equals(PluginConstant.CLASS_NAME_TEXT)) {
-                        userJavaClassName = tv.getValue();
+                        returnCode ^= false;
                         continue;
                     }
                     if (tv.getTag().equals(PluginConstant.JAR_FILE_PATH)) {
@@ -144,7 +147,7 @@ public class UpdateJUDITask extends AbstractWorksapceUpdateTask {
                         propResource.getContents().add(newProperty.getItem());
                         propResource.getContents().add(newProperty.getItem().getState());
                         EMFUtil.saveResource(propResource);
-
+                        returnCode ^= false;
                         // FolderItem folderItem = ProxyRepositoryFactory.getInstance().getFolderItem(
                         // ProjectManager.getInstance().getCurrentProject(),
                         // ERepositoryObjectType.TDQ_USERDEFINE_INDICATORS, new Path(""));
@@ -173,18 +176,23 @@ public class UpdateJUDITask extends AbstractWorksapceUpdateTask {
                     File jarPathFile = new File(jarPath);
                     if (jarPathFile.exists()) {
                         try {
-                            FilesUtils.copyFile(jarPathFile,
-                                    ResourceManager.getUDIJarFolder().getLocation().append(jarPathFile.getName()).toFile());
+                            jarFile = ResourceManager.getUDIJarFolder().getLocation().append(jarPathFile.getName()).toFile();
+                            FilesUtils.copyFile(jarPathFile, jarFile);
                         } catch (IOException e) {
                             log.error(e, e);
                         }
                     }
+                    if (jarFile == null || !jarFile.exists()) {
+                        log.error(DefaultMessagesImpl.getString("UpdateJUDITask_CanNotFoundJarFile", jarPath));//$NON-NLS-1$
+                    }
                 }
+
 
             }
 
         }
 
+        return returnCode;
     }
 
     private Resource getResource(String filePath) {
