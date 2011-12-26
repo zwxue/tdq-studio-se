0d4a0acb9432d1c91c1e7ccd4fb16a5a8c169a17
msjian <msjian@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-12-26 03:53:36 +0000

    TDQ-4220 (TDQ-4164) fixed : there is something wrong about this compute method
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_0@75222 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataquality/src/org/talend/algorithms/AlgoUtils.java
+++ org.talend.dataquality/src/org/talend/algorithms/AlgoUtils.java
@@ -80,6 +80,12 @@ public final class AlgoUtils {
         Number searchedKey = null;
 
         for (Long curValue : counts) {
+        	
+        	 // MOD msjian 2011-12-26 TDQ-4164: there is something wrong about this compute
+            searchedKey = (Number) keyIterator.next(); // CAST here
+            sumCount += curValue;
+            // TDQ-4164 ~
+            
             if (sumCount >= kthValue) {
                 // compute median
                 if (totalCount % qQuantile != 0) { // odd number of value, take the middle
@@ -94,8 +100,7 @@ public final class AlgoUtils {
                 break; // we got it.
             }
             // else loop
-            searchedKey = (Number) keyIterator.next(); // CAST here
-            sumCount += curValue;
+
         }
         return localMedian;
     }
