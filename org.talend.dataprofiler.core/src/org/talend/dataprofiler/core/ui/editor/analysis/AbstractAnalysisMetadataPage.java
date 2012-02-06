aef3dc3aedf9e6d7151620889648cd670c6f3a7a
yyi <yyi@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-02-06 06:32:22 +0000

    TDQ-4581 fixed:avoid the instantiation of the strings to optimize the performances
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_0@77869 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/analysis/AbstractAnalysisMetadataPage.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/analysis/AbstractAnalysisMetadataPage.java
@@ -510,9 +510,7 @@ public abstract class AbstractAnalysisMetadataPage extends AbstractMetadataFormP
      * @param message
      */
     protected void doLog(Logger logger, Level level, String message) {
-        if (logger.isDebugEnabled()) {
-            logger.log(level, message);
-        }
+        logger.log(level, message);
     }
 
     /**
@@ -524,11 +522,13 @@ public abstract class AbstractAnalysisMetadataPage extends AbstractMetadataFormP
     protected void logSaved(ReturnCode saved) throws DataprofilerCoreException {
         String urlString = analysis.eResource() != null ? (analysis.eResource().getURI().isFile() ? analysis.eResource().getURI()
                 .toFileString() : analysis.eResource().getURI().toString()) : PluginConstant.EMPTY_STRING;
-        if (saved.isOk()) {
-            doLog(log, Level.INFO, DefaultMessagesImpl.getString("ColumnMasterDetailsPage.success", urlString)); //$NON-NLS-1$
-        } else {
+        if (!saved.isOk()) {
             throw new DataprofilerCoreException(DefaultMessagesImpl.getString(
                     "ColumnMasterDetailsPage.problem", analysis.getName(), urlString, saved.getMessage())); //$NON-NLS-1$
+
+        } else if (log.isDebugEnabled()) {
+            // MOD yyi 2012-02-06 TDQ-4581:avoid the instantiation of the strings to optimize the performances.
+            doLog(log, Level.INFO, DefaultMessagesImpl.getString("ColumnMasterDetailsPage.success", urlString)); //$NON-NLS-1$
         }
     }
 }
