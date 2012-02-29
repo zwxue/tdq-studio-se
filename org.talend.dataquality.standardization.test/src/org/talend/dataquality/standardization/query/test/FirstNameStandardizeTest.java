77016f3dd5f017690e2fe11a97b454a8bf2ae455
sizhaoliu <sizhaoliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-02-29 14:43:49 +0000

    TDQ-4329 TDQ-4352 Add assert statements in FirstnameStandardizeTest -improve junits for FirstNameStandardize
    git-svn-id: http://talendforge.org/svn/top/trunk@79080 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataquality.standardization.test/src/org/talend/dataquality/standardization/query/test/FirstNameStandardizeTest.java
+++ org.talend.dataquality.standardization.test/src/org/talend/dataquality/standardization/query/test/FirstNameStandardizeTest.java
@@ -1,12 +1,21 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.dataquality.standardization.query.test;
+
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
-import junit.framework.TestCase;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.SimpleAnalyzer;
@@ -14,72 +23,112 @@ import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.talend.dataquality.standardization.constant.PluginConstant;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.talend.dataquality.standardization.query.FirstNameStandardize;
 
-public class FirstNameStandardizeTest extends TestCase {
+/**
+ * DOC sizhaoliu class global comment. Detailled comment
+ */
+public class FirstNameStandardizeTest {
 
-//    private String filename = "./data/TalendGivenNames.TXT";
+    private static final String indexfolder = "./data/TalendGivenNames_index"; // $NON-NLS-1$
 
-    private String indexfolder = "./data/TalendGivenNames_index"; // $NON-NLS-1$
+    private static IndexSearcher searcher = null;
 
-    private IndexSearcher searcher = null;
+    private static Analyzer searchAnalyzer = null;
 
-    private Analyzer searchAnalyzer = null;
+    private static FirstNameStandardize fnameStandardize = null;
 
-    public void testStandardizeStringString() throws IOException, ParseException {
+    private static final String inputName = "Michel"; //$NON-NLS-1$
+
+    private static final String[][] expected = { { "Michel", "AUS", "MITCHEL", "MICHELE", "MITCHEL" },
+            { "Michel", "BEL", "MICHEL", "MICHEL", "MICHEL" }, { "Michel", "DEU", "MICHEL", "MICHEL", "MICHEL" },
+            { "Michel", "ESP", "MICHEL", "ADELAIDA", "MICHEL" }, { "Michel", "FRA", "MICHEL", "MICHELE", "MICHEL" },
+            { "Michel", "ITA", "MICHELA", "MICHELA", "MICHELE" }, { "Michel", "RUS", "MICHEL", "MICHEL", "MICHEL" },
+            { "Michel", "USA", "MICHEL", "MICHELL", "MICHEL" },
+
+            { "Adrian", "AUS", "ADRIAN", "ADRIAN", "ADRIAN" }, { "Adrian", "BEL", "ADRIAN", "ADRIAN", "ADRIAN" },
+            { "Adrian", "DEU", "MARIAN", "MARIAN", "MARIAN" }, { "Adrian", "ESP", "ADRIAN", "MIRIAN", "ADRIAN" },
+            { "Adrian", "FRA", "ADRIAN", "ADRIAN", "ADRIAN" }, { "Adrian", "ITA", "ADRIANO", "ADRIANA", "ADRIANO" },
+            { "Adrian", "RUS", "BRIAN", "LARISSA", "BRIAN" }, { "Adrian", "USA", "ADRIAN", "ADRIANE", "ADRIAN" }, };
+
+
+    /**
+     * DOC sizhaoliu Comment method "setUpBeforeClass".
+     * 
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
         Directory dir = FSDirectory.open(new File(indexfolder));
         searcher = new IndexSearcher(dir);
         searchAnalyzer = new SimpleAnalyzer();
-        String inputName = "Philippe"; //$NON-NLS-1$
-        Map<String, String> information2value = new HashMap<String, String>();
-        information2value.put("country", "mosikou"); //$NON-NLS-1$ //$NON-NLS-2$
-        information2value.put("gender", "0"); //$NON-NLS-1$ //$NON-NLS-2$
-        getReplaceSearchResult(indexfolder, inputName, information2value, false);
+        fnameStandardize = new FirstNameStandardize(searcher, searchAnalyzer, 10);
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
         searcher.close();
     }
 
-    private void getReplaceSearchResult(String folderName, String inputName, Map<String, String> information2value,
-            boolean fuzzyQuery) throws IOException, ParseException {
-        if (inputName != null && folderName != null) {
-            FirstNameStandardize stdname = new FirstNameStandardize(searcher, searchAnalyzer, 10);
-            String countryText = null;
-            String genderText = null;
-            Set<String> indexKinds = information2value.keySet();
-            for (String indexKind : indexKinds) {
-                if (indexKind.equals(PluginConstant.FIRST_NAME_STANDARDIZE_COUNTRY)) {
-                    countryText = information2value.get(indexKind);
-                } else if (indexKind.equals(PluginConstant.FIRST_NAME_STANDARDIZE_GENDER)) {
-                    genderText = information2value.get(indexKind);
-                }
-            }
-            if (countryText == null && genderText == null) {
-                try {
-                    stdname.replaceName(inputName, fuzzyQuery);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else if (countryText != null && genderText != null) {
-                try {
-                    stdname.replaceNameWithCountryGenderInfo(inputName, countryText, genderText, fuzzyQuery);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else if (countryText != null && genderText == null) {
-                try {
-                    stdname.replaceNameWithCountryInfo(inputName, countryText, fuzzyQuery);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else if (countryText == null && genderText != null) {
-                try {
-                    stdname.replaceNameWithGenderInfo(inputName, genderText, fuzzyQuery);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+    /**
+     * Test method for
+     * {@link org.talend.dataquality.standardization.query.FirstNameStandardize#replaceName(java.lang.String, boolean)}.
+     */
+    @Test
+    public void testReplaceName() {
+        try {
+            String res = fnameStandardize.replaceName(inputName, true);
+            System.out.println("testReplaceName:\n" + res);
+            assertEquals("MICHEL", res);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Test method for
+     * {@link org.talend.dataquality.standardization.query.FirstNameStandardize#replaceNameWithCountryGenderInfo(java.lang.String, java.lang.String, java.lang.String, boolean)}
+     * .
+     */
+    @Test
+    public void testReplaceNameWithCountryGenderInfo() {
+        try {
+
+            System.out.println("\ntestReplaceNameWithCountryGenderInfo:");
+            for (String[] testCase : expected) {
+                String res, resF, resM = "";
+                System.out.print("{\"" + testCase[0] + "\", \"" + testCase[1] + "\", \"");
+
+                // results for query without gender info
+                res = fnameStandardize.replaceNameWithCountryInfo(testCase[0], testCase[1], true);
+                System.out.print(res + "\", \"");
+                assertEquals(testCase[2], res);
+
+                // results for female first name query
+                resF = fnameStandardize.replaceNameWithCountryGenderInfo(testCase[0], testCase[1], "F", true);
+                System.out.print(resF + "\", \"");
+                assertEquals(testCase[3], resF);
+
+                // results for female first name query
+                resM = fnameStandardize.replaceNameWithCountryGenderInfo(testCase[0], testCase[1], "M", true);
+                System.out.println(resM + "\"},");
+                assertEquals(testCase[4], resM);
             }
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
+
 }
