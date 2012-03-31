ff0039265637c892390912304a1901287f0771de
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-31 06:12:43 +0000

    TDQ-5024  Junit org.talend.dq.analysis.explore.PatternExplorerTest.testGetValidRowsStatement and testGetInvalidRowsStatement by using the org.mockito
    git-svn-id: http://talendforge.org/svn/top/trunk@80760 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management.test/src/org/talend/dq/analysis/explore/PatternExplorerTest.java
+++ org.talend.cwm.management.test/src/org/talend/dq/analysis/explore/PatternExplorerTest.java
@@ -12,31 +12,105 @@
 // ============================================================================
 package org.talend.dq.analysis.explore;
 
+import static org.easymock.EasyMock.expect;
 import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.easymock.PowerMock.mockStatic;
+import static org.powermock.api.easymock.PowerMock.replayAll;
 
 import java.util.Map;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.talend.cwm.exception.TalendException;
 import org.talend.dataquality.analysis.Analysis;
+import org.talend.dataquality.analysis.AnalysisContext;
+import org.talend.dataquality.indicators.PatternMatchingIndicator;
 import org.talend.dq.analysis.TestAnalysisCreation;
+import org.talend.dq.dbms.DbmsLanguage;
+import org.talend.dq.dbms.DbmsLanguageFactory;
 import org.talend.dq.indicators.preview.table.ChartDataEntity;
+import org.talend.dq.nodes.indicator.type.IndicatorEnum;
+
+import orgomg.cwm.foundation.softwaredeployment.DataManager;
+import orgomg.cwm.objectmodel.core.Expression;
+import orgomg.cwm.objectmodel.core.ModelElement;
 
 /**
  * DOC scorreia class global comment. Detailled comment
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ DbmsLanguageFactory.class, IndicatorEnum.class })
 public class PatternExplorerTest {
 
     private static final String EMPTY_STRING = ""; //$NON-NLS-1$
 
-    private static final String RES = "SELECT *  FROM `tbi`.`customer`  WHERE ((customer.lname = \"sunny\")) AND  `lname` REGEXP 'su.*' "; //$NON-NLS-1$
+    private static final String RES = "SELECT *  FROM `tbi`.`customer`  WHERE ((customer.lname = \"sunny\")) AND  `lname` REGEXP 'su.*'"; //$NON-NLS-1$
 
-    private static final String RES_NEG = "SELECT *  FROM `tbi`.`customer`  WHERE ((customer.lname = \"sunny\")) AND  `lname` NOT REGEXP 'su.*'  OR `lname` IS NULL "; //$NON-NLS-1$
+    private static final String RES_NEG = "SELECT *  FROM `tbi`.`customer`  WHERE ((customer.lname = \"sunny\")) AND  `lname` NOT REGEXP 'su.*' OR `lname` IS NULL "; //$NON-NLS-1$
 
     private static final String RES_VAL = "SELECT `lname` FROM `tbi`.`customer`  WHERE ((customer.lname = \"sunny\")) AND  `lname` REGEXP 'su.*' "; //$NON-NLS-1$
 
     private static final String RES_VAL_NEG = "SELECT `lname` FROM `tbi`.`customer`  WHERE ((customer.lname = \"sunny\")) AND  `lname` NOT REGEXP 'su.*'  OR `lname` IS NULL "; //$NON-NLS-1$
+
+    private PatternExplorer patternExplorer;
+
+    // used in almost every method in this class
+    private DbmsLanguage dbmsLanguage;
+
+    private PatternMatchingIndicator indicator;
+
+    @Before
+    public void setUp() throws Exception {
+        patternExplorer = new PatternExplorer();
+
+        //mock : setAnalysis(Analysis analysis) in DataExplorer
+        Analysis analysis = mock(Analysis.class);
+        AnalysisContext context = mock(AnalysisContext.class);
+        when(analysis.getContext()).thenReturn(context);
+        DataManager dataManager = mock(DataManager.class);
+        when(context.getConnection()).thenReturn(dataManager);
+        
+        // mock setEntity
+        indicator = mock(PatternMatchingIndicator.class);
+        when(indicator.eClass()).thenReturn(null);
+
+        dbmsLanguage = mock(DbmsLanguage.class);
+        when(dbmsLanguage.getRegexPatternString(indicator)).thenReturn(RES);
+        when(dbmsLanguage.quote(anyString())).thenReturn("`lname`");
+        when(dbmsLanguage.regexLike(anyString(), anyString())).thenReturn(
+                "((customer.lname = \"sunny\")) AND  `lname` REGEXP 'su.*'");
+        when(dbmsLanguage.regexNotLike(anyString(), anyString())).thenReturn(
+                "((customer.lname = \"sunny\")) AND  `lname` NOT REGEXP 'su.*'");
+
+        when(dbmsLanguage.where()).thenReturn(" WHERE ");
+        when(dbmsLanguage.and()).thenReturn(" AND ");
+        when(dbmsLanguage.from()).thenReturn(" FROM ");
+
+        // MOCKING STATIC METHODS
+        mockStatic(DbmsLanguageFactory.class);
+        expect(DbmsLanguageFactory.createDbmsLanguage(dataManager)).andReturn(dbmsLanguage);
+        replayAll();
+
+        Assert.assertTrue(patternExplorer.setAnalysis(analysis));
+
+        ChartDataEntity cdEntity = mock(ChartDataEntity.class);
+        when(cdEntity.getIndicator()).thenReturn(indicator);
+
+        // when(patternExplorer.getAnalyzedElementName(indicator)).thenReturn( "mocked_column");
+
+        mockStatic(IndicatorEnum.class);
+        expect(IndicatorEnum.findIndicatorEnum(indicator.eClass())).andReturn(IndicatorEnum.RowCountIndicatorEnum);
+
+        patternExplorer.setEnitty(cdEntity);
+
+    }
 
     /**
      * Test method for {@link org.talend.dq.analysis.explore.PatternExplorer#PatternExplorer()}.
@@ -52,49 +126,87 @@ public class PatternExplorerTest {
     }
 
     /**
-     * Test method for {@link org.talend.dq.analysis.explore.PatternExplorer#getInvalidRowsStatement()}.
+     * Mock Test method for {@link org.talend.dq.analysis.explore.PatternExplorer#getInvalidRowsStatement()}.
      */
     @Test
     public void testGetInvalidRowsStatement() {
-        TestAnalysisCreation creator = new TestAnalysisCreation();
+        ModelElement element = mock(ModelElement.class);
+        when(indicator.getAnalyzedElement()).thenReturn(element);
+        when(element.getName()).thenReturn("lname");
+        indicator.setAnalyzedElement(element);
 
-        Analysis analysis;
-        try {
-            analysis = creator.createAndRunAnalysis();
-            PatternExplorer patternExplorer = new PatternExplorer();
-            Assert.assertTrue(patternExplorer.setAnalysis(analysis));
-            ChartDataEntity cdEntity = new ChartDataEntity(analysis.getResults().getIndicators().iterator().next(), EMPTY_STRING,
-                    EMPTY_STRING);
-            patternExplorer.setEnitty(cdEntity);
-            String strStatement = patternExplorer.getInvalidRowsStatement();
-            System.out.println(strStatement);
-            Assert.assertEquals(RES_NEG, strStatement);
-        } catch (TalendException e) {
-            fail(e.getMessage());
-        }
+        when(dbmsLanguage.or()).thenReturn(" OR ");
+        when(dbmsLanguage.isNull()).thenReturn(" IS NULL ");
+
+        // for method: getFromClause()
+        Expression instantiatedExpression = mock(Expression.class);
+        when(dbmsLanguage.getInstantiatedExpression(indicator)).thenReturn(instantiatedExpression);
+        when(instantiatedExpression.getBody()).thenReturn(" FROM `tbi`.`customer` ");
+
+        String strStatement = patternExplorer.getInvalidRowsStatement();
+        // System.out.println(strStatement);
+        Assert.assertEquals(RES_NEG, strStatement);
+        // TestAnalysisCreation creator = new TestAnalysisCreation();
+        //
+        // Analysis analysis;
+        // try {
+        // analysis = creator.createAndRunAnalysis();
+        // PatternExplorer patternExplorer = new PatternExplorer();
+        // Assert.assertTrue(patternExplorer.setAnalysis(analysis));
+        // ChartDataEntity cdEntity = new ChartDataEntity(analysis.getResults().getIndicators().iterator().next(),
+        // EMPTY_STRING,
+        // EMPTY_STRING);
+        // patternExplorer.setEnitty(cdEntity);
+        // String strStatement = patternExplorer.getInvalidRowsStatement();
+        // System.out.println(strStatement);
+        // Assert.assertEquals(RES_NEG, strStatement);
+        // } catch (TalendException e) {
+        // fail(e.getMessage());
+        // }
     }
 
     /**
      * Test method for {@link org.talend.dq.analysis.explore.PatternExplorer#getValidRowsStatement()}.
      */
+    // @Test
+    // public void testGetValidRowsStatement() {
+    // TestAnalysisCreation creator = new TestAnalysisCreation();
+    //
+    // Analysis analysis;
+    // try {
+    // analysis = creator.createAndRunAnalysis();
+    // PatternExplorer patternExplorer = new PatternExplorer();
+    // Assert.assertTrue(patternExplorer.setAnalysis(analysis));
+    // ChartDataEntity cdEntity = new ChartDataEntity(analysis.getResults().getIndicators().iterator().next(),
+    // EMPTY_STRING,
+    // EMPTY_STRING);
+    // patternExplorer.setEnitty(cdEntity);
+    // String strStatement = patternExplorer.getValidRowsStatement();
+    // System.out.println(strStatement);
+    // Assert.assertEquals(RES, strStatement);
+    // } catch (TalendException e) {
+    // fail(e.getMessage());
+    // }
+    // }
+
+    /**
+     * mock test method
+     */
     @Test
     public void testGetValidRowsStatement() {
-        TestAnalysisCreation creator = new TestAnalysisCreation();
+        ModelElement element = mock(ModelElement.class);
+        when(indicator.getAnalyzedElement()).thenReturn(element);
+        when(element.getName()).thenReturn("name");
+        indicator.setAnalyzedElement(element);
 
-        Analysis analysis;
-        try {
-            analysis = creator.createAndRunAnalysis();
-            PatternExplorer patternExplorer = new PatternExplorer();
-            Assert.assertTrue(patternExplorer.setAnalysis(analysis));
-            ChartDataEntity cdEntity = new ChartDataEntity(analysis.getResults().getIndicators().iterator().next(), EMPTY_STRING,
-                    EMPTY_STRING);
-            patternExplorer.setEnitty(cdEntity);
-            String strStatement = patternExplorer.getValidRowsStatement();
-            System.out.println(strStatement);
-            Assert.assertEquals(RES, strStatement);
-        } catch (TalendException e) {
-            fail(e.getMessage());
-        }
+        // for method: getFromClause()
+        Expression instantiatedExpression = mock(Expression.class);
+        when(dbmsLanguage.getInstantiatedExpression(indicator)).thenReturn(instantiatedExpression);
+        when(instantiatedExpression.getBody()).thenReturn(" FROM `tbi`.`customer` ");
+
+        String strStatement = patternExplorer.getValidRowsStatement();
+        // System.out.println(strStatement);
+        Assert.assertEquals(RES, strStatement);
     }
 
     /**
