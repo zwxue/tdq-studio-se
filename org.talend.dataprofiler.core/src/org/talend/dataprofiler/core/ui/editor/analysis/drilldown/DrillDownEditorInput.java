fa149e81e05cd3a88967d801e178e6020c69f157
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-05-24 07:08:45 +0000

    TDQ-4691 "view row " failed for MDM analysis TDQ-5432 Fix on branch 5.0, and passed unit test
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_0@84093 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/analysis/drilldown/DrillDownEditorInput.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/analysis/drilldown/DrillDownEditorInput.java
@@ -207,6 +207,11 @@ public class DrillDownEditorInput implements IEditorInput {
         for (Object[] tableRow : newColumnElementList) {
             int columnIndex = 0;
             for (Object tableValue : tableRow) {
+                // added yyin 20120523 TDQ-4691: in MDM, when these two size not equal, this for will throw exception
+                if (tableValue == null) {
+                    if (newColumnElementList.get(0).length != columnElementList.size())
+                        continue;
+                }// ~
                 columnValue[rowIndex][columnIndex++] = tableValue == null ? "<null>" : tableValue.toString();
             }
             rowIndex++;
