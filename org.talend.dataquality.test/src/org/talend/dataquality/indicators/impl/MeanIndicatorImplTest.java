0ae30ea2767a736ef793a2aaa8b7873a248654c5
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-05-25 05:24:07 +0000

    TDQ-4936 TDQ-5442 Don't include "null" count when compute mean indicator when Java engine turns on.
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_0@84133 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataquality.test/src/org/talend/dataquality/indicators/impl/MeanIndicatorImplTest.java
@@ -0,0 +1,39 @@
+package org.talend.dataquality.indicators.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * mzhao Junit test for mean indicator
+ */
+public class MeanIndicatorImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGetMean() {
+        MeanIndicatorImpl meanInd = new MeanIndicatorImpl();
+        meanInd.setSumStr("18");
+        meanInd.setCount(6L);
+        meanInd.setNullCount(3L);
+        assertTrue(meanInd.getMean().doubleValue() == 6);
+        meanInd.setNullCount(null);
+        assertTrue(meanInd.getMean().doubleValue() == 3);
+        meanInd.setNullCount(0L);
+        assertTrue(meanInd.getMean().doubleValue() == 3);
+        meanInd.setCount(6L);
+        meanInd.setNullCount(6L);
+        assertTrue(meanInd.getMean().isNaN());
+    }
+
+}
