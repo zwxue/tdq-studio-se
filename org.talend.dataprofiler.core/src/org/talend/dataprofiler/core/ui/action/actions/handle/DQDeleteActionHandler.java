6af0fbeda4bc2e157117c0193d0eaf3ec4df62cd
sizhaoliu <sizhaoliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-06-05 14:23:32 +0000

    merge r84340 from branch 5.0 to trunk TDQ-4948: try to delete an item by clicking the "Delete" on keyboard doesn't work
    git-svn-id: http://talendforge.org/svn/top/trunk@84737 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/handle/DQDeleteActionHandler.java
@@ -0,0 +1,34 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.action.actions.handle;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.talend.dataprofiler.core.ui.action.actions.DQDeleteAction;
+
+/**
+ * Action handler for DQ item deletion
+ *
+ */
+public class DQDeleteActionHandler extends AbstractHandler {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		new DQDeleteAction().run();
+		return null;
+	}
+
+}
