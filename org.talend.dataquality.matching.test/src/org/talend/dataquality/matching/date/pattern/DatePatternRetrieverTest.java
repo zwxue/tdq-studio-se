e1a293024239494fb20a170880b07e8d2ec9605b
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-03 14:41:35 +0000

    merge r86680 from trunk to branch 5.1 TDQ-5739 get the correct path of the file used in the DatePatternRetriever test
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_1@86682 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataquality.matching.test/src/org/talend/dataquality/matching/date/pattern/DatePatternRetrieverTest.java
+++ org.talend.dataquality.matching.test/src/org/talend/dataquality/matching/date/pattern/DatePatternRetrieverTest.java
@@ -12,14 +12,14 @@
 // ============================================================================
 package org.talend.dataquality.matching.date.pattern;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.talend.commons.utils.resource.ResourceUtil;
 
 /**
  * Test class of DatePatternRetriever.
@@ -27,33 +27,19 @@ import org.junit.Test;
 public class DatePatternRetrieverTest {
 
     /**
-     * DOC scorreia Comment method "setUp".
-     * 
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    /**
-     * DOC scorreia Comment method "tearDown".
-     * 
-     * @throws java.lang.Exception
-     */
-    @After
-    public void tearDown() throws Exception {
-    }
-
-    /**
      * Test method for
      * {@link org.talend.dataquality.matching.date.pattern.DatePatternRetriever#initModel2Regex(java.lang.String[][])}.
+     * 
+     * @throws URISyntaxException
+     * @throws IOException
      */
     @Test
-    public void testInitModel2Regex() {
+    public void testInitModel2Regex() throws IOException, URISyntaxException {
+        File file = ResourceUtil.getFileFromResource(getClass(), "/data/PatternsNameAndRegularExpressions.txt"); //$NON-NLS-1$
         DatePatternRetriever dtr = new DatePatternRetriever();
-        dtr.initModel2Regex(new File("data/PatternsNameAndRegularExpressions.txt"));
+        dtr.initModel2Regex(file);
         assertNotNull(dtr.getModelMatchers());
-        assertEquals("Found " + dtr.getModelMatchers().size() + " modelMatchers. Was waiting for 32 patterns in file", true, dtr
+        assertEquals("Found " + dtr.getModelMatchers().size() + " modelMatchers. Was waiting for 32 patterns in file", true, dtr //$NON-NLS-1$//$NON-NLS-2$
                 .getModelMatchers().size() == 32);
     }
 
@@ -64,13 +50,13 @@ public class DatePatternRetrieverTest {
     @Test
     public void testHandle() {
         // string to match
-        String expr = "21 11 1999";
+        String expr = "21 11 1999"; //$NON-NLS-1$
 
         DatePatternRetriever dtr = new DatePatternRetriever();
         dtr.handle(expr);
         assertEquals(dtr.getModelMatchers().size(), 0);
 
-        ModelMatcher mm = new ModelMatcher("11 november 1999", "^[0-3][0-9](-|/| )([0-0][1-9]|10|11|12)(-|/| )(19|20)[0-9]{2}$");
+        ModelMatcher mm = new ModelMatcher("11 november 1999", "^[0-3][0-9](-|/| )([0-0][1-9]|10|11|12)(-|/| )(19|20)[0-9]{2}$"); //$NON-NLS-1$//$NON-NLS-2$
         assertEquals(0, mm.getScore());
         dtr.getModelMatchers().add(mm);
         assertEquals(dtr.getModelMatchers().size(), 1);
