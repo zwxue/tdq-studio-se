5eda889a3c86f0a115fddd7a77920a658b0ee982
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-30 09:15:13 +0000

    TDQ-5937 modify the test for the ResourceViewLabelProvider.getFileCount
    git-svn-id: http://talendforge.org/svn/top/trunk@88269 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/ui/views/provider/ResourceViewLabelProviderTest.java
+++ org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/ui/views/provider/ResourceViewLabelProviderTest.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2011 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -19,11 +19,32 @@ import java.io.InputStream;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.jfree.util.Log;
 import org.junit.Test;
+import org.talend.commons.bridge.ReponsitoryContextBridge;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.model.properties.ItemState;
+import org.talend.core.model.properties.PropertiesFactory;
+import org.talend.core.repository.model.IRepositoryFactory;
+import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.core.repository.model.RepositoryFactoryProvider;
+import org.talend.dataprofiler.core.CorePlugin;
+import org.talend.dataprofiler.core.manager.DQStructureManager;
+import org.talend.dataquality.analysis.Analysis;
+import org.talend.dataquality.analysis.AnalysisFactory;
+import org.talend.dataquality.analysis.AnalysisResult;
+import org.talend.dataquality.helpers.AnalysisHelper;
+import org.talend.dataquality.helpers.ReportHelper;
+import org.talend.dataquality.properties.TDQAnalysisItem;
+import org.talend.dataquality.properties.TDQReportItem;
+import org.talend.dataquality.properties.impl.PropertiesFactoryImpl;
+import org.talend.dq.helper.resourcehelper.AnaResourceFileHelper;
+import org.talend.resource.EResourceConstant;
+import orgomg.cwmx.analysis.informationreporting.Report;
 
 /**
  * DOC zshen class global comment. Test the method
@@ -32,12 +53,11 @@ import org.junit.Test;
 
 public class ResourceViewLabelProviderTest {
 
+    private static String[] filterExtensions = { "ana", "rep" };//$NON-NLS-1$//$NON-NLS-2$
 
-    private static String[] filterExtensions = { "ana", "rep" };
+    final private String anaFolderName = "TDQ_Data Profiling/Analyses";//$NON-NLS-1$
 
-    final private String anaFolderName = "Analyses";
-
-    final private String repFolderName = "Reports";
+    final private String repFolderName = "TDQ_Data Profiling/Reports";//$NON-NLS-1$
 
     /**
      * Test method for
@@ -47,32 +67,18 @@ public class ResourceViewLabelProviderTest {
     @Test
     public void testGetFileCount() {
 
-        IFolder anaFolder = initFolder(anaFolderName);
-        IFolder repFolder = initFolder(repFolderName);
+        IFolder anaFolder = initFolder(EResourceConstant.ANALYSIS.getPath());
+        IFolder repFolder = initFolder(EResourceConstant.REPORTS.getPath());
         ResourceViewLabelProvider reViewLabelProvider = new ResourceViewLabelProvider();
-
-        // TDQAnalysisItem createTDQAnalysisItem = PropertiesFactoryImpl.eINSTANCE.createTDQAnalysisItem();
-        // PropertiesFactory TOSPropertyFactory = org.talend.core.model.properties.impl.PropertiesFactoryImpl.eINSTANCE;
-        // org.talend.core.model.properties.Property createProperty = TOSPropertyFactory.createProperty();
-        // ItemState createItemState = TOSPropertyFactory.createItemState();
-        // createItemState.setDeleted(false);
-        // createTDQAnalysisItem.setState(TOSPropertyFactory.createItemState());
-        // createTDQAnalysisItem.setProperty(createProperty);
-        // createProperty.setItem(createTDQAnalysisItem);
-        // ElementWriterFactory.getInstance().createAnalysisWrite().save(createTDQAnalysisItem);
-        // //mock PropertyHelper
-        // PowerMockito.mockStatic(PropertyHelper.class);
-        // Mockito.when(PropertyHelper.getProperty((IFile)Mockito.any())).thenReturn(createProperty);
-        // //~mock PropertyHelper
-        // IFolder anaFolder =ResourceManager.getAnalysisFolder();
-        // IFolder repFolder =ResourceManager.getAnalysisFolder();
         int AnalysisNum = reViewLabelProvider.getFileCount(anaFolder, filterExtensions);
-        // System.out.println(AnalysisNum);
-        assertEquals(AnalysisNum, 3);
         int reportNum = reViewLabelProvider.getFileCount(repFolder, filterExtensions);
-        // System.out.println(reportNum);
         assertEquals(AnalysisNum, 3);
         assertEquals(reportNum, 3);
+    }
+
+    private void initElements() {
+        AnaResourceFileHelper.getAll();
+
     }
 
     /**
@@ -83,60 +89,111 @@ public class ResourceViewLabelProviderTest {
      * @return
      */
     public IFolder initFolder(String folderName) {
-        IWorkspace ws = ResourcesPlugin.getWorkspace();
-        IProject project = ws.getRoot().getProject("projects");
+        IRepositoryFactory repository = RepositoryFactoryProvider.getRepositoriyById("local");//$NON-NLS-1$
+        ProxyRepositoryFactory proRepInstance = ProxyRepositoryFactory.getInstance();
+        if (proRepInstance.getRepositoryFactoryFromProvider() == null) {
+            ProxyRepositoryFactory.getInstance().setRepositoryFactoryFromProvider(repository);
+        }
+        IProject project = ReponsitoryContextBridge.getRootProject();
         if (!project.exists()) {
             try {
                 project.create(null);
             } catch (CoreException e) {
-                // TODO Auto-generated catch block
+                Log.error(e, e);
                 e.printStackTrace();
             }
         }
-        // System.out.println(project);
+        CorePlugin.getDefault().initProxyRepository();
+        if (DQStructureManager.getInstance().isNeedCreateStructure()) {
+            DQStructureManager.getInstance().createDQStructure();
+        }
         IFolder aa = project.getFolder(folderName);
         if (aa.exists()) {
         } else {
             try {
-                project.open(null);
                 aa.create(true, true, null);
             } catch (CoreException e) {
-                // TODO Auto-generated catch block
+                Log.error(e, e);
                 e.printStackTrace();
             }
         }
         // for analyses
         if (anaFolderName.equals(folderName)) {
-            IFolder subfolder1 = createFolder(aa, "subfolder1");
-            IFolder subfolder2 = createFolder(aa, "subfolder2");
-            createFile(aa, "a1_0.1.ana");
-            createFile(aa, "a1_0.1.properties");
+            IFolder subfolder1 = createFolder(aa, "subfolder1");//$NON-NLS-1$
+            IFolder subfolder2 = createFolder(aa, "subfolder2");//$NON-NLS-1$
             // logic delete one
-            createFile(aa, "a4_0.1.ana");
-            createFile(aa, "a4_0.1.properties");
+            createAnalysis("a4", null, true);//$NON-NLS-1$
             // ~logic delete one
-            createFile(subfolder1, "a2_0.1.ana");
-            createFile(subfolder1, "a2_0.1.properties");
-            createFile(subfolder2, "a3_0.1.ana");
-            createFile(subfolder2, "a3_0.1.properties");
+            createAnalysis("a1", null, false);//$NON-NLS-1$
+            createAnalysis("a3", subfolder2, false);//$NON-NLS-1$
+            createAnalysis("a2", subfolder1, false);//$NON-NLS-1$
+
         }
         // for reports
         if (repFolderName.equals(folderName)) {
-            IFolder subfolder1 = createFolder(aa, "subfolder1");
-            IFolder subfolder2 = createFolder(aa, "subfolder2");
-            createFile(aa, "a1_0.1.rep");
-            createFile(aa, "a1_0.1.properties");
+            IFolder subfolder1 = createFolder(aa, "subfolder1");//$NON-NLS-1$
+            IFolder subfolder2 = createFolder(aa, "subfolder2");//$NON-NLS-1$
             // logic delete one
-            createFile(aa, "a4_0.1.rep");
-            createFile(aa, "a4_0.1.properties");
+            createReport("a4", null, true);//$NON-NLS-1$
             // ~logic delete one
-            createFile(subfolder1, "a2_0.1.rep");
-            createFile(subfolder1, "a2_0.1.properties");
-            createFile(subfolder2, "a3_0.1.rep");
-            createFile(subfolder2, "a3_0.1.properties");
+            createReport("a1", null, false);//$NON-NLS-1$
+            createReport("a3", subfolder2, false);//$NON-NLS-1$
+            createReport("a2", subfolder1, false);//$NON-NLS-1$
         }
-        // System.out.println(aa.getRawLocationURI());
+        ;
         return aa;
+    }
+
+    private void createReport(String name, IFolder folder, Boolean isDelete) {
+        IPath createPath = Path.EMPTY;
+        if (folder != null) {
+            createPath = new Path(folder.getFullPath().lastSegment());
+        }
+        Report report1 = ReportHelper.createReport(name);
+        TDQReportItem item1 = PropertiesFactoryImpl.eINSTANCE.createTDQReportItem();
+        org.talend.core.model.properties.Property property1 = PropertiesFactory.eINSTANCE.createProperty();
+        property1.setId(EcoreUtil.generateUUID());
+        property1.setItem(item1);
+        property1.setLabel(report1.getName());
+        item1.setProperty(property1);
+        item1.setReport(report1);
+        ItemState itemState = org.talend.core.model.properties.PropertiesFactory.eINSTANCE.createItemState();
+        itemState.setDeleted(isDelete);
+        item1.setState(itemState);
+
+        try {
+            ProxyRepositoryFactory.getInstance().create(item1, createPath, false);
+        } catch (PersistenceException e) {
+            Log.error(e, e);
+            e.printStackTrace();
+        }
+
+    }
+
+    private void createAnalysis(String name, IFolder folder, Boolean isDelete) {
+        IPath createPath = Path.EMPTY;
+        if (folder != null) {
+            createPath = new Path(folder.getFullPath().lastSegment());
+        }
+        Analysis analysis1 = AnalysisHelper.createAnalysis(name);
+        TDQAnalysisItem item1 = PropertiesFactoryImpl.eINSTANCE.createTDQAnalysisItem();
+        org.talend.core.model.properties.Property property1 = PropertiesFactory.eINSTANCE.createProperty();
+        property1.setId(EcoreUtil.generateUUID());
+        property1.setItem(item1);
+        property1.setLabel(analysis1.getName());
+        item1.setProperty(property1);
+        item1.setAnalysis(analysis1);
+        ItemState itemState = org.talend.core.model.properties.PropertiesFactory.eINSTANCE.createItemState();
+        itemState.setDeleted(isDelete);
+        item1.setState(itemState);
+        AnalysisResult analysisResult1 = AnalysisFactory.eINSTANCE.createAnalysisResult();
+        analysis1.setResults(analysisResult1);
+        try {
+            ProxyRepositoryFactory.getInstance().create(item1, createPath, false);
+        } catch (PersistenceException e) {
+            Log.error(e, e);
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -153,7 +210,7 @@ public class ResourceViewLabelProviderTest {
             try {
                 currFolder.create(true, true, null);
             } catch (CoreException e) {
-                // TODO Auto-generated catch block
+                Log.error(e, e);
                 e.printStackTrace();
             }
         }
@@ -174,12 +231,10 @@ public class ResourceViewLabelProviderTest {
         if (!file.exists()) {
             try {
                 InputStream resourceAsStream = ResourceViewLabelProviderTest.class.getClassLoader().getResourceAsStream(
-                        "/temp/" + parentPath.toOSString() + "/" + filName);
-                // FileInputStream aa=new FileInputStream()
-                // byte a1[] = new byte[0];
+                        "/temp/" + parentPath.toOSString() + "/" + filName);//$NON-NLS-1$//$NON-NLS-2$
                 file.create(resourceAsStream, true, null);
             } catch (CoreException e) {
-                // TODO Auto-generated catch block
+                Log.error(e, e);
                 e.printStackTrace();
             }
         }
