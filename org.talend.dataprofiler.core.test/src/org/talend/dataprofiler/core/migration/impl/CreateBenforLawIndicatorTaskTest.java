30b324f3fe5e6af9abba5cea72b2b7517cc87e5d
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-09-04 07:26:33 +0000

    TDQ-6024 unit test for benford indicator migration task
    git-svn-id: http://talendforge.org/svn/top/trunk@90075 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/migration/impl/CreateBenforLawIndicatorTaskTest.java
@@ -0,0 +1,80 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.migration.impl;
+
+import static org.junit.Assert.assertFalse;
+
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.eclipse.core.resources.IProject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.talend.commons.utils.io.FilesUtils;
+import org.talend.dataprofiler.core.helper.UnitTestBuildHelper;
+import org.talend.utils.string.StringUtilities;
+
+
+/**
+ * DOC yyin  class global comment. Detailled comment
+ */
+public class CreateBenforLawIndicatorTaskTest {
+
+    private CreateBenforLawIndicatorTask benTask;
+
+    private String projectName;
+
+    private IProject realProject;
+
+    private File projectFile;
+    /**
+     * DOC yyin Comment method "setUp".
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        this.projectName = ("A" + StringUtilities.getRandomString(7)).toUpperCase(); //$NON-NLS-1$
+        this.realProject = UnitTestBuildHelper.createRealProject(this.projectName);
+        this.projectFile = this.realProject.getWorkspace().getRoot().getLocation().append(this.projectName).toFile();
+        benTask = new CreateBenforLawIndicatorTask();
+    }
+
+    /**
+     * DOC yyin Comment method "tearDown".
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        if (this.projectFile != null) {
+            FilesUtils.deleteFile(this.projectFile, true);
+            assertFalse(this.projectFile.exists());
+        }
+
+    }
+
+    /**
+     * Test method for {@link org.talend.dataprofiler.core.migration.impl.CreateSystemIndicatorTask#doExecute()}.
+     */
+    @Test
+    public void testDoExecute() {
+        try {
+            Assert.assertTrue(benTask.doExecute());
+        } catch (Exception e) {
+            Assert.fail("Create BenfordLaw indicator migration task failed.");
+        }
+
+    }
+
+}
