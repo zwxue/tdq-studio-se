f118eb4e717eac36bb2831bef0c487de16517f38
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-09-25 09:58:44 +0000

    merge r91299 from trunk to branch 5.1 TDQ-5668, when switching context, check the editor first, if opened, close it first.
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_1@91310 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/SwitchContextAction.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/SwitchContextAction.java
@@ -14,16 +14,12 @@ package org.talend.dataprofiler.core.ui.action.actions;
 
 import org.apache.log4j.Logger;
 import org.eclipse.jface.action.Action;
-import org.talend.core.model.metadata.builder.connection.Connection;
 import org.talend.core.model.properties.ConnectionItem;
 import org.talend.core.model.properties.Item;
+import org.talend.dataprofiler.core.CorePlugin;
 import org.talend.dataprofiler.core.ImageLib;
-import org.talend.dataprofiler.core.PluginConstant;
 import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
 import org.talend.dq.nodes.ConnectionRepNode;
-import org.talend.dq.nodes.DBConnectionRepNode;
-import org.talend.dq.nodes.DFConnectionRepNode;
-import org.talend.dq.nodes.MDMConnectionRepNode;
 import org.talend.repository.ui.utils.SwitchContextGroupNameImpl;
 /**
  * DOC msjian class global comment. Detailled comment
@@ -53,28 +49,20 @@ public class SwitchContextAction extends Action {
             Item item = conRepNode.getObject().getProperty().getItem();
             if (item instanceof ConnectionItem) {
                 ConnectionItem connItem = (ConnectionItem) item;
+
+                // Added yyin 20120925 TDQ-5668, check this connection's editor is open or not, if open, close.
+                CorePlugin.getDefault().closeEditorIfOpened(connItem);
+                // ~
+                
                 boolean isUpdated = SwitchContextGroupNameImpl.getInstance().updateContextGroup(connItem);
 
-                // get contextid and file name
-                String fileStr = PluginConstant.EMPTY_STRING;
-                Connection dbcon = null;
-                if (conRepNode instanceof DBConnectionRepNode) {
-                    dbcon = ((DBConnectionRepNode) conRepNode).getDatabaseConnection();
-                } else if (conRepNode instanceof DFConnectionRepNode) {
-                    dbcon = ((DFConnectionRepNode) conRepNode).getDfConnection();
-                } else if (conRepNode instanceof MDMConnectionRepNode) {
-                    dbcon = ((MDMConnectionRepNode) conRepNode).getMdmConnection();
-                }
-                if (dbcon != null) {
-                    fileStr = dbcon.eResource().getURI().toFileString();
-                }
-                
                 if (isUpdated) {
+
                     if (log.isDebugEnabled()) {
-                        log.debug(DefaultMessagesImpl.getString("SwitchContextAction.saveMessage", fileStr, "successful"));//$NON-NLS-1$ //$NON-NLS-2$
+                        log.debug(DefaultMessagesImpl.getString("SwitchContextAction.saveMessage", "", "successful"));//$NON-NLS-1$ //$NON-NLS-2$
                     }
                 } else {
-                    log.error(DefaultMessagesImpl.getString("SwitchContextAction.saveMessage", fileStr, "failed"));//$NON-NLS-1$ //$NON-NLS-2$
+                    log.error(DefaultMessagesImpl.getString("SwitchContextAction.saveMessage", "", "failed"));//$NON-NLS-1$ //$NON-NLS-2$
                 }
             }
         }
