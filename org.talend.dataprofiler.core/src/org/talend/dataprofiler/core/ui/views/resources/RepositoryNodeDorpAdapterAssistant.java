c3f4f860f57154a0bc7c3edd062fca6323dbac1b
xqliu <xqliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-10-24 02:37:14 +0000

    TDQ-6204 TDQ-6335 1) set "AvoidUnloadResource" with true before execute the WorkUnit; 2) on trunk
    git-svn-id: http://talendforge.org/svn/top/trunk@92643 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/views/resources/RepositoryNodeDorpAdapterAssistant.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/views/resources/RepositoryNodeDorpAdapterAssistant.java
@@ -42,6 +42,7 @@ import org.eclipse.ui.navigator.CommonDropAdapter;
 import org.eclipse.ui.navigator.CommonDropAdapterAssistant;
 import org.jfree.util.Log;
 import org.talend.commons.exception.BusinessException;
+import org.talend.commons.exception.LoginException;
 import org.talend.commons.exception.PersistenceException;
 import org.talend.commons.utils.WorkspaceUtils;
 import org.talend.commons.utils.io.FilesUtils;
@@ -50,6 +51,7 @@ import org.talend.core.model.repository.Folder;
 import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.repository.model.FolderHelper;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.core.runtime.CoreRuntimePlugin;
 import org.talend.dataprofiler.core.CorePlugin;
 import org.talend.dataprofiler.core.helper.WorkspaceResourceHelper;
 import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
@@ -84,6 +86,7 @@ import org.talend.dq.nodes.ReportSubFolderRepNode;
 import org.talend.dq.nodes.SourceFileRepNode;
 import org.talend.dq.nodes.SourceFileSubFolderNode;
 import org.talend.dq.nodes.SysIndicatorDefinitionRepNode;
+import org.talend.repository.RepositoryWorkUnit;
 import org.talend.repository.model.IProxyRepositoryFactory;
 import org.talend.repository.model.IRepositoryNode;
 import org.talend.repository.model.IRepositoryNode.ENodeType;
@@ -751,10 +754,20 @@ public class RepositoryNodeDorpAdapterAssistant extends CommonDropAdapterAssista
     }
 
     private void computePath(FolderHelper folderHelper, IPath sourcePath, IPath targetPath, IPath makeRelativeTo,
-            ERepositoryObjectType type, IRepositoryNode sourceNode, IRepositoryNode targetNode) throws PersistenceException {
-        IPath sourceMakeRelativeTo = sourcePath.makeRelativeTo(makeRelativeTo);
-        IPath targetMakeRelativeTo = targetPath.makeRelativeTo(makeRelativeTo);
-        factory.moveFolder(type, sourceMakeRelativeTo, targetMakeRelativeTo);
+            final ERepositoryObjectType type, IRepositoryNode sourceNode, IRepositoryNode targetNode) throws PersistenceException {
+        final IPath sourceMakeRelativeTo = sourcePath.makeRelativeTo(makeRelativeTo);
+        final IPath targetMakeRelativeTo = targetPath.makeRelativeTo(makeRelativeTo);
+
+        RepositoryWorkUnit<Object> repositoryWorkUnit = new RepositoryWorkUnit<Object>("Move", this) { //$NON-NLS-1$
+
+            @Override
+            protected void run() throws LoginException, PersistenceException {
+                factory.moveFolder(type, sourceMakeRelativeTo, targetMakeRelativeTo);
+            }
+        };
+        repositoryWorkUnit.setAvoidUnloadResources(true);
+        CoreRuntimePlugin.getInstance().getProxyRepositoryFactory().executeRepositoryWorkUnit(repositoryWorkUnit);
+
         RepositoryNode sourceParent = sourceNode.getParent();
         RepositoryNode targetParent = targetNode.getParent();
         CorePlugin.getDefault().refreshDQView(sourceParent);
