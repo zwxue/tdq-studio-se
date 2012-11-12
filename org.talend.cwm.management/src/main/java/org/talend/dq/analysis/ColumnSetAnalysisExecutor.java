0a7757b29a9a16906ea5dc7947b0e0ffea90bc37
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-11-12 10:51:38 +0000

    TDQ-5645 make getRegex method return the best one matching.
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_1@93621 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/analysis/ColumnSetAnalysisExecutor.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/analysis/ColumnSetAnalysisExecutor.java
@@ -247,7 +247,7 @@ public class ColumnSetAnalysisExecutor extends AnalysisExecutor {
                 sql.append(this.quote(col.getName()));
                 // append comma if more columns exist
                 if (iterator.hasNext()) {
-                    sql.append(',');//$NON-NLS-1$
+                    sql.append(',');
                 }
             }
             // add from
@@ -271,7 +271,7 @@ public class ColumnSetAnalysisExecutor extends AnalysisExecutor {
                 sql.append(this.quote(column.getName()));
                 // append comma if more columns exist
                 if (iter.hasNext()) {
-                    sql.append(',');//$NON-NLS-1$
+                    sql.append(',');
                 }
             }
         }
@@ -292,7 +292,7 @@ public class ColumnSetAnalysisExecutor extends AnalysisExecutor {
         } else if (parentRelation instanceof Catalog) {
             String ownerUser = null;
             if (ConnectionUtils.isSybaseeDBProducts(dbms().getDbmsName())) {
-                ownerUser = ColumnSetHelper.getTableOwner((ModelElement) element);
+                ownerUser = ColumnSetHelper.getTableOwner(element);
             }
             sql.append(dbms().toQualifiedName(parentRelation.getName(), ownerUser, element.getName()));
         }
@@ -333,10 +333,10 @@ public class ColumnSetAnalysisExecutor extends AnalysisExecutor {
         EList<RegexpMatchingIndicator> indicators = indicator.getCompositeRegexMatchingIndicators();
         String patternNames = PluginConstant.EMPTY_STRING;
         for (RegexpMatchingIndicator rmi : indicators) {
-            if (null == rmi.getRegex()) {
+            if (null == rmi.getJavaRegex()) {
 
                 patternNames += System.getProperty("line.separator") + "\"" + rmi.getName() + "\"";//$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
-            } else if (rmi.getRegex().equals(rmi.getName())) {
+            } else if (rmi.getJavaRegex().equals(rmi.getName())) {
                 patternNames += System.getProperty("line.separator") + "\"" + rmi.getName() + "\"";//$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
             }
         }
