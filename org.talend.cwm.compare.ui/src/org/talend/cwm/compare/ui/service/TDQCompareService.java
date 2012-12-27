fd2d237d400daa84d66dc3f8c015273724e305a4
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-12-27 11:03:04 +0000

    TDQ-6663 backport on 5.1 remove one popup dialog when choose reload on the DatabaseWizard
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_1@96048 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.compare.ui/src/org/talend/cwm/compare/ui/service/TDQCompareService.java
+++ org.talend.cwm.compare.ui/src/org/talend/cwm/compare/ui/service/TDQCompareService.java
@@ -1,19 +1,12 @@
 package org.talend.cwm.compare.ui.service;
 
-import java.util.List;
-
-import org.eclipse.jface.dialogs.Dialog;
 import org.talend.core.ITDQCompareService;
 import org.talend.core.model.metadata.builder.connection.Connection;
 import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
 import org.talend.core.model.properties.ConnectionItem;
 import org.talend.cwm.compare.ui.actions.ReloadDatabaseAction;
 import org.talend.cwm.compare.ui.actions.provider.ReloadDatabaseProvider;
-import org.talend.dataprofiler.core.i18n.internal.DefaultMessagesImpl;
-import org.talend.dataprofiler.core.ui.dialog.message.DeleteModelElementConfirmDialog;
-import org.talend.dq.helper.EObjectHelper;
 import org.talend.utils.sugars.ReturnCode;
-import orgomg.cwm.objectmodel.core.ModelElement;
 
 public class TDQCompareService implements ITDQCompareService {
 
@@ -33,17 +26,18 @@ public class TDQCompareService implements ITDQCompareService {
         ReturnCode retCode = new ReturnCode(Boolean.TRUE);
         Connection conn = connectionItem.getConnection();
         if (conn instanceof DatabaseConnection) {
-            List<ModelElement> dependencyClients = EObjectHelper.getDependencyClients(conn);
-            if (!(dependencyClients == null || dependencyClients.isEmpty())) {
-                int isOk = DeleteModelElementConfirmDialog.showElementImpactConfirmDialog(null, new ModelElement[] { conn },
-                        DefaultMessagesImpl.getString("TOPRepositoryService.dependcyTile"), //$NON-NLS-1$
-                        DefaultMessagesImpl.getString("TOPRepositoryService.dependcyMessage", conn.getLabel())); //$NON-NLS-1$
-                if (isOk != Dialog.OK) {
-                    retCode.setOk(Boolean.FALSE);
-                    retCode.setMessage("The user canceled the operation!"); //$NON-NLS-1$
-                    return retCode;
-                }
-            }
+            // List<ModelElement> dependencyClients = EObjectHelper.getDependencyClients(conn);
+            // if (!(dependencyClients == null || dependencyClients.isEmpty())) {
+            // int isOk = DeleteModelElementConfirmDialog.showElementImpactConfirmDialog(null, new ModelElement[] { conn
+            // },
+            //                        DefaultMessagesImpl.getString("TOPRepositoryService.dependcyTile"), //$NON-NLS-1$
+            //                        DefaultMessagesImpl.getString("TOPRepositoryService.dependcyMessage", conn.getLabel())); //$NON-NLS-1$
+            // if (isOk != Dialog.OK) {
+            // retCode.setOk(Boolean.FALSE);
+            //                    retCode.setMessage("The user canceled the operation!"); //$NON-NLS-1$
+            // return retCode;
+            // }
+            // }
 
             ReloadDatabaseAction reloadDatabaseAction = new ReloadDatabaseAction(conn,
                     ReloadDatabaseProvider.RELOADDATABASE_MENUTEXT);
