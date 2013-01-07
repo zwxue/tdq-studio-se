ef98fc58e567e5b3c4ea0829ad319cad21f1bdad
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-01-07 16:59:26 +0000

    TDQ-6685 tests added (not related to std output problem though)
    git-svn-id: http://talendforge.org/svn/top/trunk@96463 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataquality.record.linkage.test/src/test/java/org/talend/dataquality/record/linkage/attribute/SoundexMatcherTest.java
@@ -0,0 +1,51 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataquality.record.linkage.attribute;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.talend.dataquality.record.linkage.constant.AttributeMatcherType;
+
+/**
+ * created by scorreia on Jan 7, 2013 Detailled comment
+ * 
+ */
+public class SoundexMatcherTest {
+
+    /**
+     * Test method for
+     * {@link org.talend.dataquality.record.linkage.attribute.SoundexMatcher#getWeight(java.lang.String, java.lang.String)}
+     * .
+     */
+    @Test
+    public void testGetWeight() {
+        SoundexMatcher soundexMatcher = new SoundexMatcher();
+        String a = "John"; //$NON-NLS-1$
+        String b = "Jon"; //$NON-NLS-1$
+        double matchingWeight = soundexMatcher.getMatchingWeight(a, b);
+        Assert.assertEquals(1.0d, matchingWeight);
+    }
+
+    /**
+     * Test method for {@link org.talend.dataquality.record.linkage.attribute.SoundexMatcher#getMatchType()}.
+     */
+    @Test
+    public void testGetMatchType() {
+        Assert.assertEquals(AttributeMatcherType.soundex, new SoundexMatcher().getMatchType());
+        Assert.assertEquals("soundex", new SoundexMatcher().getMatchType().name()); //$NON-NLS-1$
+        Assert.assertEquals("Soundex", new SoundexMatcher().getMatchType().getLabel()); //$NON-NLS-1$
+        Assert.assertEquals("Soundex", new SoundexMatcher().getMatchType().toString()); //$NON-NLS-1$
+    }
+
+}
