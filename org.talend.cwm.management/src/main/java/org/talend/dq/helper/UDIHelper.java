ace86c917cdf6a5343b2ef39f8a54f28c534689b
xqliu <xqliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-01-21 03:47:55 +0000

    merge r96951 from branch 5.2 to branch 5.1 TDQ-6713 if duplicate an UDI, needn't to set the IndicatorCategory
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_1@97017 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/helper/UDIHelper.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/helper/UDIHelper.java
@@ -401,4 +401,13 @@ public final class UDIHelper {
         return fileList;
     }
 
+    /**
+     * if the category belongs to User Defined Categories, return true, else return false.
+     * 
+     * @param category
+     * @return
+     */
+    public static boolean isUDICategory(IndicatorCategory category) {
+        return category.getName().startsWith("User Defined"); //$NON-NLS-1$
+    }
 }
