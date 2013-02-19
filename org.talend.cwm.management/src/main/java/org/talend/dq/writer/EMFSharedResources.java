bce3e1d53dd6e742feea8d0206a68bff9a90d16a
msjian <msjian@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-19 03:07:45 +0000

    merge r98205 from trunk to branch 5.2 TDQ-6856: delete checkMetadata process after click finish, because the check duplicate name is done when input name
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_2@98206 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/writer/EMFSharedResources.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/writer/EMFSharedResources.java
@@ -17,6 +17,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
@@ -37,6 +38,8 @@ import org.talend.resource.ResourceManager;
  * DOC scorreia class global comment. Detailled comment
  */
 public final class EMFSharedResources {
+
+    private static Logger log = Logger.getLogger(EMFSharedResources.class);
 
     private static EMFSharedResources instance;
 
@@ -130,7 +133,12 @@ public final class EMFSharedResources {
     }
 
     public synchronized Resource getResource(URI uri, boolean loadOnDemand) {
-        return resourceSet.getResource(uri, loadOnDemand);
+        try {
+            return resourceSet.getResource(uri, loadOnDemand);
+        } catch (Exception e) {
+            log.error(e.fillInStackTrace());
+            return null;
+        }
     }
 
     public Resource getResource(IFile file, boolean loadOnDemand) {
