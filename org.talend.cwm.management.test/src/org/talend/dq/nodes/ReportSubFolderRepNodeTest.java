c3fb3cbb0bbd18d1957a181269b93c57d391121c
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-20 01:52:00 +0000

    TDQ-5085 update the header and enable the checkstyle
    git-svn-id: http://talendforge.org/svn/top/trunk@98273 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management.test/src/org/talend/dq/nodes/ReportSubFolderRepNodeTest.java
+++ org.talend.cwm.management.test/src/org/talend/dq/nodes/ReportSubFolderRepNodeTest.java
@@ -1,25 +1,19 @@
 // ============================================================================
 //
-// Talend Community Edition
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
 //
-// Copyright (C) 2006-2013 Talend – www.talend.com
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
 package org.talend.dq.nodes;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,10 +40,6 @@ import org.talend.repository.ProjectManager;
 import org.talend.repository.model.IRepositoryNode;
 import org.talend.repository.model.IRepositoryNode.ENodeType;
 import orgomg.cwmx.analysis.informationreporting.Report;
-
-import static org.junit.Assert.*;
-
-import static org.mockito.Mockito.*;
 
 /**
  * DOC qiongli class global comment. Detailled comment <br/>
