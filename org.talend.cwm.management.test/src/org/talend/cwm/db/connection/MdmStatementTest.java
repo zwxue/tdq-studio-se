918580ea086798fa360fa2614981734dd90e1d2d
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-20 01:47:21 +0000

    TDQ-5626 TDQ-5139 Add the missing header
    git-svn-id: http://talendforge.org/svn/top/trunk@98270 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management.test/src/org/talend/cwm/db/connection/MdmStatementTest.java
+++ org.talend.cwm.management.test/src/org/talend/cwm/db/connection/MdmStatementTest.java
@@ -1,8 +1,19 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.cwm.db.connection;
 
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
