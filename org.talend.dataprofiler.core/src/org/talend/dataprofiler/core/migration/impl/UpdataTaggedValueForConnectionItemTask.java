32106a4c170d47c361e86c4a7e5710e8fd909191
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-21 01:55:47 +0000

    TDQ-6760 Sub 2: generate report error after importing: add migration task for Added tags , and remove its check in RunAnalysisAction
    git-svn-id: http://talendforge.org/svn/top/trunk@98366 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/migration/impl/UpdataTaggedValueForConnectionItemTask.java
@@ -0,0 +1,82 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.migration.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.ui.runtime.exception.ExceptionHandler;
+import org.talend.core.model.metadata.IMetadataConnection;
+import org.talend.core.model.metadata.MetadataFillFactory;
+import org.talend.core.model.metadata.builder.ConvertionHelper;
+import org.talend.core.model.metadata.builder.connection.Connection;
+import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
+import org.talend.core.model.metadata.builder.database.PluginConstant;
+import org.talend.core.model.properties.ConnectionItem;
+import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.core.model.repository.IRepositoryViewObject;
+import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.cwm.helper.TaggedValueHelper;
+import org.talend.dataprofiler.core.migration.AbstractWorksapceUpdateTask;
+
+
+/**
+ * DOC yyin  class global comment. Detailled comment
+ */
+public class UpdataTaggedValueForConnectionItemTask extends AbstractWorksapceUpdateTask {
+    /* (non-Javadoc)
+     * @see org.talend.dataprofiler.migration.IMigrationTask#getOrder()
+     */
+    public Date getOrder() {
+        return createDate(2013, 02, 20);
+    }
+
+    /* (non-Javadoc)
+     * @see org.talend.dataprofiler.migration.IMigrationTask#getMigrationTaskType()
+     */
+    public MigrationTaskType getMigrationTaskType() {
+        return MigrationTaskType.FILE;
+    }
+
+    /* (non-Javadoc)
+     * @see org.talend.dataprofiler.migration.AMigrationTask#doExecute()
+     */
+    @Override
+    protected boolean doExecute() throws Exception {
+        List<IRepositoryViewObject> allConnectionObject = ProxyRepositoryFactory.getInstance().getAll(
+                ERepositoryObjectType.METADATA_CONNECTIONS);
+
+        for (IRepositoryViewObject object : allConnectionObject) {
+            ConnectionItem connectionItem = (ConnectionItem) object.getProperty().getItem();
+            Connection dataProvider = connectionItem.getConnection();
+            if (dataProvider instanceof DatabaseConnection) {
+                DatabaseConnection dbConn = (DatabaseConnection) dataProvider;
+                if (PluginConstant.EMPTY_STRING.equals(TaggedValueHelper.getValueString(TaggedValueHelper.DB_PRODUCT_NAME,
+                        dataProvider))) {
+                    IMetadataConnection metaConnection = ConvertionHelper.convert(dbConn);
+                    dbConn = (DatabaseConnection) MetadataFillFactory.getDBInstance().fillUIConnParams(metaConnection, dbConn);
+                    if (dbConn != null) {
+                        try {
+                            ProxyRepositoryFactory.getInstance().save(connectionItem);
+                        } catch (PersistenceException e) {
+                            ExceptionHandler.process(e);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+}
