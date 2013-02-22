75fa83bea6ec403c30b475cdeb38c6625155f482
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-22 01:27:37 +0000

    merge r98479 from branch 5.2 to trunk TDQ-6546 Fix: the view count was not be added for catelog/schema in overview analysis.
    git-svn-id: http://talendforge.org/svn/top/trunk@98480 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/indicators/AbstractSchemaEvaluator.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/indicators/AbstractSchemaEvaluator.java
@@ -65,7 +65,7 @@ public abstract class AbstractSchemaEvaluator<T> extends Evaluator<T> {
     /**
      * 
      */
-    public static final char FILTER_SEP = ',';//$NON-NLS-1$
+    public static final char FILTER_SEP = ',';
 
     private DbmsLanguage dbmsLanguage;
 
@@ -236,7 +236,7 @@ public abstract class AbstractSchemaEvaluator<T> extends Evaluator<T> {
         } catch (SQLException e) {
 
             log.warn(Messages.getString("AbstractSchemaEvaluator.IndexException", //$NON-NLS-1$
-                    this.dbms().toQualifiedName(catalog, schema, table), e.getLocalizedMessage()), e); //$NON-NLS-1$
+                    this.dbms().toQualifiedName(catalog, schema, table), e.getLocalizedMessage()), e);
             // Oracle increments the number of cursors to close each time a new query is executed after this exception!
             reloadConnectionAfterException(catalog);
         }
@@ -359,7 +359,7 @@ public abstract class AbstractSchemaEvaluator<T> extends Evaluator<T> {
             statement.close();
             log.warn(e.getMessage() + " for SQL statement: " + sqlStatement); //$NON-NLS-1$
             if (log.isDebugEnabled()) {
-                log.debug(e, e); //$NON-NLS-1$
+                log.debug(e, e);
             }
             // some tables on Oracle give the following exception:
             // ORA-25191: cannot reference overflow table of an index-organized table
@@ -531,7 +531,8 @@ public abstract class AbstractSchemaEvaluator<T> extends Evaluator<T> {
             catalogIndic.setTableCount(catalogIndic.getTableCount() + tableCount);
             catalogIndic.setTableRowCount(catalogIndic.getTableRowCount() + schemaIndic.getTableRowCount());
             catalogIndic.setViewRowCount(catalogIndic.getViewRowCount() + schemaIndic.getViewRowCount());
-
+            // Added 20130221 TDQ-6546: add the missed the view count
+            catalogIndic.setViewCount(catalogIndic.getViewCount() + schemaIndic.getViewCount());
         } else if (!hasCatalog) { // has schema only
             // add it to list of indicators
             this.addToConnectionIndicator(schemaIndic);
@@ -694,8 +695,9 @@ public abstract class AbstractSchemaEvaluator<T> extends Evaluator<T> {
                             getConnection().getMetaData(), catalog, null);
                     if (schemas != null) {
                         for (Schema tdSchema : schemas) {
-                            if (tdSchema.getName().equals(schema.getName()))
+                            if (tdSchema.getName().equals(schema.getName())) {
                                 return true;
+                            }
                         }
                     }
                     // ~
