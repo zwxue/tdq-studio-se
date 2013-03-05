fb56006682a6ba9357cb00ad236eae3febe65498
scorreia <scorreia@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-03-05 13:57:17 +0000

    TDQ-6964 No match matcher added as an example in the test project
    git-svn-id: http://talendforge.org/svn/top/trunk@99182 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ test.mydistance/src/main/java/org/talend/mydistance/NoMatchDistance.java
@@ -0,0 +1,51 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+
+package org.talend.mydistance;
+
+import org.talend.dataquality.record.linkage.attribute.AbstractAttributeMatcher;
+import org.talend.dataquality.record.linkage.constant.AttributeMatcherType;
+
+/**
+ * @author scorreia
+ * 
+ * Example of Matching distance which returns 1 when two strings don't match and 0 when the strings match.
+ */
+public class NoMatchDistance extends AbstractAttributeMatcher {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataquality.record.linkage.attribute.IAttributeMatcher#getMatchType()
+     */
+    @Override
+    public AttributeMatcherType getMatchType() {
+        // a custom implementation should return this type AttributeMatcherType.custom
+        return AttributeMatcherType.custom;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.dataquality.record.linkage.attribute.IAttributeMatcher#getMatchingWeight(java.lang.String,
+     * java.lang.String)
+     */
+    @Override
+    public double getWeight(String arg0, String arg1) {
+        // Here goes the custom implementation of the matching distance between the two given strings.
+        // the algorithm should return a value between 0 and 1.
+
+        return (arg0.equalsIgnoreCase(arg1)) ? 0 : 1;
+    }
+
+}
