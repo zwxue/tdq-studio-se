6ce17b0710eac197ba2cc17bed7843642fe9e84e
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-03-26 04:15:16 +0000

    TDQ-7011 add junit for tdq-7011
    git-svn-id: http://talendforge.org/svn/top/trunk@100162 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.compare.test/src/org/talend/cwm/compare/DQStructureComparerTest.java
+++ org.talend.cwm.compare.test/src/org/talend/cwm/compare/DQStructureComparerTest.java
@@ -35,6 +35,7 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.talend.commons.utils.WorkspaceUtils;
+import org.talend.commons.utils.database.DB2ForZosDataBaseMetadata;
 import org.talend.core.database.EDatabaseTypeName;
 import org.talend.core.model.metadata.IMetadataConnection;
 import org.talend.core.model.metadata.MetadataFillFactory;
@@ -47,6 +48,7 @@ import org.talend.core.model.metadata.builder.util.MetadataConnectionUtils;
 import org.talend.core.runtime.CoreRuntimePlugin;
 import org.talend.cwm.helper.CatalogHelper;
 import org.talend.cwm.helper.ConnectionHelper;
+import org.talend.cwm.helper.SchemaHelper;
 import org.talend.utils.sql.metadata.constants.MetaDataConstants;
 import org.talend.utils.sugars.TypedReturnCode;
 import orgomg.cwm.resource.relational.Catalog;
@@ -137,9 +139,9 @@ public class DQStructureComparerTest {
             catalogNames.addAll(packageFilter);
             haveFilter = true;
         } else {
-            catalogNames.add("tbi");
-            catalogNames.add("test");
-            catalogNames.add("testtable");
+            catalogNames.add("tbi"); //$NON-NLS-1$
+            catalogNames.add("test"); //$NON-NLS-1$
+            catalogNames.add("testtable"); //$NON-NLS-1$
         }
         // ~
         // mock ResultSet
@@ -149,7 +151,7 @@ public class DQStructureComparerTest {
         } else {
             Mockito.when(mockCatalogResults.next()).thenReturn(true, true, true, false);
         }
-        Mockito.when(mockCatalogResults.getString(MetaDataConstants.TABLE_CAT.name())).thenReturn("tbi", "test", "testtable");
+        Mockito.when(mockCatalogResults.getString(MetaDataConstants.TABLE_CAT.name())).thenReturn("tbi", "test", "testtable"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         // ~mock
         // mock ResultSet
         ResultSet mockSchemaResults = Mockito.mock(ResultSet.class);
@@ -161,7 +163,7 @@ public class DQStructureComparerTest {
         // getCatalogs
 
         Mockito.when(mockDatabaseMetaData.getCatalogs()).thenReturn(mockCatalogResults);
-        Mockito.when(mockDatabaseMetaData.getDriverName()).thenReturn("don't match");
+        Mockito.when(mockDatabaseMetaData.getDriverName()).thenReturn("don't match"); //$NON-NLS-1$
         Mockito.when(mockDatabaseMetaData.getSchemas()).thenReturn(mockSchemaResults);
         // ~mock
 
@@ -222,12 +224,311 @@ public class DQStructureComparerTest {
 
     }
 
+    @Test
+    public void testGetRefreshedDataProviderForDB2ZOS() throws SQLException {
+        DatabaseConnection dbProvider = ConnectionFactory.eINSTANCE.createDatabaseConnection();
+        setJDBDB2ForZOSConnection(dbProvider);
+        List<Catalog> catalogPackageFilter = new ArrayList<Catalog>();
+        List<orgomg.cwm.objectmodel.core.Package> schemaPackageFilter = new ArrayList<orgomg.cwm.objectmodel.core.Package>();
+        // mock ReturnCode sql.Connection
+        TypedReturnCode<java.sql.Connection> returnCode = new TypedReturnCode<java.sql.Connection>(true);
+        java.sql.Connection mockSqlConn = Mockito.mock(java.sql.Connection.class);
+        returnCode.setObject(mockSqlConn);
+        // ~mock
+
+        // mock DatabaseMetaData
+        DatabaseMetaData mockDatabaseMetaData = Mockito.mock(DB2ForZosDataBaseMetadata.class);
+        Mockito.when(mockDatabaseMetaData.supportsCatalogsInIndexDefinitions()).thenReturn(true);
+        Mockito.when(mockDatabaseMetaData.supportsSchemasInIndexDefinitions()).thenReturn(true);
+
+        // initial the data of catalogs
+        List<String> schenaNames = new ArrayList<String>();
+        List<String> packageFilter = MetadataConnectionUtils.getPackageFilter(dbProvider, mockDatabaseMetaData, false);
+        boolean haveFilter = false;
+        if (packageFilter.size() > 0) {
+            schenaNames.addAll(packageFilter);
+            haveFilter = true;
+        } else {
+            schenaNames.add("tbi"); //$NON-NLS-1$
+            schenaNames.add("test"); //$NON-NLS-1$
+            schenaNames.add("testtable"); //$NON-NLS-1$
+        }
+        // ~
+        // mock ResultSet
+        ResultSet mockSchemaResults = Mockito.mock(ResultSet.class);
+        if (haveFilter) {
+            Mockito.when(mockSchemaResults.next()).thenReturn(true, false);
+        } else {
+            Mockito.when(mockSchemaResults.next()).thenReturn(true, true, true, false);
+        }
+        Mockito.when(mockSchemaResults.getString(MetaDataConstants.TABLE_SCHEM.name())).thenReturn("tbi", "test", "testtable"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        // ~mock
+        // mock ResultSet
+        ResultSet mockCatalogResults = Mockito.mock(ResultSet.class);
+        Mockito.when(mockCatalogResults.next()).thenReturn(false);
+        // ~mock
+
+        // getCatalogs
+
+        Mockito.when(mockDatabaseMetaData.getCatalogs()).thenReturn(mockCatalogResults);
+        Mockito.when(mockDatabaseMetaData.getDriverName()).thenReturn("don't match");
+        Mockito.when(mockDatabaseMetaData.getSchemas()).thenReturn(mockSchemaResults);
+        // ~mock
+
+        // mock CoreRuntimePlugin
+        CoreRuntimePlugin instanceMock = Mockito.mock(CoreRuntimePlugin.class);
+        PowerMockito.mockStatic(CoreRuntimePlugin.class);
+        Mockito.when(CoreRuntimePlugin.getInstance()).thenReturn(instanceMock);
+        Mockito.when(instanceMock.getRepositoryService()).thenReturn(null);
+        // ~CoreRuntimePlugin
+
+        // mock ExtractMetaDataUtils
+        PowerMockito.mockStatic(ExtractMetaDataUtils.class);
+        Mockito.when(ExtractMetaDataUtils.getConnectionMetadata(mockSqlConn)).thenReturn(mockDatabaseMetaData);
+        Mockito.when(ExtractMetaDataUtils.createDB2ForZosFakeDatabaseMetaData(mockSqlConn)).thenReturn(mockDatabaseMetaData);
+        Mockito.when(ExtractMetaDataUtils.getDatabaseMetaData(mockSqlConn, dbProvider)).thenCallRealMethod();
+        Mockito.when(ExtractMetaDataUtils.getDatabaseMetaData(mockSqlConn, dbProvider, false)).thenCallRealMethod();
+        Mockito.when(ExtractMetaDataUtils.getDatabaseMetaData(mockSqlConn, EDatabaseTypeName.IBMDB2ZOS.getXmlName(), false, ""))
+                .thenCallRealMethod();
+        // ~mock
+
+        // mock ConnectionHelper
+        PowerMockito.mockStatic(ConnectionHelper.class);
+        Set<MetadataTable> result = new HashSet<MetadataTable>();
+        Mockito.when(ConnectionHelper.getTables(dbProvider)).thenReturn(result);
+        Mockito.when(ConnectionHelper.addCatalogs((Collection<Catalog>) Mockito.any(), (Connection) Mockito.any()))
+                .thenCallRealMethod();
+        Mockito.when(ConnectionHelper.addPackages((Collection<Catalog>) Mockito.any(), (Connection) Mockito.any()))
+                .thenCallRealMethod();
+        Mockito.when(ConnectionHelper.addSchemas((Collection<Schema>) Mockito.any(), (Connection) Mockito.any()))
+                .thenCallRealMethod();
+        Mockito.when(ConnectionHelper.addPackages((Collection<Schema>) Mockito.any(), (Connection) Mockito.any()))
+                .thenCallRealMethod();
+        // ~mock
+
+        // mock MetadataFillFactory
+        MetadataFillFactory mockMetadataFillFactory = Mockito.mock(MetadataFillFactory.class);
+        PowerMockito.mockStatic(MetadataFillFactory.class);
+        Mockito.when(MetadataFillFactory.getDBInstance()).thenReturn(mockMetadataFillFactory);
+        Mockito.when(MetadataFillFactory.getDBInstance()).thenReturn(mockMetadataFillFactory);
+        Mockito.when(MetadataFillFactory.getDBInstance()).thenCallRealMethod();
+        Mockito.when(MetadataFillFactory.getDBInstance()).thenReturn(mockMetadataFillFactory);
+        Mockito.when(mockMetadataFillFactory.checkConnection((IMetadataConnection) Mockito.any())).thenReturn(returnCode);
+        Mockito.when(mockMetadataFillFactory.fillUIConnParams((IMetadataConnection) Mockito.any(), (Connection) Mockito.isNull()))
+                .thenReturn(dbProvider);
+        Mockito.when(
+                mockMetadataFillFactory.fillCatalogs((Connection) Mockito.any(), (DatabaseMetaData) Mockito.any(),
+                        Mockito.anyList())).thenCallRealMethod();
+        Mockito.when(
+                mockMetadataFillFactory.fillSchemas((Connection) Mockito.any(), (DatabaseMetaData) Mockito.any(),
+                        Mockito.anyList())).thenCallRealMethod();
+        List<Schema> schemaList = new ArrayList<Schema>();
+        Mockito.when(
+                mockMetadataFillFactory.fillSchemaToCatalog((Connection) Mockito.any(), (DatabaseMetaData) Mockito.any(),
+                        (Catalog) Mockito.any(), (List<String>) Mockito.any())).thenReturn(schemaList);
+        // ~mock
+
+        DQStructureComparer.getRefreshedDataProvider(dbProvider);
+
+        List<Schema> scheams = SchemaHelper.getSchemas(dbProvider.getDataPackage());
+
+        assertTrue(scheams.size() == schenaNames.size());
+        for (int index = 0; index < schenaNames.size(); index++) {
+            assertTrue(schenaNames.get(index).equalsIgnoreCase(scheams.get(index).getName()));
+        }
+
+        // TODO decide the num
+
+    }
+
+    @Test
+    public void testGetRefreshedDataProviderForDB2() throws SQLException {
+        DatabaseConnection dbProvider = ConnectionFactory.eINSTANCE.createDatabaseConnection();
+        setJDBDB2Connection(dbProvider);
+        List<Catalog> catalogPackageFilter = new ArrayList<Catalog>();
+        List<orgomg.cwm.objectmodel.core.Package> schemaPackageFilter = new ArrayList<orgomg.cwm.objectmodel.core.Package>();
+
+        // mock DatabaseMetaData
+        DatabaseMetaData mockDatabaseMetaData = Mockito.mock(DatabaseMetaData.class);
+        Mockito.when(mockDatabaseMetaData.supportsCatalogsInIndexDefinitions()).thenReturn(true);
+        Mockito.when(mockDatabaseMetaData.supportsSchemasInIndexDefinitions()).thenReturn(true);
+
+        // mock ReturnCode sql.Connection
+        TypedReturnCode<java.sql.Connection> returnCode = new TypedReturnCode<java.sql.Connection>(true);
+        java.sql.Connection mockSqlConn = Mockito.mock(java.sql.Connection.class);
+        Mockito.when(mockSqlConn.getMetaData()).thenReturn(mockDatabaseMetaData);
+
+        returnCode.setObject(mockSqlConn);
+        // ~mock
+
+        // initial the data of catalogs
+        List<String> schenaNames = new ArrayList<String>();
+        List<String> packageFilter = MetadataConnectionUtils.getPackageFilter(dbProvider, mockDatabaseMetaData, false);
+        boolean haveFilter = false;
+        if (packageFilter.size() > 0) {
+            schenaNames.addAll(packageFilter);
+            haveFilter = true;
+        } else {
+            schenaNames.add("tbi"); //$NON-NLS-1$
+            schenaNames.add("test"); //$NON-NLS-1$
+            schenaNames.add("testtable"); //$NON-NLS-1$
+        }
+        // ~
+        // mock ResultSet
+        ResultSet mockSchemaResults = Mockito.mock(ResultSet.class);
+        if (haveFilter) {
+            Mockito.when(mockSchemaResults.next()).thenReturn(true, false);
+        } else {
+            Mockito.when(mockSchemaResults.next()).thenReturn(true, true, true, false);
+        }
+        Mockito.when(mockSchemaResults.getString(MetaDataConstants.TABLE_SCHEM.name())).thenReturn("tbi", "test", "testtable"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        // ~mock
+        // mock ResultSet
+        ResultSet mockCatalogResults = Mockito.mock(ResultSet.class);
+        Mockito.when(mockCatalogResults.next()).thenReturn(false);
+        // ~mock
+
+        // getCatalogs
+
+        Mockito.when(mockDatabaseMetaData.getCatalogs()).thenReturn(mockCatalogResults);
+        Mockito.when(mockDatabaseMetaData.getDriverName()).thenReturn("don't match"); //$NON-NLS-1$
+        Mockito.when(mockDatabaseMetaData.getSchemas()).thenReturn(mockSchemaResults);
+        // ~mock
+
+        // mock CoreRuntimePlugin
+        CoreRuntimePlugin instanceMock = Mockito.mock(CoreRuntimePlugin.class);
+        PowerMockito.mockStatic(CoreRuntimePlugin.class);
+        Mockito.when(CoreRuntimePlugin.getInstance()).thenReturn(instanceMock);
+        Mockito.when(instanceMock.getRepositoryService()).thenReturn(null);
+        // ~CoreRuntimePlugin
+
+        // mock ExtractMetaDataUtils
+        PowerMockito.mockStatic(ExtractMetaDataUtils.class);
+        Mockito.when(ExtractMetaDataUtils.getConnectionMetadata(mockSqlConn)).thenReturn(mockDatabaseMetaData);
+        Mockito.when(ExtractMetaDataUtils.createDB2ForZosFakeDatabaseMetaData(mockSqlConn)).thenReturn(mockDatabaseMetaData);
+        Mockito.when(ExtractMetaDataUtils.getDatabaseMetaData(mockSqlConn, dbProvider)).thenCallRealMethod();
+        Mockito.when(ExtractMetaDataUtils.getDatabaseMetaData(mockSqlConn, dbProvider, false)).thenCallRealMethod();
+        Mockito.when(ExtractMetaDataUtils.getDatabaseMetaData(mockSqlConn, EDatabaseTypeName.IBMDB2.getXmlName(), false, ""))
+                .thenCallRealMethod();
+        // ~mock
+
+        // mock ConnectionHelper
+        PowerMockito.mockStatic(ConnectionHelper.class);
+        Set<MetadataTable> result = new HashSet<MetadataTable>();
+        Mockito.when(ConnectionHelper.getTables(dbProvider)).thenReturn(result);
+        Mockito.when(ConnectionHelper.addCatalogs((Collection<Catalog>) Mockito.any(), (Connection) Mockito.any()))
+                .thenCallRealMethod();
+        Mockito.when(ConnectionHelper.addPackages((Collection<Catalog>) Mockito.any(), (Connection) Mockito.any()))
+                .thenCallRealMethod();
+        Mockito.when(ConnectionHelper.addSchemas((Collection<Schema>) Mockito.any(), (Connection) Mockito.any()))
+                .thenCallRealMethod();
+        Mockito.when(ConnectionHelper.addPackages((Collection<Schema>) Mockito.any(), (Connection) Mockito.any()))
+                .thenCallRealMethod();
+        // ~mock
+
+        // mock MetadataFillFactory
+        MetadataFillFactory mockMetadataFillFactory = Mockito.mock(MetadataFillFactory.class);
+        PowerMockito.mockStatic(MetadataFillFactory.class);
+        Mockito.when(MetadataFillFactory.getDBInstance()).thenReturn(mockMetadataFillFactory);
+        Mockito.when(MetadataFillFactory.getDBInstance()).thenReturn(mockMetadataFillFactory);
+        Mockito.when(MetadataFillFactory.getDBInstance()).thenCallRealMethod();
+        Mockito.when(MetadataFillFactory.getDBInstance()).thenReturn(mockMetadataFillFactory);
+        Mockito.when(mockMetadataFillFactory.checkConnection((IMetadataConnection) Mockito.any())).thenReturn(returnCode);
+        Mockito.when(mockMetadataFillFactory.fillUIConnParams((IMetadataConnection) Mockito.any(), (Connection) Mockito.isNull()))
+                .thenReturn(dbProvider);
+        Mockito.when(
+                mockMetadataFillFactory.fillCatalogs((Connection) Mockito.any(), (DatabaseMetaData) Mockito.any(),
+                        Mockito.anyList())).thenCallRealMethod();
+        Mockito.when(
+                mockMetadataFillFactory.fillSchemas((Connection) Mockito.any(), (DatabaseMetaData) Mockito.any(),
+                        Mockito.anyList())).thenCallRealMethod();
+        List<Schema> schemaList = new ArrayList<Schema>();
+        Mockito.when(
+                mockMetadataFillFactory.fillSchemaToCatalog((Connection) Mockito.any(), (DatabaseMetaData) Mockito.any(),
+                        (Catalog) Mockito.any(), (List<String>) Mockito.any())).thenReturn(schemaList);
+        // ~mock
+
+        DQStructureComparer.getRefreshedDataProvider(dbProvider);
+
+        List<Schema> scheams = SchemaHelper.getSchemas(dbProvider.getDataPackage());
+
+        assertTrue(scheams.size() == schenaNames.size());
+        for (int index = 0; index < schenaNames.size(); index++) {
+            assertTrue(schenaNames.get(index).equalsIgnoreCase(scheams.get(index).getName()));
+        }
+
+        // TODO decide the num
+
+    }
+
     private boolean setJDBCMysqlConnection(DatabaseConnection dbProvider) {
         // General JDBC case
         dbProvider.setComment("");
         dbProvider.setSID("tbi");
         dbProvider.setDatasourceName("");
         dbProvider.setDatabaseType("MySQL");
+        dbProvider.setDbVersionString("MYSQL_5");
+        dbProvider.setDriverClass("org.gjt.mm.mysql.Driver");
+        dbProvider.setFileFieldName("");
+        dbProvider.setId("_9bw28cccEeGQNaw_qcyMFw");
+        dbProvider.setLabel("jdbcmysql1");
+        dbProvider.setNullChar("");
+        dbProvider.setPassword("shenze");
+        dbProvider.setPort("3306");
+        dbProvider.setServerName("");
+        dbProvider.setSqlSynthax("SQL Syntax");
+        dbProvider.setUiSchema("");
+        dbProvider.setStringQuote("");
+        dbProvider.setURL("jdbc:mysql://192.168.30.151:3306/tbi?noDatetimeStringSync=true");
+        dbProvider.setAdditionalParams("");
+        dbProvider.setUsername("shenze");
+        dbProvider.setDbmsId("mysql_id");
+        dbProvider.setProductId("MYSQL");
+        dbProvider.setDBRootPath("");
+        dbProvider.setSQLMode(false);
+        dbProvider.setContextMode(false);
+        dbProvider.setContextId("");
+        dbProvider.setContextName("");
+        return true;
+    }
+
+    private boolean setJDBDB2Connection(DatabaseConnection dbProvider) {
+        // General JDBC case
+        dbProvider.setComment("");
+        dbProvider.setSID("");
+        dbProvider.setDatasourceName("");
+        dbProvider.setDatabaseType("IBM DB2");
+        dbProvider.setDbVersionString("MYSQL_5");
+        dbProvider.setDriverClass("org.gjt.mm.mysql.Driver");
+        dbProvider.setFileFieldName("");
+        dbProvider.setId("_9bw28cccEeGQNaw_qcyMFw");
+        dbProvider.setLabel("jdbcmysql1");
+        dbProvider.setNullChar("");
+        dbProvider.setPassword("shenze");
+        dbProvider.setPort("3306");
+        dbProvider.setServerName("");
+        dbProvider.setSqlSynthax("SQL Syntax");
+        dbProvider.setUiSchema("");
+        dbProvider.setStringQuote("");
+        dbProvider.setURL("jdbc:mysql://192.168.30.151:3306/tbi?noDatetimeStringSync=true");
+        dbProvider.setAdditionalParams("");
+        dbProvider.setUsername("shenze");
+        dbProvider.setDbmsId("mysql_id");
+        dbProvider.setProductId("MYSQL");
+        dbProvider.setDBRootPath("");
+        dbProvider.setSQLMode(false);
+        dbProvider.setContextMode(false);
+        dbProvider.setContextId("");
+        dbProvider.setContextName("");
+        return true;
+    }
+
+    private boolean setJDBDB2ForZOSConnection(DatabaseConnection dbProvider) {
+        // General JDBC case
+        dbProvider.setComment("");
+        dbProvider.setSID("");
+        dbProvider.setDatasourceName("");
+        dbProvider.setDatabaseType(EDatabaseTypeName.IBMDB2ZOS.getXmlName());
         dbProvider.setDbVersionString("MYSQL_5");
         dbProvider.setDriverClass("org.gjt.mm.mysql.Driver");
         dbProvider.setFileFieldName("");
