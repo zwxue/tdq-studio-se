d354dbd7a2f66a3a24e927a7d731665685ebcd22
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-17 04:48:08 +0000

    TDQ-6791 fixed right click on the table of delimite file connection and choose 'edit schema' 、'add task' error
    git-svn-id: http://talendforge.org/svn/top/trunk@101328 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.rcp/src/org/talend/dataprofiler/rcp/TopService.java
+++ org.talend.dataprofiler.rcp/src/org/talend/dataprofiler/rcp/TopService.java
@@ -35,6 +35,7 @@ import org.talend.core.model.metadata.IMetadataTable;
 import org.talend.core.model.metadata.builder.connection.MetadataTable;
 import org.talend.core.model.properties.Item;
 import org.talend.core.model.repository.IRepositoryViewObject;
+import org.talend.core.utils.KeywordsValidator;
 import org.talend.dataprofiler.core.CorePlugin;
 import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
 
@@ -353,7 +354,7 @@ public class TopService implements ICoreService {
      */
     @Override
     public boolean isKeyword(String word) {
-        throw new UnsupportedOperationException();
+        return KeywordsValidator.isKeyword(word);
     }
 
     /*
