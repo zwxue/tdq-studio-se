a033efa8d96b46e1cda6feb6ea33f56bb0074ad0
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-18 09:44:41 +0000

    TDQ-6911Fix junits failure of trunk
    git-svn-id: http://talendforge.org/svn/top/trunk@101465 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management.test/src/org/talend/dq/helper/PropertyHelperTest.java
+++ org.talend.cwm.management.test/src/org/talend/dq/helper/PropertyHelperTest.java
@@ -12,59 +12,96 @@
 // ============================================================================
 package org.talend.dq.helper;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertFalse;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Rule;
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.talend.commons.exception.PersistenceException;
-import org.talend.commons.utils.WorkspaceUtils;
-import org.talend.core.model.properties.Property;
+import org.talend.core.context.Context;
+import org.talend.core.context.RepositoryContext;
+import org.talend.core.model.general.Project;
 import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.core.runtime.CoreRuntimePlugin;
+import org.talend.dataquality.indicators.CountsIndicator;
+import org.talend.dataquality.indicators.IndicatorsFactory;
+import org.talend.dataquality.indicators.definition.IndicatorDefinition;
 
-@PrepareForTest({ ProxyRepositoryFactory.class, WorkspaceUtils.class })
 public class PropertyHelperTest {
 
-    @Rule
-    public PowerMockRule powerMockRule = new PowerMockRule();
+    ProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
+
+    private static Project originalProject;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        Context ctx = CoreRuntimePlugin.getInstance().getContext();
+        RepositoryContext repositoryContext = (RepositoryContext) ctx.getProperty(Context.REPOSITORY_CONTEXT_KEY);
+        if (repositoryContext != null) {
+            originalProject = repositoryContext.getProject();
+        }
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        if (originalProject != null) {
+            Context ctx = CoreRuntimePlugin.getInstance().getContext();
+            RepositoryContext repositoryContext = (RepositoryContext) ctx.getProperty(Context.REPOSITORY_CONTEXT_KEY);
+            repositoryContext.setProject(originalProject);
+        }
+    }
+
+    /**
+     * DOC zshen Comment method "setUp".
+     * 
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        UnitTestBuildHelper.deleteCurrentProject(); //$NON-NLS-1$
+        UnitTestBuildHelper.createRealProject("testForPropertyHelperTDQ"); //$NON-NLS-1$
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        UnitTestBuildHelper.deleteCurrentProject();
+    }
 
     @Test
-    public void testExistDuplicateName() throws PersistenceException {
-        String newName = "a/";
-        String oldName = "ab";
-        ERepositoryObjectType type = mock(ERepositoryObjectType.class);
-        IRepositoryViewObject repObj1 = mock(IRepositoryViewObject.class);
-        Property prop1 = mock(Property.class);
-        when(repObj1.getProperty()).thenReturn(prop1);
-        when(prop1.getLabel()).thenReturn("a_");
-        when(prop1.getDisplayName()).thenReturn("a?");
-        IRepositoryViewObject repObj2 = mock(IRepositoryViewObject.class);
-        Property prop2 = mock(Property.class);
-        when(repObj2.getProperty()).thenReturn(prop2);
-        when(prop2.getDisplayName()).thenReturn("b");
-        when(prop2.getLabel()).thenReturn("b");
-        List<IRepositoryViewObject> ls = new ArrayList<IRepositoryViewObject>();
-        ls.add(repObj1);
-        ls.add(repObj2);
-        PowerMockito.mockStatic(ProxyRepositoryFactory.class);
-        ProxyRepositoryFactory proxFactory = mock(ProxyRepositoryFactory.class);
-        when(proxFactory.getAll(type, true, false)).thenReturn(ls);
-        when(ProxyRepositoryFactory.getInstance()).thenReturn(proxFactory);
-        boolean existDuplicateName = PropertyHelper.existDuplicateName(newName, oldName, type);
-        assertTrue(existDuplicateName);
-        // no duplicate name
-        newName = "c";
-        existDuplicateName = PropertyHelper.existDuplicateName(newName, oldName, type);
-        assertTrue(!existDuplicateName);
+    public void testRealExistDuplicateName() throws PersistenceException {
+        // --- create indicators
+        // Indicator
+        CountsIndicator createCountsIndicator = IndicatorsFactory.eINSTANCE.createCountsIndicator();
+        List<IRepositoryViewObject> all = null;
+        IndicatorDefinition createIndicatorDefinition = null;
+        try {
+            all = factory.getAll(ERepositoryObjectType.TDQ_SYSTEM_INDICATORS);
+            for (IRepositoryViewObject indicatorViewObject : all) {
+                if (indicatorViewObject.getLabel().equalsIgnoreCase("ROW COUNT")) {
+                    createIndicatorDefinition = (IndicatorDefinition) PropertyHelper.getModelElement(indicatorViewObject
+                            .getProperty());
+                    break;
+                }
+            }
+        } catch (PersistenceException e1) {
+            e1.printStackTrace();
+            Assert.fail(e1.getMessage());
+        }
 
+        createCountsIndicator.setIndicatorDefinition(createIndicatorDefinition);
+
+        boolean existDuplicateName = PropertyHelper.existDuplicateName("Copy of Row Count", "Row Count",
+                ERepositoryObjectType.TDQ_INDICATOR_ELEMENT);
+        assertFalse(existDuplicateName);
     }
+
 
 }
