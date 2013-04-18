e1a08f5e7f395f619fede7378ad10a26825fd6ed
msjian <msjian@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-18 11:18:36 +0000

    merge r101484 from trunk to branch 5.1 TDQ-6881: click cancel, not do a refresh for both duplicate and import dialog
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_1@101492 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/DuplicateAction.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/DuplicateAction.java
@@ -162,9 +162,8 @@ public class DuplicateAction extends Action {
                 }
             }
 
-            CorePlugin.getDefault().refreshWorkSpace();
-            // CorePlugin.getDefault().refreshDQView();
             if (duplicateObject != null) {
+                CorePlugin.getDefault().refreshWorkSpace();
                 selectAndReveal(duplicateObject);
             }
         } catch (BusinessException e) {
