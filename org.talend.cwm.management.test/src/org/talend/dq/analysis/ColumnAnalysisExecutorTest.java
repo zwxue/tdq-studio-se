76e2e48d3b758e995e43c5ab7914cb0cc844fe0b
msjian <msjian@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-05-08 03:36:03 +0000

    TDQ-7254: add a test class
    git-svn-id: http://talendforge.org/svn/top/trunk@102424 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management.test/src/org/talend/dq/analysis/ColumnAnalysisExecutorTest.java
+++ org.talend.cwm.management.test/src/org/talend/dq/analysis/ColumnAnalysisExecutorTest.java
@@ -12,20 +12,11 @@
 // ============================================================================
 package org.talend.dq.analysis;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.logging.Logger;
+import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
-import org.talend.core.model.metadata.builder.database.dburl.SupportDBUrlType;
-import org.talend.cwm.helper.PackageHelper;
-import org.talend.cwm.helper.SwitchHelpers;
 import org.talend.cwm.relational.RelationalFactory;
 import org.talend.cwm.relational.TdColumn;
 import org.talend.cwm.relational.TdTable;
@@ -35,18 +26,11 @@ import org.talend.dataquality.analysis.AnalysisFactory;
 import org.talend.dataquality.analysis.AnalysisParameters;
 import org.talend.dataquality.analysis.AnalysisResult;
 import org.talend.dataquality.analysis.ExecutionInformations;
-import org.talend.dataquality.helpers.AnalysisHelper;
-import org.talend.dq.dbms.DbmsLanguage;
-import org.talend.dq.dbms.DbmsLanguageFactory;
-import orgomg.cwm.foundation.softwaredeployment.DataManager;
 import orgomg.cwm.objectmodel.core.Package;
 
 /**
  * DOC msjian class global comment. Detailled comment
  */
-// @RunWith(PowerMockRunner.class)
-@PrepareForTest({ SupportDBUrlType.class, Logger.class, SwitchHelpers.class, PackageHelper.class, AnalysisHelper.class,
-        DbmsLanguageFactory.class })
 public class ColumnAnalysisExecutorTest {
 
     @Rule
@@ -59,17 +43,16 @@ public class ColumnAnalysisExecutorTest {
      */
     @Test
     public void testCreateSqlStatement() {
-
         Analysis analysis = AnalysisFactory.eINSTANCE.createAnalysis();
         AnalysisContext context = AnalysisFactory.eINSTANCE.createAnalysisContext();
-        
+
         analysis.setContext(context);
         TdColumn tdColumn = RelationalFactory.eINSTANCE.createTdColumn();
 
         TdTable tdTable = RelationalFactory.eINSTANCE.createTdTable();
-        tdTable.setName("tableName"); // $NON-NLS-1$
+        tdTable.setName("tableName"); //$NON-NLS-1$
         tdColumn.setOwner(tdTable);
-        tdColumn.setName("columnName"); // $NON-NLS-1$
+        tdColumn.setName("columnName"); //$NON-NLS-1$
 
         AnalysisParameters analysisPara = AnalysisFactory.eINSTANCE.createAnalysisParameters();
         analysisPara.setStoreData(false);
@@ -78,32 +61,18 @@ public class ColumnAnalysisExecutorTest {
 
         Package schema = orgomg.cwm.resource.relational.RelationalFactory.eINSTANCE.createSchema();// mock(Schema.class);
         tdTable.setNamespace(schema);
-        
+
         AnalysisResult analysisResult = AnalysisFactory.eINSTANCE.createAnalysisResult();
         ExecutionInformations info = AnalysisFactory.eINSTANCE.createExecutionInformations();
         analysisResult.setResultMetadata(info);
         analysis.setResults(analysisResult);
-        
-        PowerMockito.mockStatic(AnalysisHelper.class);
-        when(AnalysisHelper.getStringDataFilter(analysis)).thenReturn("data filter"); // $NON-NLS-1$
 
-        DataManager dataManager = mock(org.talend.core.model.metadata.builder.connection.Connection.class);
-        context.setConnection(dataManager);
-        
-        DbmsLanguage dbmsLanguage = DbmsLanguageFactory.createDbmsLanguage("Microsoft SQL Server", "9.0"); // $NON-NLS-1$
-                                                                                                           // $NON-NLS-2$
-        dbmsLanguage.setDbQuoteString("\""); // $NON-NLS-1$
-        PowerMockito.mockStatic(DbmsLanguageFactory.class);
-        when(DbmsLanguageFactory.createDbmsLanguage(dataManager)).thenReturn(dbmsLanguage);
-        
         Package catalog = orgomg.cwm.resource.relational.RelationalFactory.eINSTANCE.createCatalog();// mock(Catalog.class);
-        catalog.setName("catalogName"); // $NON-NLS-1$
+        catalog.setName("catalogName"); //$NON-NLS-1$
         schema.setNamespace(catalog);
-        
-        ColumnAnalysisExecutor columnAnalysisExecutor = new ColumnAnalysisExecutor();
-        assertEquals("SELECT \"columnName\" FROM \"catalogName\".\"dbo\".\"tableName\" WHERE data filter", // $NON-NLS-1$
-                columnAnalysisExecutor.createSqlStatement(analysis));
 
+        ColumnAnalysisExecutor columnAnalysisExecutor = new ColumnAnalysisExecutor();
+        assertEquals("SELECT columnName FROM tableName", columnAnalysisExecutor.createSqlStatement(analysis)); //$NON-NLS-1$
     }
 
 }
---
+++ org.talend.cwm.management.test/src/org/talend/dq/analysis/MultiColumnAnalysisExecutorTest.java
@@ -0,0 +1,120 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dq.analysis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.talend.cwm.relational.RelationalFactory;
+import org.talend.cwm.relational.TdColumn;
+import org.talend.cwm.relational.TdExpression;
+import org.talend.cwm.relational.TdTable;
+import org.talend.dataquality.analysis.Analysis;
+import org.talend.dataquality.analysis.AnalysisContext;
+import org.talend.dataquality.analysis.AnalysisFactory;
+import org.talend.dataquality.analysis.AnalysisParameters;
+import org.talend.dataquality.analysis.AnalysisResult;
+import org.talend.dataquality.analysis.AnalysisType;
+import org.talend.dataquality.analysis.ExecutionInformations;
+import org.talend.dataquality.indicators.DataminingType;
+import org.talend.dataquality.indicators.columnset.ColumnsetFactory;
+import org.talend.dataquality.indicators.columnset.SimpleStatIndicator;
+import org.talend.dataquality.indicators.definition.DefinitionFactory;
+import org.talend.dataquality.indicators.definition.IndicatorDefinition;
+import orgomg.cwm.objectmodel.core.Package;
+
+/**
+ * DOC msjian class global comment. Detailled comment
+ */
+public class MultiColumnAnalysisExecutorTest {
+
+    @Rule
+    public PowerMockRule powerMockRule = new PowerMockRule();
+
+    /**
+     * Test method for
+     * {@link org.talend.dq.analysis.MultiMultiColumnAnalysisExecutor#createSqlStatement(org.talend.dataquality.analysis.Analysis)}
+     * . this is test for columnset analysis
+     */
+    @Test
+    public void testCreateSqlStatement_1() {
+
+        Analysis analysis = AnalysisFactory.eINSTANCE.createAnalysis();
+        AnalysisContext context = AnalysisFactory.eINSTANCE.createAnalysisContext();
+        analysis.setContext(context);
+
+        Package catalog = orgomg.cwm.resource.relational.RelationalFactory.eINSTANCE.createCatalog();// mock(Catalog.class);
+        catalog.setName("tbi"); //$NON-NLS-1$
+
+        Package schema = orgomg.cwm.resource.relational.RelationalFactory.eINSTANCE.createSchema();// mock(Schema.class);
+        schema.setNamespace(catalog);
+
+        TdTable tdTable = RelationalFactory.eINSTANCE.createTdTable();
+        tdTable.setName("customer"); //$NON-NLS-1$
+        tdTable.setNamespace(schema);
+
+        TdColumn tdColumn = RelationalFactory.eINSTANCE.createTdColumn();
+        tdColumn.setOwner(tdTable);
+        tdColumn.setName("date_accnt_opened"); //$NON-NLS-1$
+
+        AnalysisParameters analysisPara = AnalysisFactory.eINSTANCE.createAnalysisParameters();
+        analysisPara.setStoreData(false);
+        // set analysis type is columnset
+        analysisPara.setAnalysisType(AnalysisType.COLUMN_SET);
+        analysis.setParameters(analysisPara);
+        context.getAnalysedElements().add(tdColumn);
+
+        AnalysisResult analysisResult = AnalysisFactory.eINSTANCE.createAnalysisResult();
+        ExecutionInformations info = AnalysisFactory.eINSTANCE.createExecutionInformations();
+        analysisResult.setResultMetadata(info);
+
+        SimpleStatIndicator simpleStatIndicator = ColumnsetFactory.eINSTANCE.createSimpleStatIndicator();
+
+        TdExpression expression = RelationalFactory.eINSTANCE.createTdExpression();
+        expression
+                .setBody("SELECT <%=__COLUMN_NAMES__%> FROM <%=__TABLE_NAME__%> <%=__WHERE_CLAUSE__%> GROUP BY <%=__GROUP_BY_ALIAS__%>"); //$NON-NLS-1$
+        expression.setLanguage("SQL"); //$NON-NLS-1$
+
+        IndicatorDefinition indicatorDefinition = DefinitionFactory.eINSTANCE.createIndicatorDefinition();
+        indicatorDefinition.getSqlGenericExpression().add(expression);
+
+        simpleStatIndicator.setIndicatorDefinition(indicatorDefinition);
+        simpleStatIndicator.getDateFunctions().add(""); //$NON-NLS-1$
+        simpleStatIndicator.getNumericFunctions().add(""); //$NON-NLS-1$
+
+        simpleStatIndicator.setDataminingType(DataminingType.NOMINAL);
+        simpleStatIndicator.getAnalyzedColumns().add(tdColumn);
+
+        analysisResult.getIndicators().add(simpleStatIndicator);
+        analysis.setResults(analysisResult);
+
+        MultiColumnAnalysisExecutor multiColumnAnalysisExecutor = new MultiColumnAnalysisExecutor();
+        assertEquals("", multiColumnAnalysisExecutor.createSqlStatement(analysis)); //$NON-NLS-1$
+        assertEquals(1, analysis.getResults().getIndicators().size());
+        assertEquals("SELECT date_accnt_opened,COUNT(*) FROM tbi.customer  GROUP BY date_accnt_opened", //$NON-NLS-1$
+                analysis.getResults().getIndicators().get(0).getInstantiatedExpressions("SQL").getBody()); //$NON-NLS-1$
+
+    }
+
+    /**
+     * Test method for
+     * {@link org.talend.dq.analysis.MultiMultiColumnAnalysisExecutor#createSqlStatement(org.talend.dataquality.analysis.Analysis)}
+     * . this is test for correlation analysis
+     */
+    @Test
+    public void testCreateSqlStatement_2() {
+        // TODO
+    }
+}
