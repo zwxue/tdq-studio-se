10c4fbfa0dbf86f7df040f2011b99071df520b44
qiongli <qiongli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-05-15 02:39:43 +0000

    TDQ-7272 in order to run well,don't use Powermock.and create a real project to  test.
    git-svn-id: http://talendforge.org/svn/top/trunk@102731 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/ui/action/provider/AbstractCommonActionProviderTest.java
+++ org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/ui/action/provider/AbstractCommonActionProviderTest.java
@@ -15,33 +15,68 @@ package org.talend.dataprofiler.core.ui.action.provider;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.actions.ActionContext;
+import org.jfree.util.Log;
+import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Rule;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.context.Context;
+import org.talend.core.context.RepositoryContext;
+import org.talend.core.database.EDatabaseTypeName;
+import org.talend.core.model.general.Project;
+import org.talend.core.model.metadata.builder.connection.ConnectionFactory;
+import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
+import org.talend.core.model.properties.DatabaseConnectionItem;
+import org.talend.core.model.properties.ItemState;
+import org.talend.core.model.properties.PropertiesFactory;
 import org.talend.core.model.repository.ERepositoryObjectType;
-import org.talend.dq.helper.RepositoryNodeHelper;
+import org.talend.core.model.repository.IRepositoryViewObject;
+import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.core.runtime.CoreRuntimePlugin;
+import org.talend.dataprofiler.core.helper.UnitTestBuildHelper;
+import org.talend.dataquality.analysis.Analysis;
+import org.talend.dataquality.analysis.AnalysisContext;
+import org.talend.dataquality.analysis.AnalysisFactory;
+import org.talend.dataquality.analysis.AnalysisParameters;
+import org.talend.dataquality.analysis.AnalysisResult;
+import org.talend.dataquality.analysis.ExecutionInformations;
+import org.talend.dataquality.indicators.CountsIndicator;
+import org.talend.dataquality.indicators.IndicatorsFactory;
+import org.talend.dataquality.indicators.definition.IndicatorDefinition;
+import org.talend.dataquality.properties.TDQAnalysisItem;
+import org.talend.dq.helper.PropertyHelper;
 import org.talend.dq.nodes.AnalysisRepNode;
-import org.talend.dq.nodes.DQRepositoryNode;
-import org.talend.dq.nodes.ReportRepNode;
+import org.talend.dq.nodes.DBConnectionRepNode;
+import org.talend.repository.localprovider.model.LocalRepositoryFactory;
+import org.talend.repository.model.IRepositoryNode.ENodeType;
+import org.talend.repository.model.RepositoryNode;
 
 /**
  * created by qiongli on 2012-9-18 Detailled comment
  * 
  */
-@PrepareForTest({ RepositoryNodeHelper.class })
 public class AbstractCommonActionProviderTest {
-
-    @Rule
-    public PowerMockRule powerMockRule = new PowerMockRule();
 
     private AbstractCommonActionProvider absCommonActionProvider = null;
 
     TreeSelection treeSel = null;
+
+    private static LocalRepositoryFactory repositoryFactory;
+
+    ProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
+
+    private static Project originalProject;
 
     /**
      * DOC qiongli Comment method "setUp".
@@ -51,12 +86,32 @@ public class AbstractCommonActionProviderTest {
     @Before
     public void setUp() throws Exception {
         absCommonActionProvider = new AbstractCommonActionProvider();
-
         ActionContext context = mock(ActionContext.class);
         treeSel = mock(TreeSelection.class);
         when(context.getSelection()).thenReturn(treeSel);
         absCommonActionProvider.setContext(context);
-        PowerMockito.mockStatic(RepositoryNodeHelper.class);
+        UnitTestBuildHelper.deleteCurrentProject("testForDeleteActionTDQ"); //$NON-NLS-1$
+        UnitTestBuildHelper.createRealProject("testForDeleteActionTDQ"); //$NON-NLS-1$
+    }
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        Context ctx = CoreRuntimePlugin.getInstance().getContext();
+        RepositoryContext repositoryContext = (RepositoryContext) ctx.getProperty(Context.REPOSITORY_CONTEXT_KEY);
+        if (repositoryContext != null) {
+            originalProject = repositoryContext.getProject();
+        }
+        repositoryFactory = new LocalRepositoryFactory();
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        repositoryFactory = null;
+        if (originalProject != null) {
+            Context ctx = CoreRuntimePlugin.getInstance().getContext();
+            RepositoryContext repositoryContext = (RepositoryContext) ctx.getProperty(Context.REPOSITORY_CONTEXT_KEY);
+            repositoryContext.setProject(originalProject);
+        }
     }
 
     /**
@@ -65,13 +120,13 @@ public class AbstractCommonActionProviderTest {
     @Test
     public void testisSelectionSameType_1() {
 
-        AnalysisRepNode anaNode = mock(AnalysisRepNode.class);
-        when(anaNode.getContentType()).thenReturn(ERepositoryObjectType.TDQ_ANALYSIS_ELEMENT);
-        ReportRepNode repNode = mock(ReportRepNode.class);
-        when(repNode.getContentType()).thenReturn(ERepositoryObjectType.TDQ_REPORT_ELEMENT);
+        RepositoryNode anaNode = createAnalysisNode("ana", null, false); //$NON-NLS-1$
+        setParentNode(anaNode, ERepositoryObjectType.TDQ_ANALYSIS_ELEMENT);
+        RepositoryNode connectionNode = createConnectionNode("conn", null, false); //$NON-NLS-1$
+        setParentNode(connectionNode, ERepositoryObjectType.METADATA_CONNECTIONS);
         Object objs[] = new Object[2];
         objs[0] = anaNode;
-        objs[1] = repNode;
+        objs[1] = connectionNode;
         when(treeSel.toArray()).thenReturn(objs);
         boolean flag = absCommonActionProvider.isSelectionSameType();
         assertFalse(flag);
@@ -83,28 +138,15 @@ public class AbstractCommonActionProviderTest {
     @Test
     public void testisSelectionSameType_2() {
 
-        AnalysisRepNode anaNode = mock(AnalysisRepNode.class);
-        when(anaNode.getContentType()).thenReturn(ERepositoryObjectType.TDQ_ANALYSIS_ELEMENT);
-        AnalysisRepNode anaNode2 = mock(AnalysisRepNode.class);
-        when(anaNode2.getContentType()).thenReturn(ERepositoryObjectType.TDQ_ANALYSIS_ELEMENT);
+        RepositoryNode anaNode = createAnalysisNode("ana", null, false); //$NON-NLS-1$
+        RepositoryNode anaNode2 = createAnalysisNode("ana2", null, false); //$NON-NLS-1$
+        setParentNode(anaNode, ERepositoryObjectType.TDQ_ANALYSIS_ELEMENT);
+        setParentNode(anaNode2, ERepositoryObjectType.TDQ_ANALYSIS_ELEMENT);
         Object objs[] = new Object[2];
         objs[0] = anaNode;
         objs[1] = anaNode2;
         when(treeSel.toArray()).thenReturn(objs);
         boolean flag = absCommonActionProvider.isSelectionSameType();
-        assertTrue(flag);
-        // there are two nodes in recycle bin
-        DQRepositoryNode node1 = mock(DQRepositoryNode.class);
-        when(node1.getContentType()).thenReturn(null);
-        DQRepositoryNode node2 = mock(DQRepositoryNode.class);
-        when(node2.getContentType()).thenReturn(null);
-        objs[0] = node1;
-        objs[1] = node2;
-        when(treeSel.toArray()).thenReturn(objs);
-
-        when(RepositoryNodeHelper.isStateDeleted(node1)).thenReturn(true);
-        when(RepositoryNodeHelper.isStateDeleted(node2)).thenReturn(true);
-        flag = absCommonActionProvider.isSelectionSameType();
         assertTrue(flag);
     }
 
@@ -114,18 +156,127 @@ public class AbstractCommonActionProviderTest {
     @Test
     public void testisSelectionSameType_3() {
 
-        DQRepositoryNode node1 = mock(DQRepositoryNode.class);
-        when(node1.getContentType()).thenReturn(null);
-        when(RepositoryNodeHelper.isStateDeleted(node1)).thenReturn(true);
-
-        ReportRepNode repNode = mock(ReportRepNode.class);
-        when(repNode.getContentType()).thenReturn(ERepositoryObjectType.TDQ_REPORT_ELEMENT);
-        when(RepositoryNodeHelper.isStateDeleted(repNode)).thenReturn(false);
+        RepositoryNode anaNode = createAnalysisNode("ana", null, false); //$NON-NLS-1$
+        RepositoryNode anaNode_deleted = createAnalysisNode("ana2", null, true); //$NON-NLS-1$
+        setParentNode(anaNode, ERepositoryObjectType.TDQ_ANALYSIS_ELEMENT);
+        setParentNode(anaNode, ERepositoryObjectType.TDQ_ANALYSIS_ELEMENT);
         Object objs[] = new Object[2];
-        objs[0] = node1;
-        objs[1] = repNode;
+        objs[0] = anaNode;
+        objs[1] = anaNode_deleted;
         when(treeSel.toArray()).thenReturn(objs);
         boolean flag = absCommonActionProvider.isSelectionSameType();
         assertFalse(flag);
     }
+
+    private RepositoryNode createConnectionNode(String name, IFolder folder, boolean isDelete) {
+        IPath createPath = Path.EMPTY;
+        if (folder != null) {
+            createPath = new Path(folder.getFullPath().lastSegment());
+        }
+        // connection
+        DatabaseConnection createConnection = ConnectionFactory.eINSTANCE.createDatabaseConnection();
+        createConnection.setName(name);
+        createConnection.setUsername("UserName"); //$NON-NLS-1$
+        createConnection.setPassword("Password"); //$NON-NLS-1$
+        createConnection.setURL("URL"); //$NON-NLS-1$
+        createConnection.setDatabaseType(EDatabaseTypeName.MYSQL.getXmlName());
+        createConnection.setContextMode(true);
+        // ~connection
+        DatabaseConnectionItem createDatabaseConnectionItem = PropertiesFactory.eINSTANCE.createDatabaseConnectionItem();
+
+        org.talend.core.model.properties.Property createDatabaseConnectionProperty = PropertiesFactory.eINSTANCE.createProperty();
+        createDatabaseConnectionProperty.setId(EcoreUtil.generateUUID());
+        createDatabaseConnectionProperty.setItem(createDatabaseConnectionItem);
+        createDatabaseConnectionProperty.setLabel(createConnection.getName());
+        createDatabaseConnectionItem.setProperty(createDatabaseConnectionProperty);
+        createDatabaseConnectionItem.setConnection(createConnection);
+        ItemState createItemState = PropertiesFactory.eINSTANCE.createItemState();
+        createItemState.setDeleted(isDelete);
+        createDatabaseConnectionItem.setState(createItemState);
+        IRepositoryViewObject repViewObject = null;
+        try {
+            factory.create(createDatabaseConnectionItem, createPath, false);
+            repViewObject = factory.getLastVersion(createDatabaseConnectionProperty.getId());
+        } catch (PersistenceException e) {
+            Log.error(e, e);
+            Assert.fail(e.getMessage());
+        }
+
+        RepositoryNode dbConnRepNode = new DBConnectionRepNode(repViewObject, null, ENodeType.TDQ_REPOSITORY_ELEMENT);
+        return dbConnRepNode;
+    }
+
+    private RepositoryNode createAnalysisNode(String name, IFolder folder, boolean isDelete) {
+        IPath createPath = Path.EMPTY;
+        if (folder != null) {
+            createPath = new Path(folder.getFullPath().lastSegment());
+        }
+        // connection
+        Analysis createAnalysis = AnalysisFactory.eINSTANCE.createAnalysis();
+        createAnalysis.setName(name);
+
+        // ~connection
+        TDQAnalysisItem createAnalysisItem = org.talend.dataquality.properties.PropertiesFactory.eINSTANCE
+                .createTDQAnalysisItem();
+
+        org.talend.core.model.properties.Property createAnaProperty = PropertiesFactory.eINSTANCE.createProperty();
+        createAnaProperty.setId(EcoreUtil.generateUUID());
+        createAnaProperty.setItem(createAnalysisItem);
+        createAnaProperty.setLabel(createAnalysis.getName());
+        createAnalysisItem.setProperty(createAnaProperty);
+        createAnalysisItem.setAnalysis(createAnalysis);
+        ItemState createItemState = PropertiesFactory.eINSTANCE.createItemState();
+        createItemState.setDeleted(isDelete);
+        createAnalysisItem.setState(createItemState);
+
+        // Indicator
+        CountsIndicator createCountsIndicator = IndicatorsFactory.eINSTANCE.createCountsIndicator();
+        List<IRepositoryViewObject> all = null;
+        IndicatorDefinition createIndicatorDefinition = null;
+        try {
+            all = factory.getAll(ERepositoryObjectType.TDQ_SYSTEM_INDICATORS);
+            for (IRepositoryViewObject indicatorViewObject : all) {
+                if (indicatorViewObject.getLabel().equalsIgnoreCase("ROW COUNT")) { //$NON-NLS-1$
+                    createIndicatorDefinition = (IndicatorDefinition) PropertyHelper.getModelElement(indicatorViewObject
+                            .getProperty());
+                    break;
+                }
+            }
+        } catch (PersistenceException e1) {
+            e1.printStackTrace();
+            Assert.fail(e1.getMessage());
+        }
+        createCountsIndicator.setIndicatorDefinition(createIndicatorDefinition);
+
+        AnalysisParameters createAnalysisParameters = AnalysisFactory.eINSTANCE.createAnalysisParameters();
+        AnalysisContext createAnalysisContext = AnalysisFactory.eINSTANCE.createAnalysisContext();
+        createAnalysis.setContext(createAnalysisContext);
+        AnalysisResult createAnalysisResult = AnalysisFactory.eINSTANCE.createAnalysisResult();
+        ExecutionInformations createExecutionInformations = AnalysisFactory.eINSTANCE.createExecutionInformations();
+        createAnalysisResult.setResultMetadata(createExecutionInformations);
+        createAnalysisResult.getIndicators().add(createCountsIndicator);
+        createAnalysis.setResults(createAnalysisResult);
+        createAnalysis.setParameters(createAnalysisParameters);
+
+        IRepositoryViewObject repViewObject = null;
+        try {
+            factory.create(createAnalysisItem, createPath, false);
+            repViewObject = factory.getLastVersion(createAnaProperty.getId());
+        } catch (PersistenceException e) {
+            Log.error(e, e);
+            Assert.fail(e.getMessage());
+        }
+        RepositoryNode anaRepNode = new AnalysisRepNode(repViewObject, null, ENodeType.TDQ_REPOSITORY_ELEMENT);
+        // set the contentType for anaRepNode from its parentNode
+        setParentNode(anaRepNode, ERepositoryObjectType.TDQ_ANALYSIS_ELEMENT);
+        // ~
+        return anaRepNode;
+    }
+
+    private void setParentNode(RepositoryNode repNode, ERepositoryObjectType objType) {
+        RepositoryNode parentNode = mock(RepositoryNode.class);
+        when(parentNode.getContentType()).thenReturn(objType);
+        repNode.setParent(parentNode);
+    }
+
 }
