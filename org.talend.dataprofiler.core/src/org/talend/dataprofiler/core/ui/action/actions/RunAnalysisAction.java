480f509dd9f973909fc3ee57c090ba0692fbb0d5
msjian <msjian@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-08-07 09:50:48 +0000

    TDQ-7508: fix when there have exceptions when run analysis, the project always local project
    git-svn-id: http://talendforge.org/svn/top/trunk@106399 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/RunAnalysisAction.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/action/actions/RunAnalysisAction.java
@@ -423,7 +423,12 @@ public class RunAnalysisAction extends Action implements ICheatSheetAction {
                 return false;
             }
             // ~
-            saveBeforeRun(anaEditor);
+            try {
+                saveBeforeRun(anaEditor);
+            } catch (Exception e) {
+                log.error(e, e);
+                return false;
+            }
         }
         return true;
     }
@@ -432,15 +437,21 @@ public class RunAnalysisAction extends Action implements ICheatSheetAction {
      * Save the analysis before run the analysis.
      * 
      * @param anaEditor
+     * @throws Exception
      */
-    private void saveBeforeRun(AnalysisEditor anaEditor) {
+    private void saveBeforeRun(AnalysisEditor anaEditor) throws Exception {
         IRepositoryFactory localRepository = RepositoryFactoryProvider
                 .getRepositoriyById(RepositoryConstants.REPOSITORY_LOCAL_ID);
         IRepositoryFactory oldRepository = ProxyRepositoryFactory.getInstance().getRepositoryFactoryFromProvider();
         ProxyRepositoryFactory.getInstance().setRepositoryFactoryFromProvider(localRepository);
         // This save action won't invoke any remote repository action such as svn commit. TDQ-7508
-        anaEditor.doSave(null);
-        ProxyRepositoryFactory.getInstance().setRepositoryFactoryFromProvider(oldRepository);
+        try {
+            anaEditor.doSave(null);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            ProxyRepositoryFactory.getInstance().setRepositoryFactoryFromProvider(oldRepository);
+        }
     }
 
     /**
