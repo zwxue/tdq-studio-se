3a6c8432b6a2dea3f384eba378391b0d47ded43d
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-08-22 08:32:25 +0000

    TDQ-7826: fix failed: org.talend.dq.analysis.ColumnAnalysisExecutorTest.testCreateSqlStatement, add one more method in this test. Add one test class with 5 test cases: AnalysisExecutorHelperTest, to test different db, get its table name. which is used in ColumnAnalysisExecutor.
    git-svn-id: http://talendforge.org/svn/top/trunk@106947 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management.test/src/org/talend/dq/analysis/ColumnAnalysisExecutorTest.java
+++ org.talend.cwm.management.test/src/org/talend/dq/analysis/ColumnAnalysisExecutorTest.java
@@ -12,11 +12,9 @@
 // ============================================================================
 package org.talend.dq.analysis;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.talend.cwm.relational.RelationalFactory;
 import org.talend.cwm.relational.TdColumn;
 import org.talend.cwm.relational.TdTable;
@@ -33,16 +31,13 @@ import orgomg.cwm.objectmodel.core.Package;
  */
 public class ColumnAnalysisExecutorTest {
 
-    @Rule
-    public PowerMockRule powerMockRule = new PowerMockRule();
-
     /**
-     * Test method for
+     * when there are some catelog
      * {@link org.talend.dq.analysis.ColumnAnalysisExecutor#createSqlStatement(org.talend.dataquality.analysis.Analysis)}
      * .
      */
     @Test
-    public void testCreateSqlStatement() {
+    public void testCreateSqlStatement_1() {
         Analysis analysis = AnalysisFactory.eINSTANCE.createAnalysis();
         AnalysisContext context = AnalysisFactory.eINSTANCE.createAnalysisContext();
 
@@ -72,7 +67,48 @@ public class ColumnAnalysisExecutorTest {
         schema.setNamespace(catalog);
 
         ColumnAnalysisExecutor columnAnalysisExecutor = new ColumnAnalysisExecutor();
-        assertEquals("SELECT columnName FROM tableName", columnAnalysisExecutor.createSqlStatement(analysis)); //$NON-NLS-1$
+        assertEquals("SELECT columnName FROM catalogName.tableName", columnAnalysisExecutor.createSqlStatement(analysis)); //$NON-NLS-1$
+    }
+
+    /**
+     * when there are some schema.
+     * {@link org.talend.dq.analysis.ColumnAnalysisExecutor#createSqlStatement(org.talend.dataquality.analysis.Analysis)}
+     * .
+     */
+    @Test
+    public void testCreateSqlStatement_2() {
+        Analysis analysis = AnalysisFactory.eINSTANCE.createAnalysis();
+        AnalysisContext context = AnalysisFactory.eINSTANCE.createAnalysisContext();
+
+        analysis.setContext(context);
+        TdColumn tdColumn = RelationalFactory.eINSTANCE.createTdColumn();
+
+        TdTable tdTable = RelationalFactory.eINSTANCE.createTdTable();
+        tdTable.setName("tableName"); //$NON-NLS-1$
+        tdColumn.setOwner(tdTable);
+        tdColumn.setName("columnName"); //$NON-NLS-1$
+
+        AnalysisParameters analysisPara = AnalysisFactory.eINSTANCE.createAnalysisParameters();
+        analysisPara.setStoreData(false);
+        analysis.setParameters(analysisPara);
+        context.getAnalysedElements().add(tdColumn);
+
+        AnalysisResult analysisResult = AnalysisFactory.eINSTANCE.createAnalysisResult();
+        ExecutionInformations info = AnalysisFactory.eINSTANCE.createExecutionInformations();
+        analysisResult.setResultMetadata(info);
+        analysis.setResults(analysisResult);
+
+        Package schema = orgomg.cwm.resource.relational.RelationalFactory.eINSTANCE.createSchema();// mock(Schema.class);
+        schema.setName("schemaName");
+        tdTable.setNamespace(schema);
+
+        Package catalog = orgomg.cwm.resource.relational.RelationalFactory.eINSTANCE.createCatalog();// mock(Catalog.class);
+        catalog.setName("catalogName"); //$NON-NLS-1$
+        schema.setNamespace(catalog);
+
+        ColumnAnalysisExecutor columnAnalysisExecutor = new ColumnAnalysisExecutor();
+        assertEquals(
+                "SELECT columnName FROM catalogName.schemaName.tableName", columnAnalysisExecutor.createSqlStatement(analysis)); //$NON-NLS-1$
     }
 
 }
