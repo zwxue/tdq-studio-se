f0bdf5cc27f486f679b77983dc7826585510f89f
qiongli <qiongli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-09-09 05:16:19 +0000

    TDQ-7961 delete code of real project.
    git-svn-id: http://talendforge.org/svn/top/trunk@107691 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/migration/impl/UpdateIndicatorForHiveTaskTest.java
+++ org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/migration/impl/UpdateIndicatorForHiveTaskTest.java
@@ -14,63 +14,45 @@ package org.talend.dataprofiler.core.migration.impl;
 
 import static org.junit.Assert.*;
 
-import java.io.File;
-
-import org.eclipse.core.resources.IProject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.talend.commons.utils.io.FilesUtils;
-import org.talend.dataprofiler.core.helper.UnitTestBuildHelper;
-import org.talend.utils.string.StringUtilities;
 
 /**
  * created by qiongli on 2012-9-6 Detailled comment
- * 
+ *
  */
 public class UpdateIndicatorForHiveTaskTest {
 
-    private String projectName = null;
 
-    private IProject realProject = null;
-
-    private File projectFile = null;
 
     private UpdateIndicatorForHiveTask updateHiveTask = null;
 
     /**
      * DOC qiongli Comment method "setUp".
-     * 
+     *
      * @throws java.lang.Exception
      */
     @Before
     public void setUp() throws Exception {
 
-        this.projectName = ("A" + StringUtilities.getRandomString(7)).toUpperCase(); //$NON-NLS-1$
-        this.realProject = UnitTestBuildHelper.createRealProject(this.projectName);
-        this.projectFile = this.realProject.getWorkspace().getRoot().getLocation().append(this.projectName).toFile();
         updateHiveTask = new UpdateIndicatorForHiveTask();
 
     }
 
     /**
      * DOC qiongli Comment method "tearDown".
-     * 
+     *
      * @throws java.lang.Exception
      */
     @After
     public void tearDown() throws Exception {
 
-        if (this.projectFile != null) {
-            FilesUtils.deleteFile(this.projectFile, true);
-            assertFalse(this.projectFile.exists());
-        }
-
     }
 
     /**
      * Test method update sql expression for hive connection in definition file.
-     * 
+     *
      * @throws Exception
      */
     @Test
