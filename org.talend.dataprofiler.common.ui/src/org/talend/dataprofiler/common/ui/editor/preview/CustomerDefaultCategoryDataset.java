5bfad596b451b77884b6ef35c1d68e31d23e6306
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-09-29 08:39:36 +0000

    TDQ-8035 refactor TopChartFactory code
    git-svn-id: http://talendforge.org/svn/top/trunk@108726 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.dataprofiler.common.ui/src/org/talend/dataprofiler/common/ui/editor/preview/CustomerDefaultCategoryDataset.java
@@ -0,0 +1,57 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.common.ui.editor.preview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.talend.dq.indicators.preview.table.ChartDataEntity;
+
+/**
+ * DOC Zqin class global comment. Detailled comment
+ */
+public class CustomerDefaultCategoryDataset extends DefaultCategoryDataset implements ICustomerDataset {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    private List<ChartDataEntity> dataEnities;
+
+    public CustomerDefaultCategoryDataset() {
+        dataEnities = new ArrayList<ChartDataEntity>();
+    }
+
+    @Override
+    public void addDataEntity(ChartDataEntity dataEntity) {
+        // TODO Auto-generated method stub
+        dataEnities.add(dataEntity);
+    }
+
+    @Override
+    public void addDataEntity(ChartDataEntity[] dataEntity) {
+        // TODO Auto-generated method stub
+        for (ChartDataEntity data : dataEntity) {
+            dataEnities.add(data);
+        }
+    }
+
+    @Override
+    public ChartDataEntity[] getDataEntities() {
+        // TODO Auto-generated method stub
+        return dataEnities.toArray(new ChartDataEntity[dataEnities.size()]);
+    }
+
+}
