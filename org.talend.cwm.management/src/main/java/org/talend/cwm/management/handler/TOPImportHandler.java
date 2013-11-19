5d22f2352fdb1da482732b264e146673291130e3
mzhao <mzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-11-19 09:22:23 +0000

    TDQ-7999 Adapt the recource loading API in DQ to handle references when importing DQ demo project (new system)
    git-svn-id: http://talendforge.org/svn/top/trunk@111139 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/cwm/management/handler/TOPImportHandler.java
+++ org.talend.cwm.management/src/main/java/org/talend/cwm/management/handler/TOPImportHandler.java
@@ -12,7 +12,13 @@
 // ============================================================================
 package org.talend.cwm.management.handler;
 
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.talend.core.repository.constants.FileConstants;
 import org.talend.repository.items.importexport.handlers.imports.ImportRepTypeHandler;
+import org.talend.repository.items.importexport.handlers.model.ItemRecord;
+import org.talend.repository.items.importexport.manager.ResourcesManager;
 
 public class TOPImportHandler extends ImportRepTypeHandler {
 
@@ -20,4 +26,71 @@ public class TOPImportHandler extends ImportRepTypeHandler {
         super();
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.talend.repository.items.importexport.handlers.imports.ImportBasicHandler#computeItemRecord(org.talend.repository
+     * .items.importexport.manager.ResourcesManager, org.eclipse.core.runtime.IPath)
+     */
+    @Override
+    public ItemRecord computeItemRecord(ResourcesManager resManager, IPath path) {
+        ItemRecord itemRecord = new ItemRecord(path);
+
+        // Load the DQ specific resource such as patterns and rules.
+        // Load resource only
+        super.loadResource(resManager, itemRecord);
+
+        IPath propertyPath = path.removeFileExtension().addFileExtension(FileConstants.PROPERTIES_EXTENSION);
+        ResourceSet resSet = itemRecord.getResourceSet();
+        itemRecord = new ItemRecord(propertyPath); 
+        itemRecord.setResourceSet(resSet);
+
+        // Load property resource.
+        super.computeProperty(resManager, itemRecord);
+        EcoreUtil.resolveAll(itemRecord.getResourceSet());
+
+        return itemRecord;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.talend.repository.items.importexport.handlers.imports.ImportBasicHandler#computeProperty(org.talend.repository
+     * .items.importexport.manager.ResourcesManager, org.talend.repository.items.importexport.handlers.model.ItemRecord)
+     */
+    @Override
+    protected void computeProperty(ResourcesManager manager, ItemRecord itemRecord) {
+        // Empty implementation
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.talend.repository.items.importexport.handlers.imports.ImportBasicHandler#isValidFile(org.eclipse.core.runtime
+     * .IPath)
+     */
+    @Override
+    protected boolean isValidFile(IPath path) {
+        String[] exts = getResourceNeededExtensions();
+        for (String ext : exts) {
+            if (path.lastSegment().endsWith(ext)) {
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.repository.items.importexport.handlers.imports.ImportBasicHandler#getResourceNeededExtensions()
+     */
+    @Override
+    protected String[] getResourceNeededExtensions() {
+        return new String[] { FileConstants.ITEM_EXTENSION, FileConstants.ANA_EXTENSION, FileConstants.REP_EXTENSION,
+                FileConstants.PAT_EXTENSION, FileConstants.RULE_EXTENSION, FileConstants.DEF_EXTENSION };
+    }
 }
