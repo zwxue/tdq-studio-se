bf4e47d38ffc9e735f7e84b556f948afa5a01835
qiongli <qiongli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-12-17 08:59:14 +0000

    TDQ-8446 create junit test for DependenciesHandler.removeConnDependencyAndSave(...). remove the unuseful junit test in AnalysisUtilsTest
    git-svn-id: http://talendforge.org/svn/top/trunk@112046 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/ui/utils/AnalysisUtilsTest.java
+++ org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/ui/utils/AnalysisUtilsTest.java
@@ -12,39 +12,11 @@
 // ============================================================================
 package org.talend.dataprofiler.core.ui.utils;
 
-import org.eclipse.core.runtime.Assert;
-import org.junit.Test;
-import org.talend.core.model.metadata.builder.connection.Connection;
-import org.talend.core.model.metadata.builder.connection.ConnectionFactory;
-import org.talend.cwm.dependencies.DependenciesHandler;
-import org.talend.dataquality.analysis.Analysis;
-import org.talend.dataquality.analysis.AnalysisContext;
-import org.talend.dataquality.analysis.AnalysisFactory;
 
 /**
  * created by xqliu on 2013-10-28 Detailled comment
  * 
  */
 public class AnalysisUtilsTest {
-
-    /**
-     * Test method for
-     * {@link org.talend.cwm.dependencies.DependenciesHandler#deleteConnectionDependency(org.talend.dataquality.analysis.Analysis)}
-     * .
-     */
-    @Test
-    public void testDeleteConnectionDependency() {
-        Analysis analysis = AnalysisFactory.eINSTANCE.createAnalysis();
-        AnalysisContext analysisContext = AnalysisFactory.eINSTANCE.createAnalysisContext();
-        Connection connection = ConnectionFactory.eINSTANCE.createConnection();
-
-        analysis.setContext(analysisContext);
-        analysisContext.setConnection(connection);
-
-        // if the connection's supplier dependency's size is 0, should remove the analysis context's connection also
-        DependenciesHandler.getInstance().deleteConnectionDependency(analysis);
-        Assert.isTrue(analysis.getContext().getConnection() == null);
-        Assert.isTrue(analysis.getClientDependency().isEmpty());
-    }
 
 }
