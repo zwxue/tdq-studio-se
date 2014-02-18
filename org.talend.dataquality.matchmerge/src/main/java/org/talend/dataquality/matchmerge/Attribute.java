47d9fb9912d1c2f5f3bca11eae95bda5243cce92
fhuaulme <fhuaulme@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-02-18 08:31:45 +0000

    Javadoc update for match & merge algorithm.
    git-svn-id: http://talendforge.org/svn/top/trunk@114148 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataquality.matchmerge/src/main/java/org/talend/dataquality/matchmerge/Attribute.java
+++ org.talend.dataquality.matchmerge/src/main/java/org/talend/dataquality/matchmerge/Attribute.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author adjout
+ * A attribute is a "column" in a {@link org.talend.dataquality.matchmerge.Record record}.
  */
 public class Attribute {
 
@@ -29,23 +29,42 @@ public class Attribute {
 
     private List<String> values;
 
+    /**
+     * Creates a new attribute (column name / value pair).
+     * @param label Name of the column.
+     * @param value Value associated with the column.
+     */
     public Attribute(String label, String value) {
         this.label = label;
         this.value = value;
     }
 
+    /**
+     * @return The column name.
+     */
     public String getLabel() {
         return label;
     }
 
+    /**
+     * @return The column's value (always as string, never typed).
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Set the merged column value.
+     * @param value A string value for the column. For custom types, provide a consistent representation of the data
+     *              since the string is used for match.
+     */
     public void setValue(String value) {
         this.value = value;
     }
 
+    /**
+     * @return All the values that lead to the merged value (i.e. the value returned by {@link #getValue()}).
+     */
     public List<String> getValues() {
         if (values == null) {
             values = new ArrayList<String>();
--- org.talend.dataquality.matchmerge/src/main/java/org/talend/dataquality/matchmerge/Record.java
+++ org.talend.dataquality.matchmerge/src/main/java/org/talend/dataquality/matchmerge/Record.java
@@ -16,6 +16,20 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * A record represents input data for the match & merge. It provides the following:
+ * <ul>
+ *     <li>An {@link #getId() id} (a value that uniquely identifies the record in the system it was created from: it could
+ *     be a database PK for example).</li>
+ *     <li>A list of {@link #getRelatedIds() related} ids: a list of ids in the source system this record represents.</li>
+ *     <li>A group {@link #getGroupId() id}: a optional group id (something that identifies previously grouped records
+ *     in the source system).</li>
+ *     <li>An optional {@link #getSource() source}: this can be used during merge (in case a source is dimmed more trustable
+ *     than another).</li>
+ *     <li>An optional {@link #getTimestamp() timestamp}: this can be used during merge (to select the least or most recently
+ *     modified record).</li>
+ * </ul>
+ */
 public class Record {
 
     public static final double MAX_CONFIDENCE = 1.0;
@@ -34,6 +48,13 @@ public class Record {
 
     private double confidence = MAX_CONFIDENCE;
 
+    /**
+     * Creates a empty record (no {@link org.talend.dataquality.matchmerge.Attribute attributes}).
+     *
+     * @param id        Id of the record in the source system.
+     * @param timestamp Last modification time (in milliseconds).
+     * @param source    A source name to indicate where the values come from.
+     */
     public Record(String id, long timestamp, String source) {
         this.id = id;
         this.timestamp = timestamp;
@@ -41,6 +62,14 @@ public class Record {
         this.attributes = new ArrayList<Attribute>();
     }
 
+    /**
+     * Creates a record with {@link org.talend.dataquality.matchmerge.Attribute attributes}.
+     *
+     * @param attributes Attributes for the new record.
+     * @param id         Id of the record in the source system.
+     * @param timestamp  Last modification time (in milliseconds).
+     * @param source     A source name to indicate where the values come from.
+     */
     public Record(List<Attribute> attributes, String id, long timestamp, String source) {
         this.attributes = attributes;
         this.id = id;
@@ -48,30 +77,59 @@ public class Record {
         this.source = source;
     }
 
+    /**
+     * @return <p>A group id that identifies the group this record belongs to. Match & merge algorithm considers records
+     * with a group id as "already processed" and if 2 records match <b>but</b> group id differs, match is considered
+     * as incorrect: this scenario happens when 2 records matches but were split into different groups by the user (in
+     * this case, match shouldn't bring into same group the 2 similar records).</p>
+     * <p>Returns <code>null</code> if no group is set.</p>
+     */
     public String getGroupId() {
         return groupId;
     }
 
+    /**
+     * @param groupId The new group id for this record.
+     * @see #getGroupId()
+     */
     public void setGroupId(String groupId) {
         this.groupId = groupId;
     }
 
+    /**
+     * @return The id as it is in the source system. It is <b>always</b> a string (i.e. not dependent on the actual id
+     * type).
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * @return The {@link org.talend.dataquality.matchmerge.Attribute attributes} for this record.
+     */
     public List<Attribute> getAttributes() {
         return attributes;
     }
 
+    /**
+     * @return A set of ids this record was built with.
+     * @see #setRelatedIds(java.util.Set)
+     */
     public Set<String> getRelatedIds() {
         return relatedIds;
     }
 
+    /**
+     * @param relatedIds A set of ids this record is linked to.
+     * @see #getRelatedIds()
+     */
     public void setRelatedIds(Set<String> relatedIds) {
         this.relatedIds = relatedIds;
     }
 
+    /**
+     * Removes all values (only the {@link org.talend.dataquality.matchmerge.Attribute attributes}).
+     */
     public void clear() {
         attributes.clear();
     }
@@ -101,10 +159,19 @@ public class Record {
         return id + " ( " + builder + ")";
     }
 
+    /**
+     * @return The "confidence" of the record. Confidence is always a double between 0 and 1. 1 indicates a high confidence
+     * (a certain match) and 0 indicates a very unreliable match.
+     */
     public double getConfidence() {
         return confidence;
     }
 
+    /**
+     * @param confidence The new confidence for this record. Confidence is always a double between 0 and 1. 1 indicates
+     *                   a high confidence (a certain match) and 0 indicates a very unreliable match.
+     * @throws java.lang.IllegalArgumentException If confidence > {@link #MAX_CONFIDENCE}.
+     */
     public void setConfidence(double confidence) {
         if (confidence > MAX_CONFIDENCE) {
             throw new IllegalArgumentException("Confidence value '" + confidence + "' is incorrect (>" + MAX_CONFIDENCE + ".");
@@ -112,10 +179,16 @@ public class Record {
         this.confidence = confidence;
     }
 
+    /**
+     * @return The last modification time for this record.
+     */
     public long getTimestamp() {
         return timestamp;
     }
 
+    /**
+     * @return The source associated with this record.
+     */
     public String getSource() {
         return source;
     }
