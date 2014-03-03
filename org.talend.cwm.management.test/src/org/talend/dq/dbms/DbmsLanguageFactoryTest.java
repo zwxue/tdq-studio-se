d9e353cb53c6db1b7bb4cab9f23e2a383be9ad85
xqliu <xqliu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-03-03 08:51:28 +0000

    Fix New Feature TDQ-8601 : Add CDH5 Support in Profiling https://jira.talendforge.org/browse/TDQ-8601 create a new junit test class DbmsLanguageFactoryTest
    git-svn-id: http://talendforge.org/svn/top/trunk@114589 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.cwm.management.test/src/org/talend/dq/dbms/DbmsLanguageFactoryTest.java
@@ -0,0 +1,39 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dq.dbms;
+
+import org.eclipse.core.runtime.Assert;
+import org.junit.Test;
+
+/**
+ * Test class for DbmsLanguageFactory.
+ */
+public class DbmsLanguageFactoryTest {
+
+    /**
+     * Test method for {@link org.talend.dq.dbms.DbmsLanguageFactory#isHive(java.lang.String)}.
+     */
+    @Test
+    public void testIsHive() {
+        String hive = "Hive"; //$NON-NLS-1$
+        Assert.isTrue(DbmsLanguageFactory.isHive(hive));
+        hive = "Apache Hive"; //$NON-NLS-1$
+        Assert.isTrue(DbmsLanguageFactory.isHive(hive));
+        hive = "Hadoop Hive"; //$NON-NLS-1$
+        Assert.isTrue(DbmsLanguageFactory.isHive(hive));
+        hive = "Hadoop"; //$NON-NLS-1$
+        Assert.isTrue(!DbmsLanguageFactory.isHive(hive));
+        hive = "Hadive"; //$NON-NLS-1$
+        Assert.isTrue(!DbmsLanguageFactory.isHive(hive));
+    }
+}
