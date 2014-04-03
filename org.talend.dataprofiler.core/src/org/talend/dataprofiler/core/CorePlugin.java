8bc8f2d4bd85b88a83f79924b74b1f491ec9c5f2
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-04-03 09:55:55 +0000

    TDQ-6523 make sure the preview function can be used when the connection is hive embedded mode. Backport 114576 from trunk to Branch 5.4
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_4@115949 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/CorePlugin.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/CorePlugin.java
@@ -233,8 +233,7 @@ public class CorePlugin extends AbstractUIPlugin {
         // MOD qiongli 2013-12-9,TDQ-8442,if the database type is not supported on DQ side,ruturn null.
         List<String> tdqSupportDBType = MetadataConnectionUtils.getTDQSupportDBTemplate();
         String username = JavaSqlFactory.getUsername(tdDataProvider);
-        boolean notSupport = (EDatabaseTypeName.HIVE.getXmlName().equalsIgnoreCase(dbType) && !"STANDALONE".equals(dbConn //$NON-NLS-1$
-                .getDbVersionString())) || EDatabaseTypeName.MSSQL.getDisplayName().equalsIgnoreCase(dbType)
+        boolean notSupport = EDatabaseTypeName.MSSQL.getDisplayName().equalsIgnoreCase(dbType)
                 && (username == null || PluginConstant.EMPTY_STRING.equals(username)) || !tdqSupportDBType.contains(dbType);
         if (notSupport) {
             MessageUI.openWarning(DefaultMessagesImpl.getString("CorePlugin.cantPreview")); //$NON-NLS-1$
