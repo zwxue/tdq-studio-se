40be5d7585c5d0ed1bf5c2fd2f51a7daf443f4d4
yyin <yyin@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-04-17 09:29:51 +0000

    TDQ-8360 revert: ModelElementMatchEngine default return must be true.
    git-svn-id: http://talendforge.org/svn/top/trunk@116541 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.compare/java/src/org/talend/cwm/compare/ModelElementMatchEngine.java
+++ org.talend.cwm.compare/java/src/org/talend/cwm/compare/ModelElementMatchEngine.java
@@ -91,7 +91,7 @@ public class ModelElementMatchEngine extends GenericMatchEngine {
             return true;
         }
         // MOD yyin 20130201 TDQ-6780, do not use "isURlChanged" any more.
-        return super.isSimilar(obj1, obj2);
+        return true;// Can not return: super.isSimilar(obj1, obj2); reload table list will have problems.
         // ~ 16538
     }
 
