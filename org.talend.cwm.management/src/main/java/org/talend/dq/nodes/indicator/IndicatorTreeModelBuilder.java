43e786bc94e6e0d8bc817f12ff7e5d772d023f99
msjian <msjian@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-04-17 07:51:21 +0000

    merge r116524 from trunk to branch 5.4 TDQ-8857: fix sometimes second-level items in indicator selection shell are blank. this is because the indicator definition becomes to proxy sometimes, caused can not get property to get Label.so we don't use the cache to avoid this.
    git-svn-id: http://talendforge.org/svn/top/branches/branch-5_4@116525 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.cwm.management/src/main/java/org/talend/dq/nodes/indicator/IndicatorTreeModelBuilder.java
+++ org.talend.cwm.management/src/main/java/org/talend/dq/nodes/indicator/IndicatorTreeModelBuilder.java
@@ -22,41 +22,52 @@ import org.talend.dq.nodes.indicator.type.IndicatorEnum;
  */
 public final class IndicatorTreeModelBuilder {
 
+    private static final String ADVANCED_LABEL = Messages.getString("IndicatorTreeModelBuilder.AdvancedStatistics"); //$NON-NLS-1$
+
+    private static final String FRAUD_LABEL = Messages.getString("IndicatorTreeModelBuilder.FraudStatistics"); //$NON-NLS-1$
+
+    private static final String PHONE_NUMBER_LABEL = Messages.getString("IndicatorTreeModelBuilder.PhonenumberStatistics"); //$NON-NLS-1$
+
+    private static final String SOUNDEX_LABEL = Messages.getString("IndicatorTreeModelBuilder.SoundexStatistics"); //$NON-NLS-1$
+
+    private static final String PATTERN_LABEL = Messages.getString("IndicatorTreeModelBuilder.PatternStatistics"); //$NON-NLS-1$
+
+    private static final String SUMMARY_LABEL = Messages.getString("IndicatorTreeModelBuilder.SummaryStatistics"); //$NON-NLS-1$
+
+    private static final String TEXT_LABEL = Messages.getString("IndicatorTreeModelBuilder.TextStatistics"); //$NON-NLS-1$
+
+    private static final String SIMPLE_LABEL = Messages.getString("IndicatorTreeModelBuilder.SimpleStatistics"); //$NON-NLS-1$
+
     private IndicatorTreeModelBuilder() {
     }
 
-    private static IndicatorCategoryNode[] indicatorCategoryNodes;
-
+    /**
+     * build Indicator Category. we DO NOT use the CACHE here, because sometimes, when the indicator definitions become
+     * to Proxy, we can not get its property to get Label display on the select indicator dialog(can prefer to TDQ-8857)
+     * 
+     * @return
+     */
     public static IIndicatorNode[] buildIndicatorCategory() {
-        if (indicatorCategoryNodes != null) {
-            return indicatorCategoryNodes;
-        }
         // build Basic Statistic categoryNode
-        IndicatorCategoryNode simpleCategoryNode = new IndicatorCategoryNode(
-                Messages.getString("IndicatorTreeModelBuilder.SimpleStatistics"), IndicatorEnum.CountsIndicatorEnum); //$NON-NLS-1$
+        IndicatorCategoryNode simpleCategoryNode = new IndicatorCategoryNode(SIMPLE_LABEL, IndicatorEnum.CountsIndicatorEnum);
 
         // build Text statistics categoryNode
-        IndicatorCategoryNode textCategoryNode = new IndicatorCategoryNode(
-                Messages.getString("IndicatorTreeModelBuilder.TextStatistics"), IndicatorEnum.TextIndicatorEnum); //$NON-NLS-1$
+        IndicatorCategoryNode textCategoryNode = new IndicatorCategoryNode(TEXT_LABEL, IndicatorEnum.TextIndicatorEnum);
 
         // build Summary Statistic categoryNode
-        IndicatorCategoryNode boxCategoryNode = new IndicatorCategoryNode(
-                Messages.getString("IndicatorTreeModelBuilder.SummaryStatistics"), IndicatorEnum.BoxIIndicatorEnum); //$NON-NLS-1$
+        IndicatorCategoryNode boxCategoryNode = new IndicatorCategoryNode(SUMMARY_LABEL, IndicatorEnum.BoxIIndicatorEnum);
 
         // build pattern finder categoryNode
         IndicatorEnum[] patternFinderEnums = new IndicatorEnum[] { IndicatorEnum.PatternFreqIndicatorEnum,
                 IndicatorEnum.PatternLowFreqIndicatorEnum, IndicatorEnum.DatePatternFreqIndicatorEnum };
-        IndicatorCategoryNode patternFinderCategoryNode = new IndicatorCategoryNode(
-                Messages.getString("IndicatorTreeModelBuilder.PatternStatistics"), //$NON-NLS-1$
-                patternFinderEnums);
+        IndicatorCategoryNode patternFinderCategoryNode = new IndicatorCategoryNode(PATTERN_LABEL, patternFinderEnums);
+
         // MOD mzhao 2009-03-05 build Soundex Statistic categoryNode
         IndicatorEnum[] soundexIndicatorEnums = new IndicatorEnum[] { IndicatorEnum.SoundexIndicatorEnum,
                 IndicatorEnum.SoundexLowIndicatorEnum };
-        IndicatorCategoryNode soundexCategoryNode = new IndicatorCategoryNode(
-                Messages.getString("IndicatorTreeModelBuilder.SoundexStatistics"), //$NON-NLS-1$
-                soundexIndicatorEnums);
-        IndicatorCategoryNode phoneCategoryNode = new IndicatorCategoryNode(
-                Messages.getString("IndicatorTreeModelBuilder.PhonenumberStatistics"), IndicatorEnum.PhoneNumbStatisticsIndicatorEnum); //$NON-NLS-1$
+        IndicatorCategoryNode soundexCategoryNode = new IndicatorCategoryNode(SOUNDEX_LABEL, soundexIndicatorEnums);
+        IndicatorCategoryNode phoneCategoryNode = new IndicatorCategoryNode(PHONE_NUMBER_LABEL,
+                IndicatorEnum.PhoneNumbStatisticsIndicatorEnum);
 
         // build Nominal Statistic categoryNode
         IndicatorEnum[] advanceIndicatorEnums = new IndicatorEnum[] { IndicatorEnum.ModeIndicatorEnum,
@@ -67,16 +78,13 @@ public final class IndicatorTreeModelBuilder {
                 IndicatorEnum.DateLowFrequencyIndicatorEnum, IndicatorEnum.WeekLowFrequencyIndicatorEnum,
                 IndicatorEnum.MonthLowFrequencyIndicatorEnum, IndicatorEnum.QuarterLowFrequencyIndicatorEnum,
                 IndicatorEnum.YearLowFrequencyIndicatorEnum, IndicatorEnum.BinLowFrequencyIndicatorEnum };
-        IndicatorCategoryNode advanceCategoryNode = new IndicatorCategoryNode(
-                Messages.getString("IndicatorTreeModelBuilder.AdvancedStatistics"), advanceIndicatorEnums); //$NON-NLS-1$
+        IndicatorCategoryNode advanceCategoryNode = new IndicatorCategoryNode(ADVANCED_LABEL, advanceIndicatorEnums);
 
         // Added yyin 20120827, TDQ-5076, build Fraud Detection category and SIndicator
         IndicatorEnum[] fraudIndicatorEnums = new IndicatorEnum[] { IndicatorEnum.BenfordLawFrequencyIndicatorEnum };
-        IndicatorCategoryNode fraudCategoryNode = new IndicatorCategoryNode(
-                Messages.getString("IndicatorTreeModelBuilder.FraudStatistics"), fraudIndicatorEnums); //$NON-NLS-1$
+        IndicatorCategoryNode fraudCategoryNode = new IndicatorCategoryNode(FRAUD_LABEL, fraudIndicatorEnums);
 
-        indicatorCategoryNodes = new IndicatorCategoryNode[] { simpleCategoryNode, textCategoryNode, boxCategoryNode,
-                advanceCategoryNode, patternFinderCategoryNode, soundexCategoryNode, phoneCategoryNode, fraudCategoryNode };
-        return indicatorCategoryNodes;
+        return new IndicatorCategoryNode[] { simpleCategoryNode, textCategoryNode, boxCategoryNode, advanceCategoryNode,
+                patternFinderCategoryNode, soundexCategoryNode, phoneCategoryNode, fraudCategoryNode };
     }
 }
