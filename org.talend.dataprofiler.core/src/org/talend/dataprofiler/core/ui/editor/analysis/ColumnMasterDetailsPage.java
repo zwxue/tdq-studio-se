e788fdd6ef09bcd429eed555e3a06d3761b2afe5
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-05-07 10:26:00 +0000

    Fix Bug TDQ-8908 : The change can not be commited Immediately on remote project https://jira.talendforge.org/browse/TDQ-8908 some svn error make me must remove this project then down load it again from svn, so commite it later
    git-svn-id: http://talendforge.org/svn/top/trunk@117174 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/analysis/ColumnMasterDetailsPage.java
+++ org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/analysis/ColumnMasterDetailsPage.java
@@ -16,7 +16,6 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
@@ -53,7 +52,6 @@ import org.talend.core.model.metadata.builder.connection.Connection;
 import org.talend.core.model.metadata.builder.connection.MetadataColumn;
 import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.repository.model.repositoryObject.MetadataXmlElementTypeRepositoryObject;
-import org.talend.cwm.dependencies.DependenciesHandler;
 import org.talend.cwm.helper.ModelElementHelper;
 import org.talend.cwm.helper.SwitchHelpers;
 import org.talend.cwm.helper.TaggedValueHelper;
@@ -731,16 +729,8 @@ public class ColumnMasterDetailsPage extends AbstractAnalysisMetadataPage implem
         ReturnCode saved = new ReturnCode(false);
         this.nameText.setText(analysisHandler.getName());
         // TDQ-5581,if has removed emlements(patten/udi),should remove dependency each other before saving.
-        HashSet<ModelElement> removedElements = treeViewer.getRemovedElements();
-        if (!removedElements.isEmpty()) {
-            DependenciesHandler.getInstance().removeDependenciesBetweenModels(analysis,
-                    new ArrayList<ModelElement>(removedElements));
-        }
         // MOD yyi 2012-02-08 TDQ-4621:Explicitly set true for updating dependencies.
         saved = ElementWriterFactory.getInstance().createAnalysisWrite().save(analysisItem, true);
-        if (saved.isOk() && !removedElements.isEmpty()) {
-            saveRemovedElements();
-        }
         // MOD yyi 2012-02-03 TDQ-3602:Avoid to rewriting all analyzes after saving, no reason to update all analyzes
         // which is depended in the referred connection.
         // Extract saving log function.
