83575f3ba0745aa5ed06f2dcdfeb642b9b61b8dd
zshen-talend <zshen@talend.com>
2014-09-03 17:46:52 +0800

    TDQ-8788 Add mapDB and pagination libraries.

---
+++ main/plugins/net.sourceforge.sqlexplorer/src/net/sourceforge/sqlexplorer/dataset/mapdb/MapDBSetDataSet.java
@@ -0,0 +1,110 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package net.sourceforge.sqlexplorer.dataset.mapdb;
+
+import java.util.List;
+
+import net.sourceforge.sqlexplorer.Messages;
+import net.sourceforge.sqlexplorer.dataset.DataSet;
+import net.sourceforge.sqlexplorer.dataset.DataSetRow;
+
+import org.talend.commons.MapDB.utils.DBSet;
+
+/**
+ * created by talend on Aug 29, 2014 Detailled comment
+ * 
+ */
+public class MapDBSetDataSet extends MapDBDataSet {
+
+    protected DBSet<Object> dataSet = null;
+
+    /**
+     * DOC talend MapDBSetDataSet constructor comment.
+     * 
+     * @param columnLabels
+     * @param data
+     */
+    public MapDBSetDataSet(String[] columnLabels, Comparable[][] data, int pageSize) {
+        super(columnLabels, data, pageSize);
+    }
+
+    public MapDBSetDataSet(String[] columnLabels, DBSet<Object> imputDBSet, int pageSize) {
+        super(columnLabels, new Comparable[0][0], pageSize);
+        this.dataSet = imputDBSet;
+        iterator = dataSet.iterator();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sourceforge.sqlexplorer.dataset.DataSet#getRowCount()
+     */
+    @Override
+    public int getRowCount() {
+        if (dataSet != null) {
+            return dataSet.size();
+        } else {
+            return super.getRowCount();
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sourceforge.sqlexplorer.dataset.DataSet#getRow(int)
+     */
+    @Override
+    public DataSetRow getRow(int index) {
+        DataSetRow returnDataSetRow = null;
+        if (iterator == null) {
+            return super.getRow(index);
+        } else {
+            if (index < 0 || index >= dataMap.size()) {
+                throw new IndexOutOfBoundsException(Messages.getString("DataSet.errorIndexOutOfRange") + index);
+            }
+            if (currentIndex > index) {
+                iterator = dataSet.iterator();
+                currentIndex = 0;
+            }
+            while (currentIndex < index) {
+                iterator.next();
+                currentIndex++;
+            }
+            Object currentData = iterator.next();
+            currentIndex++;
+            Comparable[] comparable = new Comparable[1];
+            comparable[1] = (Comparable) currentData;
+            returnDataSetRow = new DataSetRow(this, comparable);
+            return returnDataSetRow;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sourceforge.sqlexplorer.dataset.mapdb.TalendDataSet#getCurrentPageDataSet()
+     */
+    @Override
+    public DataSet getCurrentPageDataSet() {
+        Comparable[][] compareArray = new Comparable[(int) (endIndex - startIndex)][this.getColumns().length];
+        List<Object[]> subList = this.dataSet.subList(startIndex, endIndex, null);
+        for (int i = 0; i < endIndex - startIndex; i++) {
+            Object[] objArray = subList.get(i);
+            for (int j = 0; j < this.getColumns().length; j++) {
+                compareArray[i][j] = (Comparable) objArray[j];
+            }
+        }
+        return new DataSet(this.columnHeads, compareArray);
+    }
+
+}
---
+++ main/plugins/org.talend.libraries.mapdb/src/org/talend/commons/MapDB/utils/TalendSerializerBase.java
@@ -0,0 +1,100 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.commons.MapDB.utils;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import org.mapdb.SerializerBase;
+
+/**
+ * created by talend on Aug 6, 2014 Detailled comment
+ * 
+ */
+public class TalendSerializerBase extends SerializerBase implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 8375426567087046212L;
+
+    static final int TALEND_NULL = 181;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.mapdb.SerializerBase#serialize(java.io.DataOutput, java.lang.Object)
+     */
+    @Override
+    public void serialize(DataOutput out, Object obj) throws IOException {
+        // normal case
+        Object newObj = obj;
+        if (TupleEmpty.class.isInstance(obj)) {
+            out.write(TALEND_NULL);
+            return;
+        }
+        if (Timestamp.class.isInstance(obj)) {
+            newObj = obj.toString();
+        }
+        super.serialize(out, newObj);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.mapdb.SerializerBase#serialize(java.io.DataOutput, java.lang.Object,
+     * org.mapdb.SerializerBase.FastArrayList)
+     */
+    @Override
+    public void serialize(DataOutput out, Object obj, FastArrayList<Object> objectStack) throws IOException {
+        // arrayList case
+        Object newObj = obj;
+        if (TupleEmpty.class.isInstance(obj)) {
+            newObj = null;
+        }
+        if (Timestamp.class.isInstance(obj)) {
+            newObj = obj.toString();
+        }
+        super.serialize(out, newObj, objectStack);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.mapdb.SerializerBase#serializeUnknownObject(java.io.DataOutput, java.lang.Object,
+     * org.mapdb.SerializerBase.FastArrayList)
+     */
+    @Override
+    protected void serializeUnknownObject(DataOutput out, Object obj, FastArrayList<Object> objectStack) throws IOException {
+        Object newObj = obj.toString();
+        super.serialize(out, newObj, objectStack);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.mapdb.SerializerBase#deserializeUnknownHeader(java.io.DataInput, int,
+     * org.mapdb.SerializerBase.FastArrayList)
+     */
+    @Override
+    protected Object deserializeUnknownHeader(DataInput is, int head, FastArrayList<Object> objectStack) throws IOException {
+        if (TALEND_NULL == head) {
+            return new TupleEmpty();
+        }
+        return super.deserializeUnknownHeader(is, head, objectStack);
+    }
+
+}
