f30a7b3a0b543bf1198c8cde7fa30a3d34683c35
zshen-talend <zshen@talend.com>
2014-10-21 15:14:51 +0800

    fixed work item TDQ-9484 Create factory for mapDB and Validation for the indicator of columnSet analysis

---
+++ main/plugins/org.talend.dataquality/src/org/talend/dataquality/indicators/datavalidation/DataValidationImpl.java
@@ -0,0 +1,37 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataquality.indicators.datavalidation;
+
+import org.talend.cwm.indicator.DataValidation;
+
+/**
+ * created by talend on Oct 17, 2014 Detailled comment
+ * 
+ */
+public class DataValidationImpl implements DataValidation {
+
+    DataValidationImpl() {
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.cwm.indicator.DataValidation#isValid(java.lang.Object)
+     */
+    @Override
+    public boolean isValid(Object inputData) {
+        return false;
+    }
+
+}
