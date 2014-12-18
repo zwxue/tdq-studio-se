9b87b1d828db24fc358efe2e51c14d0266d2b5e2
xqliu <xqliu@talend.com>
2014-12-18 16:35:21 +0800

    TDQ-9536 add a new plugin "org.talend.dataprofiler.top.chart"

---
+++ main/plugins/org.talend.dataprofiler.top.chart/src/org/talend/dataprofiler/chart/TOPChartPlugin.java
@@ -0,0 +1,58 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.chart;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * created by yyin on 2014-12-24 Detailled comment
+ * 
+ */
+public class TOPChartPlugin extends AbstractUIPlugin {
+
+    private static final Log _logger = LogFactory.getLog(TOPChartPlugin.class);
+
+    private static final String PLUGIN_ID = "org.talend.dataprofiler.top.chart"; //$NON-NLS-1$
+
+    private static TOPChartPlugin plugin;
+
+    private BundleContext bundleContext;
+
+    /**
+     * Initialises the Plugin
+     */
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        this.bundleContext = context;
+        plugin = this;
+    }
+
+    public void error(String message) {
+        getLog().log(new Status(IStatus.ERROR, PLUGIN_ID, IStatus.ERROR, String.valueOf(message), null));
+        _logger.error(message);
+    }
+
+    public BundleContext getBundleContext() {
+        return this.bundleContext;
+    }
+
+    public static TOPChartPlugin getDefault() {
+        return plugin;
+    }
+}
---
+++ main/plugins/org.talend.dataprofiler.top.chart/src/org/talend/dataprofiler/chart/util/TalendChartComposite.java
@@ -0,0 +1,73 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.chart.util;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.jfree.chart.JFreeChart;
+import org.jfree.experimental.chart.swt.ChartComposite;
+
+/**
+ * created by talend on Dec 26, 2012 Detailled comment use this class instead of ChartComposite will avoid "no more
+ * handle" in useful
+ * 
+ */
+public class TalendChartComposite extends ChartComposite {
+
+    private Menu popupMenu;
+
+    /**
+     * DOC talend TalendChartComposite constructor comment.
+     * 
+     * @param comp
+     * @param style
+     * @param chart
+     * @param useBuffer
+     */
+    public TalendChartComposite(Composite comp, int style, JFreeChart chart, boolean useBuffer) {
+        super(comp, style, chart, useBuffer);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.jfree.experimental.chart.swt.ChartComposite#createPopupMenu(boolean, boolean, boolean, boolean)
+     */
+    @Override
+    protected Menu createPopupMenu(boolean properties, boolean save, boolean print, boolean zoom) {
+        popupMenu = super.createPopupMenu(properties, save, print, zoom);
+        return popupMenu;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.jfree.experimental.chart.swt.ChartComposite#dispose()
+     * 
+     * Override this dispose is because of ChartComposite don't dispose some default menuItem. To avoid no more handle
+     */
+    @Override
+    public void dispose() {
+        super.dispose();
+        if (popupMenu == null) {
+            return;
+        }
+        for (MenuItem item : popupMenu.getItems()) {
+            item.removeSelectionListener(this);
+            item.dispose();
+        }
+        popupMenu.dispose();
+    }
+
+}
