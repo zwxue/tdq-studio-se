7ac205952861dc98d56963dff4d1cba45efc2255
yyin-talend <yyin@talend.com>
2015-05-05 17:57:11 +0800

    TDQ-9605 fix conflict

---
+++ main/plugins/org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/wizard/database/CreateHiveTableStep2page.java
@@ -0,0 +1,59 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.wizard.database;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.widgets.Composite;
+import org.talend.core.model.properties.ConnectionItem;
+import org.talend.metadata.managment.ui.wizard.AbstractForm;
+import org.talend.repository.hdfs.ui.HDFSSchemaForm;
+import org.talend.repository.model.hdfs.HDFSConnection;
+
+/**
+ * created by yyin on 2015年4月28日 Detailled comment
+ *
+ */
+public class CreateHiveTableStep2page extends WizardPage {
+
+    private ConnectionItem connectionItem;
+
+    private HDFSConnection temConnection;
+
+    private HDFSSchemaForm schemaForm;
+
+    public CreateHiveTableStep2page(ConnectionItem connectionItem, HDFSConnection temConnection) {
+        super("CreateHiveTableStep2page"); //$NON-NLS-1$
+        this.connectionItem = connectionItem;
+        this.temConnection = temConnection;
+    }
+
+    public void createControl(final Composite parent) {
+        schemaForm = new HDFSSchemaForm(parent, connectionItem, null, this, temConnection);
+        schemaForm.setReadOnly(false);
+        AbstractForm.ICheckListener listener = new AbstractForm.ICheckListener() {
+
+            public void checkPerformed(final AbstractForm source) {
+                CreateHiveTableStep2page.this.setPageComplete(true);
+                if (source.isStatusOnError()) {
+                    setErrorMessage(source.getStatus());
+                } else {
+                    CreateHiveTableStep2page.this.schemaForm.setButtonsVisibility(true);
+                    setErrorMessage(null);
+                    setMessage(source.getStatus(), source.getStatusLevel());
+                }
+            }
+        };
+        schemaForm.setListener(listener);
+        setControl(schemaForm);
+    }
+}
