6fa95bebb076b137727fa489217776c5e90599fb
mzhao-talend <mzhao@talend.com>
2015-05-28 17:43:30 +0800

    TDQ-10318 (re)implement the semantic categor recognization from SemanticAnalyzer and do a test.

--- main/plugins/org.talend.datascience.common/src/main/java/org/talend/datascience/common/inference/semantic/SemanticAnalyzer.java
+++ main/plugins/org.talend.datascience.common/src/main/java/org/talend/datascience/common/inference/semantic/SemanticAnalyzer.java
@@ -12,10 +12,13 @@
 // ============================================================================
 package org.talend.datascience.common.inference.semantic;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.talend.dataquality.semantic.recognizer.Category;
 import org.talend.dataquality.semantic.recognizer.CategoryRecognizer;
 import org.talend.dataquality.semantic.recognizer.CategoryRecognizerBuilder;
 import org.talend.dataquality.semantic.recognizer.CategoryRecognizerBuilder.Mode;
@@ -27,67 +30,128 @@ import org.talend.datascience.common.inference.ResizableList;
  */
 public class SemanticAnalyzer implements Analyzer<SemanticType> {
 
-    private final ResizableList<SemanticType> results = new ResizableList<SemanticType>(SemanticType.class);
+	private final ResizableList<SemanticType> results = new ResizableList<SemanticType>(
+			SemanticType.class);
 
-    private final Map<Integer, CategoryRecognizer> columnIdxToCategoryRecognizer = new HashMap<Integer, CategoryRecognizer>();
+	private final Map<Integer, CategoryRecognizer> columnIdxToCategoryRecognizer = new HashMap<Integer, CategoryRecognizer>();
 
-    private Mode semanticRecognizerMode = Mode.LUCENE;
+	private Mode semanticRecognizerMode = Mode.LUCENE;
 
-    private String ddPath = null;
+	private String host = "localhost";
+	private int port = 9300;
+	private String clusterName = "elasticsearch";
 
-    private String kwPath = null;
+	private String ddPath = null;
 
-    private CategoryRecognizerBuilder builder;
+	private String kwPath = null;
 
-    // Threshold of handle to be run . since the semantic inferring will require
-    // more time than expected , we only want to run the handle method on a
-    // sample with small size.
-    private int handleThreshold = 100;
+	private CategoryRecognizerBuilder builder;
 
-    private int currentHandleCursor = 1;
+	// Threshold of handle to be run . since the semantic inferring will require
+	// more time than expected , we only want to run the handle method on a
+	// sample with small size.
+	private int handleThreshold = 100;
 
-    public SemanticAnalyzer() {
-        builder = CategoryRecognizerBuilder.newBuilder();
-        builder.setMode(semanticRecognizerMode);
-    }
+	private int currentHandleCursor = 1;
 
-    public void setDdPath(String ddPath) {
-        this.ddPath = ddPath;
-    }
+	public SemanticAnalyzer() {
+		builder = CategoryRecognizerBuilder.newBuilder();
+		builder.setMode(semanticRecognizerMode);
+	}
 
-    public void setKwPath(String kwPath) {
-        this.kwPath = kwPath;
-    }
+	public void setDdPath(String ddPath) {
+		this.ddPath = ddPath;
+	}
 
-    public void setSemanticRecognizerMode(Mode semanticRecognizerMode) {
-        this.semanticRecognizerMode = semanticRecognizerMode;
-    }
+	public void setKwPath(String kwPath) {
+		this.kwPath = kwPath;
+	}
 
-    public void setHandleThreshold(int handleThreshold) {
-        this.handleThreshold = handleThreshold;
-    }
+	public void setSemanticRecognizerMode(Mode semanticRecognizerMode) {
+		this.semanticRecognizerMode = semanticRecognizerMode;
+	}
 
-    public void init() {
-        currentHandleCursor = 1;
-        columnIdxToCategoryRecognizer.clear();
-    }
+	public void setHandleThreshold(int handleThreshold) {
+		this.handleThreshold = handleThreshold;
+	}
 
-    public boolean analyze(String... record) {
-        results.resize(record.length);
-        return true;
-    }
+	public void setHost(String host) {
+		this.host = host;
+	}
 
-    public void end() {
-        for (CategoryRecognizer rn : columnIdxToCategoryRecognizer.values()) {
-            rn.reset();
-        }
-    }
+	public void setPort(int port) {
+		this.port = port;
+	}
 
-    public List<SemanticType> getResult() {
-        return results;
-    }
+	public void setClusterName(String clusterName) {
+		this.clusterName = clusterName;
+	}
 
-    public void setSemanticProperties(String ddPath, String kwPath, Mode searchMode) {
+	public void init() {
+		currentHandleCursor = 1;
+		columnIdxToCategoryRecognizer.clear();
+		results.clear();
+		// init semantic builder
+		if (Mode.ELASTIC_SEARCH == semanticRecognizerMode) {
+			builder = builder.host(host).port(port).cluster(clusterName);
+		} else {
+			// Lucene mode
+			builder = builder.ddPath(ddPath).kwPath(kwPath);
+		}
+		builder.setMode(semanticRecognizerMode);
+	}
 
-    }
+	public boolean analyze(String... record) {
+		results.resize(record.length);
+		resizeCategoryRecognizer(record);
+
+		boolean isHanded = true;
+		if (currentHandleCursor <= handleThreshold) {
+			for (int i = 0; i < record.length; i++) {
+				columnIdxToCategoryRecognizer.get(i).process(record[i]);
+			}
+			currentHandleCursor++;
+		}
+		return isHanded;
+	}
+
+	private void resizeCategoryRecognizer(String[] record) {
+		if (columnIdxToCategoryRecognizer.size() > 0) {
+			// already resized
+			return;
+		}
+		for (int idx = 0; idx < record.length; idx++) {
+			CategoryRecognizer recognizer = builder.build();
+			columnIdxToCategoryRecognizer.put(idx, recognizer);
+		}
+	}
+
+	public void end() {
+		for (CategoryRecognizer rn : columnIdxToCategoryRecognizer.values()) {
+			rn.reset();
+		}
+	}
+
+	public List<SemanticType> getResult() {
+		Iterator<Integer> colIdxIterator = columnIdxToCategoryRecognizer
+				.keySet().iterator();
+		while (colIdxIterator.hasNext()) {
+			Integer colIdx = colIdxIterator.next();
+			Collection<Category> result = columnIdxToCategoryRecognizer.get(
+					colIdx).getResult();
+			for (Category semCategory : result) {
+				results.get(colIdx).putSemanticNameToCount(
+						semCategory.getCategoryName(), semCategory.getCount());
+			}
+		}
+		return results;
+	}
+
+	public void setSemanticProperties(String ddPath, String kwPath,
+			Mode searchMode) {
+		this.ddPath = ddPath;
+		this.kwPath = kwPath;
+		this.semanticRecognizerMode = searchMode;
+	}
+
 }
--- main/plugins/org.talend.datascience.common/src/test/java/org/talend/datascience/common/inference/semantic/SemanticAnalyzerTest.java
+++ main/plugins/org.talend.datascience.common/src/test/java/org/talend/datascience/common/inference/semantic/SemanticAnalyzerTest.java
@@ -1,50 +1,89 @@
 package org.talend.datascience.common.inference.semantic;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.talend.dataquality.semantic.recognizer.CategoryRecognizerBuilder.Mode;
 import org.talend.datascience.common.inference.AnalyzerTest;
 
 /**
- * This test is ignored for the time being because the dictionary path and key word path is hard coded, they should be
- * replaced later by elastic search server.
+ * This test is ignored for the time being because the dictionary path and key
+ * word path is hard coded, they should be replaced later by elastic search
+ * server.
  * 
  * @author zhao
  *
  */
-@Ignore
 public class SemanticAnalyzerTest extends AnalyzerTest {
 
-    public static final String ddPath = "/home/zhao/Talend/codebase/GIT/tdq-siq/org.talend.dataquality.semantic/luceneIdx/dictionary";
+	public static final String ddPath = AnalyzerTest.class.getResource(
+			"luceneIdx/dictionary").getPath();
+	public static final String kwPath = AnalyzerTest.class.getResource(
+			"luceneIdx/keyword").getPath();
 
-    public static final String kwPath = "/home/zhao/Talend/codebase/GIT/tdq-siq/org.talend.dataquality.semantic/luceneIdx/keyword";
+	SemanticAnalyzer semanticAnalyzer = null;
 
-    SemanticAnalyzer semanticAnalyzer = null;
+	private Mode mode = Mode.LUCENE;
 
-    @Before
-    public void setUp() throws Exception {
-        semanticAnalyzer = new SemanticAnalyzer();
-        semanticAnalyzer.setDdPath(ddPath);
-        semanticAnalyzer.setKwPath(kwPath);
-        semanticAnalyzer.setSemanticRecognizerMode(Mode.LUCENE);
-    }
+	@Before
+	public void setUp() throws Exception {
+		semanticAnalyzer = new SemanticAnalyzer();
+		semanticAnalyzer.setDdPath(ddPath);
+		semanticAnalyzer.setKwPath(kwPath);
+		semanticAnalyzer.setSemanticRecognizerMode(mode);
+		semanticAnalyzer.init();
+	}
 
-    @After
-    public void tearDown() throws Exception {
-        semanticAnalyzer.end();
-    }
+	@After
+	public void tearDown() throws Exception {
+		semanticAnalyzer.end();
+	}
 
-    @Test
-    public void testHandle() {
-        final List<String[]> records = getRecords(AnalyzerTest.class.getResourceAsStream("employee_1000.csv"));
-        for (String[] record : records) {
-            semanticAnalyzer.analyze(record);
-        }
-        // TODO Asserts on semantic types
-    }
+	@Test
+	public void testHandle() {
+		final List<String[]> records = getRecords(AnalyzerTest.class
+				.getResourceAsStream("employee_1000.csv"));
+		for (String[] record : records) {
+			semanticAnalyzer.analyze(record);
+		}
+		List<SemanticType> result = semanticAnalyzer.getResult();
+		int columnIndex = 0;
+		for (SemanticType semanticType : result) {
+			Map<String, Long> semanticNameToCountMap = semanticType
+					.getSemanticNameToCountMap();
+			Iterator<String> semanticKeyIt = semanticNameToCountMap.keySet()
+					.iterator();
+			System.out.println("");
+			while (semanticKeyIt.hasNext()) {
+				String semanticName = semanticKeyIt.next();
+				Long count = semanticNameToCountMap.get(semanticName);
+				System.out
+						.println("key: " + semanticName + ",  count:" + count);
+				if (columnIndex == 1) {
+					if ("ADDRESS".equalsIgnoreCase(semanticName)) {
+						Assert.assertEquals(1l, count, 0l);
+					} else if ("ORGANISATION_CULTURE"
+							.equalsIgnoreCase(semanticName)) {
+						Assert.assertEquals(1l, count, 0l);
+					} else if ("ORGANISATION_SPORT"
+							.equalsIgnoreCase(semanticName)) {
+						Assert.assertEquals(1l, count, 0l);
+					} else if ("UNKNOWN".equalsIgnoreCase(semanticName)) {
+						Assert.assertEquals(99l, count, 0l);
+					}
+				} else if (columnIndex == 10) {
+					Assert.assertEquals("DATE", semanticName);
+					Assert.assertEquals(100, count, 0);
+				}
+
+			}
+			columnIndex++;
+		}
+	}
 
 }
