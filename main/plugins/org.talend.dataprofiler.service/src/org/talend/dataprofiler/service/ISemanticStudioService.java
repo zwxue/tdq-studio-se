8fd70e9e7a7bf08a3887e4e350a67060ce423e3b
Sizhao Liu <sizhaoliu@talend.com>
2015-06-16 01:34:53 +0200

    TDQ-9109 remove a method from Semantic Studio service

--- main/plugins/org.talend.dataprofiler.service/src/org/talend/dataprofiler/service/ISemanticStudioService.java
+++ main/plugins/org.talend.dataprofiler.service/src/org/talend/dataprofiler/service/ISemanticStudioService.java
@@ -1,11 +1,10 @@
 package org.talend.dataprofiler.service;
 
 import org.talend.core.model.metadata.builder.connection.MetadataTable;
-import org.talend.dataquality.analysis.Analysis;
 
 public interface ISemanticStudioService {
 
-    void addAnalysisToRef(Analysis analysis);
+    // void addAnalysisToRef(Analysis analysis);
 
     int openSemanticDiscoveryWizard(MetadataTable metadataTable);
 
