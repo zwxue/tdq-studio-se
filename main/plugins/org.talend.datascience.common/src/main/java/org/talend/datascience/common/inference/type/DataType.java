713224098f98f94dfdf65e8e58ca17eb135ddf47
fangrou <fji@talend.com>
2015-08-19 22:02:32 +0200

    TDQ-10830/TDQ-10831: Create a new getSuggestedType method with a threshold as parameter

--- main/plugins/org.talend.datascience.common/src/main/java/org/talend/datascience/common/inference/type/DataType.java
+++ main/plugins/org.talend.datascience.common/src/main/java/org/talend/datascience/common/inference/type/DataType.java
@@ -33,21 +33,48 @@ public class DataType {
     }
 
     /**
-     * Get suggested type
+     * The default method for get suggested type with the Type.DOUBLE decision threshold equal 0.5
      * 
      * @return type suggested by system automatically given frequencies.
      */
     public Type getSuggestedType() {
+        return getSuggestedType(0.5);
+    }
+
+    /**
+     * 
+     * DOC fji Comment method "getSuggestedType".
+     * 
+     * @param threshold the Type.DOUBLE decision threshold for a integer/double mixed Numeric column: if the ratio of
+     * doubles and the sum of integers, doubles is greater than a given threshold, then suggest column Type.DOUBLE,
+     * otherwise Type.INTEGER. So, if the threshold equal 0.5 means that we keep only the most frequent data type, and a
+     * lower threshold means that a column with some double values will be considered as double type. E.g. for the input
+     * column: "1.2","3.5","2","6","7", if threshold=0.5 return Type.INTEGER; if threshold=0.1 return Type.DOUBLE.
+     * 
+     * @return enum Type : BOOLEAN, INTEGER, DOUBLE, STRING, DATE
+     * <p>
+     * Note: there is no Type.EMPTY returned, even Type.EMPTY is the most frequent, the second most frequent will be
+     * returned. E.g. for the input column: "","","","","1.2" return Type.DOUBLE. If a column is all empty, will return
+     * Type.STRING
+     */
+    public Type getSuggestedType(double threshold) {
         long max = 0;
+        long nbDouble = typeFrequencies.get(Type.DOUBLE) == null ? 0 : typeFrequencies.get(Type.DOUBLE);
+        long nbInteger = typeFrequencies.get(Type.INTEGER) == null ? 0 : typeFrequencies.get(Type.INTEGER);
         Type electedType = Type.STRING; // String by default
         for (Map.Entry<Type, Long> entry : typeFrequencies.entrySet()) {
+            if (Type.EMPTY.equals(entry.getKey())) {
+                continue;
+            }
             if (entry.getValue() > max) {
                 max = entry.getValue();
                 electedType = entry.getKey();
             }
         }
-        if (Type.EMPTY == electedType) {
-            return Type.STRING;
+        // column contains mostly numeric values (doubles + integers) and the ratio of double values is greater than a
+        // given threshold. For more informations, see https://jira.talendforge.org/browse/TDQ-10830
+        if (((nbDouble + nbInteger) > max) && (((double) nbDouble) / (nbInteger + nbDouble) >= threshold)) {
+            return Type.DOUBLE;
         }
         return electedType;
     }
