ef0ca2795f47879462f0d7e226804b890596d79e
qiongli <qiongli@talend.com>
2016-01-12 18:18:20 +0800

    TDQ-9837 add a junit test for a method.

--- test/plugins/org.talend.cwm.management.test/src/org/talend/dq/indicators/AbstractSchemaEvaluatorTest.java
+++ test/plugins/org.talend.cwm.management.test/src/org/talend/dq/indicators/AbstractSchemaEvaluatorTest.java
@@ -12,12 +12,20 @@
 // ============================================================================
 package org.talend.dq.indicators;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-import static org.powermock.api.support.membermodification.MemberMatcher.*;
-import static org.powermock.api.support.membermodification.MemberModifier.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
+import static org.powermock.api.support.membermodification.MemberModifier.stub;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Before;
@@ -26,9 +34,14 @@ import org.junit.Test;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.talend.core.model.metadata.builder.connection.Connection;
+import org.talend.core.model.metadata.builder.connection.ConnectionFactory;
 import org.talend.core.model.metadata.builder.database.dburl.SupportDBUrlType;
 import org.talend.cwm.helper.ConnectionHelper;
 import org.talend.dataquality.analysis.ExecutionLanguage;
+import org.talend.dataquality.indicators.Indicator;
+import org.talend.dataquality.indicators.schema.CatalogIndicator;
+import org.talend.dataquality.indicators.schema.ConnectionIndicator;
+import org.talend.dataquality.indicators.schema.SchemaFactory;
 import org.talend.dataquality.indicators.schema.SchemaIndicator;
 import org.talend.dq.dbms.DbmsLanguage;
 import org.talend.dq.dbms.DbmsLanguageFactory;
@@ -121,4 +134,74 @@ public class AbstractSchemaEvaluatorTest {
         assertEquals(catalogNameWithQuote, catalogName);
 
     }
+
+    @Test
+    /**
+     * 
+     * No mock. using  java reflect mechanism to set private variable.
+     * @throws SQLException
+     * @throws InstantiationException
+     * @throws IllegalAccessException
+     * @throws NoSuchFieldException
+     * @throws SecurityException
+     * @throws NoSuchMethodException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     */
+    public void testAddToConnectionIndicator2Parameters() throws SQLException, InstantiationException, IllegalAccessException,
+            NoSuchFieldException, SecurityException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException {
+
+        //
+        Connection dataProvider = ConnectionFactory.eINSTANCE.createConnection();
+        ConnectionIndicator connectionIndicator = SchemaFactory.eINSTANCE.createConnectionIndicator();
+        List<Indicator> indicators = new ArrayList<Indicator>();
+        indicators.add(connectionIndicator);
+        Class<ConnectionEvaluator> connEval = ConnectionEvaluator.class;
+
+        ConnectionEvaluator instance = connEval.newInstance();
+
+        Field field = connEval.getDeclaredField("dataProvider"); //$NON-NLS-1$
+        field.setAccessible(true);
+        field.set(instance, dataProvider);
+
+        Field field2 = connEval.getDeclaredField("elementIndics"); //$NON-NLS-1$
+        field2.setAccessible(true);
+        field2.set(instance, indicators);
+
+        SchemaIndicator schemaIndic1 = SchemaFactory.eINSTANCE.createSchemaIndicator();
+        schemaIndic1.setTableCount(3);
+        schemaIndic1.setViewCount(1);
+        schemaIndic1.setTableRowCount(100L);
+
+        SchemaIndicator schemaIndic2 = SchemaFactory.eINSTANCE.createSchemaIndicator();
+        schemaIndic2.setTableCount(2);
+        schemaIndic2.setViewCount(2);
+        schemaIndic2.setTableRowCount(95L);
+        CatalogIndicator catalogIndic = SchemaFactory.eINSTANCE.createCatalogIndicator();
+        catalogIndic.setSchemaCount(2);
+        // Catalog catalog = orgomg.cwm.resource.relational.RelationalFactory.eINSTANCE.createCatalog();
+        // Schema schema = orgomg.cwm.resource.relational.RelationalFactory.eINSTANCE.createSchema();
+
+        //        Method method = AbstractSchemaEvaluator.class.getDeclaredMethod("evalSchemaIndicLow", new Class[] { //$NON-NLS-1$
+        // CatalogIndicator.class, SchemaIndicator.class, Catalog.class, Schema.class, ReturnCode.class });
+        // method.invoke(instance, catalogIndic, schemaIndic, catalog, schema, new ReturnCode(true));
+
+        //        Method method = connEval.getDeclaredMethod("addToConnectionIndicator", CatalogIndicator.class, SchemaIndicator.class); //$NON-NLS-1$
+        // method.setAccessible(true);
+
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
+                instance.addToConnectionIndicator(catalogIndic, schemaIndic1);
+            } else {
+                instance.addToConnectionIndicator(catalogIndic, schemaIndic2);
+            }
+        }
+        assertEquals(connectionIndicator.getCatalogCount(), 1);
+        assertEquals(connectionIndicator.getSchemaCount(), 2);
+        assertEquals(connectionIndicator.getTableCount(), 5);
+        assertEquals(connectionIndicator.getViewCount(), 3);
+        assertEquals(connectionIndicator.getTableRowCount(), 195);
+
+    }
+
 }
