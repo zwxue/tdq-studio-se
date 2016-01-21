7915a5c0180332982b26087d7b0507cad3fbbef1
xqliu-talend <xqliu@talend.com>
2016-01-21 19:27:26 +0800

    TDQ-11542 don't set escapechar for the CSVReader when handle the report list file(.report.list)

--- main/plugins/org.talend.cwm.management/src/main/java/org/talend/dq/helper/FileUtils.java
+++ main/plugins/org.talend.cwm.management/src/main/java/org/talend/dq/helper/FileUtils.java
@@ -200,6 +200,19 @@ public final class FileUtils {
         return csvReader;
     }
 
+    /**
+     * create a default createCSVReader, the separator is \t
+     * 
+     * @param file
+     * @return
+     * @throws UnsupportedEncodingException
+     * @throws FileNotFoundException
+     */
+    public static CSVReader createCSVReader(File file) throws UnsupportedEncodingException, FileNotFoundException {
+        CSVReader csvReader = new CSVReader(new FileReader(file), CURRENT_SEPARATOR);
+        return csvReader;
+    }
+
     public static boolean isCSV(String fileExtName) {
         return CSV.equalsIgnoreCase(fileExtName);
     }
