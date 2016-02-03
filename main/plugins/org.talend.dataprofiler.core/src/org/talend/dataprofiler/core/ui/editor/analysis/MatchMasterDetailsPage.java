83b034e7940c4af69951bc039157be18c6d729b7
zshen-talend <zshen@talend.com>
2016-02-03 18:54:23 +0800

    bug fixed TDQ-10100 backport on the branch 6.0 Make algorithm is correctly, after import match rule on the match analysis.

--- main/plugins/org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/analysis/MatchMasterDetailsPage.java
+++ main/plugins/org.talend.dataprofiler.core/src/org/talend/dataprofiler/core/ui/editor/analysis/MatchMasterDetailsPage.java
@@ -1575,7 +1575,6 @@ public class MatchMasterDetailsPage extends AbstractAnalysisMetadataPage impleme
     }
 
     public void importMatchRule(MatchRuleDefinition matchRule, boolean overwrite) {
-        selectAlgorithmSection.setMatchRuleDef(matchRule);
         boolean isVSR = RecordMatcherType.simpleVSRMatcher.name().equals(matchRule.getRecordLinkageAlgorithm());
         selectAlgorithmSection.setSelection(isVSR);
         if (selectAlgorithmSection.isVSRMode()) {
