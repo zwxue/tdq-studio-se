f099b406ddabae21be8fdd825b1ac9e6ebddec20
zshen-talend <zshen@talend.com>
2016-02-26 19:03:48 +0800

    bug fixed TDQ-11583 bidirectional check the dependency when call the method DependenciesHandler.SetUsageDependencyOn(import second time same UDI or other model element will used that)

--- test/plugins/org.talend.cwm.management.test/src/org/talend/cwm/dependencies/DependenciesHandlerTest.java
+++ test/plugins/org.talend.cwm.management.test/src/org/talend/cwm/dependencies/DependenciesHandlerTest.java
@@ -12,8 +12,7 @@
 // ============================================================================
 package org.talend.cwm.dependencies;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
@@ -30,6 +29,7 @@ import org.talend.core.ITDQItemService;
 import org.talend.core.model.properties.DatabaseConnectionItem;
 import org.talend.core.model.properties.ItemState;
 import org.talend.core.model.properties.Property;
+import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
 import org.talend.cwm.helper.ConnectionHelper;
 import org.talend.dataquality.analysis.Analysis;
@@ -37,11 +37,17 @@ import org.talend.dataquality.analysis.AnalysisContext;
 import org.talend.dataquality.analysis.AnalysisFactory;
 import org.talend.dataquality.analysis.AnalysisResult;
 import org.talend.dataquality.helpers.AnalysisHelper;
+import org.talend.dataquality.indicators.definition.DefinitionFactory;
+import org.talend.dataquality.indicators.definition.IndicatorDefinition;
+import org.talend.dataquality.indicators.sql.IndicatorSqlFactory;
+import org.talend.dataquality.indicators.sql.UserDefIndicator;
 import org.talend.dataquality.properties.TDQAnalysisItem;
+import org.talend.dataquality.properties.TDQIndicatorDefinitionItem;
 import org.talend.dataquality.properties.impl.PropertiesFactoryImpl;
 import org.talend.dq.helper.PropertyHelper;
 import org.talend.dq.helper.UnitTestBuildHelper;
 import org.talend.dq.writer.impl.ElementWriterFactory;
+import org.talend.resource.EResourceConstant;
 import org.talend.utils.sugars.TypedReturnCode;
 import orgomg.cwm.objectmodel.core.Dependency;
 import orgomg.cwm.objectmodel.core.ModelElement;
@@ -152,6 +158,215 @@ public class DependenciesHandlerTest {
         Assert.assertNull(analysis.getContext().getConnection());
         Assert.assertEquals(true, analysis.getClientDependency().isEmpty());
 
+    }
+
+    /**
+     * Test method for
+     * {@link org.talend.cwm.dependencies.DependenciesHandler#setUsageDependencyOn(ModelElement, ModelElement)} . case1
+     * noraml case the dependency has been set between clint and supplier
+     */
+    @Test
+    public void testSetUsageDependencyOnModelElementModelElementCase1() {
+
+        // analysis init
+        TDQAnalysisItem analysisItem = PropertiesFactoryImpl.eINSTANCE.createTDQAnalysisItem();
+        Analysis analysis = AnalysisFactory.eINSTANCE.createAnalysis();
+        analysis.setName("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase1Analysis"); //$NON-NLS-1$
+        analysisItem.setAnalysis(analysis);
+        Property propertyAna = org.talend.core.model.properties.PropertiesFactory.eINSTANCE.createProperty();
+        propertyAna.setId(EcoreUtil.generateUUID());
+        propertyAna.setLabel("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase1Analysis"); //$NON-NLS-1$
+        analysisItem.setProperty(propertyAna);
+        ItemState itemState = org.talend.core.model.properties.PropertiesFactory.eINSTANCE.createItemState();
+        itemState.setDeleted(false);
+        analysisItem.setState(itemState);
+        AnalysisResult analysisResult1 = AnalysisFactory.eINSTANCE.createAnalysisResult();
+
+        analysis.setResults(analysisResult1);
+        analysis.setContext(AnalysisFactory.eINSTANCE.createAnalysisContext());
+        try {
+            ProxyRepositoryFactory.getInstance().create(analysisItem, Path.EMPTY, false);
+        } catch (PersistenceException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        // UDI init
+        TDQIndicatorDefinitionItem createTDQIndicatorDefinitionItem = PropertiesFactoryImpl.eINSTANCE
+                .createTDQIndicatorDefinitionItem();
+        UserDefIndicator createUserDefIndicator = IndicatorSqlFactory.eINSTANCE.createUserDefIndicator();
+        IndicatorDefinition createIndicatorDefinition = DefinitionFactory.eINSTANCE.createIndicatorDefinition();
+        createIndicatorDefinition.setName("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase1UDI"); //$NON-NLS-1$
+        createTDQIndicatorDefinitionItem.setIndicatorDefinition(createIndicatorDefinition);
+        createUserDefIndicator.setIndicatorDefinition(createIndicatorDefinition);
+        Property propertyUDI = org.talend.core.model.properties.PropertiesFactory.eINSTANCE.createProperty();
+        propertyUDI.setId(EcoreUtil.generateUUID());
+        propertyUDI.setLabel("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase1UDI"); //$NON-NLS-1$
+        createTDQIndicatorDefinitionItem.setProperty(propertyUDI);
+        try {
+            ProxyRepositoryFactory.getInstance().create(createTDQIndicatorDefinitionItem,
+                    new Path(EResourceConstant.USER_DEFINED_INDICATORS.getName()), false);
+        } catch (PersistenceException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        // init UDI supplier Dependency
+        analysisResult1.getIndicators().add(createUserDefIndicator);
+        ElementWriterFactory.getInstance().createAnalysisWrite().save(analysisItem, true);
+        createIndicatorDefinition.getSupplierDependency().get(0).getClient().clear();
+
+        // call the test method
+        TypedReturnCode<Dependency> setUsageDependencyOn = DependenciesHandler.getInstance().setUsageDependencyOn(analysis,
+                createIndicatorDefinition);
+        Assert.assertEquals("call setUsageDependencyOn method should return ok stauts", true, setUsageDependencyOn.isOk()); //$NON-NLS-1$
+        Assert.assertEquals(analysis, setUsageDependencyOn.getObject().getClient().get(0));
+        Assert.assertEquals(1, setUsageDependencyOn.getObject().getClient().size());
+        Assert.assertEquals(createIndicatorDefinition, setUsageDependencyOn.getObject().getSupplier().get(0));
+        Assert.assertEquals(1, setUsageDependencyOn.getObject().getSupplier().size());
+    }
+
+    /**
+     * Test method for
+     * {@link org.talend.cwm.dependencies.DependenciesHandler#setUsageDependencyOn(ModelElement, ModelElement)} . 、
+     * case2 supplier side is lost
+     */
+    @Test
+    public void testSetUsageDependencyOnModelElementModelElementCase2() {
+
+        // analysis init
+        TDQAnalysisItem analysisItem = PropertiesFactoryImpl.eINSTANCE.createTDQAnalysisItem();
+        Analysis analysis = AnalysisFactory.eINSTANCE.createAnalysis();
+        analysis.setName("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase2Analysis"); //$NON-NLS-1$
+        analysisItem.setAnalysis(analysis);
+        Property propertyAna = org.talend.core.model.properties.PropertiesFactory.eINSTANCE.createProperty();
+        propertyAna.setId(EcoreUtil.generateUUID());
+        propertyAna.setLabel("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase2Analysis"); //$NON-NLS-1$
+        analysisItem.setProperty(propertyAna);
+        ItemState itemState = org.talend.core.model.properties.PropertiesFactory.eINSTANCE.createItemState();
+        itemState.setDeleted(false);
+        analysisItem.setState(itemState);
+        AnalysisResult analysisResult1 = AnalysisFactory.eINSTANCE.createAnalysisResult();
+
+        analysis.setResults(analysisResult1);
+        analysis.setContext(AnalysisFactory.eINSTANCE.createAnalysisContext());
+        try {
+            ProxyRepositoryFactory.getInstance().create(analysisItem, Path.EMPTY, false);
+        } catch (PersistenceException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        // UDI init
+        TDQIndicatorDefinitionItem createTDQIndicatorDefinitionItem = PropertiesFactoryImpl.eINSTANCE
+                .createTDQIndicatorDefinitionItem();
+        UserDefIndicator createUserDefIndicator = IndicatorSqlFactory.eINSTANCE.createUserDefIndicator();
+        IndicatorDefinition createIndicatorDefinition = DefinitionFactory.eINSTANCE.createIndicatorDefinition();
+        createIndicatorDefinition.setName("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase2UDI"); //$NON-NLS-1$
+        createTDQIndicatorDefinitionItem.setIndicatorDefinition(createIndicatorDefinition);
+        createUserDefIndicator.setIndicatorDefinition(createIndicatorDefinition);
+        Property propertyUDI = org.talend.core.model.properties.PropertiesFactory.eINSTANCE.createProperty();
+        propertyUDI.setId(EcoreUtil.generateUUID());
+        propertyUDI.setLabel("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase2UDI"); //$NON-NLS-1$
+        createTDQIndicatorDefinitionItem.setProperty(propertyUDI);
+        try {
+            ProxyRepositoryFactory.getInstance().create(createTDQIndicatorDefinitionItem,
+                    new Path(EResourceConstant.USER_DEFINED_INDICATORS.getName()), false);
+        } catch (PersistenceException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        // init UDI supplier Dependency
+        analysisResult1.getIndicators().add(createUserDefIndicator);
+        ElementWriterFactory.getInstance().createAnalysisWrite().save(analysisItem, true);
+        createIndicatorDefinition.getSupplierDependency().get(0).getClient().clear();
+        Analysis analysisFromFile = AnalysisFactory.eINSTANCE.createAnalysis();
+        try {
+            analysis.eResource().unload();
+            IRepositoryViewObject lastVersion = ProxyRepositoryFactory.getInstance().getLastVersion(propertyAna.getId());
+            analysisFromFile = ((TDQAnalysisItem) lastVersion.getProperty().getItem()).getAnalysis();
+        } catch (PersistenceException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        // call the test method
+        TypedReturnCode<Dependency> setUsageDependencyOn = DependenciesHandler.getInstance().setUsageDependencyOn(
+                analysisFromFile, createIndicatorDefinition);
+        Assert.assertEquals("call setUsageDependencyOn method should return ok stauts", true, setUsageDependencyOn.isOk()); //$NON-NLS-1$
+        Assert.assertEquals(analysisFromFile, setUsageDependencyOn.getObject().getClient().get(0));
+        Assert.assertEquals(1, setUsageDependencyOn.getObject().getClient().size());
+        Assert.assertEquals(createIndicatorDefinition, setUsageDependencyOn.getObject().getSupplier().get(0));
+        Assert.assertEquals(1, setUsageDependencyOn.getObject().getSupplier().size());
+    }
+
+    /**
+     * Test method for
+     * {@link org.talend.cwm.dependencies.DependenciesHandler#setUsageDependencyOn(ModelElement, ModelElement)} . case3
+     * client side is lost
+     */
+    @Test
+    public void testSetUsageDependencyOnModelElementModelElementCase3() {
+
+        // analysis init
+        TDQAnalysisItem analysisItem = PropertiesFactoryImpl.eINSTANCE.createTDQAnalysisItem();
+        Analysis analysis = AnalysisFactory.eINSTANCE.createAnalysis();
+        analysis.setName("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase3Analysis"); //$NON-NLS-1$
+        analysisItem.setAnalysis(analysis);
+        Property propertyAna = org.talend.core.model.properties.PropertiesFactory.eINSTANCE.createProperty();
+        propertyAna.setId(EcoreUtil.generateUUID());
+        propertyAna.setLabel("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase3Analysis"); //$NON-NLS-1$
+        analysisItem.setProperty(propertyAna);
+        ItemState itemState = org.talend.core.model.properties.PropertiesFactory.eINSTANCE.createItemState();
+        itemState.setDeleted(false);
+        analysisItem.setState(itemState);
+        AnalysisResult analysisResult1 = AnalysisFactory.eINSTANCE.createAnalysisResult();
+
+        analysis.setResults(analysisResult1);
+        analysis.setContext(AnalysisFactory.eINSTANCE.createAnalysisContext());
+        try {
+            ProxyRepositoryFactory.getInstance().create(analysisItem, Path.EMPTY, false);
+        } catch (PersistenceException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        // UDI init
+        TDQIndicatorDefinitionItem createTDQIndicatorDefinitionItem = PropertiesFactoryImpl.eINSTANCE
+                .createTDQIndicatorDefinitionItem();
+        UserDefIndicator createUserDefIndicator = IndicatorSqlFactory.eINSTANCE.createUserDefIndicator();
+        IndicatorDefinition createIndicatorDefinition = DefinitionFactory.eINSTANCE.createIndicatorDefinition();
+        createIndicatorDefinition.setName("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase3UDI"); //$NON-NLS-1$
+        createTDQIndicatorDefinitionItem.setIndicatorDefinition(createIndicatorDefinition);
+        createUserDefIndicator.setIndicatorDefinition(createIndicatorDefinition);
+        Property propertyUDI = org.talend.core.model.properties.PropertiesFactory.eINSTANCE.createProperty();
+        propertyUDI.setId(EcoreUtil.generateUUID());
+        propertyUDI.setLabel("DependenciesHandlerTest.testSetUsageDependencyOnModelElementModelElementCase3UDI"); //$NON-NLS-1$
+        createTDQIndicatorDefinitionItem.setProperty(propertyUDI);
+        try {
+            ProxyRepositoryFactory.getInstance().create(createTDQIndicatorDefinitionItem,
+                    new Path(EResourceConstant.USER_DEFINED_INDICATORS.getName()), false);
+        } catch (PersistenceException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        // init UDI supplier Dependency
+        analysisResult1.getIndicators().add(createUserDefIndicator);
+        ElementWriterFactory.getInstance().createAnalysisWrite().save(analysisItem, true);
+        analysis.getClientDependency().get(0).getSupplier().clear();
+        IndicatorDefinition createIndicatorDefinitionFromFile = DefinitionFactory.eINSTANCE.createIndicatorDefinition();
+        try {
+            createIndicatorDefinition.eResource().unload();
+            IRepositoryViewObject lastVersion = ProxyRepositoryFactory.getInstance().getLastVersion(propertyUDI.getId());
+            createIndicatorDefinitionFromFile = ((TDQIndicatorDefinitionItem) lastVersion.getProperty().getItem())
+                    .getIndicatorDefinition();
+        } catch (PersistenceException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        // call the test method
+        TypedReturnCode<Dependency> setUsageDependencyOn = DependenciesHandler.getInstance().setUsageDependencyOn(analysis,
+                createIndicatorDefinitionFromFile);
+        Assert.assertEquals("call setUsageDependencyOn method should return ok stauts", true, setUsageDependencyOn.isOk()); //$NON-NLS-1$
+        Assert.assertEquals(analysis, setUsageDependencyOn.getObject().getClient().get(0));
+        Assert.assertEquals(1, setUsageDependencyOn.getObject().getClient().size());
+        Assert.assertEquals(createIndicatorDefinitionFromFile, setUsageDependencyOn.getObject().getSupplier().get(0));
+        Assert.assertEquals(1, setUsageDependencyOn.getObject().getSupplier().size());
     }
 
 }
