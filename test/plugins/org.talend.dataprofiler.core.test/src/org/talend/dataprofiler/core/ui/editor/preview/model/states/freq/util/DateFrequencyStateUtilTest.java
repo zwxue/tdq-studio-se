99a73d349ed389d821b92c85cfcf641145d0eb9c
qiongli <qiongli@talend.com>
2016-05-18 18:23:04 +0800

    TDQ-10547 add a junit test

---
+++ test/plugins/org.talend.dataprofiler.core.test/src/org/talend/dataprofiler/core/ui/editor/preview/model/states/freq/util/DateFrequencyStateUtilTest.java
@@ -0,0 +1,91 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.dataprofiler.core.ui.editor.preview.model.states.freq.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.talend.dataprofiler.core.helper.UnitTestBuildHelper;
+import org.talend.dataprofiler.core.model.ModelElementIndicator;
+import org.talend.dataprofiler.core.ui.editor.preview.ColumnIndicatorUnit;
+import org.talend.dataprofiler.core.ui.editor.preview.IndicatorUnit;
+import org.talend.dataprofiler.core.ui.utils.ComparatorsFactory;
+import org.talend.dataquality.indicators.IndicatorsFactory;
+import org.talend.dataquality.indicators.YearLowFrequencyIndicator;
+import org.talend.dq.indicators.preview.table.ChartDataEntity;
+import org.talend.dq.nodes.indicator.type.IndicatorEnum;
+
+/**
+ * DOC talend class global comment. Detailled comment
+ */
+public class DateFrequencyStateUtilTest {
+
+    HashMap<Object, Long> mapVal2Freq = null;
+
+    YearLowFrequencyIndicator yearLowFrequencyIndicator = null;
+
+    List<IndicatorUnit> units = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mapVal2Freq = new HashMap<Object, Long>();
+        mapVal2Freq.put(1993, 4L);
+        mapVal2Freq.put(1994, 23L);
+        mapVal2Freq.put(1996, 15L);
+        mapVal2Freq.put(1999, 11L);
+        yearLowFrequencyIndicator = IndicatorsFactory.eINSTANCE.createYearLowFrequencyIndicator();
+        yearLowFrequencyIndicator.setValueToFreq(mapVal2Freq);
+        yearLowFrequencyIndicator.setComputed(true);
+
+        units = new ArrayList<IndicatorUnit>();
+        ModelElementIndicator modelElementIndicator = UnitTestBuildHelper.createModelElementIndicator();
+        ColumnIndicatorUnit columnIndicatorUnit = new ColumnIndicatorUnit(IndicatorEnum.YearLowFrequencyIndicatorEnum,
+                yearLowFrequencyIndicator, modelElementIndicator);
+        units.add(columnIndicatorUnit);
+    }
+
+    /**
+     * Test method for
+     * {@link org.talend.dataprofiler.core.ui.editor.preview.model.states.freq.util.DateFrequencyStateUtil#getDataEntity(java.util.List, int)}
+     * .
+     */
+    @Test
+    public void testGetDataEntity_low_year_freq() {
+
+        ChartDataEntity[] dataEntity = DateFrequencyStateUtil
+                .getDataEntity(units, ComparatorsFactory.LOW_FREQUENCY_COMPARATOR_ID);
+        Assert.assertTrue(dataEntity.length == 4);
+        Assert.assertTrue("1993".equals(dataEntity[0].getKey().toString())); //$NON-NLS-1$
+        Assert.assertTrue("1999".equals(dataEntity[1].getKey().toString())); //$NON-NLS-1$
+        Assert.assertTrue("1996".equals(dataEntity[2].getKey().toString())); //$NON-NLS-1$
+        Assert.assertTrue("1994".equals(dataEntity[3].getKey().toString())); //$NON-NLS-1$
+
+    }
+
+    @Test
+    public void testGetDataEntity_year_freq() {
+
+        ChartDataEntity[] dataEntity = DateFrequencyStateUtil.getDataEntity(units, ComparatorsFactory.FREQUENCY_COMPARATOR_ID);
+        Assert.assertTrue(dataEntity.length == 4);
+        Assert.assertTrue("1994".equals(dataEntity[0].getKey().toString())); //$NON-NLS-1$
+        Assert.assertTrue("1996".equals(dataEntity[1].getKey().toString())); //$NON-NLS-1$
+        Assert.assertTrue("1999".equals(dataEntity[2].getKey().toString())); //$NON-NLS-1$
+        Assert.assertTrue("1993".equals(dataEntity[3].getKey().toString())); //$NON-NLS-1$
+
+    }
+
+}
