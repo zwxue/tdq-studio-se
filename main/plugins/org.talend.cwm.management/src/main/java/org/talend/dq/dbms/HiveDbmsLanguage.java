10c3e79c0b45300e8188f32fcb2935f0f57d148c
qiongli <qiongli@talend.com>
2016-06-30 10:56:46 +0800

    TDQ-12238 code improvment:overridemethod'getHardCodedQuoteIdentifier()' instead of 'quote(StringsqlIdentifier)'.

--- main/plugins/org.talend.cwm.management/src/main/java/org/talend/dq/dbms/HiveDbmsLanguage.java
+++ main/plugins/org.talend.cwm.management/src/main/java/org/talend/dq/dbms/HiveDbmsLanguage.java
@@ -18,7 +18,6 @@ import java.sql.Statement;
 import org.apache.commons.lang.StringUtils;
 import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
 import org.talend.cwm.relational.TdColumn;
-import org.talend.dataquality.PluginConstant;
 import org.talend.dataquality.indicators.BenfordLawFrequencyIndicator;
 import org.talend.dataquality.indicators.Indicator;
 import org.talend.utils.ProductVersion;
@@ -33,7 +32,7 @@ import orgomg.cwm.objectmodel.core.Expression;
  */
 public class HiveDbmsLanguage extends DbmsLanguage {
 
-    private final String HIVE_IDENTIFIER_QUOTE = "`"; //$NON-NLS-1$
+    private static final String HIVE_IDENTIFIER_QUOTE = "`"; //$NON-NLS-1$
 
     /**
      * DOC qiongli HiveDbmsLanguage constructor comment.
@@ -223,17 +222,7 @@ public class HiveDbmsLanguage extends DbmsLanguage {
     }
 
     @Override
-    public String quote(String sqlIdentifier) {
-        if (sqlIdentifier == null || sqlIdentifier.equals(PluginConstant.EMPTY_STRING)) {
-            return PluginConstant.EMPTY_STRING;
-        }
-        String quotedSqlIdentifier = sqlIdentifier;
-        if (!quotedSqlIdentifier.startsWith(HIVE_IDENTIFIER_QUOTE)) {
-            quotedSqlIdentifier = HIVE_IDENTIFIER_QUOTE + quotedSqlIdentifier;
-        }
-        if (!quotedSqlIdentifier.endsWith(HIVE_IDENTIFIER_QUOTE)) {
-            quotedSqlIdentifier = quotedSqlIdentifier + HIVE_IDENTIFIER_QUOTE;
-        }
-        return quotedSqlIdentifier;
+    public String getHardCodedQuoteIdentifier() {
+        return HIVE_IDENTIFIER_QUOTE;
     }
 }
