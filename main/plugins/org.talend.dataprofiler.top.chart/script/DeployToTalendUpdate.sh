5d537991d20216af206293a5e6e795ae7f717de8
xqliu-talend <xqliu@talend.com>
2016-12-20 09:48:27 +0800

    TDQ-13023 Deploy sqlexplorer and top.chart jars to Nexus for release/6.3.1

--- main/plugins/org.talend.dataprofiler.top.chart/script/DeployToTalendUpdate.sh
+++ main/plugins/org.talend.dataprofiler.top.chart/script/DeployToTalendUpdate.sh
@@ -33,8 +33,8 @@
 
 list="net.sourceforge.sqlexplorer net.sourceforge.sqlexplorer.nl \
  org.talend.dataprofiler.top.chart org.talend.dataprofiler.top.chart.nl"
-version="6.3.0" # overwrite this variable
-revision_suffix="20161024_1132-EP" # overwrite this variable
+version="6.3.1" # overwrite this variable
+revision_suffix="20161216_1026" # overwrite this variable
 repo_id="talend-update"
 repo_url="https://talend-update.talend.com/nexus/content/repositories/libraries"
 
