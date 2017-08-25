d7b468a0deb3de101c61ec044612003121415900
Liu Xinquan <xqliu@talend.com>
2017-08-25 18:31:38 +0800

    fix (TUP-18425): global check of Talendforge.org/Forums in Talend Studio (#128) (#129)
    + apps

--- main/plugins/org.talend.dataprofiler.help/src/org/talend/dataprofiler/help/BookMarkEnum.java
+++ main/plugins/org.talend.dataprofiler.help/src/org/talend/dataprofiler/help/BookMarkEnum.java
@@ -28,7 +28,7 @@ public enum BookMarkEnum {
     BoxPlot("http://en.wikipedia.org/wiki/Box_plot", "Box Plot Graphic"), //$NON-NLS-1$ //$NON-NLS-2$
     EclipseSQLExplorer("http://eclipsesql.sourceforge.net/index.php", "Eclipse SQL Explorer"), //$NON-NLS-1$ //$NON-NLS-2$
     TOSDownloadPage("http://www.talend.com/download/", "Talend.com Download Page"), //$NON-NLS-1$ //$NON-NLS-2$
-    TOSForum("http://www.talendforge.org/forum/index.php", "Talend.com Forum"), //$NON-NLS-1$ //$NON-NLS-2$
+    TOSForum("https://community.talend.com/", "Talend.com Forum"), //$NON-NLS-1$ //$NON-NLS-2$
     TOSBugtracker("http://jira.talendforge.org", "Talend.com Bugtracker"); //$NON-NLS-1$ //$NON-NLS-2$
 
     private String href;
